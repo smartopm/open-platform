e1ceaa6ab22105781f5de7f35d6be92650202119
Mark Percival <m@mdp.im>
2019-08-21 14:01:46 +0000

    Fixes for Rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
+# frozen_string_literal: true
+
+# HomeController is our Root route for the application
 class HomeController < ApplicationController
-
-  def index
-  end
-
+  def index; end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# The helper for Home Controller
 module HomeHelper
 end
