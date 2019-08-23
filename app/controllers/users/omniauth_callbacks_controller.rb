0cd9174db8a3e502df3047799a36b780e6fa51b1
Mark Percival <m@mdp.im>
2019-08-23 17:34:35 +0000

    Update rubocop and combine Class parent

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,22 +1,19 @@
 # frozen_string_literal: true
 
 # Our omniauth controller for Google oauth callbacks
-class Users
-  # Handles all our Oauth requestsh
-  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def passthru
-      redirect_to user_google_oauth2_omniauth_authorize_path
-    end
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def passthru
+    redirect_to user_google_oauth2_omniauth_authorize_path
+  end
 
-    def google_oauth2
-      @user = User.from_omniauth(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-      else
-        session['devise.google_data'] = request.env['omniauth.auth']
-      end
-      redirect_to '/'
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      sign_in @user, event: :authentication # this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+    else
+      session['devise.google_data'] = request.env['omniauth.auth']
     end
+    redirect_to '/'
   end
 end
