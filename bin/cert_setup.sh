61b3ecc31c0b535d09bd64daaeb21af58d23ef37
Mark Percival <m@mdp.im>
2019-10-01 20:01:04 +0000

    Add VPN.dgdp.site to the mix

--- bin/cert_setup.sh
+++ bin/cert_setup.sh
@@ -2,6 +2,8 @@
 
 if [ -z $1 ]; then
   openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/dev.dgdp.site/privkey.enc -out certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -d
+  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/vpn.dgdp.site/privkey.enc -out certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -d
 elif [ $1 = "e"]; then
   openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/dev.dgdp.site/privkey.enc -in certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -e
+  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/vpn.dgdp.site/privkey.enc -in certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -e
 fi
