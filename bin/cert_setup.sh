63d0d82bba363d527ed2315fce9f125114987f71
Mark Percival <m@mdp.im>
2019-10-21 16:26:01 +0000

    Use docker to create certs

--- bin/cert_setup.sh
+++ bin/cert_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/dev.dgdp.site/privkey.enc -out certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -d
-  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/vpn.dgdp.site/privkey.enc -out certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -d
-elif [ $1 = "e"]; then
-  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/dev.dgdp.site/privkey.enc -in certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -e
-  openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/vpn.dgdp.site/privkey.enc -in certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -e
+  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/dev.dgdp.site/privkey.enc -out certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -d
+  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/vpn.dgdp.site/privkey.enc -out certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -d
+elif [ $1 = "e" ]; then
+  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/dev.dgdp.site/privkey.enc -in certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -e
+  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/vpn.dgdp.site/privkey.enc -in certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -e
 fi
