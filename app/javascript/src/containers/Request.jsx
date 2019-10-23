f4a0bc1d38bb4e717d3fbed062f561eadb06c39a
OlivierJM <manolivier93@gmail.com>
2019-10-23 10:25:02 +0200

    add select menu for request reason

--- app/javascript/src/containers/Request.jsx
+++ app/javascript/src/containers/Request.jsx
@@ -1,50 +1,51 @@
-import React from 'react';
-import { useLazyQuery, useMutation } from 'react-apollo';
-import { withFormik } from 'formik';
+import React from "react";
+import { useLazyQuery, useMutation } from "react-apollo";
+import { withFormik } from "formik";
 
-import Nav from '../components/Nav'
-import RequestForm from '../components/RequestForm.jsx';
+import Nav from "../components/Nav";
+import RequestForm from "../components/RequestForm.jsx";
 import Loading from "../components/Loading.jsx";
 
-import {UserQuery} from "../graphql/queries"
-import {CreatePendingUserMutation, UpdatePendingUserMutation} from "../graphql/mutations"
-import crudHandler from "../graphql/crud_handler"
+import { UserQuery } from "../graphql/queries";
+import {
+  CreatePendingUserMutation,
+  UpdatePendingUserMutation
+} from "../graphql/mutations";
+import crudHandler from "../graphql/crud_handler";
 
-function firstName(name=''){
-  return name.split(' ')[0] || ''
+function firstName(name = "") {
+  return name.split(" ")[0] || "";
 }
 
-function lastName(name=''){
-  return name.split(' ')[1] || ''
+function lastName(name = "") {
+  return name.split(" ")[1] || "";
 }
 
-
-export default function RequestFormContainer({match, history}) {
-
-  const {isLoading, error, result, createOrUpdate, loadRecord} = crudHandler({
-    typeName: 'user',
+export default function RequestFormContainer({ match, history }) {
+  const { isLoading, error, result, createOrUpdate, loadRecord } = crudHandler({
+    typeName: "user",
     readLazyQuery: useLazyQuery(UserQuery),
     updateMutation: useMutation(UpdatePendingUserMutation),
-    createMutation: useMutation(CreatePendingUserMutation),
+    createMutation: useMutation(CreatePendingUserMutation)
   });
-  console.log('Loading', isLoading)
-  console.log('error', error)
-  console.log('result', result)
+  console.log("Loading", isLoading);
+  console.log("error", error);
+  console.log("result", result);
 
-  function submitMutation({firstName, lastName, requestReason, vehicle}) {
+  function submitMutation({ firstName, lastName, requestReason, vehicle }) {
     return createOrUpdate({
-      name: [firstName, lastName].join(' '),
+      name: [firstName, lastName].join(" "),
       requestReason,
-      vehicle,
-    })
+      vehicle
+    });
   }
 
   const FormContainer = withFormik({
     mapPropsToValues: () => ({
       firstName: firstName(result.name),
       lastName: lastName(result.name),
-      requestReason: result.requestReason || '',
-      vehicle: result.vehicle || '',
+      requestReason: result.requestReason || "",
+      vehicle: result.vehicle || ""
     }),
 
     // Custom sync validation
@@ -52,33 +53,35 @@ export default function RequestFormContainer({match, history}) {
       const errors = {};
 
       if (!values.firstName) {
-        errors.firstName = 'Required';
+        errors.firstName = "Required";
       }
       if (!values.lastName) {
-        errors.lastName = 'Required';
+        errors.lastName = "Required";
       }
       if (!values.requestReason) {
-        errors.requestReason = 'Required';
+        errors.requestReason = "Required";
       }
 
       return errors;
     },
 
     handleSubmit: (values, { setSubmitting }) => {
-      submitMutation(values).then(({data})=> {
-        setSubmitting(false)
-        history.push(`/user/${data.result.user.id}`)
-      })
+      console.log(values);
+
+      submitMutation(values).then(({ data }) => {
+        setSubmitting(false);
+        history.push(`/user/${data.result.user.id}`);
+      });
     },
 
-    displayName: 'RequestForm',
+    displayName: "RequestForm"
   })(Container);
   if (!isLoading && !result.id && !error) {
-    loadRecord({variables: {id: match.params.id}})
+    loadRecord({ variables: { id: match.params.id } });
   } else if (isLoading) {
-    return (<Loading/>)
-  } 
-  return (<FormContainer />)
+    return <Loading />;
+  }
+  return <FormContainer />;
 }
 
 export function Container(props) {
@@ -87,8 +90,7 @@ export function Container(props) {
       <Nav navName="New Request" menuButton="cancel" />
       <RequestForm {...props} />
     </div>
-  )
+  );
 }
 
-Container.displayName = "RequestForm"
-
+Container.displayName = "RequestForm";
