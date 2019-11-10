73d41f09ecffea6fa0229e1f95fb2b9b11022a14
Mark Percival <m@mdp.im>
2019-11-10 01:47:48 +0000

    Add client

--- app/javascript/src/utils/constants.js
+++ app/javascript/src/utils/constants.js
@@ -6,5 +6,12 @@ export const reasons = [
   "Passing through",
   "Other"
 ];
-export const userType = {admin: "Admin", security_guard: "Security Guard", resident: "Resident", contractor: "Contractor", prospective_client: "Prospective Client"};
+export const userType = {
+  admin: "Admin",
+  security_guard: "Security Guard",
+  resident: "Resident",
+  contractor: "Contractor",
+  prospective_client: "Prospective Client",
+  client: "Client"
+};
 export const userState = {valid: "Valid", pending: "Pending", banned: "Banned", expired: "Expired"};
