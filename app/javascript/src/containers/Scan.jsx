bdfd2fe06e76c1923fe9e42a461b7da399260da0
Olivier JM Maniraho <olivier@doublegdp.com>
2019-11-12 16:50:37 +0000

    Fixes after guard sessions

--- app/javascript/src/containers/Scan.jsx
+++ app/javascript/src/containers/Scan.jsx
@@ -21,7 +21,7 @@ export default function QRScan() {
       <Nav navName="Scan" menuButton="back" />
 
       {scanned ? (
-        <h1>Decoding...</h1>
+        <h1 className="text-center">Decoding...</h1>
       ) : (
         <QrReader
           delay={100}
