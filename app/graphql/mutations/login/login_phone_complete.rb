2e97712c3178c5cab7553ccdf164e9a7eb45012e
Mark Percival <m@mdp.im>
2019-11-20 22:02:29 +0000

    Move to using UUID instead of phone number for completion

--- app/graphql/mutations/login/login_phone_complete.rb
+++ app/graphql/mutations/login/login_phone_complete.rb
@@ -4,13 +4,13 @@ module Mutations
   module Login
     # Complete the phone number login
     class LoginPhoneComplete < BaseMutation
-      argument :phone_number, String, required: true
+      argument :id, ID, required: true
       argument :token, String, required: true
 
       field :auth_token, String, null: true
 
       def resolve(vals)
-        user = User.find_via_phone_number(vals[:phone_number])
+        user = User.find(vals[:id])
         auth_token = user.auth_token if user.verify_phone_token!(vals[:token])
 
         return { auth_token: auth_token } if auth_token
