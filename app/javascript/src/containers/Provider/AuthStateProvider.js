96cb21f82f8d71c272621714d6bf20d1f6caced3
OlivierJM <manolivier93@gmail.com>
2019-11-21 13:31:10 +0200

    add loading component to loading auth

--- app/javascript/src/containers/Provider/AuthStateProvider.js
+++ app/javascript/src/containers/Provider/AuthStateProvider.js
@@ -1,11 +1,12 @@
 // Logged in keeps track of the curent_user and current_member state
 // and passes it along as a context
-import React, {useReducer, useState, useEffect} from "react";
+import React, { useReducer, useState, useEffect } from "react";
 
 import gql from "graphql-tag";
 import { useApolloClient } from "react-apollo";
 
 import { AUTH_TOKEN_KEY } from "../../utils/apollo";
+import Loading from "../../components/Loading";
 
 export const MEMBER_ID_KEY = "CURRENT_MEMBER_ID";
 
@@ -36,23 +37,22 @@ const QUERY = gql`
 `;
 
 export default function AuthToken({ children }) {
-
   function reducer(state, action) {
-    console.log(state, action)
+    console.log(state, action);
     switch (action.type) {
-      case 'delete':
-        localStorage.removeItem(AUTH_TOKEN_KEY)
-        return {token: null};
+      case "delete":
+        localStorage.removeItem(AUTH_TOKEN_KEY);
+        return { token: null };
       default:
         if (action.token) {
-          localStorage.setItem(AUTH_TOKEN_KEY, action.token)
+          localStorage.setItem(AUTH_TOKEN_KEY, action.token);
         }
-        return {token: action.token};
+        return { token: action.token };
     }
   }
 
   const [state, setToken] = useReducer(reducer, {
-    token: localStorage.getItem(AUTH_TOKEN_KEY),
+    token: localStorage.getItem(AUTH_TOKEN_KEY)
   });
 
   return (
@@ -68,41 +68,48 @@ export const Context = AuthStateContext;
 
 // Returns a promise with currentUser query
 function getCurrentUser(client) {
-  return client.query({query: QUERY, fetchPolicy: "no-cache"})
+  return client.query({ query: QUERY, fetchPolicy: "no-cache" });
 }
 
 // Provider is the default export
-export function AuthStateProvider({ children, token, setToken}) {
-  const client = useApolloClient()
+export function AuthStateProvider({ children, token, setToken }) {
+  const client = useApolloClient();
   const [state, setState] = useState({
     loggedIn: false,
     loaded: false,
     user: null,
     setToken
-  })
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     // Reset state while we evaluate a token change
-    setState({...state, user: null, loaded:false})
+    setState({ ...state, user: null, loaded: false });
     if (token && !state.error) {
-      getCurrentUser(client).then(({data})=>{
-        setState({...state, user: data.currentUser, loaded:true, loggedIn: true})
-        }).catch((err) => {
-          setState({...state, error: err})
+      getCurrentUser(client)
+        .then(({ data }) => {
+          setState({
+            ...state,
+            user: data.currentUser,
+            loaded: true,
+            loggedIn: true
+          });
         })
+        .catch(err => {
+          setState({ ...state, error: err });
+        });
     } else {
-      setState({...state, user: null, loaded:true})
+      setState({ ...state, user: null, loaded: true });
     }
     // Get query if token changes
-  }, [token])
+  }, [token]);
 
   if (state.error && token) {
     // There's an error with the token, delete it
-    setToken({type:'delete'})
+    setToken({ type: "delete" });
   }
 
   if (!state.loaded) {
-    return <p>Loading</p>
+    return <Loading />;
   }
 
   return (
