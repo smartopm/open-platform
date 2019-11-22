ef1cc1cc3137ee62cea236b822c337ca3aafa968
OlivierJM <olivier@doublegdp.com>
2019-11-22 09:27:32 +0000

    lighting torch

--- app/javascript/src/containers/Scan.jsx
+++ app/javascript/src/containers/Scan.jsx
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QrReader from "react-qr-reader";
 import Nav from "../components/Nav";
 
 export default function QRScan() {
   const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment',
+      }
+    })
+      .then((stream) => {
+        const video = document.querySelector('video');
+        video.srcObject = stream;
+
+        // get the active track of the stream
+        const track = stream.getVideoTracks()[0];
+
+        video.addEventListener('loadedmetadata', (e) => {
+          window.setTimeout(() => (
+            onCapabilitiesReady(track.getCapabilities())
+          ), 500);
+        });
+
+        function onCapabilitiesReady(capabilities) {
+          if (capabilities.torch) {
+            track.applyConstraints({
+              advanced: [{ torch: true }]
+            })
+              .catch(e => console.log(e));
+          }
+        }
+
+      })
+      .catch(err => console.error('getUserMedia() failed: ', err));
+  })
 
   const handleScan = data => {
     if (data) {
@@ -11,6 +43,7 @@ export default function QRScan() {
       window.location = data;
     }
   };
+
 
   const handleError = err => {
     console.error(err);
@@ -23,14 +56,20 @@ export default function QRScan() {
       {scanned ? (
         <h1 className="text-center">Decoding...</h1>
       ) : (
-        <QrReader
-          delay={100}
-          torch={true}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ width: "100%" }}
-        />
-      )}
+          <>
+            <video style={{
+              display: "none"
+            }}></video>
+            <QrReader
+              delay={100}
+              torch={true}
+              onError={handleError}
+              onScan={handleScan}
+              style={{ width: "100%" }}
+            />
+
+          </>
+        )}
     </div>
   );
 }
