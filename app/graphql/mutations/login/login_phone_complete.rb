d7722c98faab9672ddd113f33602e2d733439f2d
Mark Percival <m@mdp.im>
2019-12-06 16:17:44 +0000

    Enable more logging for user activity

--- app/graphql/mutations/login/login_phone_complete.rb
+++ app/graphql/mutations/login/login_phone_complete.rb
@@ -13,9 +13,19 @@ module Mutations
         user = ::User.find(vals[:id])
         auth_token = user.auth_token if user.verify_phone_token!(vals[:token])
 
+        log(user)
+
         return { auth_token: auth_token } if auth_token
 
         raise GraphQL::ExecutionError, 'Unauthorized'
+      end
+
+      def log(user)
+        EventLog.create(
+          acting_user: user, community: user.community,
+          subject: 'user_login',
+          ref_id: nil, ref_type: nil
+        )
       end
     end
   end
