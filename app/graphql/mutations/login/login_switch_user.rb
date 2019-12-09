d1496ca2868c1cf54b1ca2a45d6ef7da026c6295
Mark Percival <m@mdp.im>
2019-12-09 17:13:48 +0000

    Ensure that we log

--- app/graphql/mutations/login/login_switch_user.rb
+++ app/graphql/mutations/login/login_switch_user.rb
@@ -16,12 +16,22 @@ module Mutations
           raise GraphQL::ExecutionError, 'Unauthorized'
         end
 
+        log(context[:current_user], user)
+
         { auth_token: user.auth_token }
       end
 
       def ensure_logged_in
         user = context[:current_user]
         raise GraphQL::ExecutionError, 'Must be logged in' unless user
+      end
+
+      def log(from_user, to_user)
+        EventLog.create(
+          acting_user: from_user, community: from_user.community,
+          subject: 'user_switch',
+          ref_id: to_user.id, ref_type: 'User'
+        )
       end
     end
   end
