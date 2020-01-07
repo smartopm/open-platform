422e9cf557ef5ad93d1520bb536ab6d6fd780d01
OlivierJM <olivier@doublegdp.com>
2020-01-07 14:31:53 +0000

    remove showroom argument

--- app/graphql/mutations/entry_request/entry_request_create.rb
+++ app/graphql/mutations/entry_request/entry_request_create.rb
@@ -11,9 +11,8 @@ module Mutations
       argument :reason, String, required: false
       argument :other_reason, String, required: false
       argument :concern_flag, GraphQL::Types::Boolean, required: false
-      # showroom
-      argument :showroom, String, required: false
       argument :source, String, required: false
+      
       field :entry_request, Types::EntryRequestType, null: true
 
       def resolve(vals)
