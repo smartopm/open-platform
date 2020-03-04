b4f306205d5e01ed959ea6c92bcf985c032b31de
OlivierJM <manolivier93@gmail.com>
2020-03-04 14:45:58 +0200

    restructure the feedback message sent to you

--- app/graphql/mutations/activity_log/add.rb
+++ app/graphql/mutations/activity_log/add.rb
@@ -35,9 +35,10 @@ module Mutations
       def send_notifications(number)
         feedback_link = "https://#{ENV['HOST']}/feedback"
         return if number.nil?
-
-        Sms.send(number, "Thank you for using our app, kindly use this
-                              link to give us feedback #{feedback_link}")
+        # disabled rubocop to keep the structure of the message
+        # rubocop:disable LineLength
+        Sms.send(number, "Thank you for using our app, kindly use this link to give us feedback #{feedback_link}")
+        # rubocop:enable LineLength
       end
     end
   end
