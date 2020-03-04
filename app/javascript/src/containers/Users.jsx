268cf668e17074ff6df97d764e6e47a90af5a26c
dennis60 <dennis@doublegdp.com>
2020-03-04 12:30:32 +0000

    Added search feature to the client list

--- app/javascript/src/containers/Users.jsx
+++ app/javascript/src/containers/Users.jsx
@@ -2,24 +2,43 @@ import React, { Fragment, useState } from 'react'
 import { useQuery, useMutation } from 'react-apollo'
 import Nav from '../components/Nav'
 import DateUtil from '../utils/dateutil'
+import { Redirect } from 'react-router-dom'
 import Loading from '../components/Loading'
 import ErrorPage from '../components/Error'
 import { UsersQuery } from '../graphql/queries'
 import { CreateNote } from '../graphql/mutations'
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableHead, TableRow, TablePagination, Button } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableHead, TableRow, TablePagination, Button, Divider, IconButton, InputBase } from '@material-ui/core';
+import SearchIcon from "@material-ui/icons/Search";
 import { ModalDialog } from "../components/Dialog";
 
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     table: {
         display: 'block',
         width: '100%',
         overflowX: 'auto',
-        
+
+    },
+    root: {
+        padding: "2px 4px",
+        display: "flex",
+        alignItems: "right",
+        width: '100%'
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1
+    },
+    iconButton: {
+        padding: 10
+    },
+    divider: {
+        height: 28,
+        margin: 4
     }
-});
+}));
 
 const StyledTableCell = withStyles(theme => ({
     head: {
@@ -47,6 +66,7 @@ export default function UsersList() {
     const limit = 50
     const [offset, setOffSet] = useState(0)
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [redirect, setRedirect] = useState(false)
     const [noteCreate, { loading: mutationLoading }] = useMutation(CreateNote);
     const { loading, error, data, refetch } = useQuery(UsersQuery, {
         variables: { limit, offset }
@@ -96,88 +116,120 @@ export default function UsersList() {
         setIsDialogOpen(!isDialogOpen)
     }
 
+
+
+    function inputToSearch() {
+        setRedirect('/search')
+        
+    }
+    
     if (loading) return <Loading />
     if (error) return <ErrorPage error={error.message} />
-
-    return (
-
+    
+    
+    if (redirect) {
         
-            <Fragment>
-                <Nav navName='Users' menuButton='back' />
+        return <Redirect push to={redirect} />
+    }
+    return (
+        
+        
+        <Fragment>
+            <Nav navName='Users' menuButton='back' />
 
-                <div className="container">
-                    <ModalDialog
-                        handleClose={handleModal}
-                        handleConfirm={handleClick}
-                        open={isDialogOpen}
-                    >
+            <div className="container">
+                <ModalDialog
+                    handleClose={handleModal}
+                    handleConfirm={handleClick}
+                    open={isDialogOpen}
+                >
 
-                        <div className='form-group'>
+                    <div className='form-group'>
 
-                            <h6>Add note for <strong>{userName}</strong> </h6>
-                            <input
-                                className='form-control'
-                                type='text'
-                                value={note}
-                                onChange={event => setNote(event.target.value)}
-                                name='note'
-                                placeholder='Type action note here'
-                            />
-                            {mutationLoading && <p className='text-center'>Saving note ...</p>}
-                        </div>
+                        <h6>Add note for <strong>{userName}</strong> </h6>
+                        <input
+                            className='form-control'
+                            type='text'
+                            value={note}
+                            onChange={event => setNote(event.target.value)}
+                            name='note'
+                            placeholder='Type action note here'
+                        />
+                        {mutationLoading && <p className='text-center'>Saving note ...</p>}
+                    </div>
 
-                    </ModalDialog>
+                </ModalDialog>
 
 
-                    <Table className={classes.table} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell>Name</StyledTableCell>
-                                <StyledTableCell align="right">Role</StyledTableCell>
-                                <StyledTableCell align="right">Phone Number</StyledTableCell>
-                                <StyledTableCell align="right">Email</StyledTableCell>
-                                <StyledTableCell align="right">Date</StyledTableCell>
-                                <StyledTableCell align="right">Note</StyledTableCell>
-                                <StyledTableCell align="right">Add Note</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.users.map(user => (
-                                <StyledTableRow key={user.id}>
-                                    <StyledTableCell component="th" scope="row">
-                                        {user.name}
-                                    </StyledTableCell>
-                                    <StyledTableCell align="right">{user.roleName}</StyledTableCell>
-                                    <StyledTableCell align="right">{user.phoneNumber || "None"}</StyledTableCell>
-                                    <StyledTableCell align="right">{user.email}</StyledTableCell>
-                                    <StyledTableCell align="right">{user.notes[0] ? DateUtil.formatDate(user.notes[0].createdAt) : "N/A"}</StyledTableCell>
-                                    <StyledTableCell align="right">{user.notes[0] ? user.notes[0].body : "None"}</StyledTableCell>
-                                    <StyledTableCell align="right">
+                <div className={classes.root}>
+                    <Fragment >
+                        <InputBase
+                            className={classes.input}
+                            type='text'
+                            placeholder="Search User"
+                            onFocus={inputToSearch}
+                            inputProps={{ "aria-label": "search User" }}
+                        />
+                        <Divider className={classes.divider} orientation="vertical" />
+                        <IconButton
+                            type="submit"
+                            className={classes.iconButton}
+                            aria-label="search"
+                        >
+                            <SearchIcon />
+                        </IconButton>
+                    </Fragment>
+                </div>
+                <Table className={classes.table} aria-label="customized table">
 
-                                        <Button color="secondary" onClick={() => handleModal(user.id, user.name)}>
-                                            +
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell align="right">Role</StyledTableCell>
+                            <StyledTableCell align="right">Phone Number</StyledTableCell>
+                            <StyledTableCell align="right">Email</StyledTableCell>
+                            <StyledTableCell align="right">Date</StyledTableCell>
+                            <StyledTableCell align="right">Note</StyledTableCell>
+                            <StyledTableCell align="right">Add Note</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.users.map(user => (
+                            <StyledTableRow key={user.id}>
+                                <StyledTableCell component="th" scope="row">
+                                    {user.name}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{user.roleName}</StyledTableCell>
+                                <StyledTableCell align="right">{user.phoneNumber || "None"}</StyledTableCell>
+                                <StyledTableCell align="right">{user.email}</StyledTableCell>
+                                <StyledTableCell align="right">{user.notes[0] ? DateUtil.formatDate(user.notes[0].createdAt) : "N/A"}</StyledTableCell>
+                                <StyledTableCell align="right">{user.notes[0] ? user.notes[0].body : "None"}</StyledTableCell>
+                                <StyledTableCell align="right">
+
+                                    <Button color="secondary" onClick={() => handleModal(user.id, user.name)}>
+                                        +
                                     </Button>
 
-                                    </StyledTableCell>
-                                </StyledTableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                    <TablePagination
-                        rowsPerPageOptions={[5, 10, 25, 50, 100]}
-                        component="div"
-                        count={data.users.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                    />
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25, 50, 100]}
+                    component="div"
+                    count={data.users.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
 
 
-                </div>
-            </Fragment >
+            </div>
+        </Fragment >
 
-        
+
     )
 }
 
