151b5159af119cf5e0226cd04b4e9bb3777aaaf8
OlivierJM <manolivier93@gmail.com>
2020-03-05 15:54:05 +0200

    add mutation specs for showroom

--- spec/models/showroom_spec.rb
+++ spec/models/showroom_spec.rb
@@ -5,17 +5,10 @@ require 'rails_helper'
 RSpec.describe Showroom, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   # create showroom entry
-  Showroom.create(
-    name: 'Olivier',
-  )
   it 'should create a showroom entry' do
-
+    Showroom.create(
+      name: 'Olivier',
+    )
     expect(Showroom.all.count).to eql 1
   end
-
-  it 'should also create an entry log entry' do
-
-    expect(Showroom.all.count).to eql 1
-  end
-
 end
