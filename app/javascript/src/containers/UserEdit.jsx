2ddce23f8540fe66a94d105f919440134e8c7f19
OlivierJM <manolivier93@gmail.com>
2020-03-05 14:09:59 +0200

    custom editing expiry date

--- app/javascript/src/containers/UserEdit.jsx
+++ app/javascript/src/containers/UserEdit.jsx
@@ -75,7 +75,7 @@ export default function FormContainer({ match, history }) {
     const values = {
       ...data,
       avatarBlobId: signedBlobId,
-      expiresAt: data.expiresAt ? addDays(new Date(data.expiresAt), data.days ? Number(data.days) : 0).toISOString() : addDays(new Date(), 1).toISOString()
+      expiresAt: data.expiresAt
     }
 
     createOrUpdate(values)
