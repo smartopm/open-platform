50c52c6b71003a487aafcad0f0f2cbe7fdb039c4
Mark Percival <m@mdp.im>
2020-03-09 15:20:21 +0000

    Update certs and process
    - Add Nick to the certs
    - Clean up bash file to be less brittle.
    - Simplify gitignore

--- bin/cert_setup.sh
+++ bin/cert_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/dev.dgdp.site/privkey.enc -out certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -d
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/vpn.dgdp.site/privkey.enc -out certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -d
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/olivier.dgdp.site/privkey.enc -out certs/olivier.dgdp.site/privkey.pem -pbkdf2 -a -d
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in certs/dennis.dgdp.site/privkey.enc -out certs/dennis.dgdp.site/privkey.pem -pbkdf2 -a -d
+  for d in certs/*/ ; do
+    echo "Decrypting $d"
+    docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -in "$d"privkey.enc -out "$d"privkey.pem -pbkdf2 -a -d
+  done
 elif [ $1 = "e" ]; then
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/dev.dgdp.site/privkey.enc -in certs/dev.dgdp.site/privkey.pem -pbkdf2 -a -e
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/vpn.dgdp.site/privkey.enc -in certs/vpn.dgdp.site/privkey.pem -pbkdf2 -a -e
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/olivier.dgdp.site/privkey.enc -in certs/olivier.dgdp.site/privkey.pem -pbkdf2 -a -e
-  docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out certs/dennis.dgdp.site/privkey.enc -in certs/dennis.dgdp.site/privkey.pem -pbkdf2 -a -e
+  for d in certs/*/ ; do
+    echo "Encrypting $d"
+    docker-compose run --rm rails openssl aes-256-cbc -salt -pass file:config/credentials/development.key -out "$d"privkey.enc -in "$d"privkey.pem -pbkdf2 -a -e
+  done
 fi
