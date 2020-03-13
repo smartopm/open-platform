f4e8866454d22de2dfae1d778091e2f12dca0b00
OlivierJM <manolivier93@gmail.com>
2020-03-13 15:34:58 +0200

    sending messages and saving

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@
 # Messages being sent out
 class Message < ApplicationRecord
   belongs_to :user
+  
   default_scope { order(created_at: :desc) }
 
   class Unauthorized < StandardError; end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_101803) do
+ActiveRecord::Schema.define(version: 2020_03_13_131737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -116,6 +116,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_101803) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.uuid "user_id"
+    t.uuid "receiver_id"
   end
 
   create_table "notes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
