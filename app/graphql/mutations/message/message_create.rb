114a572b9bc0bb2bcd2d90542a32416fa0c0472c
OlivierJM <manolivier93@gmail.com>
2020-03-13 15:44:20 +0200

    change to ==> receiver and fix rubocop issues

--- app/graphql/mutations/message/message_create.rb
+++ app/graphql/mutations/message/message_create.rb
@@ -4,22 +4,17 @@ module Mutations
   module Message
     # Create Message
     class MessageCreate < BaseMutation
-      argument :to, String, required: true
+      argument :receiver, String, required: true
       argument :sms_content, String, required: true
       argument :receiver_id, String, required: true
 
       field :messages, Types::MessageType, null: true
 
       def resolve(vals)
-        message = context[:current_user].messages.new(
-          user_id: context[:current_user].id,
-          receiver: vals[:to],
-          receiver_id: vals[:receiver_id],
-          sms_content: vals[:sms_content],
-        )
-       
+        message = context[:current_user].messages.new(vals)
+
         message.save
-        message.send_sms(vals[:to], vals[:sms_content])
+        message.send_sms(vals[:receiver], vals[:sms_content])
         return { message: message } if message.persisted?
 
         raise GraphQL::ExecutionError, message.errors.full_messages
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -26,7 +26,7 @@ module Types
     field :owner_id, ID, null: true
     field :followup_at, GraphQL::Types::ISO8601DateTime, null: true
     field :notes, [Types::NoteType], null: true, visible: { roles: %i[admin], user: :id }
-    field :messages, [Types::MessageType], null: true
+    field :messages, [Types::MessageType], null: true, visible: { roles: %i[admin], user: :id }
 
     def avatar_url
       return nil unless object.avatar.attached?
