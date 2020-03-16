8080c0ba829986b09a586c0277c585445a7adda4
dennis60 <dennis@doublegdp.com>
2020-03-16 07:39:07 +0000

    Fixed failed tests

--- app/javascript/src/containers/Messages.jsx
+++ app/javascript/src/containers/Messages.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import {Grid,Typography,Checkbox,Divider} from "@material-ui/core";
 
 export default function Messages(){
 
