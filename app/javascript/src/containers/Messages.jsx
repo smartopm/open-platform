f09ce3916aca50904f3d17973a1f15b4077e239b
dennis60 <dennis@doublegdp.com>
2020-03-16 07:28:35 +0000

    Changed Support card to Contact card

---
+++ app/javascript/src/containers/Messages.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import {Grid,Typography,Checkbox,Divider} from "@material-ui/core";
+
+export default function Messages(){
+
+        return (
+            <div>
+
+                
+                
+            </div>
+        );
+    
+}
