9324a7af6824a9651f1c4fcdea84b8dcadf63a73
OlivierJM <manolivier93@gmail.com>
2020-03-17 13:55:30 +0200

    add message input for the user

--- app/javascript/src/containers/Messages/UserMessages.jsx
+++ app/javascript/src/containers/Messages/UserMessages.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import { useParams } from "react-router-dom";
 import { useQuery } from 'react-apollo'
 import { useHistory } from "react-router-dom";
@@ -6,6 +6,7 @@ import { UserMessageQuery } from '../../graphql/queries'
 import Loading from '../../components/Loading'
 import ErrorPage from '../../components/Error'
 import { Context as AuthStateContext } from '../Provider/AuthStateProvider.js'
+import TextField from '@material-ui/core/TextField';
 
 export default function UserMessages() {
     const { id } = useParams()
@@ -20,13 +21,32 @@ export default function UserMessages() {
     if (loading) return <Loading />
     if (error) return <ErrorPage error={error.message} />
     return (
-        <ul>
-            {
-                data.userMessages.length && data.userMessages.map(message => (
-                    <li key={message.id}>{message.message}</li>
-                ))
-            }
-        </ul>
+        <Fragment>
+            <ul>
+                {
+                    data.userMessages.length && data.userMessages.map(message => (
+                        <li key={message.id}>{message.message}</li>
+                    ))
+                }
+            </ul>
+            <br />
+            <TextField
+                id="standard-full-width"
+                label="Label"
+                style={{ margin: 8 }}
+                placeholder="Placeholder"
+                // add character count here
+                helperText="120"
+                fullWidth
+                multiline
+                rows={3}
+                margin="normal"
+                variant="outlined"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+            />
+        </Fragment>
     )
 }
 
