22256d545db1037f1cd6eef565a06485fd2565f9
OlivierJM <manolivier93@gmail.com>
2020-03-17 11:52:50 +0200

    add query for all messages by a user

--- app/graphql/types/queries/message.rb
+++ app/graphql/types/queries/message.rb
@@ -9,9 +9,19 @@ module Types::Queries::Message
     field :messages, [Types::MessageType], null: true do
       description 'Get a list of messages'
     end
+
+    # Get messages for one user
+    field :user_messages, [Types::MessageType], null: true do
+      description 'Get a list of messages for one user'
+      argument :id, GraphQL::Types::ID, required: true
+    end
   end
 
   def messages
     Message.all
+  end
+
+  def user_messages(id:)
+    Message.where(user_id: id)
   end
 end
