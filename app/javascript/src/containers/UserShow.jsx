a63554ad66b0e5b1fc8453b55a87ba3d47b6392f
OlivierJM <manolivier93@gmail.com>
2020-03-18 15:12:42 +0200

    allow admins to send SMS from user profile

--- app/javascript/src/containers/UserShow.jsx
+++ app/javascript/src/containers/UserShow.jsx
@@ -184,7 +184,7 @@ export function Component({
                   authState.user.userType === 'security_guard' ? (
                     <MenuItem key={'log_entry'} onClick={onLogEntry}>
                       Log This Entry
-                  </MenuItem>
+                    </MenuItem>
                   ) : null}
                 {authState.user.userType === 'security_guard' ? (
                   <MenuItem key={'call_p'}>
@@ -205,6 +205,14 @@ export function Component({
                         className={css(styles.linkItem)}
                       >
                         Edit
+                      </Link>
+                    </MenuItem>
+                    <MenuItem key={'edit_user'}>
+                      <Link
+                        to={`/message/${data.user.id}`}
+                        className={css(styles.linkItem)}
+                      >
+                        Send SMS to {data.user.name}
                       </Link>
                     </MenuItem>
 
