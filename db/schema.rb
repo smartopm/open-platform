27ef34b4e93274bc60681b914414091b6fda0da2
Nicolas Garcia <nicolas@doublegdp.com>
2020-03-19 18:01:37 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_125732) do
+ActiveRecord::Schema.define(version: 2020_03_17_102221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -97,6 +97,16 @@ ActiveRecord::Schema.define(version: 2020_03_10_125732) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "receiver"
+    t.text "message"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "user_id"
+    t.uuid "sender_id"
+  end
+
   create_table "notes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "user_id"
     t.uuid "author_id"
@@ -109,7 +119,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_125732) do
   end
 
   create_table "showrooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "userId"
+    t.uuid "userId"
     t.string "name"
     t.string "email"
     t.string "home_address"
