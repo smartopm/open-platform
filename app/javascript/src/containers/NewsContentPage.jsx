e6ec3d8417e89f15454d3632b3172e1fd34792a7
OlivierJM <manolivier93@gmail.com>
2020-04-02 18:22:18 +0200

    resize the window accordingly

--- app/javascript/src/containers/NewsContentPage.jsx
+++ app/javascript/src/containers/NewsContentPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import IframeContainer from '../components/IframeContainer'
+import { useWindowDimensions } from '../utils/customHooks'
 
 export default function NewsContentPage() {
-  return <IframeContainer link="https://doublegdp.gitlab.io/nkwashi-content/" />
+  const { width, height } = useWindowDimensions()
+  return (
+    <IframeContainer
+      link="https://doublegdp.gitlab.io/nkwashi-content/"
+      height={height}
+      width={width}
+    />
+  )
 }
