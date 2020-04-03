f48c75604fddae3afeeb8007810ca8ef7476b08e
OlivierJM <olivier@doublegdp.com>
2020-04-03 15:50:25 +0000

    format the url and pass params

--- app/javascript/src/containers/NewsContentPage.jsx
+++ app/javascript/src/containers/NewsContentPage.jsx
@@ -5,8 +5,8 @@ import { useWindowDimensions } from '../utils/customHooks'
 
 export default function NewsContentPage() {
   const { width, height } = useWindowDimensions()
-  const { link } = useParams()
-  const url = `https://doublegdp.gitlab.io/nkwashi-content/${link || ''}`
+  const currentUrl = window.location.href 
+  const url = `https://doublegdp.gitlab.io/nkwashi-content/${formatUrl(currentUrl, 'news') || ''}`
 
   return (
     <IframeContainer
@@ -15,4 +15,12 @@ export default function NewsContentPage() {
       width={width}
     />
   )
+}
+
+function formatUrl(url, tag){
+  const urlContents = url.split('/')
+  // find index of the tag, in this case our news item
+  const tagIndex = urlContents.findIndex(item => item === tag )
+  // return the combined array after the found index
+  return urlContents.slice(tagIndex + 1).join('/')
 }
