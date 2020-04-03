6f6f2115b5b05e5e25f348c632ac1af3924f6420
OlivierJM <olivier@doublegdp.com>
2020-04-03 16:25:13 +0000

    added more tests

--- app/javascript/src/containers/NewsContentPage.jsx
+++ app/javascript/src/containers/NewsContentPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 import IframeContainer from '../components/IframeContainer'
 import { useWindowDimensions } from '../utils/customHooks'
 
 export default function NewsContentPage() {
   const { width, height } = useWindowDimensions()
-  const currentUrl = window.location.href 
+  const currentUrl = window.location.href
   const url = `https://doublegdp.gitlab.io/nkwashi-content/${formatUrl(currentUrl, 'news') || ''}`
 
   return (
@@ -17,10 +16,10 @@ export default function NewsContentPage() {
   )
 }
 
-function formatUrl(url, tag){
+export function formatUrl(url, tag) {
   const urlContents = url.split('/')
   // find index of the tag, in this case our news item
-  const tagIndex = urlContents.findIndex(item => item === tag )
+  const tagIndex = urlContents.findIndex(item => item === tag)
   // return the combined array after the found index
   return urlContents.slice(tagIndex + 1).join('/')
 }
