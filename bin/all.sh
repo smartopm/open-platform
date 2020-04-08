073615412adfa77c7df50610e52ff56ed44ee838
Nicolas Garcia <nicolas@doublegdp.com>
2020-04-08 20:45:33 +0000

    fixed react coverage.   It's even worse than previously thought

--- bin/all.sh
+++ bin/all.sh
@@ -5,6 +5,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker-compose run --rm webpacker yarn lint
-docker-compose run --rm webpacker yarn test
+docker-compose run --rm webpacker yarn test --coverage --collectCoverageFrom=app/javascript/src/**/*.{js,jsx}
 $DIR/docker_rake lint
 $DIR/docker_rake
