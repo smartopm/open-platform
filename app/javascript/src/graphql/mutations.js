a17bd2b54874e93cf05dc0e88fb81499e9c794cd
OlivierJM <olivier@doublegdp.com>
2020-04-10 15:51:25 +0000

    remove the frontend mutations

--- app/javascript/src/graphql/mutations.js
+++ app/javascript/src/graphql/mutations.js
@@ -382,14 +382,3 @@ export const MessageCreate = gql`
     }
   }
 `
-// This in the backend only updates the read status
-export const MessageUpdate = gql`
-  mutation messageUpdate($id: ID!) {
-      messageUpdate(id:$id){
-        message {
-          message
-          isRead
-        }
-      }
-    }
-` 
