3e0141642790b5b545895b5c2bb3d99b52318245
OlivierJM <olivier@doublegdp.com>
2020-04-13 12:17:30 +0000

    add an updated at field to message

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -12,5 +12,6 @@ module Types
     field :message, String, null: true
     field :is_read, Boolean, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
