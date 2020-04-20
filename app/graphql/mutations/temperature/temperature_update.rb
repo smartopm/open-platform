8bb48c3312fdc133a70d3154bfd86bd6693beccc
OlivierJM <manolivier93@gmail.com>
2020-04-20 14:32:27 +0200

    restructured the update temperature mutation

--- app/graphql/mutations/temperature/temperature_update.rb
+++ app/graphql/mutations/temperature/temperature_update.rb
@@ -13,18 +13,22 @@ module Mutations
         user = ::User.find(vals[:user_id])
         raise GraphQL::ExecutionError, 'User not found' unless user
 
-        event_log = ::EventLog.create(acting_user_id: context[:current_user].id,
-                                      community_id: user.community_id, subject: 'user_temp',
-                                      ref_id: user.id,
-                                      ref_type: 'User',
-                                      data: {
-                                        ref_name: user.name,
-                                        note: vals[:temp],
-                                        type: user.user_type,
-                                      })
+        event_log = log_temperature(user, vals[:temp])
         return { event_log: event_log, user: user } if event_log.save
 
         raise GraphQL::ExecutionError, event_log.errors.full_messages
+      end
+
+      def log_temperature(user, temp)
+        ::EventLog.create(acting_user_id: context[:current_user].id,
+                          community_id: user.community_id, subject: 'user_temp',
+                          ref_id: user.id,
+                          ref_type: 'User',
+                          data: {
+                            ref_name: user.name,
+                            note: temp,
+                            type: user.user_type,
+                          })
       end
 
       # TODO: Better auth here
