baa60cbf4088fcbd24637e92b0ba0923220f852c
OlivierJM <manolivier93@gmail.com>
2020-04-20 15:44:13 +0200

    passing args to temp mutation

--- app/graphql/mutations/temperature/temperature_update.rb
+++ app/graphql/mutations/temperature/temperature_update.rb
@@ -5,30 +5,28 @@ module Mutations
     # Create an event for temperature record
     class TemperatureUpdate < BaseMutation
       argument :temp, Integer, required: true
-      argument :user_id, ID, required: true
+      argument :ref_id, ID, required: true
+      argument :ref_name, ID, required: true
 
       field :event_log, Types::EventLogType, null: true
 
-      def resolve(vals)
-        user = ::User.find(vals[:user_id])
-        raise GraphQL::ExecutionError, 'User not found' unless user
-
-        event_log = log_temperature(user, vals[:temp])
-        return { event_log: event_log, user: user } if event_log.save
+      def resolve(temp:, ref_id:, ref_name:)
+        event_log = log_temperature(temp, ref_id, ref_name)
+        return { event_log: event_log } if event_log.save
 
         raise GraphQL::ExecutionError, event_log.errors.full_messages
       end
 
-      def log_temperature(user, temp)
-        ::EventLog.create(acting_user_id: context[:current_user].id,
-                          community_id: user.community_id, subject: 'user_temp',
-                          ref_id: user.id,
-                          ref_type: 'User',
-                          data: {
-                            ref_name: user.name,
-                            note: temp,
-                            type: user.user_type,
-                          })
+      def log_temperature(temp, id, name)
+        ::EventLog.new(acting_user_id: context[:current_user].id,
+                       community_id: context[:current_user].community_id, subject: 'user_temp',
+                       ref_id: id,
+                       ref_type: 'User',
+                       data: {
+                         ref_name: name,
+                         note: temp,
+                         type: 'Temp',
+                       })
       end
 
       # TODO: Better auth here
