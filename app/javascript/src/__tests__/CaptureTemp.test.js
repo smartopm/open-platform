22fe63a324a870fc83d7c98476ce17a60b81bf50
dennis60 <dennis@doublegdp.com>
2020-04-21 11:42:05 +0000

    added mutation mock up tp test

--- app/javascript/src/__tests__/CaptureTemp.test.js
+++ app/javascript/src/__tests__/CaptureTemp.test.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import CaptureTemp from '../components/CaptureTemp'
-
+import { MockedProvider } from '@apollo/react-testing'
 
 
 describe('temperature component', () => {
+
+    const screenProps = {
+        refId: 1,
+        refName: 'Test name'
+    }
     it('component is mounted', () => {
-        const wrapper = shallow(<CaptureTemp />)
+        const wrapper = shallow(
+            <MockedProvider mock={[]}>
+                <CaptureTemp {...screenProps} />
+            </MockedProvider>
+        )
         expect(wrapper.find('.button'))
     })
 
-    const screenProps = {
-        handleClick: jest.fn(),
-        handleTempInput: jest.fn()
-    }
-    const wrapper = shallow(<CaptureTemp {...screenProps} />)
+
+    const wrapper = shallow(
+        <MockedProvider mock={[]}>
+            <CaptureTemp {...screenProps} />
+        </MockedProvider>)
     it('It should get the temperature value', () => {
-        const {handleClick,handleTempInput} = wrapper.props()
-        expect(handleClick).toBeUndefined()
-        expect(handleTempInput).toBeUndefined()
-        
+        const { refId, refName } = wrapper.props()
+        expect(refId).toBe(refId)
+        expect(refName).toBe(refName)
 
     })
 });
