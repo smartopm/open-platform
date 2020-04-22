7c3691a65b4c5bfebd64e018b0e15e03918427c0
OlivierJM <manolivier93@gmail.com>
2020-04-22 14:18:51 +0200

    update tests for time tracking

--- app/graphql/mutations/timesheet/track_time.rb
+++ app/graphql/mutations/timesheet/track_time.rb
@@ -14,14 +14,14 @@ module Mutations
         user = ::User.find(user_id)
         raise GraphQL::ExecutionError, 'User not found' unless user
 
-        event_log = instantiate_event_log(context[:current_user], user, start_date, end_date)
+        event_log = log_user_shift(context[:current_user], user, start_date, end_date)
 
         return { event_log: event_log } if event_log.save
 
         raise GraphQL::ExecutionError, event_log.errors.full_messages
       end
 
-      def instantiate_event_log(current_user, user, start_date, end_date)
+      def log_user_shift(current_user, user, start_date, end_date)
         EventLog.new(acting_user_id: current_user.id,
                      community_id: user.community_id, subject: 'user_shift',
                      ref_id: user.id,
@@ -31,6 +31,14 @@ module Mutations
                        type: user.user_type,
                        shift: { start_date: start_date, end_date: end_date },
                      })
+      end
+
+      # TODO: update admin ==> custodian when role is implemented
+      def authorized?(_vals)
+        current_user = context[:current_user]
+        raise GraphQL::ExecutionError, 'Unauthorized' unless current_user&.admin?
+
+        true
       end
     end
   end
--- spec/graphql/mutations/time_tracker_spec.rb
+++ spec/graphql/mutations/time_tracker_spec.rb
@@ -7,16 +7,18 @@ RSpec.describe Mutations::Timesheet::TrackTime do
     let!(:user) { create(:user_with_community) }
     let!(:admin) { create(:admin_user, community_id: user.community_id) }
 
+    # TODO: Add another user once implemented (a custodian)
     let(:query) do
       <<~GQL
-      mutation($userId: ID!, $startDate: String, $endDate: String) {
-        trackTime(userId: $userId, startDate: $startDate, endDate: $endDate){
-          eventLog {
-            data 
-            refId
+        mutation($userId: ID!, $startDate: String, $endDate: String) {
+          trackTime(userId: $userId, startDate: $startDate, endDate: $endDate){
+            eventLog {
+              data
+              refId
+              subject
+            }
           }
         }
-      }
       GQL
     end
 
@@ -31,10 +33,22 @@ RSpec.describe Mutations::Timesheet::TrackTime do
                                                 current_user: admin,
                                               }).as_json
       expect(result.dig('data', 'trackTime', 'eventLog', 'refId')).to eql admin.id
-      expect(result.dig('data', 'trackTime', 'eventLog', 'refId')).not_to be_nil
+      expect(result.dig('data', 'trackTime', 'eventLog', 'subject')).to eql 'user_shift'
       expect(result.dig('data', 'trackTime', 'eventLog', 'data', 'shift')).not_to be_nil
       expect(result.dig('errors')).to be_nil
     end
 
+    it 'should not record temperature when not authorized' do
+      variables = {
+        userId: user.id,
+        startDate: '2020-04-21',
+      }
+      result = DoubleGdpSchema.execute(query, variables: variables,
+                                              context: {
+                                                current_user: user,
+                                              }).as_json
+      expect(result.dig('data', 'trackTime', 'eventLog', 'refId')).to be_nil
+      expect(result.dig('errors', 0, 'message')).to eql 'Unauthorized'
+    end
   end
 end
