201f0a5fb068f9980d5539c32c86bf3f3e3c79c6
Yoram <yoram@doublegdp.com>
2020-04-23 09:43:46 +0000

    -changed tracking ID for application

--- app/javascript/packs/react_main.jsx
+++ app/javascript/packs/react_main.jsx
@@ -63,7 +63,7 @@ import UserMessages from '../src/containers/Messages/UserMessages'
 import NewsContentPage from '../src/containers/NewsContentPage'
 import ReactGA from 'react-ga';
 // Prevent Google Analytics reporting from staging and dev domains
-const PRIMARY_DOMAINS = ['yoram.dgdp.site']
+const PRIMARY_DOMAINS = ['app.doublegdp.com']
 
 class DynamicImport extends Component {
   constructor(props) {
@@ -112,7 +112,7 @@ const Logout = () => {
   return <Redirect to="/login" />
 }
 //page tracking
-ReactGA.initialize('UA-163895873-1');
+ReactGA.initialize('G-E4KP1B4LDQ');
 
 const Analytics = props => {
   const gtag = window.gtag
