f29c232a60b8a19c5c54dd45df2016b9174dce41
OlivierJM <manolivier93@gmail.com>
2020-04-27 11:35:02 +0200

    create a timesheet table

---
+++ app/javascript/src/__tests__/CustodianTimeSheetLog.js
@@ -0,0 +1,4 @@
+
+describe('custodian timesheet buttons', () => {    
+    it.todo('@olivier add tests for the the employee timesheet')
+})
---
+++ app/models/time_sheet.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+
+class TimeSheet < ApplicationRecord
+    belongs_to :user
+    belongs_to :shift_start_event_log, optional: true, class_name: 'EventLog'
+    belongs_to :shift_end_event_log, optional: true, class_name: 'EventLog'
+    
+end
