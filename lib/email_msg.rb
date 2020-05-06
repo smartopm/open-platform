1431fbfa85fabd5bd613ceb0b2859eb87e6735a2
OlivierJM <manolivier93@gmail.com>
2020-05-06 15:33:51 +0200

    clean the email sender class and add name to the template

--- lib/email_msg.rb
+++ lib/email_msg.rb
@@ -11,23 +11,19 @@ class EmailMsg
   class UninitializedError < StandardError; end
   class EmailMsgError < StandardError; end
 
-  def self.send_welcome_msg(user_email)
+  def self.send_welcome_msg(user_email, name)
+    raise EmailMsgError, 'Email must be provided' if user_email.blank?
+
     client = SendGrid::API.new(api_key: Rails.application.credentials[:sendgrid_api_key]).client
     mail = SendGrid::Mail.new
     mail.from = SendGrid::Email.new(email: 'olivier@doublegdp.com')
     personalization = Personalization.new
     personalization.add_to(SendGrid::Email.new(email: user_email))
-    personalization.add_dynamic_template_data({
-        "community": "Nkwashi Community"
-    })
+    personalization.add_dynamic_template_data(
+      "community": 'Nkwashi Community', "name": name
+    )
     mail.add_personalization(personalization)
     mail.template_id = 'd-bec0f1bd39f240d98a146faa4d7c5235'
-
-    begin
-        response = client.mail._("send").post(request_body: mail.to_json)
-        
-    rescue Exception => e
-        Rails.logger.info e.message
-    end
+    client.mail._('send').post(request_body: mail.to_json)
   end
 end
