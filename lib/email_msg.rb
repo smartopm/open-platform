fb7099442194c1f60a0370d0924d267c83885e41
OlivierJM <manolivier93@gmail.com>
2020-05-06 17:45:02 +0200

    fix rubocop errors

--- lib/email_msg.rb
+++ lib/email_msg.rb
@@ -21,7 +21,7 @@ class EmailMsg
     personalization.add_to(SendGrid::Email.new(email: user_email))
     personalization.add_dynamic_template_data("community": community, "name": name)
     mail.add_personalization(personalization)
-    mail.template_id = "d-bec0f1bd39f240d98a146faa4d7c5235"
+    mail.template_id = 'd-bec0f1bd39f240d98a146faa4d7c5235'
     client.mail._('send').post(request_body: mail.to_json)
   end
   # rubocop:enable Metrics/AbcSize
