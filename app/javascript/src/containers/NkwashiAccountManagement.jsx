42dfa75163b5c547f7285f4d291811ad66665493
Yoram <yoram@doublegdp.com>
2020-05-07 17:00:02 +0000

    added log off notice

--- app/javascript/src/containers/NkwashiAccountManagement.jsx
+++ app/javascript/src/containers/NkwashiAccountManagement.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useWindowDimensions } from '../utils/customHooks'
 import IframeContainer from '../components/IframeContainer'
+import {  Grid } from '@material-ui/core'
 import Nav from '../components/Nav';
+import { StyleSheet, css } from 'aphrodite'
 
 export default function NkwashiAccountManagement() {
     const { width, height } = useWindowDimensions()
@@ -10,15 +12,34 @@ export default function NkwashiAccountManagement() {
     return (
 
         <React.Fragment>
-            <Nav navName="Thebe Investment Management" menuButton="back" backTo="/" />
+            <Nav navName="Thebe Investment Management" menuButton="back" backTo="/">
+                <Grid container direction="row" justify="center" alignItems="center">
+                    <Grid container justify="center" item xs={10}>
+                    <div>
+                        <p className={css(styles.navTitle)}><i>This application is providing access to your Thebe portal. Remember to log off once you are done.</i></p>
+                    </div>
+                    </Grid>
+                </Grid>
+            </Nav>
             <IframeContainer
                 link={url}
                 height={height}
                 width={width}
             />
-
         </React.Fragment>
     )
 
 }
+const styles = StyleSheet.create({
+    navTitle: {
+      top: '8px',
+      color: '#FFF',
+      display: 'flex',
+      marginLeft: 100,
+      justifyContent: "center",
+      fontSize: 12,
+      right: 10
+    }
+  })
+  
 
