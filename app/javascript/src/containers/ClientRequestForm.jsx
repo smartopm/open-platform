c869ce44b189242e5811c14c6e28dde564a021d8
Nicolas Garcia <nicolas@doublegdp.com>
2020-05-08 00:22:05 +0000

    fixed google iframe

--- app/javascript/src/containers/ClientRequestForm.jsx
+++ app/javascript/src/containers/ClientRequestForm.jsx
@@ -5,7 +5,7 @@ import Nav from '../components/Nav';
 
 export default function ClientRequestForm(){
     const { width, height } = useWindowDimensions()
-    const url = "https://forms.gle/Sdbj91Sia8EpDJiN6"
+    const url = "https://docs.google.com/forms/d/e/1FAIpQLSeC663sLzKdpxzaqzY2gdGAT5fe-Uc8lvLi1V7KdLfrralyeA/viewform?embedded=true"
     return(
 
         <React.Fragment>
