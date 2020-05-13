5b2448d8dc3a5701453e029d1ee279593985d0aa
KamranKhan <kamran@doublegdp.com>
2020-05-13 16:52:23 +0000

    Added fake data to test

--- app/javascript/src/__tests__/ContactCard.test.js
+++ app/javascript/src/__tests__/ContactCard.test.js
@@ -9,20 +9,20 @@ import {
 describe('Contact page', () => {
 
     const userDetails = {
-        id: "1d046c3c-5a28-4b0d-8b2e-758d67346ec2",
-        email: "kamran@doublegdp.com",
-        name: "Kamran Khan",
-        userType: "admin",
+        id: "863a-45eq-dsf78",
+        email: "Jam@jam.com",
+        name: "Jam J",
+        userType: "user",
         phoneNumber: "7778889999",
         expiresAt: null,
-        imageUrl: "https://lh6.googleusercontent.com/-h8heA9fT5Cs/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJMfQJ67FAkAsFwRebXE3h3NCgUdfg/photo.jpg",
+        imageUrl: "https://image.jpg",
         avatarUrl: null
     }
 
     const sendMessage = {
-        pathname: `/message/1d046c3c-5a28-4b0d-8b2e-758d67346ec2`,
+        pathname: `/message/863a-45eq-dsf78`,
         clientName: 'Contact Support',
-        clientNumber: '260974624243',
+        clientNumber: '290974624573',
         from: 'contact'
     }
 
@@ -37,7 +37,7 @@ describe('Contact page', () => {
         const { getByTestId } = render(<SupportCard handleSendMessage={sendMessage} userData={userDetails} />)
         const button = getByTestId('crf');
         fireEvent.click(button)
-        expect(window.open).toBeCalledWith("https://docs.google.com/forms/d/e/1FAIpQLSeC663sLzKdpxzaqzY2gdGAT5fe-Uc8lvLi1V7KdLfrralyeA/viewform?entry.568472638=Kamran+Khan&7778889999?entry.1055458143=7778889999:entry.1055458143=\"\"", '_blank')
+        expect(window.open).toBeCalledWith("https://docs.google.com/forms/d/e/1FAIpQLSeC663sLzKdpxzaqzY2gdGAT5fe-Uc8lvLi1V7KdLfrralyeA/viewform?entry.568472638=Jam+J&7778889999?entry.1055458143=7778889999:entry.1055458143=\"\"", '_blank')
     });
 
     it('clicks pay with mobile money then opens use window', () => {
