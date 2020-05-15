fdfc8de8b7faf887fbb4f23a511f22a4fc539cd0
OlivierJM <manolivier93@gmail.com>
2020-05-15 17:24:06 +0200

    fix tests after mutation change

--- spec/graphql/mutations/add_activity_log_spec.rb
+++ spec/graphql/mutations/add_activity_log_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe Mutations::ActivityLog::Add do
   describe 'user' do
     let!(:user) { create(:user_with_community) }
     let!(:reporting_user) { create(:user, community_id: user.community_id) }
-
     let!(:other_community_user) { create(:user_with_community) }
 
     let(:query) do
       <<~GQL
-        mutation AddActivityLogMutation($userId: ID!, $timestamp: String, $digital: Boolean, $note: String) {
-          activityLogAdd(userId: $userId, timestamp: $timestamp, digital: $digital, note: $note) {
+        mutation AddActivityLogMutation($userId: ID!, $timestamp: String, $digital: Boolean, $note: String, $subject: String) {
+          activityLogAdd(userId: $userId, timestamp: $timestamp, digital: $digital, note: $note, subject: $subject) {
             eventLog {
               actingUser {
                 id
               }
               data
+              subject
               id
             }
           }
@@ -42,7 +42,7 @@ RSpec.describe Mutations::ActivityLog::Add do
       expect(result.dig('data', 'activityLogAdd', 'eventLog', 'data', 'digital')).to eql true
     end
 
-    it 'returns should not create an invalid activity log' do
+    it 'should allow all users to create an activity log' do
       variables = {
         userId: user.id,
       }
@@ -51,8 +51,25 @@ RSpec.describe Mutations::ActivityLog::Add do
           current_user: other_community_user,
         }
       ).as_json
-      expect(result.dig('data', 'activityLogAdd')).to be_nil
-      expect(result.dig('errors')).not_to be_empty
+      expect(result.dig('data', 'activityLogAdd', 'eventLog', 'id')).not_to be_nil
+      expect(result.dig('errors')).to be_nil
+    end
+
+    it 'should create a user referred event' do
+      variables = {
+        userId: user.id,
+        subject: 'user_referred',
+      }
+      result = DoubleGdpSchema.execute(
+        query, variables: variables, context: {
+          current_user: user,
+        }
+      ).as_json
+
+      expect(result.dig('data', 'activityLogAdd', 'eventLog', 'id')).not_to be_nil
+      expect(result.dig('data', 'activityLogAdd', 'eventLog', 'actingUser', 'id')).to eql user.id
+      expect(result.dig('data', 'activityLogAdd', 'eventLog', 'subject')).to eql 'user_referred'
+      expect(result.dig('errors')).to be_nil
     end
   end
 end
