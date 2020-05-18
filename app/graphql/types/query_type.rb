4ceff222a310bbad13f69c9d51c2993885910ad2
OlivierJM <manolivier93@gmail.com>
2020-05-18 17:07:27 +0200

    remove the unused queries in query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,27 +21,6 @@ module Types
       Community.find(id) if context[:current_user]
     end
 
-    # Get a current user information
-    field :current_user, UserType, null: true do
-      description 'Get the current logged in user'
-    end
-
-    def current_user
-      return context[:current_user] if context[:current_user]
-
-      raise GraphQL::ExecutionError, 'Must be logged in to perform this action'
-    end
-
-    # Get a entry logs for a user
-    field :pending_users, [UserType], null: true do
-      description 'Get all pending members'
-    end
-
-    def pending_users
-      User.where(state: 'pending',
-                 community_id: context[:current_user].community_id)
-    end
-
     # Get a list of guards
     field :security_guards, [UserType], null: true do
       description 'Get a list of security guards for a community'
