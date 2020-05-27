30d3215bccabfa8c4651e98eb6c6992e43418ccf
Yoram <yoram@doublegdp.com>
2020-05-27 12:37:13 +0000

    refactored GA  for tabs on accoutn page

--- app/javascript/src/components/UserInformation.jsx
+++ app/javascript/src/components/UserInformation.jsx
@@ -42,7 +42,7 @@ export default function UserInformation({
   router
 }) {
   const CSMNumber = '260974624243'
-  const [tabValue, setValue] = useState(0)
+  const [tabValue, setValue] = useState('Contacts')
   const [anchorEl, setAnchorEl] = useState(null)
   const [isLoading, setLoading] = useState(false)
   const [noteCreate, { loading: mutationLoading }] = useMutation(CreateNote)
@@ -62,28 +62,16 @@ export default function UserInformation({
 
   const handleChange = (_event, newValue) => {
     setValue(newValue)
-    var page; 
-    switch (newValue) {
-      case 0:
-         page = "Contacts/"
-        break;
-      case 1:
-        page = "Notes/"
-        break;
-      case 2:
-        page = "Plots/"
-        break;
-      case 3:
-        page ="payments/"
-        break;
-      default:
-        page = "0"
-        break;
+    const pages = {
+      Contacts: 'Contacts',
+      Notes: 'Notes',
+      Plots: 'Plots',
+      Payments: 'Payments'
     }
       ReactGA.event({
       category: 'My_Account',
-      action: page,
-      eventLabel: `Tab number${newValue}`,
+      action: pages[newValue],
+      eventLabel: `Tab ${newValue}`,
       nonInteraction: true
     })
     
@@ -293,15 +281,15 @@ export default function UserInformation({
           aria-label="request tabs"
           centered
         >
-          <StyledTab label="Contact" value={0} />
+          <StyledTab label="Contact" value={'Contacts'} />
           {['admin'].includes(userType) && (
-            <StyledTab label="Notes" value={1} />
+            <StyledTab label="Notes" value={'Notes'} />
           )}
-          <StyledTab label="Plots" value={2} />
-          <StyledTab label="Payments" value={3} />
+          <StyledTab label="Plots" value={'Plots'} />
+          <StyledTab label="Payments" value={'Payments'} />
         </StyledTabs>
 
-        <TabPanel value={tabValue} index={0}>
+        <TabPanel value={tabValue}  index={'Contacts'}>
           <div className="container">
             <div className="form-group">
               <label className="bmd-label-static" htmlFor="name">
@@ -361,7 +349,7 @@ export default function UserInformation({
           </div>
         </TabPanel>
         {['admin'].includes(userType) && (
-          <TabPanel value={tabValue} index={1}>
+          <TabPanel value={tabValue}  index={'Notes'}>
             <div className="container">
               <form id="note-form">
                 <div className="form-group">
@@ -439,10 +427,10 @@ export default function UserInformation({
             </div>
           </TabPanel>
         )}
-        <TabPanel value={tabValue} index={2}>
-          <UserPlotInfo accounts={data.user.accounts} />
+        <TabPanel value={tabValue}  index={'Plots'}>
+          <UserPlotInfo accounts={data.user.accounts}  />
         </TabPanel>
-        <TabPanel value={tabValue} index={3}>
+        <TabPanel value={tabValue}  index={'Payments'}>
           <h4 className="text-center">Coming soon</h4>
         </TabPanel>
 
