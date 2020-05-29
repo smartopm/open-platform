11756877df456d93229c16268b029dac30ee487a
OlivierJM <manolivier93@gmail.com>
2020-05-29 12:32:58 +0200

    add calculatable date range

--- app/javascript/src/containers/TimeSheet/EmployeeLogs.jsx
+++ app/javascript/src/containers/TimeSheet/EmployeeLogs.jsx
@@ -8,8 +8,8 @@ import { useParams } from 'react-router'
 import ErrorPage from '../../components/Error'
 import Paginate from '../../components/Paginate'
 import Grid from '@material-ui/core/Grid'
-import dateutil, { lastDayOfTheMonth } from '../../utils/dateutil'
-import { newCatDate, zonedDate } from '../../components/DateContainer'
+import dateutil from '../../utils/dateutil'
+import { zonedDate } from '../../components/DateContainer'
 
 export default function EmployeeLogs() {
   const { id } = useParams()
@@ -17,6 +17,7 @@ export default function EmployeeLogs() {
   const date = new Date()
   const firstDay = new Date(date.getFullYear(), date.getMonth() + 1 + monthCount, 27)
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 2 + monthCount, 26)
+
   const { loading, data, error } = useQuery(UserTimeSheetQuery, {
     variables: {
       userId: id,
@@ -25,6 +26,7 @@ export default function EmployeeLogs() {
     },
     fetchPolicy: 'no-cache'
   })
+
   function paginate(action) {
     if (action === 'prev') {
       setMonthCount(monthCount - 1)
@@ -46,8 +48,8 @@ export default function EmployeeLogs() {
           Boolean(data.userTimeSheetLogs.length) &&
           data.userTimeSheetLogs[0].user.name
         }
-        lastDay={dateutil.dateToString(zonedDate(new Date()))}
-        firstDay={dateutil.dateToString(zonedDate(lastDayOfTheMonth))}
+        lastDay={dateutil.dateToString(zonedDate(lastDay))}
+        firstDay={dateutil.dateToString(zonedDate(firstDay))}
       />
 
       <Grid container direction="row" justify="center" alignItems="center">
