4a4138860fb378353031df7e1f3b8f8116ab572b
OlivierJM <manolivier93@gmail.com>
2020-06-04 14:05:49 +0200

    fix rubocop errors

--- lib/email_msg.rb
+++ lib/email_msg.rb
@@ -25,50 +25,45 @@ class EmailMsg
     mail.template_id = 'd-bec0f1bd39f240d98a146faa4d7c5235'
     client.mail._('send').post(request_body: mail.to_json)
   end
-  # rubocop:enable Metrics/AbcSize
 
-  def self.get_messages_from_sendgrid
+  def self.messages_from_sendgrid
     sg_client = SendGrid::API.new(api_key: Rails.application.credentials[:sendgrid_api_key]).client
     # Find a way to make limit and offset dynamic
     params = JSON.parse('{"limit": 100 }')
     response = sg_client.messages.get(query_params: params)
-    puts "==============using the library=============="
-    response.body
+    emails = JSON.parse(response.body)
+    emails['messages']
   end
 
   # other stuff ==> message body
-   # is_open ==> is_read
-   # msg_id ==> source_system_id
-   # type ==> email || sms
-   # sender_id ==> findByEmail from Mutale.
-   # loop through all messages coming from the API and find respective users based on their emails
+  # is_open ==> is_read
+  # msg_id ==> source_system_id
+  # type ==> email || sms
+  # sender_id ==> findByEmail from Mutale.
+  # loop through all messages coming from the API and find respective users based on their emails
 
-   def self.find_by_email(email, community_id)
-     user = User.find_by(email: email, community_id: community_id)
-     user
-   end
+  def self.find_user(email, community_id)
+    user = User.find_by(email: email, community_id: community_id)
+    user
+  end
 
-   def self.save_sendgrid_messages(community_id)
-     emails  = get_messages_from_sendgrid
-     mess = JSON.parse(emails)
-     messages = mess['messages']
-     # replace this with Mutale's email
-     # add more validation to make sure users exist before saving that user.
-     sender = find_by_email('olivier@doublegdp.com', community_id) # Admin's email, static for now
-
-     puts sender
-     messages.each do |message|
-        user = find_by_email(message['to_email'], community_id)
-        message = Message.new(
-          is_read: message['opens_count'] > 0 ? true : false,
-          sender_id: sender.id,
-          user_id: user.nil? ? SecureRandom.uuid : user.id, # temporarily save this if they don't exist
-          message: message['subject'],
-          category: 'email',
-          status: message['status'],
-          source_system_id: message['msg_id']
-        )
-        message.save
-      end
-   end
+  def self.save_sendgrid_messages(community_id)
+    emails = messages_from_sendgrid
+    # replace this with Mutale's email
+    # add more validation to make sure users exist before saving that user.
+    sender = find_user('olivier@doublegdp.com', community_id) # Admin's email, static for now
+    emails.each do |email|
+      user = find_user(email['to_email'], community_id)
+      message = Message.new(
+        is_read: (email['opens_count']).positive?, sender_id: sender.id,
+        read_at: email['last_event_time']
+        # temporarily save this if they don't exist
+        user_id: user.nil? ? SecureRandom.uuid : user.id,
+        message: email['subject'], category: 'email', status: email['status'],
+        source_system_id: email['msg_id']
+      )
+      message.save # add ! later to validate user existance before saving the message
+    end
+  end
 end
+# rubocop:enable Metrics/AbcSize
