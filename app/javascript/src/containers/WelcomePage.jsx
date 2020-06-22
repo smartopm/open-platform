1fe461688762bb62c250255715d468b5c403ed58
OlivierJM <manolivier93@gmail.com>
2020-06-22 12:41:45 +0200

    redesigned the open page

--- app/javascript/src/containers/WelcomePage.jsx
+++ app/javascript/src/containers/WelcomePage.jsx
@@ -1,58 +1,54 @@
 import React from 'react'
-import { Grid, Button, Typography, Box, Container } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import { StyleSheet, css } from 'aphrodite'
 
 export default function WelcomePage() {
     return (
-        <Grid container spacing={5} justify="flex-start">
-            <Grid item xs>
-                <Grid container direction="row">
-                    <Grid item xs={4}>
-                        <CustomButton title="Schedule a call" />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <CustomButton title="Schedule a call" />
-                    </Grid>
-                    <Grid item xs={4}>
-                        <CustomButton title="Schedule a call" />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={12} className={css(styles.fullHeightSection)}>
-                <img src="https://nkwashi.com/wp-content/uploads/2019/10/20190821-NkanshiZikoPhotography975A9968104.jpg"
-                    style={{
-                        width: '80%',
-                        margin: '0 10% 0 10%'
-                    }}
-                />
-            </Grid>
-        </Grid>
+        <>
+            
+            <div className="container_img">
+                <img className="img-fluid home_hero" src="https://nkwashi.com/wp-content/uploads/2017/02/home-hero.jpg" alt="Snow"/>
+                <br />
+                <br/>
+                <CustomButton title="Schedule a call" />
+                <CustomButton title="Book a tour" />
+                <CustomButton title="Become a client" />
+                <br />
+                <br/>
+                <p className={css(styles.mainText)}>
+                    Our house plans are designed by among the best architectural firms on the African 
+                    continent and will be made available to plot owners.
+                </p>
+                <div className="centered">
+                    <h2 >Its not just a house, its a way of life</h2>
+                </div>
+                <p className={css(styles.mainText)}>We are located in Woodlands, 11 Nalikwanda Road, Lusaka, Zambia</p>
+                
+            </div>
+        </>
     )
 }
 
-export function CustomButton({title}) {
+export function CustomButton({ title }) {
     return (
-            <Button className={css(styles.customButton)}>
-                {title}
-            </Button>
+        <Button className={css(styles.customButton)}>
+            {title}
+        </Button>
     )
 }
 
 const styles = StyleSheet.create({
     fullHeightSection: {
         height: '100vh',
-        // backgroundColor: '#66a59a',
         color: '#FFFFFF'
     },
     customButton: {
-        backgroundColor: '#66a59a',
-        color: '#FFFFFF',
         fontSize: 13,
         marginLeft: 10,
         marginRight: 10,
-        marginTop: '5vh'
     },
     mainText: {
-        marginTop: '-20vh'
+        color: '#343a40',
+        margin: 30
     }
 })
