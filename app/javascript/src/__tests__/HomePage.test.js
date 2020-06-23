29266fa1a167e9085a544073423f641050c62dab
OlivierJM <manolivier93@gmail.com>
2020-06-23 15:53:17 +0200

    add a few more tests for the home page component

--- app/javascript/src/__tests__/HomePage.test.js
+++ app/javascript/src/__tests__/HomePage.test.js
@@ -5,8 +5,8 @@ import '@testing-library/jest-dom/extend-expect'
 import { BrowserRouter } from 'react-router-dom/'
 import { MockedProvider } from '@apollo/react-testing'
 
-describe("Home",()=>{
-    const authState = {
+describe("HomePage component ",()=>{
+    const adminAuthState = {
         loaded: true,
         loggedIn: true,
         setToken: jest.fn(),
@@ -18,18 +18,127 @@ describe("Home",()=>{
           id: '11cdad78',
           imageUrl: null,
           name: 'John Doctor',
-          phoneNumber: '260971500748',
-          userType: 'security_guard'
+          phoneNumber: '260971500000',
+          userType: 'admin'
         }
       }
-    it('should render without error',()=>{
-
-        render(
+    it('should render without error and have all cards for admins',()=>{
+       const container =  render(
             <MockedProvider >
               <BrowserRouter>
-                <Homepage authState={authState}/>
+                <Homepage authState={adminAuthState}/>
             </BrowserRouter>
             </MockedProvider>
-        )
+       )
+      expect(container.queryByText('My Messages')).toBeInTheDocument()
+      expect(container.queryByText('Users')).toBeInTheDocument()
+      expect(container.queryByText('Campaigns')).toBeInTheDocument()
+      expect(container.queryByText('Notes')).toBeInTheDocument()
+      expect(container.queryByText('Time Card')).toBeInTheDocument()
+      expect(container.queryByText('Tasks')).toBeInTheDocument()
     })
+  
+  it('should render without error and have all cards for custodian', () => {
+    const custodianAuthState = {
+      loaded: true,
+      loggedIn: true,
+      setToken: jest.fn(),
+      user: {
+        avatarUrl: null,
+        community: { name: 'Nkwashi' },
+        email: '9753942',
+        expiresAt: null,
+        id: '11cdad78',
+        imageUrl: null,
+        name: 'John Doctor',
+        phoneNumber: '260971500000',
+        userType: 'custodian'
+      }
+    }
+    const container = render(
+      <MockedProvider >
+        <BrowserRouter>
+          <Homepage authState={custodianAuthState} />
+        </BrowserRouter>
+      </MockedProvider>
+    )
+    expect(container.queryByText('My Messages')).not.toBeInTheDocument()
+    expect(container.queryByText('My ID Card')).toBeInTheDocument()
+    expect(container.queryByText('Campaigns')).not.toBeInTheDocument()
+    expect(container.queryByText('Notes')).not.toBeInTheDocument()
+    expect(container.queryByText('Time Card')).toBeInTheDocument()
+    expect(container.queryByText('Tasks')).not.toBeInTheDocument()
+    expect(container.queryByText('Log Book')).not.toBeInTheDocument()
+    expect(container.queryByText('Users')).not.toBeInTheDocument()
+  })
+  it('should render without error and have all cards for client', () => {
+    const clientAuthState = {
+      loaded: true,
+      loggedIn: true,
+      setToken: jest.fn(),
+      user: {
+        avatarUrl: null,
+        community: { name: 'Nkwashi' },
+        email: '9753942',
+        expiresAt: null,
+        id: '11cdad78',
+        imageUrl: null,
+        name: 'John Doctor',
+        phoneNumber: '260971500000',
+        userType: 'client'
+      }
+    }
+    const container = render(
+      <MockedProvider >
+        <BrowserRouter>
+          <Homepage authState={clientAuthState} />
+        </BrowserRouter>
+      </MockedProvider>
+    )
+    expect(container.queryByText('My Messages')).toBeInTheDocument()
+    expect(container.queryByText('My ID Card')).toBeInTheDocument()
+    expect(container.queryByText('Campaigns')).not.toBeInTheDocument()
+    expect(container.queryByText('Notes')).not.toBeInTheDocument()
+    expect(container.queryByText('Time Card')).not.toBeInTheDocument()
+    expect(container.queryByText('Tasks')).not.toBeInTheDocument()
+    expect(container.queryByText('Log Book')).not.toBeInTheDocument()
+    expect(container.queryByText('Users')).not.toBeInTheDocument()
+    expect(container.queryByText('Referrals')).toBeInTheDocument()
+  })
+  
+  it('should render without error and have all cards for prospective client', () => {
+    const prospectAuthState = {
+      loaded: true,
+      loggedIn: true,
+      setToken: jest.fn(),
+      user: {
+        avatarUrl: null,
+        community: { name: 'Nkwashi' },
+        email: '9753942',
+        expiresAt: null,
+        id: '11cdad78',
+        imageUrl: null,
+        name: 'John Doctor',
+        phoneNumber: '260971500000',
+        userType: 'prospective_client'
+      }
+    }
+    const container = render(
+      <MockedProvider >
+        <BrowserRouter>
+          <Homepage authState={prospectAuthState} />
+        </BrowserRouter>
+      </MockedProvider>
+    )
+    expect(container.queryByText('My Messages')).toBeInTheDocument()
+    expect(container.queryByText('My ID Card')).toBeInTheDocument()
+    expect(container.queryByText('Campaigns')).not.toBeInTheDocument()
+    expect(container.queryByText('Notes')).not.toBeInTheDocument()
+    expect(container.queryByText('Time Card')).not.toBeInTheDocument()
+    expect(container.queryByText('Tasks')).not.toBeInTheDocument()
+    expect(container.queryByText('Log Book')).not.toBeInTheDocument()
+    expect(container.queryByText('Users')).not.toBeInTheDocument()
+    expect(container.queryByText('Referrals')).not.toBeInTheDocument()
+    expect(container.queryByText('Client Request Form')).not.toBeInTheDocument()
+  })
 })
