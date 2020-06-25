cd7067c26330767c5516e04f648abd58a3d84bc1
OlivierJM <manolivier93@gmail.com>
2020-06-25 17:21:23 +0200

    rolled back migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_104005) do
+ActiveRecord::Schema.define(version: 2020_06_07_102652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -73,15 +73,6 @@ ActiveRecord::Schema.define(version: 2020_06_25_104005) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["community_id"], name: "index_campaigns_on_community_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.uuid "post_id"
-    t.uuid "community_id"
-    t.uuid "user_id"
-    t.text "comment"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "communities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
