1e517cc22fbf3de3c38fc062ce04e833eedbce64
dennis60 <dennis@doublegdp.com>
2020-06-25 11:58:38 +0000

    Added share buttons for different platforms

--- app/javascript/src/components/ShareButton.jsx
+++ app/javascript/src/components/ShareButton.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import FacebookIcon from '@material-ui/icons/Facebook'
-import Fab from "@material-ui/core/Fab";
+import Fab from "@material-ui/core/Fab"
+import Box from "@material-ui/core/Box"
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import Popper from '@material-ui/core/Popper'
+import ShareIcon from '@material-ui/icons/Share';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import TwitterIcon from '@material-ui/icons/Twitter'
 import ReactGA from 'react-ga'
 
 export function ShareButton({ url }) {
+    const [openPopper, setOpenPopper] = useState(false)
+     const [anchorEl, setAnchorEl] = useState(null);
     function shareFacebook() {
         ReactGA.event({
         category: 'ShareToFB',
@@ -15,7 +24,38 @@ export function ShareButton({ url }) {
             'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
 
     }
+    function handleClick(event){
+        setOpenPopper(!openPopper)
+        setAnchorEl(anchorEl ? null : event.currentTarget);
+    }
     return (
+        <>
+
+        <Popper 
+        placement="top"
+        open={openPopper}
+        anchorEl={anchorEl}
+        >
+
+        <Box style={{display: 'flex', flexDirection: 'column', margin: 10 }}>
+            <Fab>
+                <TwitterIcon />
+            </Fab>
+            <Fab>
+                <LinkedInIcon />
+            </Fab>
+            <Fab onClick={shareFacebook}>
+                <FacebookIcon />    
+            </Fab>
+             <Fab>
+                <MailOutlineIcon />    
+            </Fab>
+             <Fab>
+                <WhatsAppIcon />    
+            </Fab>
+        </Box>
+        </Popper>
+        
         <Fab variant="extended"
             style={{
                 position: 'fixed',
@@ -23,11 +63,12 @@ export function ShareButton({ url }) {
                 right: 57
             }}
             color="primary"
-            onClick={shareFacebook}
+            onClick={handleClick}
         >
-            <FacebookIcon />
+        <ShareIcon />
             {"  "} Share
         </Fab>
+        </>
     )
 }
 
