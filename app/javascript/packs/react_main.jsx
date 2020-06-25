03cdc02e20fb653bff58227bfb7944a25732603f
OlivierJM <manolivier93@gmail.com>
2020-06-25 13:24:52 +0200

    re addd news pages to logged in only

--- app/javascript/packs/react_main.jsx
+++ app/javascript/packs/react_main.jsx
@@ -187,11 +187,9 @@ const App = () => {
                   {/* <Route path="/welcome_page" component={WelcomePage} /> */}
 
                   {/* Spike page */}
-                  <Route path="/nkwashi_news" exact component={NewsPage} />
-                  <Route path="/nkwashi_news/:slug" exact component={Posts} />
                   <Route path="/nkwashi_news/post/:id" exact component={PostPage} />
 
-                  
+
                   <LoggedInOnly>
                     <Switch>
                       <Route path="/" exact component={Home} />
@@ -265,6 +263,8 @@ const App = () => {
 
                       <Route path="/client_request_from" exact component={ClientRequestForm} />
 
+                      <Route path="/nkwashi_news" exact component={NewsPage} />
+                      <Route path="/nkwashi_news/:slug" exact component={Posts} />
 
 
                       <AdminRoutes>
