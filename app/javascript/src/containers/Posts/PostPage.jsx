35131ddd57614f777323e63f0495f189234cf3c5
OlivierJM <manolivier93@gmail.com>
2020-06-29 12:33:21 +0200

    show all pages in an iframe

--- app/javascript/src/containers/Posts/PostPage.jsx
+++ app/javascript/src/containers/Posts/PostPage.jsx
@@ -1,17 +1,18 @@
 import React, { Fragment, useContext } from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { wordpressEndpoint } from '../../utils/constants'
-import { useFetch } from '../../utils/customHooks'
-import PostContent from '../../components/NewsPage/PostContent'
+import { useFetch, useWindowDimensions } from '../../utils/customHooks'
 import {ShareButton} from '../../components/ShareButton'
 import Nav from '../../components/Nav'
 import { Context as AuthStateContext } from "../../containers/Provider/AuthStateProvider";
 import { Spinner } from '../../components/Loading'
+import IframeContainer from '../../components/IframeContainer'
 
 export default function PostPage() {
     const { id } = useParams()
     const authState = useContext(AuthStateContext);
     const currentUrl = window.location.href
+    const {width, height} = useWindowDimensions()
     const { response, error } = useFetch(`${wordpressEndpoint}/posts/${id}`)
     if (error) {
         return error.message
@@ -22,10 +23,11 @@ export default function PostPage() {
     if (response.categories?.Private && !authState.loggedIn) {
         return <Redirect to="/welcome" />
     }
+    console.log(response)
     return (
         <Fragment>
             <Nav menuButton="back" backTo={authState.loggedIn ? '/nkwashi_news/posts' : '/welcome'} />
-            <PostContent response={response} />
+            <IframeContainer link={response?.URL || ""} width={width} height={height} />
             <ShareButton url={currentUrl} />
         </Fragment>
     )
