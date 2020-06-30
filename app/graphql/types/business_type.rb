51a994358427c166c92a7adbd05b4b271d7ff5c6
OlivierJM <manolivier93@gmail.com>
2020-06-30 12:42:32 +0200

    change required status

--- app/graphql/types/business_type.rb
+++ app/graphql/types/business_type.rb
@@ -6,9 +6,9 @@ module Types
     field :id, ID, null: false
     field :user_id, ID, null: false
     field :name, String, null: false
-    field :email, String, null: false
+    field :email, String, null: true
     field :phone_number, String, null: false
-    field :address, String, null: false
+    field :address, String, null: true
     field :status, String, null: true
     field :operation_hours, String, null: true
     field :home_url, String, null: true
