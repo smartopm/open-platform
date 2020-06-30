5973e7de2c08f5cbf038800a1c8337769adf18e7
OlivierJM <manolivier93@gmail.com>
2020-06-30 12:32:18 +0200

    add more fields in the business table

--- db/migrate/20200630101726_create_businesses.rb
+++ db/migrate/20200630101726_create_businesses.rb
@@ -4,11 +4,15 @@ class CreateBusinesses < ActiveRecord::Migration[6.0]
       t.references :community, null: false, type: :uuid, foreign_key: true
       t.references :user, null: false, type: :uuid, foreign_key: true
       t.string :name
-      t.boolean :verified
+      t.string :status
       t.string :home_url
       t.string :category
       t.text :description
       t.string :image_url
+      t.string :email
+      t.string :phone_number
+      t.string :address
+      t.string :operation_hours
 
       t.timestamps
     end
