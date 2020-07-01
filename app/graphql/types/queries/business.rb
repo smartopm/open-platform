1a1ac7c43cece88a35709f55344ebc03d9e81fda
OlivierJM <manolivier93@gmail.com>
2020-07-01 11:34:13 +0200

    clean up

--- app/graphql/types/queries/business.rb
+++ app/graphql/types/queries/business.rb
@@ -11,11 +11,17 @@ module Types::Queries::Business
       argument :offset, Integer, required: false
       argument :limit, Integer, required: false
     end
-    
-    # Get business entries
+
+    # Get business
     field :business, Types::BusinessType, null: true do
       description 'Get a business by id'
       argument :id, GraphQL::Types::ID, required: true
+    end
+
+    # Get business for the user, using the user id
+    field :user_business, [Types::BusinessType], null: true do
+      description 'Get business by its owner id'
+      argument :user_id, GraphQL::Types::ID, required: true
     end
   end
 
@@ -30,7 +36,18 @@ module Types::Queries::Business
   end
 
   def business(id:)
-    business = context[:current_user].businesses.find_by(id: id)
+    raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user].blank?
+
+    business = Business.find_by(community_id: context[:current_user].community_id, id: id)
     business
+  end
+
+  def user_business(user_id:)
+    raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user].blank?
+
+    u_business = User.find_by(
+      community_id: context[:current_user].community_id, id: user_id,
+    ).businesses.all
+    u_business
   end
 end
