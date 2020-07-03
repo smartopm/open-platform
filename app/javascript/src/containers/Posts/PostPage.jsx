5e7cfad9d82f5acc5828610a3d4fe5da09b8d511
Yoram <yoram@doublegdp.com>
2020-07-03 20:57:08 +0000

    comments experiments

--- app/javascript/src/containers/Posts/PostPage.jsx
+++ app/javascript/src/containers/Posts/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react'
+import React, { Fragment, useContext, useState } from 'react'
 import { useParams, Redirect } from 'react-router-dom'
 import { useQuery, useMutation } from 'react-apollo'
 import { Button } from '@material-ui/core'
@@ -6,64 +6,143 @@ import { wordpressEndpoint } from '../../utils/constants'
 import { useFetch, useWindowDimensions } from '../../utils/customHooks'
 import { ShareButton } from '../../components/ShareButton'
 import Nav from '../../components/Nav'
-import { Context as AuthStateContext } from "../../containers/Provider/AuthStateProvider";
+import { Context as AuthStateContext } from '../../containers/Provider/AuthStateProvider'
 import { Spinner } from '../../components/Loading'
 import IframeContainer from '../../components/IframeContainer'
 import { PostDiscussionQuery, PostCommentsQuery } from '../../graphql/queries'
 import Comments from '../../components/Discussion/Comment'
 import { DiscussionMutation } from '../../graphql/mutations'
+import { StyleSheet, css } from 'aphrodite'
+import {
+  Dialog,
+  ListItemText,
+  ListItem,
+  List,
+  Divider,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+//   CloseIcon,
+  Slide
+} from '@material-ui/core'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 export default function PostPage() {
-    const { id } = useParams()
-    const authState = useContext(AuthStateContext);
-    const currentUrl = window.location.href
-    const { width, height } = useWindowDimensions()
-    const { response } = useFetch(`${wordpressEndpoint}/posts/${id}`)
+  const { id } = useParams()
+  const authState = useContext(AuthStateContext)
+  const currentUrl = window.location.href
+  const { width, height } = useWindowDimensions()
+  const { response } = useFetch(`${wordpressEndpoint}/posts/${id}`)
 
-    const queryResponse = useQuery(PostDiscussionQuery, {
-        variables: { postId: id }
+  const queryResponse = useQuery(PostDiscussionQuery, {
+    variables: { postId: id }
+  })
+  const { loading, data, refetch } = useQuery(PostCommentsQuery, {
+    variables: { postId: id }
+  })
+  const [discuss] = useMutation(DiscussionMutation)
+
+  function createDiscussion(title, id) {
+    discuss({
+      variables: { postId: id.toString(), title }
     })
-    const { loading, data, refetch } = useQuery(PostCommentsQuery, {
-        variables: { postId: id }
-    })
-    const [discuss] = useMutation(DiscussionMutation)
+      .then(() => queryResponse.refetch())
+      .catch(err => console.log(err.message))
+  }
+  const [open, setOpen] = React.useState(false)
 
-    function createDiscussion(title, id) {
-        discuss({
-            variables: { postId: id.toString(), title }
-        })
-        .then(() => queryResponse.refetch())
-        .catch(err => console.log(err.message))
-    }
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
 
-    if (!response || queryResponse.loading || loading) {
-        return <Spinner />
-    }
-    // instead of redirecting, ask them to log in
-    if (response.categories?.Private && !authState.loggedIn) {
-        return <Redirect to="/welcome" />
-    }
-    return (
-        <Fragment>
-            <Nav menuButton="back" backTo={authState.loggedIn ? '/nkwashi_news' : '/welcome'} />
-            <div className="post_page">
-                <IframeContainer link={response?.URL || ""} width={width} height={height} />
-                <ShareButton url={currentUrl} />
-            </div>
-            {
-                queryResponse.data.postDiscussion ? (
-                    <Comments
-                        comments={data.postComments}
-                        refetch={refetch}
-                        discussionId={queryResponse.data.postDiscussion.id}
-                    />
-                )
-                    : (
-                        <Button variant="outlined" onClick={() => createDiscussion(response?.title, response?.ID)}>
-                            Create Discussion
-                        </Button>
-                    )
-            }
-        </Fragment>
-    )
+  const handleClose = () => {
+    setOpen(false)
+  }
+  if (!response || queryResponse.loading || loading) {
+    return <Spinner />
+  }
+  // instead of redirecting, ask them to log in
+  if (response.categories?.Private && !authState.loggedIn) {
+    return <Redirect to="/welcome" />
+  }
+  return (
+    <Fragment>
+      <Nav
+        menuButton="back"
+        backTo={authState.loggedIn ? '/nkwashi_news' : '/welcome'}
+      />
+      <div className="post_page">
+        <IframeContainer
+          link={response?.URL || ''}
+          width={width}
+          height={height}
+        />
+        <ShareButton url={currentUrl} />
+      </div>
+
+      <div className={css(styles.commetns)}>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        View Comments
+      </Button>
+
+      </div>
+      <div> 
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+        <AppBar >
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              {/* <CloseIcon /> */}
+            </IconButton>
+            <Typography variant="h6">
+              Comments
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              close
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <br/>
+        <br/>
+        <br/>
+        {queryResponse.data.postDiscussion ? (
+          <Comments
+            comments={data.postComments}
+            refetch={refetch}
+            discussionId={queryResponse.data.postDiscussion.id}
+          />
+        ) : (
+          <Button
+            variant="outlined"
+            onClick={() => createDiscussion(response?.title, response?.ID)}
+          >
+            Create Discussion
+          </Button>
+        )}
+        {/* <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
+          </ListItem>
+        </List> */}
+      </Dialog>
+      </div>
+    </Fragment>
+  )
 }
+
+const styles = StyleSheet.create({
+  commetns: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    right: 0
+    // paddingBottom: "60px"
+  }
+})
