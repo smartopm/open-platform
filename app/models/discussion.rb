ee9c3cef3be1db0012ca2302816dd5e3958b852e
OlivierJM <manolivier93@gmail.com>
2020-07-05 10:59:23 +0200

    add many through relationship to discussion

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,6 +5,6 @@ class Discussion < ApplicationRecord
   belongs_to :user
   belongs_to :community
   has_many :comments, dependent: :destroy
-
+  has_many :users, through: :discussion_users
   default_scope { order(created_at: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,8 @@ class User < ApplicationRecord
   has_many :time_sheets, dependent: :destroy
   has_many :accounts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :discussions, dependent: :destroy
+  has_many :discussions_users, dependent: :destroy
+  has_many :discussions, through: :discussions_users
   has_many :businesses, dependent: :destroy
 
   has_one_attached :avatar
