d5c1e8def3266b6958aea076e4168ad386b4ff6e
Nicolas Garcia <nicolas@doublegdp.com>
2020-07-07 15:06:05 +0000

    fixed missing method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,24 @@
 # Primary ApplicationController
 class ApplicationController < ActionController::Base
   helper_method :current_member
+  before_action :current_community
   before_action :set_paper_trail_whodunnit
 
   def new_session_path(_scope)
     user_google_oauth2_omniauth_authorize_path
+  end
+
+  def current_community
+    community_list = { 'app.doublegdp.com' => 'Nkwashi',
+                       'double-gdp-staging.herokuapp.com' => 'Nkwashi' }
+
+    if request.domain == 'dgdp.site'
+      @site_community = Community.find_by(name: 'Nkwashi')
+    else
+      dom = "#{request.subdomain}.#{request.domain}"
+      @site_community = Community.find_by(name: community_list[dom])
+    end
+    @site_community
   end
 
   # For now we can assume that each user is just a member of one community
