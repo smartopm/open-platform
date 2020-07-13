cb546bab0ee9196c899262a58eb90b4b3e8dd20d
Yoram <yoram@doublegdp.com>
2020-07-13 18:34:10 +0000

    implemented map on user page

---
+++ app/javascript/src/components/GeoMap.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import { Map, GeoJSON, Marker, TileLayer, Popup } from 'react-leaflet'
+
+const center = [-15.524234821346493, 28.65281581878662]
+
+function geoJSONStyle(feature) {
+  return {
+    color: feature.properties.stroke,
+    weight: 1,
+    fillOpacity: 0.5,
+    fillColor: feature.properties.fill
+  }
+}
+
+function onEachFeature(feature, layer) {
+  if (feature.properties.name) {
+    layer.bindTooltip(feature.properties.name, {
+      permanent: true,
+      direction: 'center',
+      offset: [0, 0],
+      className: 'text-label'
+    })
+  }
+}
+
+export default function GeoMap({ GeoJSONData }) {
+  const [activePlot, setActivePlot] = useState(null)
+  return (
+    <div>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+        .leaflet-tooltip-top:before, 
+        .leaflet-tooltip-bottom:before, 
+        .leaflet-tooltip-left:before, 
+        .leaflet-tooltip-right:before {
+          border: none !important;
+        }
+        .text-label {
+          font-size: 1.75em;
+          background-color: none;
+          border-color: none;
+          background: none;
+          border: none;
+          box-shadow: none;
+        }
+        .leaflet-container {
+          height: 800px;
+          width: 100%;
+          margin: 0 auto;
+        }
+        `
+        }}
+      ></style>
+      <Map center={center} zoom={15} className={css(styles.mapContainer)}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON
+          data={GeoJSONData}
+          style={geoJSONStyle}
+          onEachFeature={onEachFeature}
+          onClick={() => {
+            setActivePlot(GeoJSONData)
+          }}
+        />
+        {activePlot && (
+          <Popup position={center}>
+            <div>
+              <h1> Plots Number 1234</h1>
+            </div>
+          </Popup>
+        )}
+
+        {/* <Polygon color="purple" positions={multiPolygon}/> */}
+      </Map>
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({
+  mapContainer: {}
+})
--- app/javascript/src/components/UserPlotInfo.jsx
+++ app/javascript/src/components/UserPlotInfo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { css, StyleSheet } from 'aphrodite'
 import { dateToString } from '../components/DateContainer'
-
+import { Grid } from '@material-ui/core'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import GeoData from '../data/nkwashi_plots.json'
+import GeoMap from '../components/GeoMap'
 // Todo: Refactor this to use best practices of React
 
 export function UserPlotInfo(props) {
@@ -14,37 +17,68 @@ export function UserPlotInfo(props) {
     })
 
   function plotInformation() {
-      return(
-    <div className="container">
-      <p data-testid="no_plot">No plots information available. </p>
-    </div>
-      )
-}
+    return (
+      <div className="container">
+        <p data-testid="no_plot">No plots information available. </p>
+      </div>
+    )
+  }
   if (accounts && accounts.length > 0 && land_parcels.length > 0) {
-
     const convertedDateTime = dateToString(accounts[0].updatedAt)
     return (
       <div className="container">
-        <p>Plots associated with this account:</p>
-        <ol data-testid="parcel_list">
-          {land_parcels.map((plot, index) => (
-            <li key={index}>{plot.parcelNumber}</li>
-          ))}
-        </ol>
-        <p>
-          This data was updated on {convertedDateTime}. If Something seems
-          incorrect, contact our
-          <span className={css(styles.supportLink)}>
-            &nbsp;
-            <Link
-              data-testid="support_link"
-              to="/contact"
-              className={css(styles.routeLink)}
-            >
-              Support Team.
-            </Link>
-          </span>
-        </p>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+        .leaflet-tooltip-top:before, 
+        .leaflet-tooltip-bottom:before, 
+        .leaflet-tooltip-left:before, 
+        .leaflet-tooltip-right:before {
+          border: none !important;
+        }
+        .text-label {
+          font-size: 1.75em;
+          background-color: none;
+          border-color: none;
+          background: none;
+          border: none;
+          box-shadow: none;
+        }
+        .leaflet-container {
+          height: 800px;
+          width: 100%;
+          margin: 0 auto;
+        }
+        `
+          }}
+        ></style>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <p>Plots associated with this account:</p>
+            <ol data-testid="parcel_list">
+              {land_parcels.map((plot, index) => (
+                <li key={index}>{plot.parcelNumber}</li>
+              ))}
+            </ol>
+            <p>
+              This data was updated on {convertedDateTime}. If Something seems
+              incorrect, contact our
+              <span className={css(styles.supportLink)}>
+                &nbsp;
+                <Link
+                  data-testid="support_link"
+                  to="/contact"
+                  className={css(styles.routeLink)}
+                >
+                  Support Team.
+                </Link>
+              </span>
+            </p>
+          </Grid>
+          <Grid item xs={12}>
+            <GeoMap GeoJSONData={GeoData} />
+          </Grid>
+        </Grid>
       </div>
     )
   }
