ca231f669eb380b6cd60a1d4cc33bdfc7df08367
OlivierJM <olivier@doublegdp.com>
2020-07-15 10:28:27 +0000

    add labels and user labels

---
+++ app/graphql/types/queries/label.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# label queries
+module Types::Queries::Label
+  extend ActiveSupport::Concern
+
+  included do
+    # Get label entries
+    field :labels, [Types::LabelType], null: true do
+      description 'Get all labels'
+    end
+
+    # Get label for the user, using the user id
+    field :user_label, [Types::LabelType], null: true do
+      description 'Get label by its owner id'
+      argument :user_id, GraphQL::Types::ID, required: true
+    end
+  end
+
+  def labels
+    raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user].blank?
+
+    context[:current_user].community.labels
+  end
+
+  def user_label(user_id:)
+    raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user].blank?
+
+    context[:current_user].find_a_user(user_id).labels
+  end
+end
