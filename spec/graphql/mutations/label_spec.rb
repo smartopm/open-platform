70576a1daed0c1648a4f9f3157a3bbdca903661b
OlivierJM <olivier@doublegdp.com>
2020-07-22 15:44:58 +0000

    add spec for duplicate label creation

--- spec/graphql/mutations/label_spec.rb
+++ spec/graphql/mutations/label_spec.rb
@@ -24,10 +24,20 @@ RSpec.describe Mutations::Label do
                                          current_user: admin,
                                          site_community: user.community,
                                        }).as_json
+      dub_result = DoubleGdpSchema.execute(query, context: {
+                                        current_user: admin,
+                                        site_community: user.community,
+                                      }).as_json
       expect(result.dig('data', 'labelCreate', 'label', 'id')).not_to be_nil
       expect(result.dig('data', 'labelCreate', 'label', 'shortDesc')).to eql 'green'
       expect(result.dig('errors')).to be_nil
+
+      expect(dub_result.dig('data', 'labelCreate', 'label', 'id')).to be_nil
+      expect(dub_result.dig('data', 'labelCreate', 'label', 'shortDesc')).to be_nil
+      expect(dub_result.dig('errors')).not_to be_nil
+      expect(dub_result.dig('errors', 0, 'message')).to include 'Duplicate label'
     end
+
     it 'returns error when user is not admin' do
       result = DoubleGdpSchema.execute(query,
                                        context: {
