173464ff35e6f484636fa3cc8e22208f3e20bbe7
OlivierJM <olivier@doublegdp.com>
2020-07-27 10:42:21 +0000

    add community_id to notes(migration)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_104558) do
+ActiveRecord::Schema.define(version: 2020_07_27_093241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -245,6 +245,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_104558) do
     t.datetime "due_date"
     t.string "category"
     t.uuid "assigned_to"
+    t.uuid "community_id"
   end
 
   create_table "showrooms", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
