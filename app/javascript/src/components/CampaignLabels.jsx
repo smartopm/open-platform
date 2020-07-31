d5b2883d2954d7c73c35b9ab9be5e83dc107de9d
dennis60 <dennis@doublegdp.com>
2020-07-31 13:48:37 +0000

    Created campaign label component

---
+++ app/javascript/src/components/CampaignLabels.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { TextField, Chip } from '@material-ui/core'
+
+export default function CampaignLabels() {
+    return (
+        <div>
+            
+            
+        </div>
+    )
+}
