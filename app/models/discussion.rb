1309b2ed81f04f5e6a4593eb63d12e0635d7f30b
Nicolas Garcia <nicolas@doublegdp.com>
2020-07-31 05:35:43 +0000

    @yoram, this should fix the issue you were having.   there s still a reactjs sync bug.   but you can fix that.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -9,13 +9,13 @@ class Discussion < ApplicationRecord
   has_many :users, through: :discussion_users
   default_scope { order(created_at: :desc) }
 
-  def follow_or_unfollow_discussion(user_id)
-    a_discussion = DiscussionUser.find_by(user_id: user_id)
+  def follow_or_unfollow_discussion(user)
+    a_discussion = DiscussionUser.find_by(user_id: user.id)
 
     if a_discussion.present?
       a_discussion.delete
     else
-      DiscussionUser.create!(user_id: user_id, discussion_id: self[:id])
+      DiscussionUser.create!(user_id: user.id, discussion_id: self[:id])
     end
   end
 end
