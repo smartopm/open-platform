789986c5d9f4e47065b7b601c50bbe8a38150a37
OlivierJM <olivier@doublegdp.com>
2020-08-04 18:23:46 +0000

    make user chips clickable

--- app/javascript/src/components/UserChip.jsx
+++ app/javascript/src/components/UserChip.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Chip, Avatar } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 import { forceLinkHttps } from '../utils/helpers'
 
 /**
@@ -8,12 +9,14 @@ import { forceLinkHttps } from '../utils/helpers'
  * @param {Object} props anything else that the Chip components uses from material-ui
  * @returns
  */
-export function UserChip({user, ...props}) {
+export function UserChip({ user, ...props }) {
+  const history = useHistory()
   return (
     <Chip
       style={{ margin: 5 }}
       variant="outlined"
       label={user.name}
+      onClick={() => history.push(`/user/${user.id}`)}
       avatar={<Avatar src={forceLinkHttps(user.imageUrl)} alt={user.name} />}
       {...props}
     />
