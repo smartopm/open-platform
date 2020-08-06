5199ca650ca9da4ec01dae6e20f813652fd1ce11
OlivierJM <olivier@doublegdp.com>
2020-08-06 11:20:49 +0000

    clean todo list

--- app/javascript/src/components/Notes/TodoList.jsx
+++ app/javascript/src/components/Notes/TodoList.jsx
@@ -1,7 +1,21 @@
 import React, { useState, Fragment } from 'react'
 import EditIcon from '@material-ui/icons/Edit'
 import { ModalDialog } from '../Dialog'
-import { createMuiTheme, Chip, Divider, Fab, Dialog, DialogTitle, DialogContent, FormControl, InputLabel, Select, Input, MenuItem, Button } from '@material-ui/core'
+import {
+  createMuiTheme,
+  Chip,
+  Divider,
+  Fab,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  FormControl,
+  InputLabel,
+  Select,
+  Input,
+  MenuItem,
+  Button
+} from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 import { formatDistance } from 'date-fns'
@@ -24,7 +38,6 @@ import ErrorPage from '../Error'
 import Paginate from '../Paginate'
 import CenteredContent from '../CenteredContent'
 import { dateToString } from '../DateContainer'
-
 
 // component needs a redesign both implementation and UI
 export default function TodoList({
@@ -59,7 +72,7 @@ export default function TodoList({
         offset,
         limit,
         query: assignee.map(query => `assignees = "${query}"`).join(' OR ')
-      },
+      }
     }
   )
   const [assignUserToNote] = useMutation(AssignUser)
@@ -105,63 +118,61 @@ export default function TodoList({
     }
   }
 
-
   function handleAssigneeInputChange(event) {
     setAssignee(event.target.value)
   }
-
 
   if (isLoading) return <Loading />
   if (tasksError) return <ErrorPage error={tasksError.message} />
 
   return (
     <Fragment>
-    <div className="container" data-testid="todo-container">
-      <ModalDialog
-        open={isDialogOpen}
-        handleClose={handleModal}
-        handleConfirm={saveDate}
-      >
-        <ThemeProvider theme={theme}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDatePicker
-              variant="inline"
-              format="MM/dd/yyyy"
-              margin="normal"
-              id="date-picker-inline"
-              label="Pick due date for this todo"
-              value={selectedDate}
-              onChange={handleDateChange}
-              KeyboardButtonProps={{
-                'aria-label': 'change date'
-              }}
-            />
-          </MuiPickersUtilsProvider>
-        </ThemeProvider>
-      </ModalDialog>
-      
-      <Dialog
-        // fullScreen={fullScreen}
-        open={open}
-        fullWidth={true}
-        maxWidth={'lg'}
-        onClose={openModal}
-        aria-labelledby="task_modal"
-          >
-        <DialogTitle id="task_modal">
+      <div className="container" data-testid="todo-container">
+        <ModalDialog
+          open={isDialogOpen}
+          handleClose={handleModal}
+          handleConfirm={saveDate}
+        >
+          <ThemeProvider theme={theme}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Pick due date for this todo"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date'
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </ThemeProvider>
+        </ModalDialog>
+
+        <Dialog
+          // fullScreen={fullScreen}
+          open={open}
+          fullWidth={true}
+          maxWidth={'lg'}
+          onClose={openModal}
+          aria-labelledby="task_modal"
+        >
+          <DialogTitle id="task_modal">
             <CenteredContent>
-                <span>Create a task</span>
+              <span>Create a task</span>
             </CenteredContent>
-        </DialogTitle>
-        <DialogContent>
+          </DialogTitle>
+          <DialogContent>
             <TaskForm
               refetch={refetch}
               close={() => setModalOpen(!open)}
               assignUser={assignUnassignUser}
               users={liteData?.users}
             />
-        </DialogContent>
-      </Dialog>
+          </DialogContent>
+        </Dialog>
 
         <div classes={classes.root}>
           <CenteredContent>
@@ -171,141 +182,154 @@ export default function TodoList({
               handleInputChange={handleAssigneeInputChange}
               classes={classes}
               resetFilter={() => setAssignee([])}
-            /> 
+            />
           </CenteredContent>
-          <br/>
-        <ul className={css(styles.list)}>
-          {isLoading ? (
-            <Loading />
-          ) : data.flaggedNotes.length ? (
-            data.flaggedNotes.map(note => (
-              <li key={note.id} className={`${css(styles.listItem)}`}>
-                <div className="custom-control custom-checkbox text">
-                  <input
-                    type="checkbox"
-                    checked={note.completed}
-                    onChange={() => handleCompleteNote(note.id, note.completed)}
-                    className="custom-control-input"
-                    id={`todo-check-${note.id}`}
-                  />
-                  <label
-                    className="custom-control-label"
-                    htmlFor={`todo-check-${note.id}`}
-                    style={{
-                      textDecoration: note.completed && 'line-through',
-                      fontSize: 17
-                    }}
-                  >
-                    {note.body} {'  '}
-                    <br />
-                    <br />
-                    <span>
-                      By <i>{note.author.name}</i>
-                    </span>
-                  </label>
-
-                  <label style={{ float: 'right', fontSize: 17 }}>
-                    <span>
-                      Due at:{note.dueDate ? `  ${dateToString(note.dueDate)}` : ' Never'}
-                    </span>
-                  </label>
-                  {'  '}
-                  <EditIcon
-                    style={{
-                      float: 'right',
-                      cursor: 'pointer'
-                    }}
-                    fontSize="small"
-                    color="inherit"
-                    onClick={() => handleModal(note.id)}
-                  />
-
-                  <br />
-                  <span style={{ marginRight: 10 }}>
-                    Created{' '}
-                    <i>
-                      {formatDistance(new Date(note.createdAt), new Date(), {
-                        addSuffix: true,
-                        includeSeconds: true
-                      })}
-                    </i>
-                  </span>
-                  <span style={{ float: 'right' }}>
-                    Associated with <i>{note.user.name}</i>
-                  </span>
-                </div>
-                <br />
-                {/* notes assignees */}
-                {note.assignees.map(user => (
-                  <UserChip
-                    key={user.id}
-                    user={user}
-                    size="medium"
-                    onDelete={() => handleDelete(user.id, note.id)}
-                  />
-                ))}
-               
-                {/* loader */}
-                {loaded && id === note.id ? (
-                  <Spinner />
-                ) : (
-                  <Chip
-                    key={note.id}
-                    variant="outlined"
-                    label={autoCompleteOpen && id === note.id ? 'Close' : 'Add Assignee' }
-                    size="medium"
-                    icon={ autoCompleteOpen && id === note.id ? <CancelIcon /> : <AddCircleIcon />}
-                    onClick={event => handleOpenAutoComplete(event, note.id)}
-                  />
-                )}
-              {/* error message */}
-              <br />
-              {Boolean(message.length) && <span>{message}</span>}
-              <br />
-              <br />
-
-                {/* autocomplete for assignees */}
-                {
-                // avoid opening autocomplete box for other notes
-                autoCompleteOpen && id === note.id && (
-                  <Autocomplete
-                    clearOnEscape
-                    clearOnBlur
-                    loading={loading}
-                    id={note.id}
-                    options={liteData.users}
-                    getOptionLabel={option => option.name}
-                    style={{ width: 300 }}
-                    onChange={(_evt, value) => {
-                      // if nothing selected, ignore and move on
-                      if (!value) {
-                        return
+          <br />
+          <ul className={css(styles.list)}>
+            {isLoading ? (
+              <Loading />
+            ) : data.flaggedNotes.length ? (
+              data.flaggedNotes.map(note => (
+                <li key={note.id} className={`${css(styles.listItem)}`}>
+                  <div className="custom-control custom-checkbox text">
+                    <input
+                      type="checkbox"
+                      checked={note.completed}
+                      onChange={() =>
+                        handleCompleteNote(note.id, note.completed)
                       }
-                      // assign or unassign the user here
-                      assignUnassignUser(note.id, value.id)
-                    }}
-                    renderInput={params => (
-                      <TextField {...params} placeholder="Name of assignee" />
-                    )}
-                  />
-                  )}
-                <Divider />
-              </li>
-            ))
-          ) : (
-            <span>No Actions yet</span>
-              )}
-          
-        </ul>
-      </div>
+                      className="custom-control-input"
+                      id={`todo-check-${note.id}`}
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor={`todo-check-${note.id}`}
+                      style={{
+                        textDecoration: note.completed && 'line-through',
+                        fontSize: 17
+                      }}
+                    >
+                      {note.body} {'  '}
+                      <br />
+                      <br />
+                      <span>
+                        By <i>{note.author.name}</i>
+                      </span>
+                    </label>
 
-      <CenteredContent>
-        <Paginate
-          offSet={offset}
-          limit={limit}
-          active={true}
-          handlePageChange={paginate}
-        />
+                    <label style={{ float: 'right', fontSize: 17 }}>
+                      <span>
+                        Due at:
+                        {note.dueDate
+                          ? `  ${dateToString(note.dueDate)}`
+                          : ' Never'}
+                      </span>
+                    </label>
+                    {'  '}
+                    <EditIcon
+                      style={{
+                        float: 'right',
+                        cursor: 'pointer'
+                      }}
+                      fontSize="small"
+                      color="inherit"
+                      onClick={() => handleModal(note.id)}
+                    />
+
+                    <br />
+                    <span style={{ marginRight: 10 }}>
+                      Created{' '}
+                      <i>
+                        {formatDistance(new Date(note.createdAt), new Date(), {
+                          addSuffix: true,
+                          includeSeconds: true
+                        })}
+                      </i>
+                    </span>
+                    <span style={{ float: 'right' }}>
+                      Associated with <i>{note.user.name}</i>
+                    </span>
+                  </div>
+                  <br />
+                  {/* notes assignees */}
+                  {note.assignees.map(user => (
+                    <UserChip
+                      key={user.id}
+                      user={user}
+                      size="medium"
+                      onDelete={() => handleDelete(user.id, note.id)}
+                    />
+                  ))}
+
+                  {/* loader */}
+                  {loaded && id === note.id ? (
+                    <Spinner />
+                  ) : (
+                    <Chip
+                      key={note.id}
+                      variant="outlined"
+                      label={
+                        autoCompleteOpen && id === note.id
+                          ? 'Close'
+                          : 'Add Assignee'
+                      }
+                      size="medium"
+                      icon={
+                        autoCompleteOpen && id === note.id ? (
+                          <CancelIcon />
+                        ) : (
+                          <AddCircleIcon />
+                        )
+                      }
+                      onClick={event => handleOpenAutoComplete(event, note.id)}
+                    />
+                  )}
+                  {/* error message */}
+                  <br />
+                  {Boolean(message.length) && <span>{message}</span>}
+                  <br />
+                  <br />
+
+                  {/* autocomplete for assignees */}
+                  {// avoid opening autocomplete box for other notes
+                  autoCompleteOpen && id === note.id && (
+                    <Autocomplete
+                      clearOnEscape
+                      clearOnBlur
+                      loading={loading}
+                      id={note.id}
+                      options={liteData.users}
+                      getOptionLabel={option => option.name}
+                      style={{ width: 300 }}
+                      onChange={(_evt, value) => {
+                        // if nothing selected, ignore and move on
+                        if (!value) {
+                          return
+                        }
+                        // assign or unassign the user here
+                        assignUnassignUser(note.id, value.id)
+                      }}
+                      renderInput={params => (
+                        <TextField {...params} placeholder="Name of assignee" />
+                      )}
+                    />
+                  )}
+                  <Divider />
+                </li>
+              ))
+            ) : (
+              <span>No Actions yet</span>
+            )}
+          </ul>
+        </div>
+
+        <CenteredContent>
+          <Paginate
+            offSet={offset}
+            limit={limit}
+            active={true}
+            handlePageChange={paginate}
+          />
         </CenteredContent>
         <Fab
           variant="extended"
@@ -313,25 +337,29 @@ export default function TodoList({
           className={`btn ${css(styles.getStartedButton)} `}
         >
           Create task
-         </Fab>
+        </Fab>
       </div>
- 
     </Fragment>
   )
 }
 
-
-export function FilterComponent({ handleInputChange, list, stateList, classes, resetFilter }) {
+export function FilterComponent({
+  handleInputChange,
+  list,
+  stateList,
+  classes,
+  resetFilter
+}) {
   return (
     <FormControl className={classes.formControl}>
-    <InputLabel id="demo-mutiple-chip-label">Filter by Assignee</InputLabel>
-    <Select
-      labelId="select-by-assignee"
-      id="assignee-chip"
-      multiple
-      value={stateList}
-      onChange={handleInputChange}
-      input={<Input id="select-by-assignee" />}
+      <InputLabel id="demo-mutiple-chip-label">Filter by Assignee</InputLabel>
+      <Select
+        labelId="select-by-assignee"
+        id="assignee-chip"
+        multiple
+        value={stateList}
+        onChange={handleInputChange}
+        input={<Input id="select-by-assignee" />}
         renderValue={selected => (
           <div>
             {selected.map((value, i) => (
@@ -339,34 +367,19 @@ export function FilterComponent({ handleInputChange, list, stateList, classes, r
             ))}
           </div>
         )}
-    >
-      {list.map(item => (
+      >
+        {list.map(item => (
           <MenuItem key={item.id} value={item.name}>
             {item.name}
           </MenuItem>
         ))}
-    </Select>
-    {Boolean(stateList.length) && (
-      <Button onClick={resetFilter}>Clear Filter</Button>
-    )}
-  </FormControl>
+      </Select>
+      {Boolean(stateList.length) && (
+        <Button onClick={resetFilter}>Clear Filter</Button>
+      )}
+    </FormControl>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const useStyles = makeStyles({
   root: {
@@ -378,8 +391,8 @@ const useStyles = makeStyles({
   },
   formControl: {
     minWidth: 160,
-    maxWidth: 300,
-  },
+    maxWidth: 300
+  }
 })
 
 // this should be in one place, basically just one theme
@@ -412,7 +425,7 @@ const theme = createMuiTheme({
 const styles = StyleSheet.create({
   list: {
     margin: 0,
-    padding: 0,
+    padding: 0
   },
   listItem: {
     position: 'relative',
@@ -420,13 +433,13 @@ const styles = StyleSheet.create({
     padding: 15
   },
   getStartedButton: {
-    color: "#FFF",
+    color: '#FFF',
     backgroundColor: '#69ABA4',
     height: 51,
-    boxShadow: "none",
+    boxShadow: 'none',
     position: 'fixed',
     bottom: 20,
     right: 57,
-    marginLeft: '30%',
-},
+    marginLeft: '30%'
+  }
 })
