3d9931ba7ad9affaf5d042da74157f7f877e6e69
Saurabh Shinde <saurabh@doublegdp.com>
2020-08-06 15:04:03 +0000

    Added batch time condition in job and fixed expired method

--- app/jobs/campaign_metrics_job.rb
+++ app/jobs/campaign_metrics_job.rb
@@ -6,7 +6,7 @@ class CampaignMetricsJob < ApplicationJob
 
   def perform(campaign_id, user_id_list)
     campaign = Campaign.find(campaign_id)
-    return if campaign.expired?
+    return if campaign.expired? || campaign.batch_time.blank?
 
     clicks_count = EventLog.for_days(Campaign::EXPIRATION_DAYS).by_user_activity
                            .with_acting_user_id(user_id_list.split(',')).count
