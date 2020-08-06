42128eb6eba92ba838ba9064eee181c2bf8991c2
Yoram <yoram@doublegdp.com>
2020-08-06 14:57:29 +0000

    removed log from compaing list component

--- app/javascript/src/components/CampaignList.jsx
+++ app/javascript/src/components/CampaignList.jsx
@@ -9,14 +9,14 @@ import Loading from '../components/Loading'
 import ErrorPage from '../components/Error'
 import { styles } from '../components/ShareButton'
 import { dateTimeToString, dateToString } from '../components/DateContainer'
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import Badge from '@material-ui/core/Badge';
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import Badge from '@material-ui/core/Badge'
 
 export default function CampaignList() {
   const history = useHistory()
-  
+
   //const theme = useContext(ThemeContext)
   const { data, error, loading } = useQuery(allCampaigns, {
     fetchPolicy: 'cache-and-network'
@@ -29,55 +29,116 @@ export default function CampaignList() {
   }
   if (loading) return <Loading />
   if (error) return <ErrorPage />
-console.log(data.campaigns[0].batchTime)
+
   return (
     <div className="container">
       {data.campaigns.map(c => (
         <Fragment key={c.id}>
-    <div>
-        <Grid container spacing={2}>
-          <Grid item container direction="column" spacing={2}>
-            <Grid item>
-              <Typography className={css(style.logTitle)} gutterBottom variant="subtitle1" data-testid="c_name">{c.name}</Typography>
-              <Typography className={css(style.subTitle)} variant="body2" data-testid="c_message" color="textSecondary">{c.message}</Typography>
-            </Grid>
-            <Grid item>
-              <Typography className={css(style.subTitle)} variant="body2" gutterBottom >
-                <strong>Scheduled Date: </strong>{dateToString(c.batchTime)}  <strong>Scheduled Time: </strong>{dateTimeToString(new Date(c.batchTime))}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Grid item container direction="row" spacing={2}>
+          <div>
+            <Grid container spacing={2}>
+              <Grid item container direction="column" spacing={2}>
                 <Grid item>
-                  <Badge max={9999} color="primary" badgeContent={c.campaignMetrics.totalScheduled}>
-                    <Typography className={css(style.subTitle)} >Total Scheduled</Typography>
-                  </Badge>
+                  <Typography
+                    className={css(style.logTitle)}
+                    gutterBottom
+                    variant="subtitle1"
+                    data-testid="c_name"
+                  >
+                    {c.name}
+                  </Typography>
+                  <Typography
+                    className={css(style.subTitle)}
+                    variant="body2"
+                    data-testid="c_message"
+                    color="textSecondary"
+                  >
+                    {c.message}
+                  </Typography>
                 </Grid>
                 <Grid item>
-                  <Badge max={9999} color="primary" badgeContent={c.campaignMetrics.totalSent}>
-                    <Typography className={css(style.subTitle)} >Total Sent</Typography>
-                  </Badge>
+                  <Typography
+                    className={css(style.subTitle)}
+                    variant="body2"
+                    gutterBottom
+                  >
+                    <strong>Scheduled Date: </strong>
+                    {dateToString(c.batchTime)}{' '}
+                    <strong>Scheduled Time: </strong>
+                    {dateTimeToString(new Date(c.batchTime))}
+                  </Typography>
                 </Grid>
                 <Grid item>
-                  <Badge max={9999} color="primary" badgeContent={c.campaignMetrics.totalClicked}>
-                    <Typography className={css(style.subTitle)} >Total Clicked</Typography>
-                  </Badge>
+                  <Grid item container direction="row" spacing={2}>
+                    <Grid item>
+                      <Badge
+                        max={9999}
+                        color="primary"
+                        badgeContent={c.campaignMetrics.totalScheduled}
+                      >
+                        <Typography className={css(style.subTitle)}>
+                          Total Scheduled
+                        </Typography>
+                      </Badge>
+                    </Grid>
+                    <Grid item>
+                      <Badge
+                        max={9999}
+                        color="primary"
+                        badgeContent={c.campaignMetrics.totalSent}
+                      >
+                        <Typography className={css(style.subTitle)}>
+                          Total Sent
+                        </Typography>
+                      </Badge>
+                    </Grid>
+                    <Grid item>
+                      <Badge
+                        max={9999}
+                        color="primary"
+                        badgeContent={c.campaignMetrics.totalClicked}
+                      >
+                        <Typography className={css(style.subTitle)}>
+                          Total Clicked
+                        </Typography>
+                      </Badge>
+                    </Grid>
+                    <Grid item>
+                      <Badge
+                        max={9999}
+                        color="primary"
+                        badgeContent={String(
+                          (100 * c.campaignMetrics.totalClicked) /
+                            (c.campaignMetrics.totalSent &&
+                            c.campaignMetrics.totalSent > 0
+                              ? c.campaignMetrics.totalSent
+                              : 1)
+                        )}
+                      >
+                        <Typography className={css(style.subTitle)}>
+                          % Success
+                        </Typography>
+                      </Badge>
+                    </Grid>
+                  </Grid>
                 </Grid>
                 <Grid item>
-                  <Badge max={9999} color="primary" badgeContent={String((100 * c.campaignMetrics.totalClicked) /(c.campaignMetrics.totalSent && c.campaignMetrics.totalSent > 0 ? c.campaignMetrics.totalSent : 1))}>
-                    <Typography className={css(style.subTitle)}>% Success</Typography>
-                  </Badge> 
+                  <Typography
+                    variant="body1"
+                    style={{ cursor: 'pointer', color: '#009688' }}
+                  >
+                    <Link
+                      data-testid="more_details_btn"
+                      href="#"
+                      style={{ cursor: 'pointer', color: '#009688' }}
+                      onClick={event => routeToAction(event, c.id)}
+                    >
+                      More Details
+                    </Link>
+                  </Typography>
                 </Grid>
-              </Grid>                             
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="body1" style={{ cursor: 'pointer', color: '#009688' }}>
-                <Link data-testid="more_details_btn" href="#" style={{ cursor: 'pointer', color: '#009688' }} onClick={event => routeToAction(event, c.id)}>More Details</Link>
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-    </div>
+          </div>
           <div className="border-top my-3" />
         </Fragment>
       ))}
