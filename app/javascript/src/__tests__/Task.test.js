36256c5564e9f812e6f79183f70667458c6a72d8
OlivierJM <olivier@doublegdp.com>
2020-08-07 15:11:38 +0000

    add tests for task

---
+++ app/javascript/src/__tests__/Task.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Task } from '../components/Notes/Task'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('component that centers divs', () => {
+  it('should not render with wrong props', () => {
+    const props = {
+        note: {
+            body: 'Note example',
+            id: '23',
+            author: {
+                name: 'Johnsc'
+            },
+            user: {
+                name: 'somebody'
+            }
+        },
+        message: '',
+        users: [],
+        handleCompleteNote: jest.fn(),
+        loaded: false,
+        loading: false,
+        classes: {}
+    }
+    const container = render(<Task {...props} />)
+    expect(container.queryByText('Note example')).toBeInTheDocument()
+    expect(container.queryByText('Johnsc')).toBeInTheDocument()
+    expect(container.queryByText('somebody')).toBeInTheDocument()
+    expect(container.queryByText('Associated with somebody')).toBeInTheDocument()
+  })
+})
