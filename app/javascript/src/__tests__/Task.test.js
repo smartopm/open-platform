758e12de0cf795aa43fb179c4008f06bf9c0aa35
OlivierJM <olivier@doublegdp.com>
2020-08-07 15:46:04 +0000

    add date to task tests

--- app/javascript/src/__tests__/Task.test.js
+++ app/javascript/src/__tests__/Task.test.js
@@ -6,31 +6,34 @@ import '@testing-library/jest-dom/extend-expect'
 describe('component that centers divs', () => {
   it('should not render with wrong props', () => {
     const props = {
-        note: {
-            body: 'Note example',
-            id: '23',
-            author: {
-                name: 'Johnsc'
-            },
-            user: {
-                name: 'somebody'
-            }
+      note: {
+        body: 'Note example',
+        id: '23',
+        createdAt: new Date(),
+        author: {
+          name: 'Johnsc'
         },
-        message: '',
-        users: [],
-        handleCompleteNote: jest.fn(),
-        loaded: false,
-        loading: false,
-        classes: {},
-        assignUnassignUser: jest.fn(),
-        handleDelete: jest.fn(),
-        handleModal: jest.fn(),
+        user: {
+          name: 'somebody'
+        },
+        assignees: []
+      },
+      message: '',
+      users: [],
+      handleCompleteNote: jest.fn(),
+      loaded: false,
+      loading: false,
+      classes: {},
+      assignUnassignUser: jest.fn(),
+      handleDelete: jest.fn(),
+      handleModal: jest.fn()
     }
     const container = render(<Task {...props} />)
     expect(container.queryByText('Note example')).toBeInTheDocument()
     expect(container.queryByText('Johnsc')).toBeInTheDocument()
     expect(container.queryByText('somebody')).toBeInTheDocument()
-    expect(container.queryByText('Associated with somebody')).toBeInTheDocument()
+    expect(
+      container.queryByText('Associated with somebody')
+    ).toBeInTheDocument()
   })
 })
-
