a38f6c21655a536fb4fa54701ba4b26598d11e58
OlivierJM <olivier@doublegdp.com>
2020-08-10 18:25:28 +0000

    update comment tests

--- app/javascript/src/__tests__/Comments.test.js
+++ app/javascript/src/__tests__/Comments.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { CommentBox, CommentSection } from '../components/Discussion/Comment'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { BrowserRouter } from 'react-router-dom'
 
 jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn())
 
@@ -13,14 +14,24 @@ const props = {
         message: "I am a comment",
         isLoading: false
     },
-    handleCommentChange: jest.fn()
+    handleCommentChange: jest.fn(),
+    upload: {
+        handleFileUpload: jest.fn(),
+        status: 'DONE',
+        url: 'https://dev.dgdp.site/activestorage'
+      }
 }
 
 describe('CommentBox', () => {
 
     it('should render with wrong props', () => {
-        const container = render(<CommentBox {...props} />)
+        const container = render(
+          <BrowserRouter>
+            <CommentBox {...props} />
+          </BrowserRouter>
+        )
         expect(container.queryByText('Send')).toBeInTheDocument()
+        expect(container.queryByText('Image uploaded')).toBeInTheDocument()
 
         const comment = container.queryByTestId('comment_content')
         fireEvent.change(comment, { target: { value: 'This is a good comment' } })
@@ -33,12 +44,18 @@ describe('CommentSection', () => {
     const props = {
         user: { name: "someimagesource" },
         createdAt: new Date(),
-        comment: "This is another comment"
+        comment: "This is another comment",
+        imageUrl: 'https://dev.dgdp.site/activestorage'
     }
 
     it('should render with wrong props', () => {
-        const container = render(<CommentSection {...props} />)
+        const container = render(
+            <BrowserRouter>
+                <CommentSection {...props} />
+            </BrowserRouter>
+        )
         expect(container.queryByText('someimagesource')).toBeInTheDocument()
         expect(container.queryByText('This is another comment')).toBeInTheDocument()
+        expect(container.queryByAltText('image for This is another comment')).toBeInTheDocument()
     })
 })
