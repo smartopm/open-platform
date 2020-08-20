2a5630d1b061adbb66e40038a9e53e3cfdaafc0b
dennis60 <dennis@doublegdp.com>
2020-08-20 17:54:45 +0000

    Added unit test

---
+++ app/javascript/src/__tests__/FormLinks.test.js
@@ -0,0 +1,19 @@
+import React from 'react' 
+import { render,fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import FormLinkList from '../components/FormLinkList';
+
+describe('Shows the google form links', () => {
+
+    it('It should render with no errors', () => {
+
+        const container = render(
+            <FormLinkList />
+        )
+
+        expect(container.queryByTestId('forms-crf')).toBeInTheDocument()
+        expect(container.queryByTestId('forms-building-permit')).toBeInTheDocument() 
+        expect(container.queryByTestId('forms-link-building-permit')).toBeInTheDocument()
+    });    
+    
+});
