d1de24e87d5d1277ebfa9bab7f0a818a4efdc225
tolulope3 <tolulope@doublegdp.com>
2020-08-20 17:07:22 +0000

    add some tetsts

--- app/javascript/src/__tests__/DateFilterComponent.test.js
+++ app/javascript/src/__tests__/DateFilterComponent.test.js
@@ -3,7 +3,41 @@ import DateFilterComponent from '../components/DateFilterComponent'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-describe('component that centers divs', () => {
+describe('filter users for campaign component', () => {
+  it('should display logged in to option', () => {
+    const props = {
+      handleFilterInputChange: jest.fn(),
+      classes: {},
+      filterType: "log_to",
+      handleDateChangeFrom: jest.fn(),
+      handleDateChangeTo: jest.fn(),
+      handleDateChangeOn: jest.fn(),
+      selectDateFrom: "2020-08-16",
+      selectDateTo: "",
+      selectDateOn: "",
+      resetFilter: jest.fn()
+    }
+    const container = render(<DateFilterComponent {...props} />)
+    expect(container.queryByText('Logged in to')).toBeInTheDocument()
+  })
+
+  it('should display logged in on option', () => {
+    const props = {
+      handleFilterInputChange: jest.fn(),
+      classes: {},
+      filterType: "log_on",
+      handleDateChangeFrom: jest.fn(),
+      handleDateChangeTo: jest.fn(),
+      handleDateChangeOn: jest.fn(),
+      selectDateFrom: "2020-08-16",
+      selectDateTo: "",
+      selectDateOn: "",
+      resetFilter: jest.fn()
+    }
+    const container = render(<DateFilterComponent {...props} />)
+    expect(container.queryByText('Logged in on')).toBeInTheDocument()
+  })
+
   it('should render with correct props', () => {
     const props = {
       handleFilterInputChange: jest.fn(),
@@ -19,6 +53,7 @@ describe('component that centers divs', () => {
     }
     const container = render(<DateFilterComponent {...props} />)
     expect(container.queryByText('Filter for Campaign')).toBeInTheDocument()
+    expect(container.queryByText('Logged in from')).toBeInTheDocument()
     expect(container.queryByText('Clear Filter')).toBeInTheDocument()
     expect(container.queryByText('Clear Filter')).not.toBeDisabled()
   })
