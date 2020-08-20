3523b6a7dbb9fe459b38a1ebb7f84a31c675fcca
tolulope3 <tolulope@doublegdp.com>
2020-08-20 11:18:02 +0000

    admin can filter users using login time

---
+++ app/javascript/src/components/DateFilterComponent.jsx
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react'
+import {
+  Button,
+  MenuItem,
+  Select,
+  Grid,
+  FormControl,
+  InputLabel
+} from '@material-ui/core'
+import { filterUserByLoggedin } from '../utils/constants'
+import DatePicker from "./DatePickerDialog"
+
+export default function DateFilterComponent({
+  handleFilterInputChange,
+  classes,
+  filterType,
+  handleDateChangeFrom,
+  handleDateChangeTo,
+  handleDateChangeOn,
+  selectDateFrom,
+  selectDateTo,
+  selectDateOn,
+  resetFilter
+}) {
+  return (
+    <Grid item xs={'auto'}>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-mutiple-chip-label" className={classes.inputLabel}>Filter for Campaign</InputLabel>
+        <Select
+          labelId="select-by-loggedin"
+          id="loggedin-filter"
+          value={filterType}
+          onChange={handleFilterInputChange}
+        >
+          {Object.entries(filterUserByLoggedin).map(([key, val]) => (
+            <MenuItem key={key} value={key}>
+              {val}
+            </MenuItem>
+          ))}
+        </Select>
+        {filterType === 'log_from' && (
+          <Fragment>
+            from: <DatePicker handleDateChange={handleDateChangeFrom} selectedDate={selectDateFrom} />
+          </Fragment>)}
+          {filterType === "log_to" && (
+            <Fragment>
+            to: <DatePicker handleDateChange={handleDateChangeTo} selectedDate={selectDateTo} />
+            </Fragment>
+          )}
+          {filterType === "log_on" && (
+            <Fragment>
+            on: <DatePicker handleDateChange={handleDateChangeOn} selectedDate={selectDateOn} />
+            </Fragment>
+          )}
+
+        {Boolean(filterType.length) && (
+          <Button size="small" onClick={resetFilter}>Clear Filter</Button>
+        )}
+      </FormControl>
+    </Grid>
+  )
+}
