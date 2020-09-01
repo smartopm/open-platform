1caa2ac2761ea46774bbe42b35d69c05f893a8fe
OlivierJM <olivier@doublegdp.com>
2020-09-01 18:13:12 +0000

    fix tests

--- app/javascript/src/components/User/UserMerge.jsx
+++ app/javascript/src/components/User/UserMerge.jsx
@@ -5,7 +5,6 @@ import { useMutation } from 'react-apollo'
 import { MergeUsersMutation } from '../../graphql/mutations'
 import { ModalDialog } from '../Dialog'
 import CenteredContent from '../CenteredContent'
-import { saniteError } from '../../utils/helpers'
 
 // find a user a call the mutation to merge them
 const initialData = {
@@ -39,6 +38,7 @@ export default function UserMerge({ userId, close }) {
     }
     setConfirmOpen(!open)
   }
+  
   return (
     <>
       <ModalDialog
@@ -61,7 +61,7 @@ export default function UserMerge({ userId, close }) {
 
       {message && (
         <CenteredContent>
-          <Typography variant="body2">{saniteError([], message)}</Typography>
+          <Typography variant="body2">{message}</Typography>
         </CenteredContent>
       )}
 
