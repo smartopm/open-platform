05048f680326df4cc14469a132b569b167d53943
OlivierJM <olivier@doublegdp.com>
2020-09-03 16:48:37 +0000

    remove a test implementation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,16 +8,10 @@ class Comment < ApplicationRecord
   belongs_to :discussion
 
   has_one_attached :image
-  after_create :send_alert
 
   default_scope { order(created_at: :desc) }
 
   IMAGE_ATTACHMENTS = {
     image_blob_id: :image,
   }.freeze
-
-  # testing the email alert on new comment
-  def send_alert
-    CommentsAlert.send_email_alert(user.community.name)
-  end
 end
