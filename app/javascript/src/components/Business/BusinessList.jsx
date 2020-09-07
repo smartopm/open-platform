86b7d115566481685c790635fec53b41fa7e8bc5
OlivierJM <olivier@doublegdp.com>
2020-09-07 13:29:25 +0000

    add a business form

--- app/javascript/src/components/Business/BusinessList.jsx
+++ app/javascript/src/components/Business/BusinessList.jsx
@@ -1,45 +1,104 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { List, ListItem, ListItemAvatar, Divider, Typography, Box } from '@material-ui/core'
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Divider,
+  Typography,
+  Box,
+  Fab,
+  Dialog,
+  DialogTitle,
+  DialogContent
+} from '@material-ui/core'
+import { StyleSheet, css } from 'aphrodite'
 import Avatar from '../Avatar'
+import CenteredContent from '../CenteredContent'
+import BusinessForm from './BusinessForm'
 
 export default function BusinessList({ businessData }) {
-    return (
-      <div className="container">
-        <List>
-          {
-                    businessData.businesses.map(business => (
-                      <Link key={business.id} to={`/business/${business.id}`} className="card-link">
-                        <ListItem key={business.id}>
-                          <ListItemAvatar>
-                            {
-                            // eslint-disable-next-line 
-                            }<Avatar imageUrl={business.imageUrl} style="medium" />
-                          </ListItemAvatar>
-                          <Box
-                            style={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    width: '100%',
-                                    marginLeft: 30
-                                }}
-                          >
-                            <Typography variant="subtitle1" data-testid="business-name">
-                              {business.name}
-                            </Typography>
-                            <Typography variant="caption" data-testid="business-category">
-                              {business.category}
-                            </Typography>
-                          </Box>
-                          <Divider variant="middle" />
-                        </ListItem>
-                      </Link>
-                    ))
-                }
-        </List>
+  const [open, setModalOpen] = useState(false)
 
-      </div>
-
-    )
+  function openModal() {
+    setModalOpen(!open)
+  }
+  return (
+    <div className="container">
+      <Dialog
+        // fullScreen
+        open={open}
+        fullWidth
+        maxWidth="lg"
+        onClose={openModal}
+        aria-labelledby="task_modal"
+      >
+        <DialogTitle id="task_modal">
+          <CenteredContent>
+            <span>Create a task</span>
+          </CenteredContent>
+        </DialogTitle>
+        <DialogContent>
+          <BusinessForm />
+        </DialogContent>
+      </Dialog>
+      <List>
+        {businessData.businesses.map(business => (
+          <Link
+            key={business.id}
+            to={`/business/${business.id}`}
+            className="card-link"
+          >
+            <ListItem key={business.id}>
+              <ListItemAvatar>
+                <Avatar 
+                  imageUrl={business.imageUrl} 
+                    // eslint-disable-next-line
+                  style="medium" />
+              </ListItemAvatar>
+              <Box
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                  marginLeft: 30
+                }}
+              >
+                <Typography variant="subtitle1" data-testid="business-name">
+                  {business.name}
+                </Typography>
+                <Typography variant="caption" data-testid="business-category">
+                  {business.category}
+                </Typography>
+              </Box>
+              <Divider variant="middle" />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Fab
+        variant="extended"
+        onClick={openModal}
+        color="primary"
+        // eslint-disable-next-line no-use-before-define
+        className={`btn ${css(styles.taskButton)} `}
+      >
+        Create a Business
+      </Fab>
+    </div>
+  )
 }
+
+const styles = StyleSheet.create({
+    taskButton: {
+      height: 51,
+      boxShadow: 'none',
+      position: 'fixed',
+      bottom: 20,
+      right: 57,
+      marginLeft: '30%',
+      color: '#FFFFFF'
+    }
+  })
+  
