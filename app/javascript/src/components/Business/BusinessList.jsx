e0236eb1e2fbfc821a827c9913fe26fd3306596a
OlivierJM <olivier@doublegdp.com>
2020-09-07 12:00:17 +0000

    re-enable eslint and add business categories

--- app/javascript/src/components/Business/BusinessList.jsx
+++ app/javascript/src/components/Business/BusinessList.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { List, ListItem, ListItemAvatar, Divider, Typography, Box } from '@material-ui/core'
@@ -6,38 +5,41 @@ import Avatar from '../Avatar'
 
 export default function BusinessList({ businessData }) {
     return (
-        <div className="container">
-            <List>
-                {
+      <div className="container">
+        <List>
+          {
                     businessData.businesses.map(business => (
-                        <Link key={business.id} to={`/business/${business.id}`} className={`card-link`}>
-                            <ListItem key={business.id}>
-                                <ListItemAvatar>
-                                    <Avatar imageUrl={business.imageUrl} style={"medium"} />
-                                </ListItemAvatar>
-                                <Box
-                                    style={{
+                      <Link key={business.id} to={`/business/${business.id}`} className="card-link">
+                        <ListItem key={business.id}>
+                          <ListItemAvatar>
+                            {
+                            // eslint-disable-next-line 
+                            }<Avatar imageUrl={business.imageUrl} style="medium" />
+                          </ListItemAvatar>
+                          <Box
+                            style={{
                                     display: 'flex',
                                     flexDirection: 'row',
                                     justifyContent: 'space-between',
                                     width: '100%',
                                     marginLeft: 30
-                                }}>
-                                    <Typography variant="subtitle1" data-testid="business-name">
-                                        {business.name}
-                                    </Typography>
-                                    <Typography variant="caption" data-testid="business-category">
-                                        {business.category}
-                                    </Typography>
-                                </Box>
-                                <Divider variant="middle" />
-                            </ListItem>
-                        </Link>
+                                }}
+                          >
+                            <Typography variant="subtitle1" data-testid="business-name">
+                              {business.name}
+                            </Typography>
+                            <Typography variant="caption" data-testid="business-category">
+                              {business.category}
+                            </Typography>
+                          </Box>
+                          <Divider variant="middle" />
+                        </ListItem>
+                      </Link>
                     ))
                 }
-            </List>
+        </List>
 
-        </div>
+      </div>
 
     )
 }
--- app/javascript/src/components/Business/BusinessProfile.jsx
+++ app/javascript/src/components/Business/BusinessProfile.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable */
 import React, { useState, useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { Typography, withStyles, Tab, Button, Box, ListItem, List } from '@material-ui/core'
-import { StyledTabs, TabPanel } from '../../components/Tabs'
-import Status from '../../components/StatusBadge'
-import { Context as AuthStateContext } from '../../containers/Provider/AuthStateProvider.js'
 import { useHistory } from 'react-router-dom'
+import { Typography, withStyles, Tab, Button, Box, ListItem, List } from '@material-ui/core'
+import { StyledTabs, TabPanel } from "../Tabs"
+import Status from "../StatusBadge"
+import { Context as AuthStateContext } from '../../containers/Provider/AuthStateProvider'
 import Avatar from '../Avatar'
 
 export const StyledTab = withStyles({
@@ -13,6 +11,7 @@ export const StyledTab = withStyles({
         textTransform: 'none',
         color: 'inherit'
     }
+// eslint-disable-next-line react/jsx-props-no-spreading
 })(props => <Tab {...props} />)
 
 
@@ -43,84 +42,94 @@ export default function BusinessProfile({ profileData }) {
         setValue(newValue)
     }
     return (
-        <div className="container">
-            <div className="row d-flex justify-content-between">
-                <div className="col-4 d-flex justify-content-end align-items-center">
-                    <Avatar imageUrl={profileData.imageUrl} style="big"/>
-                </div>
-                <div className="col-8 justify-content-around" data-testid="details-holder">
-                    <Typography variant="h6" arial-label='pf-company-name'>
-                        {profileData.name}
-                    </Typography>
-                    <Typography variant="subtitle2" data-testid="pf-number" arial-label='pf-phone-number'>
-                        {profileData.phoneNumber}
-                    </Typography >
-                    <Typography variant="subtitle2" arial-label='pf-email-address'>
-                        {profileData.email}
-                    </Typography >
-                    <Typography variant="subtitle2" arial-label='pf-address'>
-                        {profileData.address}
-                    </Typography >
-                    <Typography variant="subtitle2">
+      <div className="container">
+        <div className="row d-flex justify-content-between">
+          <div className="col-4 d-flex justify-content-end align-items-center">
+            {            
+                // eslint-disable-next-line
+                }<Avatar imageUrl={profileData.imageUrl} style="big" />
+          </div>
+          <div className="col-8 justify-content-around" data-testid="details-holder">
+            <Typography variant="h6" arial-label='pf-company-name'>
+              {profileData.name}
+            </Typography>
+            <Typography variant="subtitle2" data-testid="pf-number" arial-label='pf-phone-number'>
+              {profileData.phoneNumber}
+            </Typography>
+            <Typography variant="subtitle2" arial-label='pf-email-address'>
+              {profileData.email}
+            </Typography>
+            <Typography variant="subtitle2" arial-label='pf-address'>
+              {profileData.address}
+            </Typography>
+            <Typography variant="subtitle2">
 
-                        <a><span onClick={() => openLink(profileData.homeUrl)}>{profileData.homeUrl}</span></a>
-                    </Typography >
-                    <Box style={{ width: '50%', marginTop: 5 }}><Status label={profileData.status} /></Box>
-                </div>
-            </div>
-
-            <br />
-            <div>
-                <StyledTabs
-                    value={tabValue}
-                    onChange={handleChange}
-                    aria-label="request tabs"
-                    centered
-                >
-                    <StyledTab label="Profile" value={'Profile'} />
-                    <StyledTab label="Operating Hours" value={'Operating Hours'} />
-                    <StyledTab label="Relevant Posts" value={'Relevant Posts'} />
-                </StyledTabs>
-                <TabPanel value={tabValue} index={'Profile'}>
-
-                    <Typography variant="h6">Description</Typography>
-                    <Typography variant="body1" arial-label='pf-description'>
-                        {profileData.description || 'No Description'}
-                    </Typography>
-
-                </TabPanel>
-                <TabPanel value={tabValue} index={'Operating Hours'}>
-                    <div className='d-flex  justify-content-center'>
-                        <div>
-                            <Typography variant="h6">
-                                Operating Hours
-                            </Typography>
-                            <br />
-                            <p>
-                                {profileData.operationHours}
-                            </p>
-                        </div>
-                    </div>
-                </TabPanel>
-                <TabPanel value={tabValue} index={'Relevant Posts'}>
-                    <div>
-                        <Typography variant="h6">Links</Typography>
-                        <List>
-                            {profileData?.links ? (Object.entries(profileData.links).map((k, v) => (
-                                <ListItem key={k}>
-                                    <Link>
-                                        {v}
-                                    </Link>
-                                </ListItem>
-                            ))) : ''}
-                        </List>
-                    </div>
-                </TabPanel>
-
-                <div className='container d-flex justify-content-center'>
-                    <Button onClick={handleButtonClick} color="primary" >Ask about business</Button>
-                </div>
-            </div>
+              <span 
+                onClick={() => openLink(profileData.homeUrl)} 
+                onKeyPress={() => openLink(profileData.homeUrl)}
+                role="link"
+                tabIndex={0}
+              >
+                {profileData.homeUrl}
+              </span>
+            </Typography>
+            <Box style={{ width: '50%', marginTop: 5 }}><Status label={profileData.status} /></Box>
+          </div>
         </div>
+
+        <br />
+        <div>
+          <StyledTabs
+            value={tabValue}
+            onChange={handleChange}
+            aria-label="request tabs"
+            centered
+          >
+            <StyledTab label="Profile" value="Profile" />
+            <StyledTab label="Operating Hours" value="Operating Hours" />
+            <StyledTab label="Relevant Posts" value="Relevant Posts" />
+          </StyledTabs>
+          <TabPanel value={tabValue} index="Profile">
+
+            <Typography variant="h6">Description</Typography>
+            <Typography variant="body1" arial-label='pf-description'>
+              {profileData.description || 'No Description'}
+            </Typography>
+
+          </TabPanel>
+          <TabPanel value={tabValue} index="Operating Hours">
+            <div className='d-flex  justify-content-center'>
+              <div>
+                <Typography variant="h6">
+                  Operating Hours
+                </Typography>
+                <br />
+                <p>
+                  {profileData.operationHours}
+                </p>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel value={tabValue} index="Relevant Posts">
+            <div>
+              <Typography variant="h6">Links</Typography>
+              {/* TODO: refactor this */}
+              <List>
+                {profileData?.links ? (Object.entries(profileData.links).map((k, v) => (
+                  <ListItem key={k}>
+                    <span>
+                      {v}
+                    </span>
+                  </ListItem>
+                 ))) : ''}
+              </List>
+            </div>
+          </TabPanel>
+
+          <div className='container d-flex justify-content-center'>
+            <Button onClick={handleButtonClick} color="primary">Ask about business</Button>
+          </div>
+        </div>
+      </div>
     )
 }
