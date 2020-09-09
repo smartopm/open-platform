71e87f1bc6c6701b7cff8c12f3ad9a1a2dc59781
OlivierJM <manolivier93@gmail.com>
2020-09-09 15:24:35 +0200

    re-enable eslint

--- app/javascript/src/__tests__/Userlabels.test.js
+++ app/javascript/src/__tests__/Userlabels.test.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
 import React from 'react'
 import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect'
 import { MockedProvider } from '@apollo/react-testing'
 import UserLabels from '../components/UserLabels'
 import { UserLabelsQuery } from '../graphql/queries'
-import { BrowserRouter } from 'react-router-dom'
 
 describe('It should test the user label component', () => {
 
@@ -31,28 +30,26 @@ describe('It should test the user label component', () => {
     it('It should render component', () => {
 
         const container = render(
-            <MockedProvider mocks={mockData} addTypename={false}>
-                <BrowserRouter>
-                    <UserLabels userId={"59927651-9bb4-4e47-8afe-0989d03d210d"} />
-                </BrowserRouter>
-            </MockedProvider>
+          // eslint-disable-next-line react/jsx-filename-extension
+          <MockedProvider mocks={mockData} addTypename={false}>
+            <BrowserRouter>
+              <UserLabels userId="59927651-9bb4-4e47-8afe-0989d03d210d" />
+            </BrowserRouter>
+          </MockedProvider>
         )
         expect(container.queryByTestId("chip-label")).toBeDefined();
     })
 
     it('it should display the chip', async () => {
         const container = render(
-            <MockedProvider mocks={mockData} addTypename={false}>
-                <BrowserRouter>
-                    <UserLabels userId={"59927651-9bb4-4e47-8afe-0989d03d210d"} />
-                </BrowserRouter>
-            </MockedProvider>
+          <MockedProvider mocks={mockData} addTypename={false}>
+            <BrowserRouter>
+              <UserLabels userId="59927651-9bb4-4e47-8afe-0989d03d210d" />
+            </BrowserRouter>
+          </MockedProvider>
         )
         expect(container.queryByTestId("chip-label")).toBeDefined();
         await new Promise(resolve => setTimeout(resolve, 500));
         expect(container.queryByTestId('chip-label')).toBeNull()
-
-
     })
-
 });
