74dcd6dcb86d04d79fa4411ef805afae7494fc2d
OlivierJM <manolivier93@gmail.com>
2020-09-09 09:23:12 +0200

    add templates to communities

--- app/javascript/src/__tests__/Userlabels.test.js
+++ app/javascript/src/__tests__/Userlabels.test.js
@@ -7,13 +7,13 @@ import UserLabels from '../components/UserLabels'
 import { UserLabelsQuery } from '../graphql/queries'
 import { BrowserRouter } from 'react-router-dom'
 
-
 describe('It should test the user label component', () => {
+
     const mockData = [
         {
             request: {
                 query: UserLabelsQuery,
-                variables: { userId: "678890" }
+                variables: { userId: "59927651-9bb4-4e47-8afe-0989d03d210d" }
             },
             result: {
                 data: {
@@ -26,16 +26,20 @@ describe('It should test the user label component', () => {
                 }
             }
         }]
+
+
     it('It should render component', () => {
+
         const container = render(
             <MockedProvider mocks={mockData} addTypename={false}>
                 <BrowserRouter>
-                    <UserLabels userId={"678890"} />
+                    <UserLabels userId={"59927651-9bb4-4e47-8afe-0989d03d210d"} />
                 </BrowserRouter>
             </MockedProvider>
         )
         expect(container.queryByTestId("chip-label")).toBeDefined();
     })
+
     it('it should display the chip', async () => {
         const container = render(
             <MockedProvider mocks={mockData} addTypename={false}>
@@ -47,5 +51,8 @@ describe('It should test the user label component', () => {
         expect(container.queryByTestId("chip-label")).toBeDefined();
         await new Promise(resolve => setTimeout(resolve, 500));
         expect(container.queryByTestId('chip-label')).toBeNull()
+
+
     })
+
 });
