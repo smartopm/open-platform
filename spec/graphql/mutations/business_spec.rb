8d57dd1d2bbfb1b11c769481efa576fe66e56ba6
OlivierJM <olivier@doublegdp.com>
2020-09-10 09:41:34 +0000

    fix rubocop errors

--- spec/graphql/mutations/business_spec.rb
+++ spec/graphql/mutations/business_spec.rb
@@ -65,39 +65,40 @@ RSpec.describe Mutations::Business do
     describe 'creating a business' do
       let!(:current_user) { create(:user_with_community, user_type: 'admin') }
 
-  # create a business for the user
-  let!(:user_business) do
-    create(:business, user_id: current_user.id, community_id: current_user.community_id,
-                      status: 'verified')
-  end
+      # create a business for the user
+      let!(:user_business) do
+        create(:business, user_id: current_user.id, community_id: current_user.community_id,
+                          status: 'verified')
+      end
 
-  let(:query) do
-    <<~GQL
-      mutation DeleteBusiness(
-        $id: ID!
-      ) {
-        businessDelete(
-          id: $id
-          ){
-            businessDelete
-          }
+      let(:query) do
+        <<~GQL
+          mutation DeleteBusiness(
+            $id: ID!
+          ) {
+            businessDelete(
+              id: $id
+              ){
+                businessDelete
+              }
+            }
+        GQL
+      end
+
+      it 'deletes a business' do
+        variables = {
+          id: user_business.id,
         }
-    GQL
-  end
 
-  it 'deletes a business' do
-    variables = {
-      id: user_business.id,
-    }
+        result = DoubleGdpSchema.execute(query, variables: variables,
+                                                context: {
+                                                  current_user: current_user,
+                                                  site_community: current_user.community,
+                                                }).as_json
 
-    result = DoubleGdpSchema.execute(query, variables: variables,
-                                            context: {
-                                              current_user: current_user,
-                                              site_community: current_user.community,
-                                            }).as_json
-
-    expect(result.dig('data', 'businessDelete', 'businessDelete')).to eql true
-    expect(result.dig('errors')).to be_nil
+        expect(result.dig('data', 'businessDelete', 'businessDelete')).to eql true
+        expect(result.dig('errors')).to be_nil
+      end
     end
   end
 end
