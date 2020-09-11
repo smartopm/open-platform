38ece03dc07fe471fa7a673fbb35cdcff1519717
tolulope3 <tolulope@doublegdp.com>
2020-09-11 11:51:30 +0000

    fix campain component issues

--- app/graphql/types/campaign_type.rb
+++ app/graphql/types/campaign_type.rb
@@ -7,8 +7,8 @@ module Types
     field :community_id, ID, null: false
     field :name, String, null: false
     field :campaign_type, String, null: false
-    field :message, String, null: false
-    field :user_id_list, String, null: false
+    field :message, String, null: true
+    field :user_id_list, String, null: true
     field :subject, String, null: true
     field :status, String, null: true
     field :pre_header, String, null: true
@@ -17,7 +17,7 @@ module Types
     field :labels, [Types::LabelType], null: true
     field :start_time, GraphQL::Types::ISO8601DateTime, null: true
     field :end_time, GraphQL::Types::ISO8601DateTime, null: true
-    field :batch_time, GraphQL::Types::ISO8601DateTime, null: false
+    field :batch_time, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: true
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: true
--- db/migrate/20200910090220_add_status_to_campaign.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToCampaign < ActiveRecord::Migration[6.0]
-  def change
-    add_column :campaigns, :status, :string
-  end
-end
