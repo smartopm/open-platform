f7ccec0e24f775caf96e1d382a6b5d677efb7e25
OlivierJM <olivier@doublegdp.com>
2020-09-16 13:49:29 +0000

    update the controller

--- app/controllers/sendgrid_controller.rb
+++ app/controllers/sendgrid_controller.rb
@@ -2,17 +2,14 @@
 
 # Method responsbile for handling sendgrid inbound emails
 class SendgridController < ApplicationController
-    # authenticate user here
-    # before_action :ensure_admin
-
+    
     def webhook
         begin
-        request_load = JSON.parse(request.body)
-        puts request_load
-        puts "================================"
+        request_load = request
+        # get the response for 
         rescue Exception => ex
         # Find a way of properly channeling this error 
-        render :json => {:status => 400, :error => "Webhook failed"} and return
+        render :json => {:status => 400, :error => ex} and return
         end
         # I want to get the received json and save in in Message table
         render :json => {:status => 200}
