a10e0e580d7b92acc0170c160dcaf0468f8684f8
tolulope3 <tolulope@doublegdp.com>
2020-09-17 11:01:42 +0000

    fix eslint issues

--- app/javascript/src/containers/Discussions/Discussions.jsx
+++ app/javascript/src/containers/Discussions/Discussions.jsx
@@ -1,18 +1,17 @@
-/* eslint-disable */
-import React, { Fragment, useState, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { Fab, useMediaQuery, Dialog, DialogTitle, DialogContent, Button } from '@material-ui/core'
+import { useQuery } from 'react-apollo'
+import { css } from 'aphrodite'
+import { useTheme } from '@material-ui/core/styles';
 import Nav from '../../components/Nav'
 import DiscussionList from '../../components/Discussion/DiscussionList'
 import { DiscussionsQuery } from '../../graphql/queries'
-import { useQuery } from 'react-apollo'
 import Loading, { Spinner } from '../../components/Loading'
 import ErrorPage from '../../components/Error'
-import { css } from 'aphrodite'
 import { styles } from '../../components/ShareButton'
 import Discuss from '../../components/Discussion/Discuss'
-import { useTheme } from '@material-ui/core/styles';
 import CenteredContent from '../../components/CenteredContent'
-import { Context as AuthStateContext } from '../Provider/AuthStateProvider.js'
+import { Context as AuthStateContext } from '../Provider/AuthStateProvider'
 
 export default function Discussions() {
     const limit = 20
@@ -41,9 +40,7 @@ export default function Discussions() {
             updateQuery: (prev, { fetchMoreResult }) => {
                 if (!fetchMoreResult) return prev
                 setLoading(false)
-                return Object.assign({}, prev, {
-                    discussions: [...prev.discussions, ...fetchMoreResult.discussions]
-                })
+                return { ...prev, discussions: [...prev.discussions, ...fetchMoreResult.discussions]}
             }
         })
     }
@@ -53,48 +50,50 @@ export default function Discussions() {
         return <ErrorPage title={error.message || error} />
     }
     return (
-        <div>
-            <Fragment>
-                <Nav navName="Discussion Topics" menuButton="back" backTo="/" />
-                <Dialog
-                    fullScreen={fullScreen}
-                    open={open}
-                    fullWidth={true}
-                    maxWidth={'lg'}
-                    onClose={openModal}
-                    aria-labelledby="responsive-dialog-title"
-                >
-                    <DialogTitle id="responsive-dialog-title">
-                        <CenteredContent>
-                            <span>Create a discussion topic</span>
-                        </CenteredContent>
-                    </DialogTitle>
-                    <DialogContent>
-                        <Discuss update={updateList}/>
-                    </DialogContent>
-                </Dialog>
+      <div>
+        <>
+          <Nav navName="Discussion Topics" menuButton="back" backTo="/" />
+          <Dialog
+            fullScreen={fullScreen}
+            open={open}
+            fullWidth
+            maxWidth="lg"
+            onClose={openModal}
+            aria-labelledby="responsive-dialog-title"
+          >
+            <DialogTitle id="responsive-dialog-title">
+              <CenteredContent>
+                <span>Create a discussion topic</span>
+              </CenteredContent>
+            </DialogTitle>
+            <DialogContent>
+              <Discuss update={updateList} />
+            </DialogContent>
+          </Dialog>
                 
-                <DiscussionList data={data.discussions} />
-                {
+          <DiscussionList data={data.discussions} />
+          {
                     data.discussions.length >= limit && (
-                        <CenteredContent>
-                            <Button
-                                variant="outlined"
-                                onClick={fetchMoreDiscussions}>
-                                {isLoading ? <Spinner /> : 'Load more discussions'}
-                            </Button>
-                        </CenteredContent>
+                    <CenteredContent>
+                      <Button
+                        variant="outlined"
+                        onClick={fetchMoreDiscussions}
+                      >
+                        {isLoading ? <Spinner /> : 'Load more discussions'}
+                      </Button>
+                    </CenteredContent>
                     )
                 }
-                {userType === 'admin' && (
-                    <Fab variant="extended"
-                    onClick={openModal}
-                    className={`btn ${css(styles.getStartedButton)} `}
-                >
-                    Create discussion topic
-                </Fab>
+          {userType === 'admin' && (
+            <Fab
+              variant="extended"
+              onClick={openModal}
+              className={`btn ${css(styles.getStartedButton)} `}
+            >
+              Create discussion topic
+            </Fab>
                 )}
-            </Fragment>
-        </div>
+        </>
+      </div>
     )
 }
