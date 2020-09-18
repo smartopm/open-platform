6448e13493a330d05f12cdc9bdbf41081230249b
OlivierJM <manolivier93@gmail.com>
2020-09-18 12:13:50 +0200

    add more tests on the front-end

--- app/javascript/src/__tests__/Events.test.js
+++ app/javascript/src/__tests__/Events.test.js
@@ -1,41 +1,59 @@
-/* eslint-disable */
 import React from 'react'
-import Events from '../components/Events'
 import { render,} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { BrowserRouter } from 'react-router-dom/'
-import { MockedProvider } from '@apollo/react-testing'
+import Events from '../components/Events'
 
-describe("Should Render Events Component",()=>
-{
+describe("Should Render Events Component", () => {
     const data = {
         result : [
             {
                 id : "1",
                 subject : "user_active",
-                description : "	User Yoram Gondwe was active",      
+                sentence : "User john doe was active",      
                 createdAt : new Date(),      
-                timestamp : new Date(),  
+                timestamp : new Date(), 
+                data: {
+                    digital: false,
+                } 
             },
             {
                 id : "2",
-                subject : "user_active",
-                description : "	User Yoram Gondwe was active",      
+                subject : "user_entry",
+                sentence : "User john doe was recorded leaving",      
                 createdAt : new Date(),      
                 timestamp : new Date(),  
+                data: {}
             },
             
         ]
     }
-    it(" shoudl render tabel titles",()=>{
+    it("should render proper data", () => {
         const {getByText} = render(
-            <MockedProvider mock={[]} >
-              <BrowserRouter>
-                <Events data = {data}/>
-            </BrowserRouter>
-            </MockedProvider>
+          <BrowserRouter>
+            <Events data={data} />
+          </BrowserRouter>
         )
         expect(getByText('Subject')).toBeInTheDocument()
         expect(getByText('Description')).toBeInTheDocument() 
+        expect(getByText('user_entry')).toBeInTheDocument() 
+        expect(getByText('user_active')).toBeInTheDocument() 
+        expect(getByText('Print Scan')).toBeInTheDocument() 
+        expect(getByText('User john doe was recorded leaving')).toBeInTheDocument() 
+        expect(getByText('User john doe was active')).toBeInTheDocument() 
+    })
+
+    it("should not render any data when no events is provided", () => {
+        const emptyData = {  result: []}
+        const container = render(
+          <BrowserRouter>
+            <Events data={emptyData} />
+          </BrowserRouter>
+          )
+          expect(container.queryByText('user_entry')).toBeNull()
+          expect(container.queryByText('user_active')).toBeNull()
+          expect(container.queryByText('Print Scan')).toBeNull()
+          expect(container.queryByText('User john doe was recorded leaving')).toBeNull()
+          expect(container.queryByText('User john doe was active')).toBeNull()
     })
 })
