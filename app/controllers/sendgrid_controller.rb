8ff56915eef01a597801ba9a0cee31e5bbc36815
OlivierJM <olivier@doublegdp.com>
2020-09-21 12:39:41 +0000

    fix rubocop

--- app/controllers/sendgrid_controller.rb
+++ app/controllers/sendgrid_controller.rb
@@ -1,41 +1,40 @@
 # frozen_string_literal: true
 
-# Method responsbile for handling sendgrid inbound emails
+# Method responsible for handling sendgrid inbound emails
 class SendgridController < ApplicationController
-    skip_before_action :verify_authenticity_token, if: :valid_webhook_token?
+  skip_before_action :verify_authenticity_token, if: :valid_webhook_token?
 
-    def webhook
-        begin
-        # email comes like this user <user@gdp.com> get the value in angle brackets
-        email = params["from"].match /\<([^}]+)\>/
-        name = params["from"].match /(?<=\s|^)\w+(?=\s)/
-        # Find a user 
-        sender = @site_community.users.find_by(email: email[1])
-        if sender.nil?
-            user = @site_community.users.create!(name: name[0], email: email[1], user_type: "visitor")
-            generate_msg(user, "#{params['subject']} \n \n #{params['text']}")
-        end
+  def webhook
+    begin
+      # email comes like this user <user@gdp.com> get the value in angle brackets
+      email = params['from'].match /\<([^}]+)\>/
+      name = params['from'].match /(?<=\s|^)\w+(?=\s)/
+      # Find a user
+      sender = @site_community.users.find_by(email: email[1])
+      if sender.nil?
+        user = @site_community.users.create!(name: name[0], email: email[1], user_type: 'visitor')
+        generate_msg(user, "#{params['subject']} \n \n #{params['text']}")
+      end
 
-        generate_msg(sender, "#{params['subject']} \n \n #{params['text']}")
-        rescue Exception => ex
-        render :json => {:status => 400, :error => ex} and return
-        end
-        render :json => {:status => 200}
+      generate_msg(sender, "#{params['subject']} \n \n #{params['text']}")
+    rescue StandardError => e
+      render(json: { status: 400, error: e }) && (return)
     end
+    render json: { status: 200 }
+  end
 
-    private
+  private
 
-    def valid_webhook_token?
-        params[:token] == ENV["SENDGRID_WEBHOOK_TOKEN"]
-    end
+  def valid_webhook_token?
+    params[:token] == ENV['SENDGRID_WEBHOOK_TOKEN']
+  end
 
-    def generate_msg(user, body)
-        user.messages.create(
-            is_read: false, sender_id: user.id,
-            created_at: Time.zone.now,
-            message: body,
-            category: 'email',
-          )
-    end
-
+  def generate_msg(user, body)
+    user.messages.create(
+      is_read: false, sender_id: user.id,
+      created_at: Time.zone.now,
+      message: body,
+      category: 'email'
+    )
+  end
 end
