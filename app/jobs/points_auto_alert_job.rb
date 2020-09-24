e866afc0f88670b6845bb0f2030d9e838364ddd5
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2020-09-24 20:00:51 +0000

    Add sidekiq-scheduler

--- app/jobs/points_auto_alert_job.rb
+++ app/jobs/points_auto_alert_job.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 # Alert clients to get more points
-class PointsAutoAlert < ApplicationJob
+class PointsAutoAlertJob < ApplicationJob
   queue_as :default
 
   def perform
     Community.all.each do |c|
-      template_id = c.templates['points_auto_alert_template_id']
+      template_id = c.templates&.dig('points_auto_alert_template_id')
       next unless template_id
 
       c.users.where(user_type: 'client').find_each do |u|
