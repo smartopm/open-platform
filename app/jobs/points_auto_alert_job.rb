6aa7436be428effd41ce9cf2b80178d2580239c8
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2020-09-24 13:28:38 +0000

    Add points auto alert job

---
+++ app/jobs/points_auto_alert_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Alert clients to get more points
+class PointsAutoAlert < ApplicationJob
+  queue_as :default
+
+  def perform
+    Community.all.each do |c|
+      template_id = c.templates['points_auto_alert_template_id']
+      next unless template_id
+
+      c.users.where(user_type: ['prospective_client', 'client', 'visitor']).find_each do |u|
+        EmailMsg.send_mail(u.email, template_id, {})
+      end
+    end
+  end
+end
