24685d375e850f92471fc066a31dbb36147cf98a
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2020-09-25 09:02:28 +0000

    Fix Rubocop

--- spec/jobs/points_auto_alert_job_spec.rb
+++ spec/jobs/points_auto_alert_job_spec.rb
@@ -3,17 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe PointsAutoAlertJob, type: :job do
-  let!(:community) { create(:community, templates: { points_auto_alert_template_id: 'fgcagv5r2yr67' }) }
+  let!(:community) do
+    create(:community, templates: {
+             points_auto_alert_template_id: 'fgcagv5r2yr67',
+           })
+  end
   let!(:user) { create(:user, community: community, user_type: 'client') }
 
   describe '#perform' do
-    it "enqueues the job" do
+    it 'enqueues the job' do
       expect do
         PointsAutoAlertJob.perform_later
       end.to have_enqueued_job
     end
 
-    it "invokes EmailMsg" do
+    it 'invokes EmailMsg' do
       expect(EmailMsg).to receive(:send_mail).with(user.email, 'fgcagv5r2yr67')
       perform_enqueued_jobs { described_class.perform_later }
     end
