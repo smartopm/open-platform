c36fdcb1251a75ae24fd82d6624161dc00ead80d
Saurabh Shinde <saurabh@doublegdp.com>
2020-09-29 10:30:19 +0000

    Assign user id list from backend on campaign create through user screen

--- app/graphql/mutations/campaign/campaign_create_through_users.rb
+++ app/graphql/mutations/campaign/campaign_create_through_users.rb
@@ -6,15 +6,15 @@ module Mutations
     class CampaignCreateThroughUsers < BaseMutation
       include Helpers::Campaign
 
-      argument :filters, String, required: false
-      argument :user_id_list, String, required: true
+      argument :labels, String, required: false
+      argument :user_type, String, required: false
 
       field :campaign, Types::CampaignType, null: true
 
       def resolve(vals)
         campaign = campaign_object
-        campaign.name = campaign_name(vals[:filters])
-        campaign.user_id_list = vals[:user_id_list]
+        campaign.name = campaign_name(vals[:labels])
+        campaign.user_id_list = user_id_list(vals).pluck(:id).join(',')
         raise GraphQL::ExecutionError, campaign.errors.full_message unless campaign.save!
 
         { campaign: campaign }
@@ -29,8 +29,19 @@ module Mutations
         campaign
       end
 
-      def campaign_name(filters)
-        filters ? filters.tr(',', '_') : I18n.t('campaign.default_name')
+      def campaign_name(labels)
+        labels ? labels.tr(',', '_') : I18n.t('campaign.default_name')
+      end
+
+      def user_id_list(vals)
+        labels = vals[:labels]&.split(',')
+        user_types = vals[:user_type]&.split(',')
+        return ::User.all if labels.nil? && user_types.nil?
+        return ::User.joins(:labels).where('labels.short_desc IN (?)', labels) if user_types.nil?
+        return ::User.where('user_type IN (?)', user_types) if labels.nil?
+
+        ::User.joins(:labels).where('labels.short_desc IN (?) AND user_type IN (?)',
+          labels, user_types).pluck(:id).join(',')
       end
 
       def authorized?(_vals)
--- app/javascript/src/graphql/mutations.js
+++ app/javascript/src/graphql/mutations.js
@@ -620,8 +620,8 @@ export const NotificationPreference = gql`
 }
 `
 export const CampaignCreateThroughUsers = gql `
-  mutation campaignCreateThroughUsers($filters: String, $userIdList: String!){
-    campaignCreateThroughUsers(filters: $filters, userIdList: $userIdList){
+  mutation campaignCreateThroughUsers($labels: String, $userType: String){
+    campaignCreateThroughUsers(labels: $labels, userType: $userType){
       campaign{
         id
       }
