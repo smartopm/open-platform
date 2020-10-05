601619f76f69200c211c8277f209c301e744e251
OlivierJM <manolivier93@gmail.com>
2020-10-05 15:08:06 +0200

    change label

--- app/javascript/src/components/HomePage.jsx
+++ app/javascript/src/components/HomePage.jsx
@@ -7,6 +7,7 @@ import { useTranslation } from 'react-i18next'
 import PlaylistAddCheckIcon from '@material-ui/icons/PlaylistAddCheck'
 import HelpIcon from '@material-ui/icons/Help'
 import PersonIcon from '@material-ui/icons/Person'
+import LabelIcon from '@material-ui/icons/Label';
 import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty'
 import LogIcon from '@material-ui/icons/Assignment'
 import MessageIcon from '@material-ui/icons/Message'
@@ -245,7 +246,7 @@ export default function Homepage({ authState }) {
     {
       title: 'Labels',
       path: `/labels`,
-      icon: <HelpIcon fontSize="large" />,
+      icon: <LabelIcon fontSize="large" />,
       access: [
         'admin'
       ]
