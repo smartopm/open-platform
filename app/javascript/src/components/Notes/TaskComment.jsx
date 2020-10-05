1ad6eaa8850219f17794509efca23b347a7cd6f3
tolulope3 <tolulope@doublegdp.com>
2020-10-05 12:22:50 +0000

    fix eslint

--- app/javascript/src/components/Notes/TaskComment.jsx
+++ app/javascript/src/components/Notes/TaskComment.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
 import { useQuery } from 'react-apollo'
 import { useParams } from 'react-router-dom'
@@ -9,7 +9,6 @@ import PropTypes from 'prop-types'
 import ChatIcon from '@material-ui/icons/Chat';
 import CommentTextField from './CommentField'
 import { CommentQuery } from '../../graphql/queries'
-import Loading from "../Loading"
 import ErrorPage from "../Error"
 
 export default function TaskComment({ authState }) {
@@ -20,18 +19,32 @@ export default function TaskComment({ authState }) {
     errorPolicy: 'all'
   })
   const [open, setOpen] = useState(false)
-
-  // if (commentLoad) return <Loading />
+  
   if (error) return <ErrorPage />
   return (
     <>
       <div style={{ display: 'flex', marginBottom: "10px" }}>
         <Typography variant="caption" style={{ color: '#69ABA4', marginRight: "15px" }} gutterBottom>
-            {commentData?.task.noteComments.length} Comments
+          {commentData?.task.noteComments.length}
+          {' '}
+          Comments
         </Typography>
         <Typography variant="caption" style={{ cursor: 'pointer', color: '#69ABA4' }} gutterBottom>
-          {open ? (<div onClick={() => setOpen(false)}>| <span style={{ marginLeft: "10px" }}>Collapse Comments</span></div>) 
-            : (<div style={{ display: 'flex' }} onClick={() => setOpen(true)}><ChatIcon />  <span style={{ marginLeft: "5px" }}>Comment</span></div>)}
+          {open ? (
+            // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+            <div onClick={() => setOpen(false)}>
+              |
+              <span style={{ marginLeft: "10px" }}>Collapse Comments</span>
+            </div>
+) 
+            : (
+              // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+              <div style={{ display: 'flex' }} onClick={() => setOpen(true)}>
+                <ChatIcon />  
+                {' '}
+                <span style={{ marginLeft: "5px" }}>Comment</span>
+              </div>
+)}
         </Typography>
       </div>
       {open && <CommentTextField data={commentData} refetch={refetch} authState={authState} />}
@@ -43,5 +56,6 @@ TaskComment.defaultProps = {
   authState: {}
  }
  TaskComment.propTypes = {
+   // eslint-disable-next-line react/forbid-prop-types
    authState: PropTypes.object
  }
