e1649bcacbefaecccdb22fdbf7bf57678509e004
OlivierJM <olivier@doublegdp.com>
2020-10-07 10:36:30 +0000

    linking form from the list

--- app/javascript/src/components/Forms/FormList.jsx
+++ app/javascript/src/components/Forms/FormList.jsx
@@ -1,8 +1,18 @@
-/* eslint-disable */
-import React, { useContext } from 'react'
-import { Button, List } from '@material-ui/core'
-import CenteredContent from '../CenteredContent'
+import React, { Fragment } from 'react'
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  Divider,
+  Typography,
+  Box
+} from '@material-ui/core'
+import AssignmentIcon from '@material-ui/icons/Assignment'
 import { useQuery } from 'react-apollo'
+import { useHistory } from 'react-router'
+import CenteredContent from '../CenteredContent'
 import FormLinks from './FormLinks'
 import { FormsQuery } from '../../graphql/queries'
 import Loading from '../Loading'
@@ -10,17 +20,47 @@ import ErrorPage from '../Error'
 
 export default function FormLinkList() {
   const { data, error, loading } = useQuery(FormsQuery)
+  const history = useHistory()
 
   if (loading) return <Loading />
   if (error) return <ErrorPage />
-  
+ 
   return (
     <div>
-      <List
-        data-testid="forms-link-holder"
-        style={{ cursor: 'pointer' }}
-      >
+      <List data-testid="forms-link-holder" style={{ cursor: 'pointer' }}>
         <FormLinks />
+        {data.forms.map(form => (
+          <Fragment key={form.id}>
+            <Divider variant="middle" />
+            <ListItem
+              key={form.id}
+              data-testid="forms-link-building-permit"
+              onClick={() => history.push(`/form/${form.id}`)}
+            >
+              <ListItemAvatar data-testid="forms-link-building-icon">
+                <Avatar>
+                  <AssignmentIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <Box
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                  marginLeft: 30
+                }}
+              >
+                <Typography
+                  variant="subtitle1"
+                  data-testid="forms-building-permit"
+                >
+                  {form.name}
+                </Typography>
+              </Box>
+            </ListItem>
+          </Fragment>
+        ))}
         <CenteredContent>
           <Button color="primary">Create a Form</Button>
         </CenteredContent>
@@ -28,4 +68,3 @@ export default function FormLinkList() {
     </div>
   )
 }
-
