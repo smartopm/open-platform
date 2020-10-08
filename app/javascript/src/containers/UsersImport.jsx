fe047de7dd297ccea32ce0fe60371765d75208a7
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2020-10-08 06:51:59 +0000

    Render error message

--- app/javascript/src/containers/UsersImport.jsx
+++ app/javascript/src/containers/UsersImport.jsx
@@ -7,20 +7,30 @@ import { Button, Grid } from '@material-ui/core'
 import Nav from '../components/Nav'
 import { ImportCreate } from '../graphql/mutations'
 import CenteredContent from '../components/CenteredContent'
+import Loading from '../components/Loading'
+import { sanitizeText } from '../utils/helpers'
 
 export default function UsersImport() {
   const [importCreate] = useMutation(ImportCreate)
   const [csvString, setCsvString] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const history = useHistory()
 
   function createImport() {
+    setIsLoading(true)
     importCreate({
       variables: { csvString }
     })
       .then(res => {
-        console.log(res)
+        console.log(JSON.parse(res.data.usersImport.message))
+        formatErrorMessage(JSON.parse(res.data.usersImport.message))
+        setIsLoading(false)
       })
-      .catch(err => console.log(err.message))
+      .catch((err) => {
+        setIsLoading(false)
+        console.log(err.message)
+      })
   }
 
   function onCancel() {
@@ -41,41 +51,67 @@ export default function UsersImport() {
     reader.readAsText(file)
   }
 
+  function formatErrorMessage(messageObject) {
+    let message = 'The following error(s) occurred, fix and try again: <br><br>'
+    Object.keys(messageObject).forEach((row) => {
+      message += `Row ${row}: <br>`
+      messageObject[row].forEach(err => {
+        message += `&nbsp; ${err} <br>`
+      })
+    })
+    setErrorMessage(message)
+  }
+
   return (
     <>
       <Nav navName="Bulk Import" menuButton="back" backTo="/users" />
-      <Grid container justify="center" style={{ marginTop: '200px' }}>
-        <input
-          accept=".csv"
-          className={css(styles.inputField)}
-          id="contained-button-file"
-          type="file"
-          onChange={processCsv}
-        />
-      </Grid>
-      <br />
-      {csvString.length && (
-        <CenteredContent>
-          <Button
-            variant="contained"
-            aria-label="business_cancel"
-            color="secondary"
-            className={css(styles.cancelBtn)}
-            onClick={onCancel}
-          >
-            Cancel
-          </Button>
-          <Button
-            variant="contained"
-            type="submit"
-            aria-label="business_submit"
-            color="primary"
-            onClick={createImport}
-            className={css(styles.importBtn)}
-          >
-            Import
-          </Button>
-        </CenteredContent>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          {errorMessage && (
+            <div className={css(styles.errorContainer)}>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: sanitizeText(errorMessage)
+                }}
+              />
+            </div>
+          )}
+          <Grid container justify="center" style={{ marginTop: '200px' }}>
+            <input
+              accept=".csv"
+              className={css(styles.inputField)}
+              id="contained-button-file"
+              type="file"
+              onChange={processCsv}
+            />
+          </Grid>
+          <br />
+          {csvString.length > 0 && !errorMessage && (
+            <CenteredContent>
+              <Button
+                variant="contained"
+                aria-label="business_cancel"
+                color="secondary"
+                className={css(styles.cancelBtn)}
+                onClick={onCancel}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                type="submit"
+                aria-label="business_submit"
+                color="primary"
+                onClick={createImport}
+                className={css(styles.importBtn)}
+              >
+                Import
+              </Button>
+            </CenteredContent>
+          )}
+        </div>
       )}
     </>
   )
@@ -96,6 +132,15 @@ const styles = StyleSheet.create({
   },
 
   inputField: {
-    width: '201px'
+    width: '201px',
+    overflow: 'hidden'
+  },
+
+  errorContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    maxHeight: '300px',
+    overflowY: 'scroll',
+    margin: '5vh 0 -20vh 0'
   }
 })
