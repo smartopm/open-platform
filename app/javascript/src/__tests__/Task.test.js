8717d935b41ed90d9c810b57fa1795bd099317f0
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2020-10-14 08:18:33 +0000

    Add frontend tests

--- app/javascript/src/__tests__/Task.test.js
+++ app/javascript/src/__tests__/Task.test.js
@@ -1,9 +1,10 @@
-/* eslint-disable */
 import React from 'react'
-import Task from '../components/Notes/Task'
+import { ApolloProvider } from 'react-apollo'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { BrowserRouter } from 'react-router-dom/'
+import { createClient } from '../utils/apollo'
+import Task from '../components/Notes/Task'
 
 // find elements that are available when DOM mounts then check the visual contents
 describe('Task Component', () => {
@@ -35,19 +36,21 @@ describe('Task Component', () => {
     }
     const container = render(
       <BrowserRouter>
-        <Task {...props} />
+        <ApolloProvider client={createClient}>
+          <Task {...props} />
+        </ApolloProvider>
       </BrowserRouter>
     )
-    
+
     expect(container.queryByText('Note example')).toBeInTheDocument()
     expect(container.queryByText('Johnsc')).toBeInTheDocument()
     expect(container.queryByText('somebody')).toBeInTheDocument()
     expect(container.queryByText(/never/i)).toBeInTheDocument()
-    const btn = container.queryByText(/Mark as complete/i)
-    expect(btn).toBeInTheDocument()
-    expect(btn).not.toBeDisabled()
-    
-    fireEvent.click(btn)
+    const completeBtn = container.queryByText(/Mark as complete/i)
+    expect(completeBtn).toBeInTheDocument()
+    expect(completeBtn).not.toBeDisabled()
+
+    fireEvent.click(completeBtn)
     expect(mck).toHaveBeenCalled()
     const assigneeBtn = container.queryByText(/Add Assignee/i)
     expect(assigneeBtn).toBeInTheDocument()
@@ -55,5 +58,10 @@ describe('Task Component', () => {
     expect(container.queryByText(/Add Assignee/i)).not.toBeInTheDocument()
     expect(container.queryByText(/created this note/i)).toBeInTheDocument()
     expect(container.queryByText(/Tester/i)).toBeInTheDocument()
+
+    const reminderBtn = container.queryByText(/Remind me later/i)
+    expect(reminderBtn).toBeInTheDocument()
+    fireEvent.click(reminderBtn)
+    expect(mck).toHaveBeenCalled()
   })
 })
