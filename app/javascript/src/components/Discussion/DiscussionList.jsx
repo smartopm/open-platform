ca2e8cf50823de029e98e52da3e843ef30b3f96b
OlivierJM <manolivier93@gmail.com>
2020-10-16 10:58:03 +0200

    add delete icon and prevent bubling events

--- app/javascript/src/components/Discussion/DiscussionList.jsx
+++ app/javascript/src/components/Discussion/DiscussionList.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable */
 import React from 'react'
-import { List, ListItem, Divider, ListItemText, Typography, ListItemAvatar } from '@material-ui/core';
+import { List, ListItem, Divider, ListItemText, Typography, ListItemAvatar, IconButton } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import DeleteIcon from '@material-ui/icons/Delete'
 import { truncateString } from '../../utils/helpers';
 import Avatar from '../Avatar';
 import { css, StyleSheet } from 'aphrodite';
 
 
 export default function DiscussionList({ data }) {
+    function deleteDiscussion(e, id){
+        e.stopPropagation();
+        e.preventDefault()
+        // call the delete mutation right here
+        console.log(id)
+    }
     return (
         <div className={css(styles.discussionList)}>
             <List >
                 {data.length ? data.map(discussion => (
-                    <Link key={discussion.id} to={`/discussions/${discussion.id}`} className={`card-link`}>
+                    <Link key={discussion.id} /* to={`/discussions/${discussion.id}`} */ onClick={() => console.log('clicked ')} className={`card-link`}>
                         <ListItem alignItems="flex-start">
                             <ListItemAvatar style={{ marginRight: 20 }}>
                                 <Avatar user={discussion.user} />
@@ -33,6 +40,11 @@ export default function DiscussionList({ data }) {
                                             {discussion.user.name}
                                         </Typography>
                                         { discussion.description ? ` — ${truncateString(discussion.description, 100)}` : ''}
+                                        <span style={{ float: 'right' }}>
+                                        <IconButton onClick={event => deleteDiscussion(event, discussion.id)} edge="end" aria-label="delete" className={css(styles.deleteBtn)}>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                        </span>
                                     </React.Fragment>
                                 }
                             />
