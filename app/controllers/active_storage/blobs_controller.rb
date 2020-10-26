0384737685fbdecd3924014bb43d40ec64fcf66b
OlivierJM <manolivier93@gmail.com>
2020-10-26 15:11:43 +0200

    verify attachment ownership before displaying to the user

--- app/controllers/active_storage/blobs_controller.rb
+++ app/controllers/active_storage/blobs_controller.rb
@@ -6,8 +6,8 @@ class ActiveStorage::BlobsController < ActiveStorage::BaseController
   include Authorizable
 
   def show
-    if check_auth
-      redirect_to @blob.service_url(disposition: params[:disposition])
+    if auth_user.present?
+      redirect_to @blob.service_url(disposition: params[:disposition]) if owner_verified?
     else
       redirect_to '/'
     end
@@ -15,8 +15,14 @@ class ActiveStorage::BlobsController < ActiveStorage::BaseController
 
   private
 
-  def check_auth
+  def auth_user
     auth = auth_context(request)
-    auth[:current_user].present?
+    auth[:current_user]
+  end
+
+  def owner_verified?
+    record_id = ActiveStorage::Attachment.find_by(blob_id: @blob.id).record_id
+    user_id = UserFormProperty.find_by(id: record_id).user_id
+    auth_user.admin? || user_id.eql?(auth_user.id)
   end
 end
