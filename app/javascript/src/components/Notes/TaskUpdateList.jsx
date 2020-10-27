03a90a45c07dda7bd0096fadc121ca91c1a79156
tolulope3 <tolulope@doublegdp.com>
2020-10-27 17:08:38 +0000

    fix update

--- app/javascript/src/components/Notes/TaskUpdateList.jsx
+++ app/javascript/src/components/Notes/TaskUpdateList.jsx
@@ -17,64 +17,65 @@ export default function TaskUpdateList({ data }) {
 
   return(
     <>
-      {!data || data.length === 0 && (
+      {!data || data.length === 0 ? (
         <div>
           <p>No update available</p>
         </div>
+      ) : (
+        data.map(history => (
+          <div key={history.id}>
+            {history.action === 'create' && history.noteEntityType === 'NoteComment' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<AddBoxOutlinedIcon className={classes.icon} />} 
+                content='added a new comment'
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'description' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<DescriptionOutlinedIcon className={classes.icon} />} 
+                content={`change description from ${history.initialValue || 'empty value'} to ${history.updatedValue || 'empty value'}`}
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'due_date' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<EventNoteOutlinedIcon className={classes.icon} />} 
+                content={`change due date from ${dateToString(history.initialValue) || 'empty value'} to ${dateToString(history.updatedValue) || 'empty value'}`}
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'completed' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<AssignmentTurnedInOutlinedIcon className={classes.icon} />} 
+                content={`change complete status from ${history.initialValue === 't' ? 'true' : 'false' || 'empty value'} to ${history.updatedValue === 't' ? 'true' : 'false' || 'empty value'}`}
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'body' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<CreateOutlinedIcon className={classes.icon} />} 
+                content='updated the body of the task'
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'category' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<ClassOutlinedIcon className={classes.icon} />} 
+                content={`change category from ${history.initialValue || 'empty value'} to ${history.updatedValue || 'empty value'}`}
+              />
+            )}
+            {history.action === 'update' && history.attrChanged === 'flaged' && (
+              <TaskUpdateItem
+                user={history.user.name} 
+                icon={<FlagOutlinedIcon className={classes.icon} />} 
+                content={`change flagged status from ${history.initialValue === 't' ? 'true' : 'false' || 'empty value'} to ${history.updatedValue === 't' ? 'true' : 'false' || 'empty value'}`}
+              />
+            )}
+          </div>
+        ))
       )}
-      {data?.map(history => (
-        <div key={history.id}>
-          {history.action === 'create' && history.noteEntityType === 'NoteComment' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<AddBoxOutlinedIcon className={classes.icon} />} 
-              content='added a new comment'
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'description' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<DescriptionOutlinedIcon className={classes.icon} />} 
-              content={`change description from ${history.initialValue || 'empty value'} to ${history.updatedValue || 'empty value'}`}
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'due_date' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<EventNoteOutlinedIcon className={classes.icon} />} 
-              content={`change due date from ${dateToString(history.initialValue) || 'empty value'} to ${dateToString(history.updatedValue) || 'empty value'}`}
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'completed' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<AssignmentTurnedInOutlinedIcon className={classes.icon} />} 
-              content={`change complete status from ${history.initialValue === 't' ? 'true' : 'false' || 'empty value'} to ${history.updatedValue === 't' ? 'true' : 'false' || 'empty value'}`}
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'body' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<CreateOutlinedIcon className={classes.icon} />} 
-              content='updated the body of the task'
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'category' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<ClassOutlinedIcon className={classes.icon} />} 
-              content={`change category from ${history.initialValue || 'empty value'} to ${history.updatedValue || 'empty value'}`}
-            />
-          )}
-          {history.action === 'update' && history.attrChanged === 'flaged' && (
-            <TaskUpdateItem
-              user={history.user.name} 
-              icon={<FlagOutlinedIcon className={classes.icon} />} 
-              content={`change flagged status from ${history.initialValue === 't' ? 'true' : 'false' || 'empty value'} to ${history.updatedValue === 't' ? 'true' : 'false' || 'empty value'}`}
-            />
-          )}
-        </div>
-      ))}
     </>
   )
 }
