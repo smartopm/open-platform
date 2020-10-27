951ea8e6e982fddc2edcc932770fe93fa52bed33
Tolu Olaniyan <toluola7@gmail.com>
2020-10-27 16:27:32 +0100

    add tests

--- app/javascript/src/__tests__/TaskUpdateItem.jsx
+++ app/javascript/src/__tests__/TaskUpdateItem.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom/'
 import { MockedProvider } from '@apollo/react-testing'
-import TaskUpdateList from '../components/Notes/TaskUpdateList'
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
+import TaskUpdateItem from '../components/Notes/TaskUpdateItem'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Comment Card Component', () => {
-  const data = [
-    {
-      id: 'jkfer',
-      action: 'ckjer'
-    }
-  ]
+  const user = 'tolulope'
+  const content = 'content'
   
 
   it('render without error', () => {
     render(
       <MockedProvider>
         <BrowserRouter>
-          <TaskUpdateList
-            data={data}
+          <TaskUpdateItem
+            user={user}
+            content={content}
+            icon={<AddBoxOutlinedIcon />}
           />
         </BrowserRouter>
       </MockedProvider>
