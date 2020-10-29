5df5825529e4e66e017fec7814b2d6792f842549
OlivierJM <manolivier93@gmail.com>
2020-10-29 16:09:07 +0200

    refactor the user form to user model

--- app/graphql/mutations/form/form_user_update.rb
+++ app/graphql/mutations/form/form_user_update.rb
@@ -11,18 +11,10 @@ module Mutations
       field :form_user, Types::FormUsersType, null: true
 
       def resolve(vals)
-        form_user = current_user_form(vals[:form_id], vals[:user_id])
+        form_user = context[:current_user].user_form(vals[:form_id], vals[:user_id])
         return add_user_form_properties(form_user, vals) if form_user.present?
 
         raise GraphQL::ExecutionError, 'Record not found'
-      end
-
-      def current_user_form(form_id, user_id)
-        if context[:current_user].admin?
-          context[:site_community].forms.find(form_id).form_users.find_by(user_id: user_id)
-        else
-          context[:current_user].form_users.find_by(form_id: form_id)
-        end
       end
 
       def add_user_form_properties(form_user, vals)
