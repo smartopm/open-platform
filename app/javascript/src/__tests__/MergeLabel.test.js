eb10950738042dcef451f83516e32b6df1df9b88
Tolu Olaniyan <toluola7@gmail.com>
2020-11-03 13:40:23 +0100

    add more tests

--- app/javascript/src/__tests__/MergeLabel.test.js
+++ app/javascript/src/__tests__/MergeLabel.test.js
@@ -7,7 +7,7 @@ import '@testing-library/jest-dom/extend-expect'
 
 describe('Merge Label Component', () => {
   const handleClose = jest.fn
-  const open = jest.fn
+  const open = true
   const mergeData = {
     id: 'jwhekw'
   }
@@ -24,5 +24,22 @@ describe('Merge Label Component', () => {
         </BrowserRouter>
       </MockedProvider>
     )
+  })
+
+  it('test if modal is rendered', () => {
+    const container = render(
+      <MockedProvider>
+        <BrowserRouter>
+          <MergeLabel
+            mergeData={mergeData}
+            open={open}
+            handleClose={handleClose}
+          />
+        </BrowserRouter>
+      </MockedProvider>
+    )
+
+    expect(container.queryByText('Merging this label will move all users from this label into the selected label')).toBeInTheDocument()
+    expect(container.queryByText('Merge this label into:')).toBeInTheDocument()
   })
 })
