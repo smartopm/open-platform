8d7ba6c602d4d09b949dc051ef5aa3ac1369d395
tolulope3 <tolulope@doublegdp.com>
2020-11-13 13:35:24 +0000

    format comment component

--- app/javascript/src/containers/Comments/CommentPage.jsx
+++ app/javascript/src/containers/Comments/CommentPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Nav from '../../components/Nav'
-import { CommentsPostQuery } from '../../graphql/queries'
-import Loading from '../../components/Loading'
-import ErrorPage from '../../components/Error'
 import Comments from '../../components/Comments/Comments'
 
 export default function DiscussonPage() {
-
-    return (
-      <div>
-        <Nav navName="Comments" menuButton="back" backTo="/" />
-        <Comments />
-      </div>
-    )
+  return (
+    <div>
+      <Nav navName="Comments" menuButton="back" backTo="/" />
+      <Comments />
+    </div>
+  )
 }
