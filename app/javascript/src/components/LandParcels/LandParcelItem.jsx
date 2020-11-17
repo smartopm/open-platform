277eb401b75f6870ab0ae6dbef094e1041892cf5
tolulope3 <tolulope@doublegdp.com>
2020-11-17 20:50:35 +0000

    add proptype

--- app/javascript/src/components/LandParcels/LandParcelItem.jsx
+++ app/javascript/src/components/LandParcels/LandParcelItem.jsx
@@ -13,9 +13,9 @@ export default function ParcelItem({ parcel }) {
   const classes = useStyles();
 
   return (
-    <ListItem key={parcel.id} className={classes.labelItem}>
+    <ListItem key={parcel.id} className={classes.parcelItem}>
       <Grid container spacing={3}>
-        <Grid item xs={2} className={classes.labelGrid}>
+        <Grid item xs={2} className={classes.parcelGrid}>
           <Typography 
             variant="subtitle1" 
             data-testid="label-title" 
@@ -23,37 +23,37 @@ export default function ParcelItem({ parcel }) {
             {parcel.parcelNumber}
           </Typography>
         </Grid>
-        <Grid item xs={2} className={classes.labelGrid}>
+        <Grid item xs={2} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-users">
             {parcel.address1}
           </Typography>
         </Grid>
-        <Grid item xs={2} className={classes.labelGrid}>
+        <Grid item xs={2} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.address2}
           </Typography>
         </Grid>
-        <Grid item xs={2} className={classes.labelGrid}>
+        <Grid item xs={2} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.city}
           </Typography>
         </Grid>
-        <Grid item xs={1} className={classes.labelGrid}>
+        <Grid item xs={1} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.postalCode}
           </Typography>
         </Grid>
-        <Grid item xs={1} className={classes.labelGrid}>
+        <Grid item xs={1} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.stateProvince}
           </Typography>
         </Grid>
-        <Grid item xs={1} className={classes.labelGrid}>
+        <Grid item xs={1} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.country}
           </Typography>
         </Grid>
-        <Grid item xs={1} className={classes.labelGrid}>
+        <Grid item xs={1} className={classes.parcelGrid}>
           <Typography variant="subtitle1" data-testid="label-description">
             {parcel.parcelType}
           </Typography>
@@ -63,26 +63,30 @@ export default function ParcelItem({ parcel }) {
   )
 }
 
-// LabelItem.propTypes = {
-//     label: PropTypes.shape({
-//         id: PropTypes.string,
-//         shortDesc: PropTypes.string,
-//         color: PropTypes.string,
-//         description: PropTypes.string,
-//         userCount: PropTypes.number
-//     }).isRequired,
-//     userType: PropTypes.string.isRequired,
-//     refetch: PropTypes.func.isRequired,
-// }
+ParcelItem.propTypes = {
+    parcel: PropTypes.shape({
+        id: PropTypes.string,
+        parcelNumber: PropTypes.string,
+        address1: PropTypes.string,
+        address2: PropTypes.string,
+        city: PropTypes.string,
+        postalCode: PropTypes.string,
+        stateProvince: PropTypes.string,
+        country: PropTypes.string,
+        parcelType: PropTypes.string
+    }).isRequired,
+    userType: PropTypes.string.isRequired,
+    refetch: PropTypes.func.isRequired,
+}
 
 const useStyles = makeStyles(() => ({
-  labelItem: {
+  parcelItem: {
       borderBottomStyle: 'solid',
       borderBottomColor: '#F6F6F6',
       borderBottom: 10,
       backgroundColor: '#FFFFFF'
   },
-  labelGrid: {
+  parcelGrid: {
     marginTop: '8px'
   }
 }));
