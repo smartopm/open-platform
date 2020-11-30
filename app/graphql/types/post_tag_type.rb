8ff89a21227ef3252403ef7db85086afbdca499b
OlivierJM <manolivier93@gmail.com>
2020-11-30 21:43:45 +0200

    update post_tag type

--- app/graphql/types/post_tag_type.rb
+++ app/graphql/types/post_tag_type.rb
@@ -5,7 +5,8 @@ module Types
   class PostTagType < Types::BaseObject
     field :id, ID, null: false
     field :community_id, ID, null: false
-    field :title, String, null: false
+    field :name, String, null: false
+    field :slug, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
