6a75dbcbde5df4a3964be96ea468e744390ed364
OlivierJM <manolivier93@gmail.com>
2020-12-01 14:33:46 +0200

    add specs for models

---
+++ spec/models/post_tag_users_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostTagUser, type: :model do
+  describe 'schema' do
+    it { is_expected.to have_db_column(:user_id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:post_tag_id).of_type(:uuid) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:post_tag) }
+  end
+end
