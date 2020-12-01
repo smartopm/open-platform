1ad2e7123816fdae9a6db6a20ce880af5d50a777
Saurabh Shinde <saurabh@doublegdp.com>
2020-12-01 09:36:48 +0000

    Replaced user id with message id in link string

--- app/models/message.rb
+++ app/models/message.rb
@@ -44,7 +44,7 @@ class Message < ApplicationRecord
 
     new_message = ''
     text = 'Click this link to reply to this message in our app '
-    link = "https://#{HostEnv.base_url(user.community)}/message/#{user_id}"
+    link = "https://#{HostEnv.base_url(user.community)}/message/#{id}"
     new_message = "#{sender[:name]} from #{user.community.name} said: \n" if add_prefix
     new_message += message
     new_message += "\n\n#{text} \n#{link}" if include_reply_link?
