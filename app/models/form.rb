b07ae7eac04b89034ba10e5879fbad952c7999c8
OlivierJM <olivier@doublegdp.com>
2020-12-01 11:51:14 +0000

    add a deleted status to form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -12,5 +12,5 @@ class Form < ApplicationRecord
   scope :by_user_type, lambda { |user|
     where('expires_at > ?', Time.zone.now).where(status: 1) if user.user_type != 'admin'
   }
-  enum status: { draft: 0, published: 1 }
+  enum status: { draft: 0, published: 1, deleted: 2 }
 end
