62a4bfac5ccaea8c961ccf3d957ee8292c6a807f
OlivierJM <manolivier93@gmail.com>
2020-12-01 22:42:27 +0200

    add a default scope for forms

--- app/models/form.rb
+++ app/models/form.rb
@@ -9,6 +9,9 @@ class Form < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :expires_at, presence: true
+
+  default_scope { where('status != ?', 2) }
+
   scope :by_user_type, lambda { |user|
     where('expires_at > ?', Time.zone.now).where(status: 1) if user.user_type != 'admin'
   }
