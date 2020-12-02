7d5caf9b68c0c78d95ae4cc567be5cef5c2d07c7
OlivierJM <manolivier93@gmail.com>
2020-12-02 16:17:22 +0200

    add post tag spec

--- spec/jobs/post_tags_alert_job_spec.rb
+++ spec/jobs/post_tags_alert_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PostTagsAlertJob, type: :job do
   let!(:user) { create(:user_with_community) }
 
   describe '#perform_later get community tags' do
-    it 'should enqueue a job check list of all community tags' do
+    it 'should enqueue a job to send email to users if there is a new post' do
       community = FactoryBot.create(:community)
       ActiveJob::Base.queue_adapter = :test
       expect do
@@ -14,8 +14,9 @@ RSpec.describe PostTagsAlertJob, type: :job do
       end.to have_enqueued_job
     end
     it 'does not invoke EmailMsg when no user follows any tag' do
+      community = FactoryBot.create(:community)
       expect(EmailMsg).not_to receive(:send_mail)
-      perform_enqueued_jobs { described_class.perform_later }
+      perform_enqueued_jobs { described_class.perform_later(community.name) }
     end
   end
 end
