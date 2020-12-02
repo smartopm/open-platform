a69ef83bc47016883e0d83a7ae7c6844b96b2a62
OlivierJM <manolivier93@gmail.com>
2020-12-02 15:41:49 +0200

    fix rubocop

--- spec/jobs/community_post_tags_job_spec.rb
+++ spec/jobs/community_post_tags_job_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CommunityPostTagsJob, type: :job do
   let!(:user) { create(:user_with_community) }
 
   describe '#perform_later get community tags' do
-    it "should enqueue a job check list of all community tags" do
+    it 'should enqueue a job check list of all community tags' do
       community = FactoryBot.create(:community)
       ActiveJob::Base.queue_adapter = :test
       expect do
--- spec/jobs/post_tags_alert_job.rb
+++ spec/jobs/post_tags_alert_job.rb
@@ -6,7 +6,7 @@ RSpec.describe PostTagsAlertJob, type: :job do
   let!(:user) { create(:user_with_community) }
 
   describe '#perform_later get community tags' do
-    it "should enqueue a job check list of all community tags" do
+    it 'should enqueue a job check list of all community tags' do
       community = FactoryBot.create(:community)
       ActiveJob::Base.queue_adapter = :test
       expect do
@@ -14,8 +14,8 @@ RSpec.describe PostTagsAlertJob, type: :job do
       end.to have_enqueued_job
     end
     it 'does not invoke EmailMsg when no user follows any tag' do
-        expect(EmailMsg).not_to receive(:send_mail)
-        perform_enqueued_jobs { described_class.perform_later }
-      end
+      expect(EmailMsg).not_to receive(:send_mail)
+      perform_enqueued_jobs { described_class.perform_later }
+    end
   end
 end
