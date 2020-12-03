b5acfbef97d34fc9e3d716601fbd3fbdfdc46031
OlivierJM <manolivier93@gmail.com>
2020-12-03 22:39:15 +0200

    fix issue with adding multiple numbers

--- app/javascript/src/components/Community/CommunitySettingsPage.jsx
+++ app/javascript/src/components/Community/CommunitySettingsPage.jsx
@@ -1,136 +1,136 @@
 /* eslint-disable react/forbid-prop-types */
 import React, { useState } from 'react'
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Divider from '@material-ui/core/Divider'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
+import { makeStyles } from '@material-ui/core/styles'
+// import Avatar from '@material-ui/core/Avatar'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
 // import PropTypes from 'prop-types'
-import { Container, IconButton } from '@material-ui/core';
-import { DeleteOutline } from '@material-ui/icons';
+import { Container, IconButton } from '@material-ui/core'
+import { DeleteOutline } from '@material-ui/icons'
 
-export default function CommunitySettingsPage(){
- 
-  const data = {
-    id: 'eujgt',
-    logoUrl: 'url',
-    supportEmail: [
-      {
-        sales: 'toluola@gmail.com',
-        customer_care: 'tolu@gmail.com'
-      }
-    ],
-    supportNumber: [
-      {
-        sales: '47954',
-        customer_care: '12345'
-      }
-    ]
+export default function CommunitySettingsPage() {
+  const numbers = {
+    phone_number: '',
+    category: ''
   }
 
-  const [numberOptions, setNumberOptions] = useState([""])
-  const [emailOptions, setEmailOptions] = useState(data.supportEmail)
+  const [numberOptions, setNumberOptions] = useState([numbers])
+  const [emailOptions, setEmailOptions] = useState([])
   const classes = useStyles()
-    
-  function handleAddNumberOption(){
-    setNumberOptions([...numberOptions, ""])
+
+  function handleAddNumberOption() {
+    setNumberOptions([...numberOptions, numbers])
   }
 
-  function handleNumberRemove(id){
+  function updateItem(index, newValue) {
+      setNumberOptions([
+        ...numberOptions.slice(0, index),
+        { ...numberOptions[index], ...newValue},
+        ...numberOptions.slice(index + 1)
+      ])
+  }
+  function handleNumberRemove(id) {
     const values = numberOptions
-    // radio buttons should have at least one choice 
+    // radio buttons should have at least one choice
     if (values.length !== 1) {
       values.splice(id, 1)
     }
     setNumberOptions([...values])
   }
 
-  function handleAddEmailOption(){
-    setEmailOptions([...emailOptions, ""])
+  function handleAddEmailOption() {
+    setEmailOptions([...emailOptions, ''])
   }
 
-  function handleNumberChange(event, index){
-    const values = numberOptions
-    values[index] = event.target.value
-    setNumberOptions(values)
-}
-
+  function handleNumberChange(event, index) {
+    updateItem(index, { [event.target.name]: event.target.value })
+  }
   console.log(numberOptions)
-  // console.log(emailOptions)
   return (
     <Container>
-      <Typography variant='h6'>Community Logo</Typography>
-      <Typography variant='caption'>You can change your community logo here</Typography>
+      <Typography variant="h6">Community Logo</Typography>
+      <Typography variant="caption">
+        You can change your community logo here
+      </Typography>
       <div className={classes.avatar}>
-        <Avatar alt="avatar-image" src={data.logoUrl} style={{height: '70px', width: '70px'}} />
+        {/* <Avatar
+          alt="avatar-image"
+          src={data.logoUrl}
+          style={{ height: '70px', width: '70px' }}
+        /> */}
         <div className={classes.upload}>
-          <Typography variant='caption' style={{fontWeight: 'bold', marginLeft: '10px'}}>Upload new logo</Typography>
+          <Typography
+            variant="caption"
+            style={{ fontWeight: 'bold', marginLeft: '10px' }}
+          >
+            Upload new logo
+          </Typography>
           <div>
-            <Button
-              variant="contained"
-              component="label"
-            >
+            <Button variant="contained" component="label">
               Choose File
-              <input
-                type="file"
-                hidden
-              />
+              <input type="file" hidden />
             </Button>
           </div>
         </div>
-        <Divider style={{color: 'blue'}} />
+        <Divider style={{ color: 'blue' }} />
       </div>
-      <div className={classes.information} style={{marginTop: '40px'}}>
-        <Typography variant='h6'>Support Contact Information</Typography>
-        <Typography variant='caption'>Make changes to your contact information here.</Typography>
-        {
-          numberOptions.map((val, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div className={classes.textField} key={i}>
-              <TextField 
-                id="input" 
-                label="Phone Number" 
-                onClick={handleNumberChange} 
-                defaultValue=""
-                type="number"
-              />
-              <TextField
-                id="select-category"
-                style={{width: '200px', marginLeft: '40px'}}
-                select
-                label="Select Category"
-                defaultValue="sales"
-              >
-                <MenuItem value="sales">Sales</MenuItem>
-                <MenuItem value="customer_care">Customer Care</MenuItem>
-              </TextField>
-              <IconButton
-                style={{ marginTop: 13 }}
-                onClick={() => handleNumberRemove(i)}
-                aria-label="remove"
-              >
-                <DeleteOutline />
-              </IconButton>
-            </div>
-          ))
-        }
-        <div 
-          className={classes.addIcon} 
-          role="button" 
+      <div className={classes.information} style={{ marginTop: '40px' }}>
+        <Typography variant="h6">Support Contact Information</Typography>
+        <Typography variant="caption">
+          Make changes to your contact information here.
+        </Typography>
+        {numberOptions.map((val, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div className={classes.textField} key={i}>
+            <TextField
+              id="input"
+              label="Phone Number"
+              onChange={event => handleNumberChange(event, i)}
+              value={numberOptions[i].phone_number}
+              type="number"
+              name="phone_number"
+            />
+            <TextField
+              id="select-category"
+              style={{ width: '200px', marginLeft: '40px' }}
+              select
+              label="Select Category"
+              value={val.category}
+              onChange={event => handleNumberChange(event, i)}
+              name="category"
+            >
+              <MenuItem value="sales">Sales</MenuItem>
+              <MenuItem value="customer_care">Customer Care</MenuItem>
+            </TextField>
+            <IconButton
+              style={{ marginTop: 13 }}
+              onClick={() => handleNumberRemove(i)}
+              aria-label="remove"
+            >
+              <DeleteOutline />
+            </IconButton>
+          </div>
+        ))}
+        <div
+          className={classes.addIcon}
+          role="button"
           onClick={handleAddNumberOption}
-          onKeyDown={handleAddNumberOption}
+          onKeyDown={() => {}}
           tabIndex={0}
         >
           <AddCircleOutlineIcon />
-          <div style={{marginLeft: '10px', color: 'secondary'}}>
-            <Typography align='center' variant='caption'>Add New Phone Number</Typography>
+          <div style={{ marginLeft: '10px', color: 'secondary' }}>
+            <Typography align="center" variant="caption">
+              Add New Phone Number
+            </Typography>
           </div>
         </div>
       </div>
-      <div className={classes.information} style={{marginTop: '40px'}}>
+      <div className={classes.information} style={{ marginTop: '40px' }}>
         {/* {
           Object.entries(data.supportEmail).map(([key, val]) => (
             <div className={classes.textField} key={val}>
@@ -149,20 +149,24 @@ export default function CommunitySettingsPage(){
             </div>
           ))
         } */}
-        <div 
+        <div
           className={classes.addIcon}
-          role="button" 
+          role="button"
           onClick={handleAddEmailOption}
           onKeyDown={handleAddEmailOption}
           tabIndex={0}
         >
           <AddCircleOutlineIcon />
-          <div style={{marginLeft: '10px', color: 'secondary'}}>
-            <Typography align='center' variant='caption'>Add New Email Address</Typography>
+          <div style={{ marginLeft: '10px', color: 'secondary' }}>
+            <Typography align="center" variant="caption">
+              Add New Email Address
+            </Typography>
           </div>
         </div>
         <div className={classes.button}>
-          <Button variant='contained' color='primary'>UPDATE COMMUNITY SETTINGS</Button>
+          <Button disableElevation variant="contained" color="primary">
+            UPDATE COMMUNITY SETTINGS
+          </Button>
         </div>
       </div>
     </Container>
@@ -197,7 +201,7 @@ const useStyles = makeStyles({
   button: {
     marginTop: '15px'
   }
-});
+})
 
 // CommunityPage.propTypes = {
 //     data: PropTypes.shape({
