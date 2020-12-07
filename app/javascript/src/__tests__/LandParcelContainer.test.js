f12157a3f06c9b60399dedc17a65c8bf7a3ff939
Tolu Olaniyan <toluola7@gmail.com>
2020-12-07 18:03:47 +0100

    add tests

---
+++ app/javascript/src/__tests__/LandParcelContainer.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
+import { MockedProvider } from '@apollo/react-testing'
+import LandParcel from '../containers/LandParcels/LandParcel'
+import '@testing-library/jest-dom/extend-expect'
+
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn())
+describe('ActionFlows', () => {
+  it('renders necessary elements', async () => {
+    const container = render(
+      <MockedProvider addTypename={false}>
+        <BrowserRouter>
+          <LandParcel />
+        </BrowserRouter>
+      </MockedProvider>
+    )
+
+    expect(container.queryByText('Parcels and Properties')).toBeInTheDocument()
+  })
+})
