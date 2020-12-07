c7adcee2caae5668c66653890f507384428e6aaf
OlivierJM <manolivier93@gmail.com>
2020-12-07 19:01:58 +0200

    add event timeline to storybook

--- app/javascript/src/components/TimeLine.jsx
+++ app/javascript/src/components/TimeLine.jsx
@@ -5,12 +5,15 @@ import TimelineSeparator from '@material-ui/lab/TimelineSeparator'
 import TimelineConnector from '@material-ui/lab/TimelineConnector'
 import TimelineContent from '@material-ui/lab/TimelineContent'
 import TimelineDot from '@material-ui/lab/TimelineDot'
-import CreateIcon from '@material-ui/icons/Create';
+import CreateIcon from '@material-ui/icons/Create'
 import { Typography } from '@material-ui/core'
 import PropTypes from 'prop-types'
 import { dateFormatter } from './DateContainer'
 
-export default function FormTimeline({ data }) {
+export default function EventTimeLine({ data }) {
+  if (!data || !data.length) {
+    return <span>No Events Yet</span>
+  }
   return (
     <Timeline style={{ marginLeft: '-95%' }}>
       {data.map(item => (
@@ -25,7 +28,9 @@ export default function FormTimeline({ data }) {
             <Typography variant="subtitle1" component="p">
               {item.sentence}
             </Typography>
-            <Typography variant="body2">{dateFormatter(item.createdAt)}</Typography>
+            <Typography variant="body2">
+              {dateFormatter(item.createdAt)}
+            </Typography>
           </TimelineContent>
         </TimelineItem>
       ))}
@@ -33,9 +38,20 @@ export default function FormTimeline({ data }) {
   )
 }
 
-FormTimeline.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    sentence: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired
-  })).isRequired
+EventTimeLine.propTypes = {
+  /**
+   * Array of events that has a sentence and a createdAt
+   */
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * could be event name or title, in this case a sentence for what happened
+       */
+      sentence: PropTypes.string.isRequired,
+      /**
+       * The date when the event was created(important)
+       */
+      createdAt: PropTypes.string.isRequired
+    })
+  ).isRequired
 }
---
+++ stories/EventTimeLine.stories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { startOfToday, startOfWeek, startOfYesterday } from 'date-fns'
+import EventTimeLine from '../app/javascript/src/components/TimeLine'
+
+export default {
+  title: 'Components/EventTimeLine',
+  component: EventTimeLine
+}
+
+const TimeLineTemplate = args => <EventTimeLine {...args} />
+
+export const TimeLine = TimeLineTemplate.bind({})
+const data = [
+    {
+        sentence: 'Profile for user 2 updated',
+        createdAt: startOfToday()
+    },
+    {
+        sentence: 'User2 invited another user',
+        createdAt: startOfYesterday()
+    },
+    {
+        sentence: 'User got created',
+        createdAt: startOfWeek()
+    }
+]
+TimeLine.args = { data }
+
+export const EmptyTimeLine = TimeLineTemplate.bind({})
+
+EmptyTimeLine.args = {
+    data: []
+}
