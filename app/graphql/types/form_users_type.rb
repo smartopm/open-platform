af2b548259de907f907c3c1f1c94509c4c5f5d03
OlivierJM <manolivier93@gmail.com>
2020-12-08 22:16:50 +0200

    allow the form to be null

--- app/graphql/types/form_users_type.rb
+++ app/graphql/types/form_users_type.rb
@@ -5,7 +5,7 @@ module Types
   class FormUsersType < Types::BaseObject
     field :id, ID, null: false
     field :user, Types::UserType, null: false
-    field :form, Types::FormType, null: false
+    field :form, Types::FormType, null: true
     field :status, String, null: true
     field :status_updated_by, Types::UserType, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
