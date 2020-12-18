d6beabbff6e8393c95da75c69356c6b1166eeb1c
Saurabh Shinde <saurabh@doublegdp.com>
2020-12-18 07:51:08 +0000

    Added specs

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  describe 'schema' do
+    it { is_expected.to have_db_column(:id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:user_id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:invoice_id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:amount).of_type(:integer) }
+    it { is_expected.to have_db_column(:payment_status).of_type(:integer) }
+    it { is_expected.to have_db_column(:payment_type).of_type(:string) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:invoice) }
+  end
+end
