3be9e043869749d69e68051e94eb1428612a5177
OlivierJM <manolivier93@gmail.com>
2020-12-23 21:24:47 +0200

    add tests to verify visibility of payment buttons

--- app/javascript/src/__tests__/Payment/InvoiceItem.test.js
+++ app/javascript/src/__tests__/Payment/InvoiceItem.test.js
@@ -21,12 +21,39 @@ describe('Invoice Item Component', () => {
     const container = render(
       <BrowserRouter>
         <MockedProvider>
-          <InvoiceItem invoice={invoiceMock} />
+          <InvoiceItem invoice={invoiceMock} userType="client" />
         </MockedProvider>
       </BrowserRouter>)
+    // shows the invoice status when user is not admin 
     expect(container.queryByTestId('amount').textContent).toContain('Invoice amount: k200')
     expect(container.queryByTestId('duedate').textContent).toContain('Due at: 2020-09-12')
     expect(container.queryByTestId('landparcel').textContent).toContain('Parcel number: Plot-123')
+    expect(container.queryByTestId('pay-button')).toBeNull()
+    expect(container.queryByTestId('status').textContent).toContain('In-Progress')
+    expect(
+      container.queryByText('Invoice for a plot at Nkwashi')
+    ).toBeInTheDocument()
+  })
+
+  it('check when user is admin', () => {
+    const invoiceMock = {
+      description: 'Invoice for a plot at Nkwashi',
+      amount: 200,
+      dueDate: '2020-09-12',
+      status: 'in_progress',
+      landParcel: {
+        parcelNumber: 'Plot-123'
+      }
+    }
+    const container = render(
+      <BrowserRouter>
+        <MockedProvider>
+          <InvoiceItem invoice={invoiceMock} userType="admin" />
+        </MockedProvider>
+      </BrowserRouter>)
+    // shows make payment button when user is admin 
+    expect(container.queryByTestId('pay-button').textContent).toContain('make payment')
+    expect(container.queryByTestId('status').textContent).toContain('make payment')
     expect(
       container.queryByText('Invoice for a plot at Nkwashi')
     ).toBeInTheDocument()
