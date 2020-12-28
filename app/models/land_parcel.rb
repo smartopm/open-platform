4a2c4e9e20adef1827b632760742cef4f813cea8
Saurabh Shinde <saurabh@doublegdp.com>
2020-12-28 10:08:14 +0000

    Added unique validation to land parcel

--- app/models/land_parcel.rb
+++ app/models/land_parcel.rb
@@ -6,5 +6,6 @@ class LandParcel < ApplicationRecord
   has_many :land_parcel_accounts, dependent: :destroy
   has_many :accounts, through: :land_parcel_accounts
 
+  validates :parcel_number, uniqueness: true
   default_scope { order('created_at DESC') }
 end
