ed4b96d928d4c543a268af0f95d1be4d4ad684ff
OlivierJM <olivier@doublegdp.com>
2021-01-06 18:25:22 +0000

    add queries for email templates

---
+++ app/graphql/types/queries/email_template.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# email template queries
+module Types::Queries::EmailTemplate
+    extend ActiveSupport::Concern
+  
+    included do
+      # Get email template
+      field :email_template, Types::EmailTemplateType, null: true do
+        description 'get an email template for the provided id'
+        argument :id, GraphQL::Types::ID, required: true
+      end
+      # Get email templates
+      field :email_templates, [Types::EmailTemplateType], null: true do
+        description 'get all email templates'
+      end
+    end
+  
+    def email_template(id:)
+      raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user]&.blank?
+  
+      email = context[:site_community].email_templates.find(id)
+      raise GraphQL::ExecutionError, 'email template not found' if email.nil?
+  
+      email
+    end
+
+    def email_templates
+      raise GraphQL::ExecutionError, 'Unauthorized' if context[:current_user]&.blank?
+  
+      context[:site_community].email_templates
+    end
+  end
+  
