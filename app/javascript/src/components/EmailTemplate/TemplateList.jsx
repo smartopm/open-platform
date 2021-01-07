0dc7524292ad06810f2ac88538a371722869eeba
OlivierJM <manolivier93@gmail.com>
2021-01-07 14:19:57 +0200

    list community template in a list

--- app/javascript/src/components/EmailTemplate/TemplateList.jsx
+++ app/javascript/src/components/EmailTemplate/TemplateList.jsx
@@ -1,6 +1,6 @@
-// have a list of all templates for the community
 import React from 'react'
 import { useQuery } from 'react-apollo'
+import { MenuItem } from '@material-ui/core'
 import { EmailTemplatesQuery } from '../../graphql/queries'
 import { Spinner } from '../Loading'
 
@@ -8,6 +8,7 @@ export default function TemplateList(){
     const {loading, error, data} = useQuery(EmailTemplatesQuery)
     if(loading) return <Spinner />
     if(error) return error.message
-    console.log(data.emailTemplates)
-    return 'some list here'
+    return data.emailTemplates.map(template => (
+      <MenuItem key={template.id} value={template.id}>{template.name}</MenuItem>
+    ))
 }
