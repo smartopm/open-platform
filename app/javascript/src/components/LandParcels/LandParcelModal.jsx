f7505c667f2ce428423bc5ddd4dc0839a5960ee8
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-01-17 09:10:40 +0000

    Implement 'Add Valuation' - FE

--- app/javascript/src/components/LandParcels/LandParcelModal.jsx
+++ app/javascript/src/components/LandParcels/LandParcelModal.jsx
@@ -1,14 +1,28 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
-import { makeStyles } from "@material-ui/core/styles"
-import { TextField, InputAdornment } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  TextField,
+  InputAdornment,
+  Typography,
+  IconButton
+} from '@material-ui/core'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline'
+import { DeleteOutline } from '@material-ui/icons'
 import { CustomizedDialogs } from '../Dialog'
 import { StyledTabs, StyledTab, TabPanel } from '../Tabs'
 import DatePickerDialog from '../DatePickerDialog'
 import { Context as AuthStateContext } from '../../containers/Provider/AuthStateProvider'
 import { currencies } from '../../utils/constants'
 
-export default function LandParcelModal({ open, handelClose, handleSubmit, modalType, landParcel }) {
+export default function LandParcelModal({
+  open,
+  handelClose,
+  handleSubmit,
+  modalType,
+  landParcel
+}) {
   const classes = useStyles()
   const [parcelNumber, setParcelNumber] = useState('')
   const [address1, setAddress1] = useState('')
@@ -19,10 +33,11 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
   const [parcelType, setParcelType] = useState('')
   const [country, setCountry] = useState('')
   const [tabValue, setTabValue] = useState('Details')
+  const [valuationFields, setValuationFields] = useState([])
+
   const isFormReadOnly = modalType === 'details'
   const authState = useContext(AuthStateContext)
   const currency = currencies[authState.user?.community.currency] || ''
-
 
   function handleParcelSubmit() {
     if (handleSubmit) {
@@ -43,11 +58,42 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
     setTabValue(newValue)
   }
 
+  function onChangeValuationField(event, index) {
+    updateValuationField(event.target.name, event.target.value, index)
+  }
+
+  function onChangeValuationDateField(date, index) {
+    updateValuationField('startDate', date, index)
+  }
+
+  function updateValuationField(name, value, index) {
+    const fields = [ ...valuationFields ]
+    fields[index] = { ...fields[index], [name]: value }
+    setValuationFields(fields)
+  }
+
+  function addValuation() {
+    setValuationFields([
+      ...valuationFields,
+      { amount: '', startDate: new Date() }
+    ])
+  }
+
+  function removeValuation(index) {
+    const valuationOptions = valuationFields
+    valuationOptions.splice(index, 1)
+    setValuationFields([...valuationOptions])
+  }
+
   return (
     <CustomizedDialogs
       open={open}
       handleModal={handelClose}
-      dialogHeader={modalType === 'new' ? "New Property" : `Parcel ${landParcel.parcelNumber}`}
+      dialogHeader={
+        modalType === 'new'
+          ? 'New Property'
+          : `Parcel ${landParcel.parcelNumber}`
+      }
       handleBatchFilter={handleParcelSubmit}
       saveAction={modalType === 'details' ? 'Edit Parcel' : 'Save'}
     >
@@ -66,7 +112,10 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             autoFocus
             margin="dense"
             id="parcel-number"
-            inputProps={{ "data-testid": "parcel-number", readOnly: isFormReadOnly }}
+            inputProps={{
+              'data-testid': 'parcel-number',
+              readOnly: isFormReadOnly
+            }}
             label="Parcel Number"
             type="text"
             defaultValue={landParcel?.parcelNumber}
@@ -78,7 +127,7 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="address1"
             label="Address1"
-            inputProps={{ "data-testid": "address1", readOnly: isFormReadOnly }}
+            inputProps={{ 'data-testid': 'address1', readOnly: isFormReadOnly }}
             type="text"
             defaultValue={landParcel?.address1}
             value={modalType === 'new' ? address1 : undefined}
@@ -88,7 +137,7 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="address2"
             label="Address2"
-            inputProps={{ "data-testid": "address2", readOnly: isFormReadOnly }}
+            inputProps={{ 'data-testid': 'address2', readOnly: isFormReadOnly }}
             type="text"
             defaultValue={landParcel?.address2}
             value={modalType === 'new' ? address2 : undefined}
@@ -98,7 +147,7 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="city"
             label="city"
-            inputProps={{ "data-testid": "city", readOnly: isFormReadOnly }}
+            inputProps={{ 'data-testid': 'city', readOnly: isFormReadOnly }}
             type="text"
             defaultValue={landParcel?.city}
             value={modalType === 'new' ? city : undefined}
@@ -108,7 +157,10 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="state-province"
             label="State Province"
-            inputProps={{ "data-testid": "state-province", readOnly: isFormReadOnly }}
+            inputProps={{
+              'data-testid': 'state-province',
+              readOnly: isFormReadOnly
+            }}
             type="text"
             defaultValue={landParcel?.stateProvince}
             value={modalType === 'new' ? stateProvince : undefined}
@@ -119,7 +171,7 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             id="country"
             label="Country"
             type="text"
-            inputProps={{ "data-testid": "country", readOnly: isFormReadOnly }}
+            inputProps={{ 'data-testid': 'country', readOnly: isFormReadOnly }}
             defaultValue={landParcel?.country}
             value={modalType === 'new' ? country : undefined}
             onChange={e => setCountry(e.target.value)}
@@ -128,7 +180,10 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="parcel-type"
             label="Parcel Type"
-            inputProps={{ "data-testid": "parcel-type", readOnly: isFormReadOnly }}
+            inputProps={{
+              'data-testid': 'parcel-type',
+              readOnly: isFormReadOnly
+            }}
             type="text"
             defaultValue={landParcel?.parcelType}
             value={modalType === 'new' ? parcelType : undefined}
@@ -138,7 +193,10 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
             margin="dense"
             id="postal-code"
             label="Postal Code"
-            inputProps={{ "data-testid": "postal-code", readOnly: isFormReadOnly }}
+            inputProps={{
+              'data-testid': 'postal-code',
+              readOnly: isFormReadOnly
+            }}
             type="number"
             defaultValue={landParcel?.postalCode}
             value={modalType === 'new' ? postalCode : undefined}
@@ -150,38 +208,98 @@ export default function LandParcelModal({ open, handelClose, handleSubmit, modal
         <div>Coming soon!!</div>
       </TabPanel>
       <TabPanel value={tabValue} index="Valuation History">
-        {
-          landParcel?.valuations?.length ?
-          landParcel?.valuations.map((valuation) => (
-            <div className={classes.parcelForm} key={valuation.id}>
-              <TextField
-                autoFocus
-                margin="dense"
-                InputProps={{
-                  "data-testid": "valuation-amount", readOnly: isFormReadOnly,
-                  startAdornment: <InputAdornment position="start">{currency}</InputAdornment>
-                }}
-                // eslint-disable-next-line react/jsx-no-duplicate-props
-                inputProps={{ style: { paddingTop: '6px' } }}
-                label="Amount"
-                type="text"
-                defaultValue={valuation.amount}
-                required
-              />
-              <DatePickerDialog
-                label="Start Date"
-                selectedDate={valuation.startDate}
-                handleDateChange={() => {}}
-                inputProps={{readOnly: true}}
-                required
-              />
+        {modalType === 'details' &&
+          (landParcel?.valuations?.length ? (
+            landParcel?.valuations.map(valuation => (
+              <div className={classes.parcelForm} key={valuation.id}>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  InputProps={{
+                    'data-testid': 'valuation-amount',
+                    readOnly: isFormReadOnly,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {currency}
+                      </InputAdornment>
+                    )
+                  }}
+                  // eslint-disable-next-line react/jsx-no-duplicate-props
+                  inputProps={{ style: { paddingTop: '6px' } }}
+                  label="Amount"
+                  type="text"
+                  defaultValue={valuation.amount}
+                  required
+                />
+                <DatePickerDialog
+                  label="Start Date"
+                  selectedDate={valuation.startDate}
+                  handleDateChange={() => {}}
+                  inputProps={{ readOnly: true }}
+                  required
+                />
+              </div>
+            ))
+          ) : (
+            <div>No Valuations Yet</div>
+          ))}
+        {modalType === 'new' &&
+          valuationFields.map((_field, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div style={{ display: 'flex' }} key={index}>
+              <div className={classes.parcelForm}>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  InputProps={{
+                    'data-testid': 'valuation-amount',
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {currency}
+                      </InputAdornment>
+                    )
+                  }}
+                  // eslint-disable-next-line react/jsx-no-duplicate-props
+                  inputProps={{ style: { paddingTop: '6px' } }}
+                  label="Amount"
+                  name="amount"
+                  type="text"
+                  value={valuationFields[index].amount}
+                  onChange={e => onChangeValuationField(e, index)}
+                  required
+                />
+                <DatePickerDialog
+                  label="Start Date"
+                  selectedDate={valuationFields[index].startDate}
+                  handleDateChange={date =>
+                    onChangeValuationDateField(date, index)}
+                  required
+                />
+              </div>
+              <div className={classes.removeIcon}>
+                <IconButton
+                  style={{ marginTop: 13 }}
+                  onClick={() => removeValuation(index)}
+                  aria-label="remove"
+                >
+                  <DeleteOutline />
+                </IconButton>
+              </div>
             </div>
-          )) :
-          <div>No Valuations Yet</div>
-        }
+          ))}
+        {modalType === 'new' && (
+          <div className={classes.addIcon} role="button" onClick={addValuation} tabIndex={0}>
+            <AddCircleOutlineIcon />
+            <div style={{ marginLeft: '6px', color: 'secondary' }}>
+              <Typography align="center" variant="caption">
+                Add Valuation
+              </Typography>
+            </div>
+          </div>
+        )}
       </TabPanel>
     </CustomizedDialogs>
-  );
+  )
 }
 
 const useStyles = makeStyles(() => ({
@@ -190,8 +308,18 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'column',
     width: '400px',
     marginBottom: '30px'
+  },
+  addIcon: {
+    display: 'flex',
+    marginTop: '20px',
+    color: '#6CAA9F',
+    cursor: 'pointer'
+  },
+  removeIcon: {
+    marginTop: '25px',
+    marginLeft: '10px'
   }
-}));
+}))
 
 LandParcelModal.defaultProps = {
   handleSubmit: () => {},
