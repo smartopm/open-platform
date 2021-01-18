fc639b2ad0641aeff84f007ca816f9009ab61185
OlivierJM <manolivier93@gmail.com>
2021-01-18 18:50:14 +0200

    add status report tests

--- app/javascript/src/components/User/SubStatusReport.jsx
+++ app/javascript/src/components/User/SubStatusReport.jsx
@@ -39,7 +39,6 @@ export default function SubStatusReportDialog({ handleClose, open }) {
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [open]);
-
   return (
     <Dialog
       onClose={handleClose}
