6fd3d7eda486cad98ac920298f3c676ec2038b1a
Saurabh Shinde <saurabh@doublegdp.com>
2021-01-20 11:43:37 +0000

    Changed invoice number mechanism to use sequences

--- db/migrate/20210115134903_add_invoice_number_to_invoice_table.rb
+++ db/migrate/20210115134903_add_invoice_number_to_invoice_table.rb
@@ -1,5 +1,13 @@
 class AddInvoiceNumberToInvoiceTable < ActiveRecord::Migration[6.0]
-  def change
+  def self.up
     add_column :invoices, :invoice_number, :integer
+    
+    execute "CREATE SEQUENCE global_seq START 1101"
+  end
+
+  def self.down
+    remove_column :invoices, :invoice_number
+
+    execute "DROP SEQUENCE global_seq"
   end
 end
