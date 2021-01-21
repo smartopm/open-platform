f5d043282aad3ac3d605114df30afd5b768e1288
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-01-21 13:39:48 +0000

    Always build rails (for now)

--- bin/integration_tests.sh
+++ bin/integration_tests.sh
@@ -2,14 +2,13 @@
 
 echo "Starting services..."
 # TODO(Nurudeen): Figure out a way to detect if we need to rebuild or not
-# docker-compose -f docker-compose.ci.yml up -d
-bin/rebuild_integration_tests.sh
+docker-compose -f docker-compose.ci.yml build rails && docker-compose -f docker-compose.ci.yml up -d
 
 sleep 60
 echo "Services are up and ready"
 
 echo "Preparing test DB..."
-bin/docker_rails db:test:prepare
+./bin/docker_rails db:test:prepare
 
 echo "Running Cypress tests..."
 docker-compose -f docker-compose.ci.yml -f cypress.yml up --exit-code-from cypress
