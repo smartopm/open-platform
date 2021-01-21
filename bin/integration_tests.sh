8b36a592e981ce6dfa8b112633fbcd0feed2b670
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-01-21 15:20:23 +0000

    Increase wait-time for server to start

--- bin/integration_tests.sh
+++ bin/integration_tests.sh
@@ -2,13 +2,16 @@
 
 echo "Starting services..."
 # TODO(Nurudeen): Figure out a way to detect if we need to rebuild or not
-docker-compose -f docker-compose.ci.yml build rails && docker-compose -f docker-compose.ci.yml up -d
+docker-compose -f docker-compose.ci.yml build && docker-compose -f docker-compose.ci.yml up -d
 
-sleep 60
+sleep 120
 echo "Services are up and ready"
 
 echo "Preparing test DB..."
 ./bin/docker_rails db:test:prepare
+
+echo "Compiling all packs..."
+./bin/docker_rails webpacker:compile
 
 echo "Running Cypress tests..."
 docker-compose -f docker-compose.ci.yml -f cypress.yml up --exit-code-from cypress
