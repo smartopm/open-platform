07aecce67657f615945883fbbb0375c4adc5e982
Saurabh Shinde <saurabh@doublegdp.com>
2021-01-25 14:02:55 +0000

    Invoice will debit amount from wallet

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,9 +7,16 @@ class Invoice < ApplicationRecord
   belongs_to :user
   belongs_to :created_by, class_name: 'User', optional: true
 
+  after_create :collect_payment
+
   has_many :payment_invoices, dependent: :destroy
   has_many :payments, through: :payment_invoices
 
   enum status: { in_progress: 0, paid: 1, late: 2, cancelled: 3 }
   scope :by_status, ->(status) { where(status: status) if status.present? }
+
+  def collect_payment
+    paid! if amount < user.wallet.balance
+    user.wallet.update_balance(amount, 'debit')
+  end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -10,11 +10,27 @@ class Wallet < ApplicationRecord
     self.currency = DEFAULT_CURRENCY if currency.nil?
   end
 
-  def update_balance(amount)
+  def update_balance(amount, type = 'credit')
+    return credit_amount(amount) if type.eql?('credit')
+
+    debit_amount(amount)
+  end
+
+  def credit_amount(amount)
     if pending_balance > amount
       update(pending_balance: (pending_balance - amount))
     else
       update(balance: (balance + amount - pending_balance), pending_balance: 0)
+    end
+    balance
+  end
+
+  def debit_amount(amount)
+    if balance > amount
+      update(balance: (balance - amount))
+    else
+      pending_balance = amount - balance
+      update(balance: 0, pending_balance: pending_balance)
     end
     balance
   end
