d3eeae3442513e1281fae8fa75582f6e4fd4fea6
OlivierJM <manolivier93@gmail.com>
2021-01-26 16:00:36 +0200

    add comments and fix linter

--- app/graphql/types/queries/wallet.rb
+++ app/graphql/types/queries/wallet.rb
@@ -2,46 +2,48 @@
 
 # wallet queries
 module Types::Queries::Wallet
-    extend ActiveSupport::Concern
-  
-    included do
-      # Get wallets
-      field :user_wallets, [Types::WalletType], null: true do
-        description 'Get all wallets'
-        argument :user_id, GraphQL::Types::ID, required: true
-        argument :offset, Integer, required: false
-        argument :limit, Integer, required: false
-      end
+  extend ActiveSupport::Concern
 
-      # Get wallets transactions
-      field :user_wallet_transactions, [Types::WalletTransactionType], null: true do
-        description 'Get all wallets transactions'
-        argument :user_id, GraphQL::Types::ID, required: true
-        argument :offset, Integer, required: false
-        argument :limit, Integer, required: false
-      end
-    end
-  
-    def user_wallets(user_id: nil, offset: 0, limit: 100)
-      raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user]&.admin?
-      raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user].id == user_id ||
-                                                           context[:current_user].admin?
-
-      user = User.allowed_users(context[:current_user]).find(user_id)
-      raise GraphQL::ExecutionError, 'User not found' if user.blank?
-
-        user.wallets.order(created_at: :desc).limit(limit).offset(offset)
+  included do
+    # Get wallets
+    field :user_wallets, [Types::WalletType], null: true do
+      description 'Get all wallets'
+      argument :user_id, GraphQL::Types::ID, required: true
+      argument :offset, Integer, required: false
+      argument :limit, Integer, required: false
     end
 
-    def user_wallet_transactions(user_id: nil, offset: 0, limit: 100)
-      raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user]&.admin?
-      raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user].id == user_id ||
-                                                           context[:current_user].admin?
-
-      user = User.allowed_users(context[:current_user]).find(user_id)
-      raise GraphQL::ExecutionError, 'User not found' if user.blank?
-
-        user.wallet_transactions.order(created_at: :desc).limit(limit).offset(offset)
+    # Get wallets transactions
+    field :user_wallet_transactions, [Types::WalletTransactionType], null: true do
+      description 'Get all wallets transactions'
+      argument :user_id, GraphQL::Types::ID, required: true
+      argument :offset, Integer, required: false
+      argument :limit, Integer, required: false
     end
   end
-  
+
+  def user_wallets(user_id: nil, offset: 0, limit: 100)
+    user = verified_user(user_id)
+
+    user.wallets.order(created_at: :desc).limit(limit).offset(offset)
+  end
+
+  def user_wallet_transactions(user_id: nil, offset: 0, limit: 100)
+    user = verified_user(user_id)
+    user.wallet_transactions.order(created_at: :desc).limit(limit).offset(offset)
+  end
+
+  # It would be good to put this elsewhere to use it in other queries
+  # rubocop:disable Metrics/AbcSize
+  def verified_user(user_id)
+    raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user]&.admin?
+    raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user].id == user_id ||
+                                                         context[:current_user].admin?
+
+    user = User.allowed_users(context[:current_user]).find(user_id)
+    raise GraphQL::ExecutionError, 'User not found' if user.blank?
+
+    user
+  end
+  # rubocop:enable Metrics/AbcSize
+end
