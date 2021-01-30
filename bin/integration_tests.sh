fbcbbc779ecd2282932fdc4352cca59ab3759139
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-01-30 23:14:30 +0000

    Allow dev services and e2e test to run at the same time

--- bin/integration_tests.sh
+++ bin/integration_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export env $(cat cypress/.env.ci)
+
 echo "Starting services..."
 # TODO(Nurudeen): Figure out a way to detect if we need to rebuild or not
 docker-compose -f docker-compose.ci.yml build && docker-compose -f docker-compose.ci.yml up -d
@@ -9,9 +11,6 @@ echo "Services are up and ready"
 
 echo "Preparing test DB..."
 docker-compose -f docker-compose.ci.yml run --rm rails rails db:create db:schema:load
-
-echo "Compiling assets..."
-docker-compose -f docker-compose.ci.yml run --rm rails rails webpacker:compile
 
 echo "Running Cypress tests..."
 docker-compose -f docker-compose.ci.yml -f cypress.yml up --exit-code-from cypress
