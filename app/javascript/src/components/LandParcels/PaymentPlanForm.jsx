a9c0d13751449196c329026b683be7b02b5678fa
OlivierJM <manolivier93@gmail.com>
2021-02-08 13:28:30 +0200

    create a separate component for payment plan form

---
+++ app/javascript/src/components/LandParcels/PaymentPlanForm.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MenuItem, TextField } from '@material-ui/core';
+import DatePickerDialog from '../DatePickerDialog';
+import { paymentPlanStatus } from '../../utils/constants';
+
+export default function PaymentPlanForm({ planState, updatePlanState }) {
+  function update(event) {
+    const { name, value } = event.target;
+    updatePlanState({ ...planState, [name]: value });
+  }
+  return (
+    <>
+      <TextField
+        autoFocus
+        margin="normal"
+        id="purchase_plan"
+        inputProps={{ 'data-testid': 'purchase_plan' }}
+        label="Purchase Plan"
+        value={planState.planType}
+        onChange={update}
+        name="planType"
+        style={{ width: '100%' }}
+        required
+        select
+      >
+        <MenuItem value="lease">Lease</MenuItem>
+        <MenuItem value="other">Other</MenuItem>
+      </TextField>
+      <TextField
+        autoFocus
+        margin="normal"
+        id="status"
+        inputProps={{ 'data-testid': 'status' }}
+        label="Status"
+        value={planState.status}
+        onChange={update}
+        name="status"
+        style={{ width: '100%' }}
+        required
+        select
+      >
+        {Object.entries(paymentPlanStatus).map(([key, val]) => (
+          <MenuItem key={key} value={key}>
+            {val}
+          </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+        margin="normal"
+        id="percentage"
+        label="Percentage"
+        inputProps={{ 'data-testid': 'percentage' }}
+        value={planState.percentage}
+        onChange={update}
+        name="percentage"
+        style={{ width: '100%' }}
+      />
+      <DatePickerDialog
+        selectedDate={planState.startDate}
+        handleDateChange={date => updatePlanState({ ...planState, startDate: date })}
+        label="Start Date"
+        required
+      />
+    </>
+  );
+}
+
+PaymentPlanForm.propTypes = {
+  planState: PropTypes.shape({
+    status: PropTypes.string,
+    planType: PropTypes.string,
+    startDate: PropTypes.string,
+    percentage: PropTypes.string
+  }).isRequired,
+  updatePlanState: PropTypes.func.isRequired
+};
