bdeef94b81576890758aa04645cdbc71662092c9
Tolu Olaniyan <toluola7@gmail.com>
2021-02-09 11:13:43 +0100

    design the fullscreen modal for receipt

--- app/javascript/src/components/Payments/PaymentReceipt.jsx
+++ app/javascript/src/components/Payments/PaymentReceipt.jsx
@@ -6,9 +6,7 @@ export default function PaymentReceipt({ paymentData, open, handleClose }){
   return (
     <>
       {console.log(paymentData)}
-      <FullScreenDialog open={open} handleClose={handleClose} title='Payment Receipt'>
-        Hello
-      </FullScreenDialog>
+      <FullScreenDialog open={open} handleClose={handleClose} title='Payment Receipt' />
     </>
   )
 }
