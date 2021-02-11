4f387a424ac3f48c0e13b69c6e633b7a95c00299
OlivierJM <manolivier93@gmail.com>
2021-02-11 14:08:41 +0200

    clean up and remove edit and add plot

--- app/javascript/src/components/UserPlotInfo.jsx
+++ app/javascript/src/components/UserPlotInfo.jsx
@@ -1,116 +1,67 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable no-use-before-define */
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { css, StyleSheet } from 'aphrodite'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { css, StyleSheet } from 'aphrodite';
+import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
-import AddIcon from '@material-ui/icons/Add';
-import Tooltip from '@material-ui/core/Tooltip';
-import CreateIcon from '@material-ui/icons/Create';
-import { dateToString } from "./DateContainer"
-import PlotModal from "./PlotOpen"
-import EditModal from './EditPlot'
-import UserPlotMap from './UserPlotMap'
+import { dateToString } from './DateContainer';
+import UserPlotMap from './UserPlotMap';
 
-export default function UserPlotInfo({ userId, account, refetch, userType }) {
-  const [plotNumber, setPlotNumber] = useState([])
-  const [addOpen, setAddOpen] = useState(false)
-  const [editOpen, setEditOpen] = useState(false)
-  const [plotData, setPlotData] = useState({})
+export default function UserPlotInfo({ account }) {
+  const [plotNumber, setPlotNumber] = useState([]);
 
-  function handlePlotData(plot){
-    setPlotData(plot)
-    setEditOpen(true)
-  } 
-
-  function setData(){
-    if (account[0]?.landParcels[0]){
+  function setData() {
+    if (account[0]?.landParcels[0]) {
       account[0].landParcels.forEach(plot => {
-        setPlotNumber(...plotNumber, ...plot.parcelNumber)
-      })
+        setPlotNumber(...plotNumber, ...plot.parcelNumber);
+      });
     }
   }
 
   useEffect(() => {
-    setData()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [account])
+    setData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [account]);
 
-    return (
-      <>
-        {account && account[0]?.landParcels?.length > 0 ? (
-          <div className="container">
-            <div className={css(styles.body)}>
-              <div>
-                <div style={{display: 'flex'}}>
-                  <Typography variant='body1'><b>Plots associated with this account:</b></Typography>
-                  {userType === 'admin' && (
-                  <div 
-                    style={{display: 'flex', marginLeft: 'auto', order: 2, color: '#69aba4', cursor: 'pointer'}}
-                    onClick={() => setAddOpen(true)}
-                  >
-                    <AddIcon style={{marginRight: '5px', paddingBottom: '2px'}} />
-                    <Typography variant='body2'>Add Plots</Typography>
-                  </div>
-                )}
-                </div>
-                {account[0].landParcels.map((plot, index) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <div style={{display: 'flex'}} key={index}>
-                    <li className={css(styles.plotNumber)}>{plot.parcelNumber}</li>
-                    {userType === 'admin' && (
-                      <div style={{margin: '20px 10px', color: '#69ABA4', cursor: 'pointer'}}>
-                        <Tooltip title="Edit Plot" placement="top">
-                          <CreateIcon onClick={() => handlePlotData(plot)} />
-                        </Tooltip>
-                      </div>
-                    )}
-                  </div>
-                ))}
-                <Typography variant='body2'>
-                  This data was updated on 
-                  {' '}
-                  {dateToString(account[0]?.updatedAt)}
-                  . If Something seems
-                  incorrect, contact our
-                  <span className={css(styles.supportLink)}>
-                  &nbsp;
-                    <Link
-                      data-testid="support_link"
-                      to="/contact"
-                      className={css(styles.routeLink)}
-                    >
-                      Support Team.
-                    </Link>
-                  </span>
+  return (
+    <>
+      {account && account[0]?.landParcels?.length > 0 ? (
+        <div className="container">
+          <div className={css(styles.body)}>
+            <div>
+              <div style={{ display: 'flex' }}>
+                <Typography variant="body1">
+                  <b>Plots associated with this account:</b>
                 </Typography>
               </div>
-              <div className={css(styles.mapContainer)}>
-                <UserPlotMap plotData={account[0].landParcels} />
-              </div>
+              {account[0].landParcels.map((plot, index) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <div style={{ display: 'flex' }} key={index}>
+                  <li className={css(styles.plotNumber)}>{plot.parcelNumber}</li>
+                </div>
+              ))}
+              <Typography variant="body2">
+                {`This data was updated on ${dateToString(account[0]?.updatedAt)}. If Something seems incorrect, contact our`}
+                <span className={css(styles.supportLink)}>
+                  &nbsp;
+                  <Link data-testid="support_link" to="/contact" className={css(styles.routeLink)}>
+                    Support Team.
+                  </Link>
+                </span>
+              </Typography>
+            </div>
+            <div className={css(styles.mapContainer)}>
+              <UserPlotMap plotData={account[0].landParcels} />
             </div>
           </div>
-        ) : (
-          <div className="container" style={{display: 'flex', margin: '20px 150px'}}>
-            <p data-testid="no_plot">No plots information available</p>
-            {userType === 'admin' && (
-            <div 
-              style={{display: 'flex', marginLeft: 'auto', order: 2, color: '#69aba4', cursor: 'pointer'}}
-              onClick={() => setAddOpen(true)}
-            >
-              <AddIcon style={{marginRight: '5px', paddingBottom: '2px'}} />
-              <Typography variant='body2'>Add Plots</Typography>
-            </div>
-            )}
-          </div>
-        )}
-        <PlotModal open={addOpen} handleClose={() => setAddOpen(false)} accountId={account[0]?.id} userId={userId} refetch={refetch} />
-        <EditModal open={editOpen} handleClose={() => setEditOpen(false)} refetch={refetch} data={plotData} />
-      </>
-    )
-  }
+        </div>
+      ) : (
+        <div className="container" style={{ display: 'flex', margin: '20px 150px' }}>
+          <p data-testid="no_plot">No plots information available</p>
+        </div>
+      )}
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   supportLink: {
@@ -129,7 +80,7 @@ const styles = StyleSheet.create({
   },
   body: {
     display: 'flex',
-    flexDirection: 'column',       
+    flexDirection: 'column',
     alignItems: 'center'
   },
   routeLink: {
@@ -147,26 +98,25 @@ const styles = StyleSheet.create({
   mapContainer: {
     borderStyle: 'solid',
     borderWidth: '1px',
-    width: '100%',
+    width: '100%'
   }
-})
+});
 
 UserPlotInfo.defaultProps = {
-  userId: '',
   account: [],
-  userType: ''
- }
- 
+};
+
 UserPlotInfo.propTypes = {
-  account: PropTypes.arrayOf(PropTypes.shape({
+  account: PropTypes.arrayOf(
+    PropTypes.shape({
       id: PropTypes.string,
-      landParcels: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        plotNumber: PropTypes.string
-      })),
+      landParcels: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string,
+          plotNumber: PropTypes.string
+        })
+      ),
       updatedAt: PropTypes.string
-    })),
-  refetch: PropTypes.func.isRequired,
-  userId: PropTypes.string,
-  userType: PropTypes.string
-}
+    })
+  ),
+};
