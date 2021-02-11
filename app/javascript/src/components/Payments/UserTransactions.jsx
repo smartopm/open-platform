b9a7cbd44a206867861b57231bcd70125a84ffd8
Tolu Olaniyan <toluola7@gmail.com>
2021-02-11 14:33:48 +0100

    fix errors

--- app/javascript/src/components/Payments/UserTransactions.jsx
+++ app/javascript/src/components/Payments/UserTransactions.jsx
@@ -57,10 +57,10 @@ export function renderTransactions(transaction, currency) {
       content={transaction.__typename === 'WalletTransaction' 
             ? `${currency}${transaction.amount}` 
             : (
-              <div>
+              <>
                 <Text content={`Amount: ${currency}${transaction.amount}`} />
                 <Text content={`Pending Amount: ${currency}${transaction.pendingAmount}`} />
-              </div>
+              </>
               )}
     />, 
     Balance: <GridText 
