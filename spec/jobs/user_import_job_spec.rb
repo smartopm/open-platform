580ac188f98941af6040521a53c2987dd74d5aff
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-02-19 17:18:50 +0000

    Fix tests

---
+++ spec/jobs/user_import_job_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UserImportJob, type: :job do
+  let!(:non_admin) { create(:user_with_community) }
+  let!(:user) { create(:admin_user, community_id: non_admin.community_id) }
+  csv_string = "Name,Email primary,Phone number primary,Phone number secondary 1,Phone number secondary 2,User type,Labels,State,Expiration date,Notes on client\nThomas Shalongolo,thomas@gmail.com,+234979063360,,,Prospective Client,Residency program Waitlist;Some other label,valid,,some notes here\nJide Babs,jide@gmail.com,+260979013360,,,Prospective Client,Residency program Waitlist;Some other label,pending,,some notes here"
+
+  describe '#perform_later' do
+    before do
+      ActiveJob::Base.queue_adapter = :test
+    end
+    after do
+      clear_enqueued_jobs
+      clear_performed_jobs
+    end
+    it 'should match enqueued job arguments' do
+      UserImportJob.perform_later(csv_string, 'A File.csv', user)
+      expect(UserImportJob).to have_been_enqueued.with(csv_string, 'A File.csv', user)
+    end
+
+    it 'should update assignee_note reminder_job_id with new job id' do
+      prev_user_count = User.count
+      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+
+      UserImportJob.perform_later(csv_string, 'A File.csv', user)
+
+      expect(User.count).to eql(prev_user_count + 2)
+    end
+  end
+end
--- spec/models/import_log_spec.rb
+++ spec/models/import_log_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ImportLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'schema' do
+    it { is_expected.to have_db_column(:community_id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:user_id).of_type(:uuid) }
+    it { is_expected.to have_db_column(:failed).of_type(:boolean) }
+    it { is_expected.to have_db_column(:file_name).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:community) }
+    it { is_expected.to belong_to(:user) }
+  end
 end
