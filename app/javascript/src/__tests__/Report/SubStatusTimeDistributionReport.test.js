c18df682a9016568eddd01239e2337e29c8f03fd
victor okeugo <victor@doublegdp.com>
2021-02-19 19:25:39 +0000

    fix tests

--- app/javascript/src/__tests__/Report/SubStatusTimeDistributionReport.test.js
+++ app/javascript/src/__tests__/Report/SubStatusTimeDistributionReport.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import SubStatusTimeDistributionReport from '../components/User/SubStatusTimeDistributionReport'
-import { userSubStatus } from '../utils/constants';
+import SubStatusTimeDistributionReport from '../../components/User/SubStatusTimeDistributionReport'
+import { userSubStatus } from '../../utils/constants';
 
 describe('<SubStatusTimeDistributionReport />', () => {
   it('render correctly', () => {
