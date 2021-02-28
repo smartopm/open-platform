10a4f24925722a801aa56aca4f19030a3932a3fe
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-02-28 17:49:27 +0000

    Re-push rspec beyond 90 again

--- spec/graphql/types/queries/email_template_spec.rb
+++ spec/graphql/types/queries/email_template_spec.rb
@@ -26,6 +26,19 @@ RSpec.describe Types::Queries::EmailTemplate do
       GQL
     end
 
+    let(:email_template) do
+      <<~GQL
+        query emailTemplate (
+          $id: ID!
+        ) {
+          emailTemplate(id: $id) {
+              id
+              name
+            }
+          }
+      GQL
+    end
+
     it 'should retrieve list of email templates' do
       result = DoubleGdpSchema.execute(email_templates, context: {
                                          current_user: admin,
@@ -37,10 +50,56 @@ RSpec.describe Types::Queries::EmailTemplate do
       expect(result.dig('data', 'emailTemplates', 0, 'id')).to_not be_nil
     end
 
+    it 'should query a template' do
+      result = DoubleGdpSchema.execute(email_template,
+                                       variables: { id: comm_templates.id },
+                                       context: {
+                                         current_user: admin,
+                                         site_community: current_user.community,
+                                       }).as_json
+
+      expect(result['errors']).to be_nil
+      expect(result.dig('data', 'emailTemplate', 'id')).to eq(comm_templates.id)
+    end
+
+    it 'should raise unauthorized error if user is not available' do
+      result = DoubleGdpSchema.execute(email_template,
+                                       variables: { id: comm_templates.id },
+                                       context: {
+                                         current_user: nil,
+                                         site_community: current_user.community,
+                                       }).as_json
+
+      expect(result.dig('data', 'emailTemplate')).to be_nil
+      expect(result.dig('errors', 0, 'message')).to include 'Unauthorized'
+    end
+
+    it 'should raise unauthorized error if user is not an admin' do
+      result = DoubleGdpSchema.execute(email_template,
+                                       variables: { id: comm_templates.id },
+                                       context: {
+                                         current_user: another_user,
+                                         site_community: current_user.community,
+                                       }).as_json
+
+      expect(result.dig('data', 'emailTemplate')).to be_nil
+      expect(result.dig('errors', 0, 'message')).to include 'Unauthorized'
+    end
+
     it 'should return unauthorized when not admin' do
       result = DoubleGdpSchema.execute(email_templates,
                                        context: {
                                          current_user: another_user,
+                                         site_community: another_user.community,
+                                       }).as_json
+      expect(result.dig('data', 'emailTemplates')).to be_nil
+      expect(result.dig('errors', 0, 'message')).to include 'Unauthorized'
+    end
+
+    it 'should return unauthorized when current user is nil' do
+      result = DoubleGdpSchema.execute(email_templates,
+                                       context: {
+                                         current_user: nil,
                                          site_community: another_user.community,
                                        }).as_json
       expect(result.dig('data', 'emailTemplates')).to be_nil
