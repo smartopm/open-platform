ae667b3f87bd7024e1dfc60220c65f8c1d7bc116
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-02-28 13:42:37 +0000

    Write tests for Form container

---
+++ app/javascript/src/__tests__/Forms/FormPage.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom/';
+import { MockedProvider } from '@apollo/react-testing';
+import FormPage from '../../containers/Forms/FormPage';
+
+
+
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
+
+describe('FormPage Component', () => {
+  it('renders loader when loading form', () => {
+    const container = render(
+      <MockedProvider>
+        <BrowserRouter>
+          <FormPage />
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    expect(container.queryByTestId('loader')).toBeInTheDocument();
+  });
+});
