605edfee575d0c77c65b38e336f06940f6d6ea96
OlivierJM <manolivier93@gmail.com>
2021-03-02 11:45:50 +0200

    add an update transaction mutation and event

---
+++ app/graphql/mutations/transaction/wallet_transaction_update.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Mutations
+  module Transaction
+    # update transactions
+    class WalletTransactionUpdate < BaseMutation
+      argument :transaction_id, ID, required: true
+      argument :bank_name, String, required: false
+      argument :cheque_number, String, required: false
+      argument :transaction_number, String, required: false
+      argument :status, String, required: false
+      argument :source, String, required: true
+
+      field :wallet_transaction, Types::WalletTransactionType, null: true
+
+      def resolve(vals)
+        transaction = context[:site_community].wallet_transactions.find(vals[:transaction_id])
+        raise GraphQL::ExecutionError, 'Payment not found' if transaction.nil?
+
+        ActiveRecord::Base.transaction do
+          context[:current_user].generate_events('payment_update', transaction)
+          return { wallet_transaction: transaction } if transaction.update!(vals)
+        end
+        raise GraphQL::ExecutionError, transaction.errors.full_messages
+      end
+
+      def authorized?(_vals)
+        return true if context[:current_user]&.admin?
+
+        raise GraphQL::ExecutionError, 'Unauthorized'
+      end
+    end
+  end
+end
