9e4a479e4ae98de0ffe35c0fafb7f1b4b9ff6217
tolulope3 <tolulope@doublegdp.com>
2021-03-08 16:10:15 +0000

    fix graph  alignment

--- app/javascript/src/components/Payments/InvoiceGraph.jsx
+++ app/javascript/src/components/Payments/InvoiceGraph.jsx
@@ -23,7 +23,7 @@ export default function InvoiceGraph({ handleClick }){
   }
   return (
     <>
-      <div style={matches ? {width: '80%', margin: '30px 150px', border: '1px solid #E7E7E7'} : {width: '100%', margin: '10px', border: '1px solid #E7E7E7'}}>
+      <div style={matches ? {width: '80%', margin: '30px 150px', border: '1px solid #E7E7E7'} : {width: '100%', margin: '10px 0', border: '1px solid #E7E7E7'}}>
         {data?.invoiceAccountingStats && data?.invoiceAccountingStats?.length && (
           <div>
             <div style={{background: '#FAFEFE', borderBottom: '1px solid #C3DCD8', padding: '25px'}}>
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -61,12 +61,6 @@ class Invoice < ApplicationRecord
     )
   end
 
-  # def modify_balance
-  #   return unless cancelled?
-
-  #   user.wallet.settle_pending_balance(amount)
-  # end
-
   def settle_amount
     pending_amount = amount - user.wallet.balance
     if pending_amount.positive?
