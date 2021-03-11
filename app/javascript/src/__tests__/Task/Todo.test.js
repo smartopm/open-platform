bee285a96c45b99ddb075d461c334ca2f9e224a0
OlivierJM <manolivier93@gmail.com>
2021-03-11 18:10:06 +0200

    add tests for requests

--- app/javascript/src/__tests__/Task/Todo.test.js
+++ app/javascript/src/__tests__/Task/Todo.test.js
@@ -11,7 +11,7 @@ import { TaskStatsQuery } from '../../graphql/queries';
 
 jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
 
-describe('Support main page', () => {
+describe('Todo list main page', () => {
   const data = {
     user: {
       id: 'a54d6184-b10e-4865-bee7-7957701d423d',
@@ -23,7 +23,7 @@ describe('Support main page', () => {
       }
     }
   };
-  it('renders the support page correctly', async () => {
+  it('renders the todo list page correctly', async () => {
     const mocks = [
       {
         request: {
@@ -53,7 +53,7 @@ describe('Support main page', () => {
     await act(async () => {
       container = render(
         <ApolloProvider client={createClient}>
-          <Context.Provider value={data.user}>
+          <Context.Provider value={data}>
             <MockedProvider mocks={mocks} addTypename={false}>
               <BrowserRouter>
                 <Todo history={{ push: pushMock }} />
