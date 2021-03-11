833cd92835ed270913bcfc6eb074875d90bd9679
OlivierJM <manolivier93@gmail.com>
2021-03-11 16:08:30 +0200

    tests home for security guards

---
+++ app/javascript/src/__tests__/GuardHome.test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { act, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { MockedProvider } from '@apollo/react-testing';
+import { Context } from '../containers/Provider/AuthStateProvider';
+import { createClient } from '../utils/apollo';
+import { SecurityGuards } from '../graphql/queries';
+import { HomeGuard as GuardHome } from '../containers/GuardHome';
+
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
+
+describe('Support main page', () => {
+  const data = {
+    user: {
+      id: 'a54d6184-b10e-4865-bee7-7957701d423d',
+      name: 'Another somebodyy',
+      userType: 'client',
+      expiresAt: null,
+      community: {
+        supportName: 'Support Officer'
+      }
+    }
+  };
+  it('renders the support page correctly', async () => {
+    const mocks = [
+      {
+        request: {
+          query: SecurityGuards
+        },
+        result: {
+          data: {
+            securityGuards: [
+                {
+                    name: "guard 1",
+                    id: "2743-4232e2-23ewed2",
+                    phoneNumber: "00909898392",
+                }
+            ]
+          }
+        }
+      }
+    ];
+
+    const tMock = jest.fn();
+    let container;
+    await act(async () => {
+      container = render(
+        <ApolloProvider client={createClient}>
+          <Context.Provider value={data.user}>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <BrowserRouter>
+                <GuardHome translate={tMock} />
+              </BrowserRouter>
+            </MockedProvider>
+          </Context.Provider>
+        </ApolloProvider>
+      );
+    });
+
+    expect(container.queryByTestId('loader')).toBeInTheDocument()
+  });
+
+  it('should check for errors occured when fetching ', async () => {
+    const mocks = [
+        {
+          request: {
+            query: SecurityGuards
+          },
+          error: new Error('Something happpened')
+        }
+      ];
+
+      const tMock = jest.fn();
+      let container;
+      await act(async () => {
+        container = render(
+          <ApolloProvider client={createClient}>
+            <Context.Provider value={data.user}>
+              <MockedProvider mocks={mocks} addTypename={false}>
+                <BrowserRouter>
+                  <GuardHome translate={tMock} />
+                </BrowserRouter>
+              </MockedProvider>
+            </Context.Provider>
+          </ApolloProvider>
+        );
+      });
+
+      expect(container.queryByTestId('loader')).toBeInTheDocument()
+
+      await waitFor(() => {
+        expect(container.queryByText('Network error: Something happpened')).toBeInTheDocument();
+      }, 50);
+  })
+});
---
+++ app/javascript/src/__tests__/Task/Todo.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { act, render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { MockedProvider } from '@apollo/react-testing';
+import { Context } from '../../containers/Provider/AuthStateProvider';
+import { createClient } from '../../utils/apollo';
+import Todo from '../../containers/Todo';
+import { TaskStatsQuery } from '../../graphql/queries';
+
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
+
+describe('Support main page', () => {
+  const data = {
+    user: {
+      id: 'a54d6184-b10e-4865-bee7-7957701d423d',
+      name: 'Another somebodyy',
+      userType: 'client',
+      expiresAt: null,
+      community: {
+        supportName: 'Support Officer'
+      }
+    }
+  };
+  it('renders the support page correctly', async () => {
+    const mocks = [
+      {
+        request: {
+          query: TaskStatsQuery
+        },
+        result: {
+          data: {
+            taskStats: {
+              completedTasks: 9,
+              tasksDueIn10Days: 35,
+              tasksDueIn30Days: 35,
+              tasksOpen: 42,
+              tasksOpenAndOverdue: 35,
+              overdueTasks: 35,
+              tasksWithNoDueDate: 10,
+              myOpenTasks: 25,
+              totalCallsOpen: 2,
+              totalFormsOpen: 1
+            }
+          }
+        }
+      }
+    ];
+
+    const pushMock = jest.fn();
+    let container;
+    await act(async () => {
+      container = render(
+        <ApolloProvider client={createClient}>
+          <Context.Provider value={data.user}>
+            <MockedProvider mocks={mocks} addTypename={false}>
+              <BrowserRouter>
+                <Todo history={{ push: pushMock }} />
+              </BrowserRouter>
+            </MockedProvider>
+          </Context.Provider>
+        </ApolloProvider>
+      );
+    });
+    // here what happens is that, because of the authstate, if the user isn't admin then it autoreroutes to home
+    expect(pushMock).toBeCalled();
+    await waitFor(() => {
+      expect(container.queryByText('Home')).toBeInTheDocument();
+    }, 50);
+  });
+});
