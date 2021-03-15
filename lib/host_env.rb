9f6b127eb9c1044bd632128947c4b32d7a250724
OlivierJM <manolivier93@gmail.com>
2021-03-15 13:10:07 +0200

    fix rubocop

--- lib/host_env.rb
+++ lib/host_env.rb
@@ -6,9 +6,7 @@ class HostEnv
   # Needs to be updated and made more generic as more communities start to come in
   def self.base_url(community)
     if ENV['APP_ENV'].eql?('production')
-      if community.name.eql?('Doublegdp')
-        return 'demo.doublegdp.com'
-      end
+      return 'demo.doublegdp.com' if community.name.eql?('Doublegdp')
 
       return ENV['HOST']
     end
