8632cadf9c2b6c2d0ea1d89df5647a7ae0a38005
OlivierJM <manolivier93@gmail.com>
2021-03-16 14:20:32 +0200

    hide forms on other communities

--- app/javascript/src/__tests__/FormLinks.test.js
+++ app/javascript/src/__tests__/FormLinks.test.js
@@ -1,43 +1,32 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import FormLinks from '../components/Forms/FormLinks'
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import FormLinks from '../components/Forms/FormLinks';
 
 describe('Shows the google form links', () => {
   it('It should render with no errors', () => {
-    const container = render(<FormLinks />)
+    const container = render(<FormLinks community="Nkwashi" />);
 
-    expect(container.queryByTestId('forms-crf')).toBeInTheDocument()
-    expect(container.queryByTestId('forms-building-permit')).toBeInTheDocument()
-    expect(
-      container.queryByTestId('forms-link-building-permit')
-    ).toBeInTheDocument()
-  })
-
-  it('It should render containers with no errors', () => {
-    const container = render(<FormLinks />)
-    expect(container.queryByTestId('forms-link-crf')).toBeInTheDocument()
-    expect(container.queryByTestId('forms-link-crf-icon')).toBeInTheDocument()
-    expect(
-      container.queryByTestId('forms-link-building-icon')
-    ).toBeInTheDocument()
-  })
+    expect(container.queryByTestId('forms-crf')).toBeInTheDocument();
+    expect(container.queryByTestId('forms-building-permit')).toBeInTheDocument();
+    expect(container.queryByTestId('forms-link-building-permit')).toBeInTheDocument();
+  });
 
   it('It should click link and open new tab', () => {
-    window.open = jest.fn()
-    const container = render(<FormLinks />)
-    const buildPermit = container.queryByTestId('forms-link-building-permit')
-    const clientForm = container.queryByTestId('forms-link-crf')
-    fireEvent.click(buildPermit)
+    window.open = jest.fn();
+    const container = render(<FormLinks community="Nkwashi" />);
+    const buildPermit = container.queryByTestId('forms-link-building-permit');
+    const clientForm = container.queryByTestId('forms-link-crf');
+    fireEvent.click(buildPermit);
     expect(window.open).toBeCalledWith(
       'https://docs.google.com/forms/d/e/1FAIpQLSe6JmeKp9py650r7NQHFrNe--5vKhsXa9bFF9kmLAjbjYC_ag/viewform',
       '_blank'
-    )
-    fireEvent.click(clientForm)
+    );
+    fireEvent.click(clientForm);
     expect(window.open).toBeCalledWith(
       'https://docs.google.com/forms/d/e/1FAIpQLSe6JmeKp9py650r7NQHFrNe--5vKhsXa9bFF9kmLAjbjYC_ag/viewform',
       '_blank'
-    )
-  })
-})
+    );
+  });
+});
