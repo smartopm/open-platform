fd4cab237fb58bc6f2df464f83b3cb9da2cfd14a
tolulope3 <tolulope@doublegdp.com>
2021-03-18 21:19:23 +0000

    add more tests

---
+++ app/javascript/src/__tests__/Payment/TabbedPayments.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom/';
+import { MockedProvider } from '@apollo/react-testing';
+import TabbedPayment from '../../components/Payments/TabbedPayments';
+
+describe('Tabbed Payment Component', () => {
+  const authState = {
+    user: {
+      userType: 'admin',
+      community: {
+        currency: 'k',
+        locale: 'nkw'
+      }
+    }
+  }
+
+  it('renders Tabbed Payment Templates', async () => {
+    await act(async () => {
+    const container = render(
+      <MockedProvider>
+        <BrowserRouter>
+          <TabbedPayment authState={authState} />
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+    expect(container.queryByText('Invoices')).toBeInTheDocument();
+    expect(container.queryByText('Payments')).toBeInTheDocument();
+    })
+  });
+});
