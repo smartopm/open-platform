efa07e46390db60241add49160271d744236c9df
OlivierJM <manolivier93@gmail.com>
2021-03-26 14:22:45 +0200

    fix the news page

--- app/javascript/src/containers/Posts/Posts.jsx
+++ app/javascript/src/containers/Posts/Posts.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import PostsList from '../../components/NewsPage/PostList'
-import Nav from '../../components/Nav'
 
 export default function Posts() {
     return (
       <>
-        <Nav navName="Nkwashi News" menuButton="back" backTo="/" />
         <PostsList />
       </>
     )
--- app/javascript/src/modules/News/index.js
+++ app/javascript/src/modules/News/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const News = () => <div>News Module</div>;
+import News from '../../containers/Posts/Posts'
 
 export default {
   routeProps: {
