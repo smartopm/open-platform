b96c71632683ffb261d5e6df6c5f3c5e098832d0
OlivierJM <manolivier93@gmail.com>
2021-04-02 12:50:23 +0200

    fix routes

--- app/javascript/src/modules/Users/UserRoutes.js
+++ app/javascript/src/modules/Users/UserRoutes.js
@@ -1,11 +1,10 @@
 // These are exportable routes to clean up the route file
-// to: '/user/id:',
-// to: '/messages/:id',
-// to: '/plots/id:'
 import UserShow from '../../containers/UserShow';
 import UserEdit from '../../containers/UserEdit';
 import UserLogs from '../../containers/AllLogs/UserLogs';
 import IdPrintPage from '../../containers/IdPrint';
+
+// name in here is only used as key in routes, make sure it is unique
 
 const routes = [
   {
@@ -13,29 +12,32 @@ const routes = [
       path: '/user/:id',
       component: UserShow
     },
-    accessibleBy: ['admin']
+    accessibleBy: ['admin'],
+    name: 'user_info'
   },
   {
     routeProps: {
       path: '/user/:id/logs',
       component: UserLogs
     },
-    name: 'User Logs',
-    accessibleBy: ['admin']
+    accessibleBy: ['admin'],
+    name: 'user_logs'
   },
   {
     routeProps: {
       path: '/user/:id/edit',
       component: UserEdit
     },
-    accessibleBy: ['admin']
+    accessibleBy: ['admin'],
+    name: 'user_edit'
   },
   {
     routeProps: {
       path: '/print/:id/',
       component: IdPrintPage
     },
-    accessibleBy: ['admin']
+    accessibleBy: ['admin'],
+    name: 'print_id'
   }
 ];
 
