d2e479c27189bbea3a799032dd223428111bcf60
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-04-13 12:05:07 +0000

    Fix tests

---
+++ app/javascript/src/__tests__/Payment/UserPaymentPlanItem.test.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import PaymentPlan, {
+  renderPlan
+} from '../../modules/Payments/Components/UserTransactions/UserPaymentPlanItem';
+import currency from '../../__mocks__/currency';
+import { createClient } from '../../utils/apollo';
+
+describe('Render Payment Plan Item', () => {
+  const plan = {
+    amount: 200,
+    percentage: '50',
+    planType: 'lease',
+    createdAt: '2021-03-01T09:55:05Z',
+    plotBalance: 0,
+    pendingBalance: 10,
+    startDate: '2021-03-01T09:55:05Z',
+    status: 'active',
+    id: '3b464fb7-bb2b-41cb-9245-9300b6d8a729',
+    invoices: [
+      {
+        id: 'a54d6184-b10e-4865-bee7-7957701d423d',
+        amount: 500,
+        createdAt: '2021-03-01T09:55:05Z',
+        dueDate: '2021-03-06T09:55:05Z',
+        status: 'paid',
+        invoiceNumber: 123
+      }
+    ],
+    landParcel: {
+      id: '233b1634-bf08-4ece-a213-b3f120a1e009',
+      parcelNumber: 'Plot-1343'
+    }
+  };
+
+  const plans = [plan];
+
+  it('should render the payment plan item component', () => {
+    const container = render(
+      <BrowserRouter>
+        <ApolloProvider client={createClient}>
+          <PaymentPlan
+            plans={plans}
+            currencyData={currency}
+            refetch={() => {}}
+            walletRefetch={() => {}}
+          />
+        </ApolloProvider>
+      </BrowserRouter>
+    );
+
+    expect(container.getAllByTestId('plot-number')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('balance')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('start-date')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('percentage')[0]).toBeInTheDocument();
+
+    const planClick = container.queryByTestId('summary');
+    fireEvent.click(planClick);
+
+    expect(container.getAllByTestId('issue-date')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('description')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('payment-date')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('status')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('amount')[0]).toBeInTheDocument();
+    expect(container.getAllByTestId('due-date')[0]).toBeInTheDocument();
+  });
+
+  it('should check if renderPlan works as expected', () => {
+    const results = renderPlan(plan, currency);
+    expect(results).toBeInstanceOf(Object);
+    expect(results).toHaveProperty('Plot Number');
+    expect(results).toHaveProperty('Balance');
+    expect(results).toHaveProperty('Start Date');
+    expect(results).toHaveProperty('% of total valuation');
+
+    const balanceContainer = render(results.Balance);
+
+    expect(balanceContainer.queryByTestId('balance').textContent).toContain(10);
+  });
+});
