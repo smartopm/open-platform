5b3e85611b3bb39bb2eb59209c298aab0e5209a2
victor okeugo <victor@doublegdp.com>
2021-04-14 21:13:00 +0000

    add loader when reverting payment

--- app/javascript/src/__tests__/DeleteDialogueTest.test.js
+++ app/javascript/src/__tests__/DeleteDialogueTest.test.js
@@ -4,16 +4,32 @@ import '@testing-library/jest-dom/extend-expect'
 import DeleteDialogue from "../components/Business/DeleteDialogue"
 
 describe('It should render the dialog box for delete', () => {
-    const container = render(
-      <DeleteDialogue
-        open
-        handleClose={jest.fn()}
-        handleDelete={jest.fn()}
-        title="business"
-        handleAction={jest.fn}
-      />
-    )
     it('It should render with dialog', () => {
+      const container = render(
+        <DeleteDialogue
+          open
+          handleClose={jest.fn()}
+          handleDelete={jest.fn()}
+          title="business"
+          handleAction={jest.fn}
+          loading={false}
+        />
+      )
       expect(container.queryByText('Are you sure you want to delete this business?')).toBeInTheDocument()
+      expect(container.queryByTestId('confirm_action')).toBeInTheDocument()
+    });
+    it('It should render loader when confirm action is clicked', () => {
+      const container = render(
+        <DeleteDialogue
+          open
+          handleClose={jest.fn()}
+          handleDelete={jest.fn()}
+          title="business"
+          handleAction={jest.fn}
+          loading
+        />
+      )
+      expect(container.queryByText('Are you sure you want to delete this business?')).toBeInTheDocument()
+      expect(container.queryByTestId('confirm_action')).toBeNull()
     });
 });
