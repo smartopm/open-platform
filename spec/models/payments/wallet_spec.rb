196b696309f0a45e7dcebc05add408de9ca514f6
Saurabh Shinde <saurabh@doublegdp.com>
2021-04-22 16:49:03 +0530

    Modularization of rails models

--- spec/models/payments/wallet_spec.rb
+++ spec/models/payments/wallet_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Payments::Wallet, type: :model do
   end
 
   describe 'associations' do
-    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:user).class_name('Users::User') }
   end
 
   describe 'Instance Methods' do
