484e037363eb567cf2d79147d08128bebec872e8
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-04-27 21:38:53 +0000

    Test sendgrid-email lib

---
+++ spec/lib/email_msg_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'email_msg'
+
+RSpec.describe EmailMsg do
+  before { Rails.env.stub(test?: false) }
+
+  describe '.send_email' do
+    it 'initializes sendgrid email class' do
+      expect(SendGrid::Email).to receive(:new).twice.and_return(
+        SendGrid::Email.new(email: 'gmail@gmail.com'),
+      )
+      EmailMsg.send_mail('gmail@gmail.com', '12345')
+    end
+  end
+
+  describe '.send_mail_from_db' do
+    it 'initializes sendgrid email class' do
+      expect(SendGrid::Email).to receive(:new).twice.and_return(
+        SendGrid::Email.new(email: 'gmail@gmail.com'),
+      )
+      EmailMsg.send_mail_from_db('gmail@gmail.com', OpenStruct.new(subject: 'My Mail'), [])
+    end
+  end
+end
