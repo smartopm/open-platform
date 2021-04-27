691c0cd6312764b0d254eb217ab52395bd28c74c
tolulope3 <tolulope@doublegdp.com>
2021-04-27 11:19:45 +0000

    fix rubocop

--- app/graphql/types/queries/invoice.rb
+++ app/graphql/types/queries/invoice.rb
@@ -125,7 +125,8 @@ module Types::Queries::Invoice
   def invoices_stat_details(query:)
     raise GraphQL::ExecutionError, 'Unauthorized' unless context[:current_user].admin?
 
-    invoices = context[:site_community].invoices.not_paid.not_cancelled.eager_load(:user, :land_parcel)
+    invoices = context[:site_community].invoices.not_paid.not_cancelled
+                                       .eager_load(:user, :land_parcel)
     case query
     when '00-30'
       invoices.where('due_date >= ? AND due_date <= ?', 30.days.ago, Time.zone.today)
