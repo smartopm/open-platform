2584571484cb306eef42b4bcc21cec0ae3e7c533
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-04-27 18:42:46 +0000

    Push test for post_tags_alert_job

--- spec/jobs/post_tags_alert_job_spec.rb
+++ spec/jobs/post_tags_alert_job_spec.rb
@@ -3,18 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe PostTagsAlertJob, type: :job do
-  let!(:user) { create(:user_with_community) }
+  let!(:community) { create(:community, templates: { post_alert_template_id: 'fgcagv5r2yr67' }) }
+  let!(:user) { create(:user_with_community, community: community) }
+  let!(:post_tag) { create(:post_tag, community_id: community.id, slug: 'hir') }
+
+  before { Rails.env.stub(production?: true) }
 
   describe '#perform_later get community tags' do
     it 'should enqueue a job to send email to users if there is a new post' do
-      community = FactoryBot.create(:community)
+      user.post_tags << post_tag
       ActiveJob::Base.queue_adapter = :test
       expect do
         PostTagsAlertJob.perform_later(community.name)
       end.to have_enqueued_job
     end
     it 'does not invoke EmailMsg when no user follows any tag' do
-      community = FactoryBot.create(:community)
+      user.post_tags << post_tag
+      allow(URI).to receive(:open).and_return('<html />')
       expect(EmailMsg).not_to receive(:send_mail)
       perform_enqueued_jobs { described_class.perform_later(community.name) }
     end
