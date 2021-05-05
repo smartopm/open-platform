a9e8c9c9650e5a1aa6fc657d806f2e9f7eff290a
victor okeugo <victor@doublegdp.com>
2021-05-05 10:30:07 +0000

    move to tasks module

--- app/javascript/src/modules/Tasks/__tests__/TaskData.test.js
+++ app/javascript/src/modules/Tasks/__tests__/TaskData.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
-import renderTaskData, { LinkToUser } from '../modules/Tasks/Components/RenderTaskData';
+import renderTaskData, { LinkToUser } from '../Components/RenderTaskData';
 
 describe('Task Data components ', () => {
   it('should render proper the link to user component', () => {
