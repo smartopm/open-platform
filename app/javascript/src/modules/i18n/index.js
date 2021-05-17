a7a4b05f3dbcc70094f1ec0a5877e289444a09a4
tolulope3 <tolulope@doublegdp.com>
2021-05-17 09:31:48 +0000

    uncomment i18n config

--- app/javascript/src/modules/i18n/index.js
+++ app/javascript/src/modules/i18n/index.js
@@ -2,17 +2,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
-// import PhraseInContextEditorPostProcessor from 'i18next-phrase-in-context-editor-post-processor';
+import PhraseInContextEditorPostProcessor from 'i18next-phrase-in-context-editor-post-processor';
 
 i18n
   .use(Backend)
   .use(initReactI18next)
-  // .use(
-  //   new PhraseInContextEditorPostProcessor({
-  //     phraseEnabled: true,
-  //     projectId: 'c6cc13fd16dfbacd426574fa4401d762'
-  //   })
-  // )
+  .use(
+    new PhraseInContextEditorPostProcessor({
+      phraseEnabled: true,
+      projectId: 'c6cc13fd16dfbacd426574fa4401d762'
+    })
+  )
   .init({
     lng: 'en-US',
     fallbackLng: 'en-US',
