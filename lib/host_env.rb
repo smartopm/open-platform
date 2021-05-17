b98b5f44ad839c6013f7bc6bdd98fc4dbe954af5
tolulope3 <tolulope@doublegdp.com>
2021-05-17 19:37:41 +0000

    return base_url based on the comunity name

--- lib/host_env.rb
+++ lib/host_env.rb
@@ -5,12 +5,19 @@ class HostEnv
   # Method to return respective hostnames for creating links in email, sms and image url
   # Needs to be updated and made more generic as more communities start to come in
   def self.base_url(community)
-    if ENV['APP_ENV'].eql?('production')
-      return 'demo.doublegdp.com' if community.name.eql?('Doublegdp')
-
-      return ENV['HOST']
-    end
+    prod_url(community.name) if ENV['APP_ENV'].eql?('production')
 
     community.hostname
+  end
+
+  def prod_url(community_name)
+    case community_name
+    when 'DoubleGDP'
+      'demo.doublegdp.com'
+    when 'Ciudad Morazán'
+      'morazancity.doublegdp.com'
+    else
+      ENV['HOST']
+    end
   end
 end
