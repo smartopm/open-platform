093583655c6c858fd4135a2fea67b5948a0d6b33
victor okeugo <victor@doublegdp.com>
2021-05-21 14:42:34 +0000

    remove unique constraint

--- db/migrate/20210521142938_remove_unique_contraint_on_email_template.rb
+++ db/migrate/20210521142938_remove_unique_contraint_on_email_template.rb
@@ -1,6 +1,6 @@
 class RemoveUniqueContraintOnEmailTemplate < ActiveRecord::Migration[6.0]
   def change
-    remove_index :email_templates, :name, unique: true
+    remove_index :email_templates, :name
     add_index :email_templates, :name
   end
 end
