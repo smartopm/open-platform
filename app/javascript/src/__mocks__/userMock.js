ea4251b4c5c0a23d2fcfb6027805679a0b219ad3
OlivierJM <manolivier93@gmail.com>
2021-05-24 15:33:10 +0200

    theme the community name

---
+++ app/javascript/src/__mocks__/userMock.js
@@ -0,0 +1,17 @@
+const userMock = {
+    user: {
+      id: 'a54d6184-b10e-4865-bee7-7957701d423d',
+      name: 'Another somebodyy',
+      userType: 'client',
+      expiresAt: null,
+      community: {
+        supportName: 'Support Officer',
+        themeColors: {
+          primaryColor: "#nnn",
+          secondaryColor: "#nnn"
+        }
+      }
+    }
+  };
+
+  export default userMock;
