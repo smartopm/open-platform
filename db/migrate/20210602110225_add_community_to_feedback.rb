1dedd137f661c4f96489a73a8f2df35cc11c1303
OlivierJM <manolivier93@gmail.com>
2021-06-02 14:58:15 +0200

    update feedback to respective community credit: @nurudeen1

--- db/migrate/20210602110225_add_community_to_feedback.rb
+++ db/migrate/20210602110225_add_community_to_feedback.rb
@@ -1,8 +1,10 @@
 class AddCommunityToFeedback < ActiveRecord::Migration[6.0]
   def change
     add_reference :feedbacks, :community, type: :uuid, index: true, foreign_key: true
-    # Nkwashi community has most of the feedback as of now 
-    nkwashi_community = Community.find_by(name: "Nkwashi")
-    Feedback.update_all(community_id: nkwashi_community.id)
+
+    Feedback.find_each do |f|
+      f.update!(community_id: f.user.community.id)
+    end
+    
   end
 end
