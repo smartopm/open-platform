c9be22d32b1d48b114864f8bf59763a82a92b238
victor okeugo <victor@doublegdp.com>
2021-06-02 09:30:09 +0000

    make referral accessiblle tto only client and resident

--- app/javascript/src/modules/Referrals/index.js
+++ app/javascript/src/modules/Referrals/index.js
@@ -6,13 +6,8 @@ export default {
     component: Referral
   },
   accessibleBy: [
-    'admin',
     'client',
-    'prospective_client',
-    'contractor',
     'resident',
-    'visitor',
-    'custodian'
   ],
   name: t => t('misc.referrals'),
   featureName: 'Referral',
