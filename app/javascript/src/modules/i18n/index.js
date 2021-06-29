b56339bbce5e7565ebf30ff170bc27cd923fdf09
OlivierJM <manolivier93@gmail.com>
2021-06-29 12:45:28 +0200

    replace the xhr with http

--- app/javascript/src/modules/i18n/index.js
+++ app/javascript/src/modules/i18n/index.js
@@ -1,10 +1,7 @@
 /* istanbul ignore file */
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-xhr-backend';
-
-// Disabled the in-context editor integration for now 
-// import PhraseInContextEditorPostProcessor from 'i18next-phrase-in-context-editor-post-processor';
+import Backend from 'i18next-http-backend';
 
 i18n
   .use(Backend)
