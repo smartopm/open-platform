9bbef5a3e2af0d38241011b203bf9f34b41c8437
OlivierJM <manolivier93@gmail.com>
2021-06-30 19:04:16 +0200

    add translations to the entry note dialog

--- app/javascript/src/shared/dialogs/EntryNoteDialog.jsx
+++ app/javascript/src/shared/dialogs/EntryNoteDialog.jsx
@@ -5,22 +5,23 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next';
 import useDialogStyles from './dialogStyles';
 
 export default function EntryNoteDialog({ open, handleDialogStatus, observationHandler, children }) {
   const classes = useDialogStyles();
-
+  const { t } = useTranslation('logbook')
   return (
     <>
       <Dialog fullWidth open={open} onClose={handleDialogStatus} aria-labelledby="entry-dialog-title">
         <DialogTitle id="entry-dialog-title" className={classes.title}>
-          Observations
+          {t('logbook.observation_title')}
         </DialogTitle>
         <DialogContent>
           <DialogContentText />
           <TextField
             id="outlined-multiline-static"
-            label="Add your observation here"
+            label={t('logbook.add_your_observation')}
             rows={5}
             variant="outlined"
             value={observationHandler.value}
