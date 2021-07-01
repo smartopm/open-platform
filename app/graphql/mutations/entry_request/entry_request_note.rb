fec9c66ae4758e020e3ff59195d00b08964c3f95
OlivierJM <manolivier93@gmail.com>
2021-07-01 16:23:13 +0200

    change from notes to note string

--- app/graphql/mutations/entry_request/entry_request_note.rb
+++ app/graphql/mutations/entry_request/entry_request_note.rb
@@ -7,22 +7,17 @@ module Mutations
       argument :id, ID, required: true
       argument :note, String, required: false
 
-      field :event, Types::EntryRequestType, null: true
+      field :entry, Types::EventLogType, null: true
 
-      # rubocop:disable Metrics/AbcSize
       def resolve(vals)
         request = context[:site_community].entry_requests.find_by(id: vals[:id])
         raise GraphQL::ExecutionError, I18n.t('errors.entry_request.not_found') unless request
 
-        # Add an id to make it easy to identify observations
-        data = [{ id: SecureRandom.uuid, time: Time.zone.now, note: vals[:note] }]
+        evt = context[:current_user].generate_events('observation_log', request, note: vals[:note])
+        raise GraphQL::ExecutionError, evt.errors.full_messages if evt.blank?
 
-        event = context[:current_user].generate_events('observation_log', request, notes: data)
-        raise GraphQL::ExecutionError, event.errors.full_messages if event.blank?
-
-        { event: event }
+        { event: evt }
       end
-      # rubocop:enable Metrics/AbcSize
 
       # Verifies if current user is present or not.
       def authorized?(_vals)
