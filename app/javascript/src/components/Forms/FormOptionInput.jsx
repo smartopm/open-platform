7c89b2dafaf9be336c5d9a9ec36f2a408bb25a60
Saurabh Shinde <saurabh@doublegdp.com>
2021-07-02 21:17:25 +0530

    Update phone number field from TextField to Phoneinput

--- app/javascript/src/components/Forms/FormOptionInput.jsx
+++ app/javascript/src/components/Forms/FormOptionInput.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { IconButton, TextField, Typography } from '@material-ui/core'
 import { AddCircleOutline, DeleteOutline } from '@material-ui/icons'
+import PhoneInput from 'react-phone-input-2'
+import { useTranslation } from 'react-i18next';
 import { propAccessor } from '../../utils/helpers'
 
 /**
@@ -11,6 +13,7 @@ import { propAccessor } from '../../utils/helpers'
  * @example Phone Number => Phone Number 1, Phone Number 2
  */ 
 export default function FormOptionInput({ options, setOptions, label }) {
+  const { t } = useTranslation('common');
   function handleOptionChange(event, index){
     const values = options
     values[parseInt(index, 10)] = event.target.value
@@ -32,17 +35,54 @@ export default function FormOptionInput({ options, setOptions, label }) {
         options.map((value, i) => (
           // eslint-disable-next-line react/no-array-index-key
           <div key={i}>
-            <TextField
-              label={`${label} ${i}`}
-              variant="outlined"
-              size="small"
-              style={{ width: 300 }}
-              value={propAccessor(options, i)?.info}
-              onChange={event => handleOptionChange(event, i)}
-              margin="normal"
-              autoFocus
-              required
-            />
+            <label
+              htmlFor='phoneNumber'
+              className="MuiFormLabel-root MuiInputLabel-shrink
+              MuiInputLabel-root"
+            >
+              Secondary Phone Number&nbsp;
+              {i}
+              <span aria-hidden="true" className="MuiFormLabel-asterisk MuiInputLabel-asterisk">
+              &nbsp;*
+              </span>
+            </label>
+            {(label === t('common:form_fields.secondary_number'))
+              ? (
+                <PhoneInput
+                  value={propAccessor(options, i)?.info}
+                  enableSearch
+                  inputProps={{
+                   name: 'phoneNumber',
+                   required: true
+                 }}
+                  placeholder={t('common:form_placeholders.phone_number')}
+                  onChange={val => {
+                  const values = options
+                  values[parseInt(i, 10)] = val
+                  setOptions([...options])
+                 }}
+                  preferredCountries={['hn', 'zm', 'ng', 'in', 'us']}
+                  containerStyle={{
+                   width: '30%',
+                   display: 'inline-block',
+                   marginRight: '5px'
+                }}
+                />
+)
+              : (
+                <TextField
+                  label={`${label} ${i}`}
+                  variant="outlined"
+                  size="small"
+                  style={{ width: 300 }}
+                  value={propAccessor(options, i)?.info}
+                  onChange={event => handleOptionChange(event, i)}
+                  margin="normal"
+                  autoFocus
+                  required
+                />
+)}
+
             <IconButton
               style={{ marginTop: 13 }}
               onClick={handleRemoveOption}
