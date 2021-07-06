cd7ffca6c926a7ab4b7071b96ac0d62bc6346eb6
OlivierJM <manolivier93@gmail.com>
2021-07-06 23:02:15 +0200

    update current event log when exiting

--- app/graphql/mutations/entry_request/entry_request_note.rb
+++ app/graphql/mutations/entry_request/entry_request_note.rb
@@ -5,28 +5,42 @@ module Mutations
     # Add an observation note to an entry request
     class EntryRequestNote < BaseMutation
       argument :id, ID, required: false
+      argument :event_log_id, ID, required: false
       argument :ref_type, String, required: false
       argument :note, String, required: false
 
       field :event, Types::EventLogType, null: true
 
       # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def resolve(vals)
         if vals[:note].blank?
           raise GraphQL::ExecutionError, I18n.t('errors.entry_request.empty_note')
         end
 
-        log = vals[:ref_type]&.constantize&.find_by(
-          id: vals[:id],
-          community_id: context[:site_community].id,
-        )
+        ActiveRecord::Base.transaction do
+          log = vals[:ref_type]&.constantize&.find_by(
+            id: vals[:id],
+            community_id: context[:site_community].id,
+          )
+          update_prev_log(vals[:event_log_id])
 
-        evt = context[:current_user].generate_events('observation_log', log, note: vals[:note])
-        raise GraphQL::ExecutionError, evt.errors.full_messages if evt.blank?
+          evt = context[:current_user].generate_events('observation_log', log, note: vals[:note])
+          raise GraphQL::ExecutionError, evt.errors.full_messages if evt.blank?
 
-        { event: evt }
+          { event: evt }
+        end
       end
       # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
+
+      def update_prev_log(event_id)
+        event = context[:site_community].event_logs.find_by(id: event_id)
+        return if event.nil?
+
+        event.data['exited'] = true
+        event.save!
+      end
 
       # Verifies if current user is present or not.
       def authorized?(_vals)
