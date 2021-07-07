fb78546554ee88288bf3ed716e972f83e9227fd4
OlivierJM <manolivier93@gmail.com>
2021-07-07 14:41:13 +0200

    make the expires_at optional on forms

--- app/javascript/src/components/Forms/FormList.jsx
+++ app/javascript/src/components/Forms/FormList.jsx
@@ -116,7 +116,6 @@ export default function FormLinkList({ userType, community }) {
           >
             <DateAndTimePickers
               label="Form Expiry Date"
-              required
               selectedDateTime={expiresAt}
               handleDateChange={handleDateChange}
               pastDate
--- app/models/forms/form.rb
+++ app/models/forms/form.rb
@@ -11,13 +11,8 @@ module Forms
     has_many :form_users, dependent: :destroy
 
     validates :name, presence: true, uniqueness: true
-    validates :expires_at, presence: true
 
     default_scope { where.not(status: 2) }
-
-    scope :by_user_type, lambda { |user|
-      where('expires_at > ?', Time.zone.now).where(status: 1) if user.user_type != 'admin'
-    }
     enum status: { draft: 0, published: 1, deleted: 2 }
   end
 end
