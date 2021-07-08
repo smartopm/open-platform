207f501031dc4b8c749ae92c77e864232dcd1c1b
OlivierJM <manolivier93@gmail.com>
2021-07-08 17:11:32 +0200

    add more tests

---
+++ app/javascript/src/modules/Forms/__tests__/containers/FormBuilderPage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom/';
+import { MockedProvider } from '@apollo/react-testing';
+import FormBuilderPage from '../../containers/FormBuilderPage';
+
+
+
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
+
+describe('FormBuilderPage Component', () => {
+  it('renders FormBuilder text', () => {
+    render(
+      <MockedProvider>
+        <BrowserRouter>
+          <FormBuilderPage />
+        </BrowserRouter>
+      </MockedProvider>
+    );
+
+  });
+});
