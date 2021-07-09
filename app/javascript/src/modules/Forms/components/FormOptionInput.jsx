35040e3b9fa99db5255e5c73d6f44e76e47602ce
OlivierJM <manolivier93@gmail.com>
2021-07-09 14:55:16 +0200

    add i18n to form option and form property creation

--- app/javascript/src/modules/Forms/components/FormOptionInput.jsx
+++ app/javascript/src/modules/Forms/components/FormOptionInput.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IconButton, TextField, Typography } from '@material-ui/core'
-import { AddCircleOutline, DeleteOutline } from '@material-ui/icons'
-import PhoneInput from 'react-phone-input-2'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IconButton, TextField, Typography } from '@material-ui/core';
+import { AddCircleOutline, DeleteOutline } from '@material-ui/icons';
+import PhoneInput from 'react-phone-input-2';
 import { useTranslation } from 'react-i18next';
-import { propAccessor } from '../../../utils/helpers'
+import { propAccessor } from '../../../utils/helpers';
 
 /**
  *
@@ -14,102 +14,95 @@ import { propAccessor } from '../../../utils/helpers'
  */
 export default function FormOptionInput({ options, setOptions, label }) {
   const { t } = useTranslation('common');
-  function handleOptionChange(event, index){
-    const values = options
-    values[parseInt(index, 10)] = event.target.value
-    setOptions([...values])
+  function handleOptionChange(event, index) {
+    const values = options;
+    values[parseInt(index, 10)] = event.target.value;
+    setOptions([...values]);
   }
 
-  function handleAddOption(){
-    setOptions([...options, ""])
+  function handleAddOption() {
+    setOptions([...options, '']);
   }
 
-  function handleRemoveOption(index){
-    const values = options
-    values.splice(index, 1)
-    setOptions([...values])
+  function handleRemoveOption(index) {
+    const values = options;
+    values.splice(index, 1);
+    setOptions([...values]);
   }
   return (
     <div>
-      {
-        options.map((value, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={i}>
-            {(label === t('common:form_fields.secondary_number'))
-              ? (
-                <>
-                  <label
-                    htmlFor='phoneNumber'
-                    className="MuiFormLabel-root MuiInputLabel-shrink
+      {options.map((value, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i}>
+          {label === t('form_fields.secondary_number') ? (
+            <>
+              <label
+                htmlFor="phoneNumber"
+                className="MuiFormLabel-root MuiInputLabel-shrink
               MuiInputLabel-root"
-                  >
-                    Secondary Phone Number&nbsp;
-                    {i}
-                    <span aria-hidden="true" className="MuiFormLabel-asterisk MuiInputLabel-asterisk">
-              &nbsp;*
-                    </span>
-                  </label>
-                  <PhoneInput
-                    value={propAccessor(options, i)?.info}
-                    enableSearch
-                    inputProps={{
-                   name: 'phoneNumber',
-                   required: true
-                 }}
-                    placeholder={t('common:form_placeholders.phone_number')}
-                    onChange={val => {
-                  const values = options
-                  values[parseInt(i, 10)] = val
-                  setOptions([...options])
-                 }}
-                    preferredCountries={['hn', 'zm', 'ng', 'in', 'us']}
-                    containerStyle={{
-                   width: '30%',
-                   display: 'inline-block',
-                   marginRight: '5px'
+              >
+                {t('form_fields.secondary_number')}
+                {i}
+                <span aria-hidden="true" className="MuiFormLabel-asterisk MuiInputLabel-asterisk">
+                  &nbsp;*
+                </span>
+              </label>
+              <PhoneInput
+                value={propAccessor(options, i)?.info}
+                enableSearch
+                inputProps={{
+                  name: 'phoneNumber',
+                  required: true,
                 }}
-                  />
-                </>
-)
-              : (
-                <TextField
-                  label={`${label} ${i}`}
-                  variant="outlined"
-                  size="small"
-                  style={{ width: 300 }}
-                  value={propAccessor(options, i)?.info}
-                  onChange={event => handleOptionChange(event, i)}
-                  margin="normal"
-                  autoFocus
-                  required
-                />
-)}
+                placeholder={t('form_placeholders.phone_number')}
+                onChange={(val) => {
+                  const values = options;
+                  values[parseInt(i, 10)] = val;
+                  setOptions([...options]);
+                }}
+                preferredCountries={['hn', 'zm', 'ng', 'in', 'us']}
+                containerStyle={{
+                  width: '30%',
+                  display: 'inline-block',
+                  marginRight: '5px',
+                }}
+              />
+            </>
+          ) : (
+            <TextField
+              label={`${label} ${i}`}
+              variant="outlined"
+              size="small"
+              style={{ width: 300 }}
+              value={propAccessor(options, i)?.info}
+              onChange={(event) => handleOptionChange(event, i)}
+              margin="normal"
+              autoFocus
+              required
+            />
+          )}
 
-            <IconButton
-              style={{ marginTop: 13 }}
-              onClick={handleRemoveOption}
-              aria-label="remove"
-            >
-              <DeleteOutline />
-            </IconButton>
-          </div>
-        ))
-      }
+          <IconButton style={{ marginTop: 13 }} onClick={handleRemoveOption} aria-label="remove">
+            <DeleteOutline />
+          </IconButton>
+        </div>
+      ))}
       <IconButton onClick={handleAddOption} aria-label="add" style={{ marginLeft: -15 }}>
         <AddCircleOutline />
         <Typography color="primary" style={{ marginLeft: 10 }}>
-          { `  Add ${label}` }
+          {t('form_actions.add_type', { label })}
         </Typography>
       </IconButton>
     </div>
-  )
+  );
 }
 
 export function FormOptionWithOwnActions({ actions, value, id }) {
+  const { t } = useTranslation('common');
   return (
     <div>
       <TextField
-        label={`option ${id}`}
+        label={t('misc.option_with_count', { id })}
         variant="outlined"
         size="small"
         defaultValue={value}
@@ -127,8 +120,7 @@ export function FormOptionWithOwnActions({ actions, value, id }) {
         <DeleteOutline />
       </IconButton>
     </div>
-
-  )
+  );
 }
 
 FormOptionInput.propTypes = {
@@ -144,14 +136,14 @@ FormOptionInput.propTypes = {
   /**
    * Description of a field being updated
    */
-  label: PropTypes.string.isRequired
-}
+  label: PropTypes.string.isRequired,
+};
 
 FormOptionWithOwnActions.propTypes = {
   actions: PropTypes.shape({
     handleRemoveOption: PropTypes.func,
-    handleOptionChange: PropTypes.func
+    handleOptionChange: PropTypes.func,
   }).isRequired,
   value: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired
-}
+  id: PropTypes.number.isRequired,
+};
