3ef474700cee06fc83c9a4159795b9fabf89d972
OlivierJM <manolivier93@gmail.com>
2021-07-14 15:06:10 +0200

    add testss for creating a new property f.e

--- app/javascript/src/modules/Forms/__tests__/FormPropertyCreateForm.test.js
+++ app/javascript/src/modules/Forms/__tests__/FormPropertyCreateForm.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { MockedProvider } from '@apollo/react-testing';
 import FormPropertyCreateForm from '../components/FormPropertyCreateForm';
 import { FormPropertyQuery } from '../graphql/forms_queries';
-import { FormPropertyUpdateMutation } from '../graphql/forms_mutation';
+import { FormPropertyUpdateMutation, FormPropertyCreateMutation } from '../graphql/forms_mutation';
 
 describe('Form that creates other forms component', () => {
   // create a query mock
@@ -33,7 +33,7 @@ describe('Form that creates other forms component', () => {
       }
     }
   };
-  const createPropertyMock = {
+  const updatePropertyMock = {
     request: {
       query: FormPropertyUpdateMutation,
       variables: {
@@ -60,11 +60,38 @@ describe('Form that creates other forms component', () => {
       }
     }
   };
+  const createPropertyMock = {
+    request: {
+      query: FormPropertyCreateMutation,
+      variables: {
+        formId: '39c3b38e-136d-42e0',
+        fieldName: '',
+        fieldType: '',
+        required: false,
+        adminUse: false,
+        order: '1',
+        fieldValue: [{ value: '', label: '' }]
+      }
+    },
+    result: {
+      data: {
+        formPropertiesCreate: {
+          formProperty: {
+            id: '5290d212-edf8-4c1e-a20b',
+            fieldName: '',
+            fieldType: '',
+            __typename: 'FormProperties'
+          },
+          __typename: 'FormPropertiesCreatePayload'
+        }
+      }
+    }
+  };
   it('it should render with no errors and should update the form property', async () => {
     const closeMock = jest.fn();
     const refetchMock = jest.fn();
     const container = render(
-      <MockedProvider mocks={[mocks, createPropertyMock]} addTypename>
+      <MockedProvider mocks={[mocks, updatePropertyMock]} addTypename>
         <FormPropertyCreateForm
           propertyId={mocks.request.variables.formPropertyId}
           formId="39c3b38e-136d-42e0"
@@ -92,4 +119,30 @@ describe('Form that creates other forms component', () => {
       expect(container.queryByText('misc.updated_form_property')).toBeInTheDocument();
     }, 50);
   });
+
+  it('should create a form property when a form is submitted', async () => {
+    const closeMock = jest.fn();
+    const refetchMock = jest.fn();
+    const container = render(
+      <MockedProvider mocks={[mocks, createPropertyMock]} addTypename>
+        <FormPropertyCreateForm
+          formId={createPropertyMock.request.variables.formId}
+          refetch={refetchMock}
+          close={closeMock}
+        />
+      </MockedProvider>
+    );
+    expect(container.queryByText('form_fields.field_name')).toBeInTheDocument();
+    expect(container.queryByTestId('form_property_submit')).toBeInTheDocument();
+    expect(container.queryByText('actions.add_form_property')).toBeInTheDocument();
+
+    fireEvent.change(container.queryByTestId('field_name'), { target: { value: 'Field 1' } });
+    fireEvent.submit(container.queryByTestId('form_property_submit'));
+
+    await waitFor(() => {
+      expect(closeMock).not.toBeCalled();
+      expect(refetchMock).toBeCalled();
+      expect(container.queryByText('misc.created_form_property')).toBeInTheDocument();
+    }, 50);
+  })
 });
