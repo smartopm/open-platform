3bb41b37d05052db4edcd4d741cde5efa3653e7c
OlivierJM <manolivier93@gmail.com>
2021-07-19 20:25:09 +0200

    remove unused icon in the index file of report

--- app/javascript/src/modules/Reports/index.js
+++ app/javascript/src/modules/Reports/index.js
@@ -1,14 +1,9 @@
-import React from 'react'
-import { SearchOutlined } from '@material-ui/icons';
 import ReportList from './components/Report';
 
 export default {
   routeProps: {
     path: '/reports',
     component: ReportList
-  },
-  styleProps: {
-    icon: <SearchOutlined />
   },
   name: t => t('menu.report'),
   featureName: 'Reports',
@@ -18,7 +13,7 @@ export default {
       routeProps: {
         path: '/customs_report', // reports/:id
         exact: true,
-        component: ReportList
+        component: ReportList // change to actual component that shows one report
       },
       name: 'Customs Report',
       accessibleBy: ['admin']
