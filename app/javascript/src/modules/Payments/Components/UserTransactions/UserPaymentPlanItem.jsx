575bb9c10e47b9bf3115e08f6617632d18474296
Saurabh Shinde <saurabh@doublegdp.com>
2021-07-21 11:31:00 +0530

    Added modal UI to transfer payment plan

--- app/javascript/src/modules/Payments/Components/UserTransactions/UserPaymentPlanItem.jsx
+++ app/javascript/src/modules/Payments/Components/UserTransactions/UserPaymentPlanItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { useMutation, useLazyQuery } from 'react-apollo';
