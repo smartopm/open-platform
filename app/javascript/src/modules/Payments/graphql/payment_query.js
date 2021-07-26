de921d4d54b5afc810e700a74f963c803ed38104
tolulope3 <tolulope@doublegdp.com>
2021-07-26 09:45:03 +0000

    add ids

--- app/javascript/src/modules/Payments/graphql/payment_query.js
+++ app/javascript/src/modules/Payments/graphql/payment_query.js
@@ -53,9 +53,11 @@ export const UserPlans = gql`
       endDate
       frequency
       coOwners {
+        id
         name
       }
       landParcel {
+        id
         parcelNumber
       }
       planPayments {
@@ -65,16 +67,20 @@ export const UserPlans = gql`
         status
         receiptNumber
         paymentPlan {
+          id
           pendingBalance
           landParcel {
+            id
             parcelNumber
           }
         }
         userTransaction {
+          id
           source
           transactionNumber
           allocatedAmount
           depositor {
+            id
             name
           }
         }
