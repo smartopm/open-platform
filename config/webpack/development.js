8c30dd15ff78e3e195d1a227efdab16e5f70aef1
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-03 18:46:03 +0000

    Upgrade webpacker to v6

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,18 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const { devServer } = require("@rails/webpacker");
-// eslint-disable-next-line import/no-extraneous-dependencies
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const webpackConfig = require('./base')
 
-const environment = require("./environment");
-
-environment.plugins.append(
-    'ReactRefreshWebpackPlugin',
-    new ReactRefreshWebpackPlugin({
-      overlay: {
-        sockPort: devServer.port
-      }
-    })
-  )
-
-module.exports = environment.toWebpackConfig();
+module.exports = webpackConfig
