91a3ced0066b478daacc5a1907aa5ae18a38993a
OlivierJM <manolivier93@gmail.com>
2021-08-06 14:31:46 +0200

    add utils to help count and group extra hours

--- app/javascript/src/modules/Reports/utils.js
+++ app/javascript/src/modules/Reports/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import { dateToString } from "../../components/DateContainer";
 
 /**
@@ -18,3 +19,37 @@ import { dateToString } from "../../components/DateContainer";
     }
     return data.value;
   }
+
+export function checkExtraShifts(formattedShifts){
+  const shifts = formattedShifts;
+    const extras = []
+    for (let index = 0; index < shifts.length; index++) {
+      const current = shifts[Number(index)];
+      const next = shifts.length === index + 1 ? current : shifts[index + 1];
+      if (+next[0] > +current[0]) {
+        extras.push([current[0], next[1]])
+        shifts.splice(index, 1)
+      }
+      // eslint-disable-next-line eqeqeq
+      if(next == current){
+        extras.push([next[0], next[1]])
+      }
+    }
+   return extras
+}
+
+
+export function countShifts(shifts) {
+  const extraHours = checkExtraShifts(shifts)
+  const hours = []
+  for (let index = 0; index < extraHours.length; index++) {
+    const element = extraHours[Number(index)];
+    const diff = +element[1] - +element[0]
+    hours.push(diff)
+  }
+  return hours.reduce((a, b) => a + b)
+}
+
+export function formatShifts(entryShift, exitShift){
+  return entryShift.map((e, i) => [e.value, exitShift[Number(i)].value])
+}
