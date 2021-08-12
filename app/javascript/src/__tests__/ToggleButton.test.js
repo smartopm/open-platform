57a4150c567de14347b7fbb63cb726c0e022949c
victor okeugo <victor@doublegdp.com>
2021-08-12 08:10:15 +0000

    add test

--- app/javascript/src/__tests__/ToggleButton.test.js
+++ app/javascript/src/__tests__/ToggleButton.test.js
@@ -4,24 +4,44 @@ import {
     render
 } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import ToggleButton from "../components/Campaign/ToggleButton"
+import ToggleButton, { MultipleToggler } from "../components/Campaign/ToggleButton"
 
 describe('It should render the toggle button delete', () => {
   const mock = jest.fn()
-  const container = render(
-    <ToggleButton
-      type='draft'
-      handleType={mock}
-      data={{
-        type: 'draft',
-        antiType: 'scheduled'
-      }}
-    />
-  )
+ 
   it('It should render toggle button', () => {
-    expect(container.queryByText('Draft')).toBeInTheDocument()
-    expect(container.queryByText('Scheduled')).toBeInTheDocument()
-    fireEvent.click(container.queryByLabelText('draft'))
+    const container = render(
+      <ToggleButton
+        type='draft'
+        handleType={mock}
+        data={{
+          type: 'draft',
+          antiType: 'scheduled'
+        }}
+      />
+    )
+
+    expect(container.queryAllByText('Draft')[0]).toBeInTheDocument()
+    expect(container.queryAllByText('Scheduled')[0]).toBeInTheDocument()
+    fireEvent.click(container.queryAllByLabelText('draft')[0])
     expect(mock).toBeCalled()
   });
+
+  it('It should render multiple toggle button', () => {
+    const container = render(
+      <MultipleToggler
+        type='plots'
+        handleType={mock}
+        options={['plots', 'map', 'houses']}
+      />
+    )
+    expect(container.queryByText('Plots')).toBeInTheDocument()
+    expect(container.queryByText('Map')).toBeInTheDocument()
+    expect(container.queryByText('Houses')).toBeInTheDocument()
+    fireEvent.click(container.queryByLabelText('plots'))
+    expect(mock).toBeCalled()
+    fireEvent.click(container.queryByLabelText('houses'))
+    expect(mock).toBeCalled()
+  });
+
 });
