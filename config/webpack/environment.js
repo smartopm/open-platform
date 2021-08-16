d7465b7d85fe6400fd44cb728347bdbc9e7e089f
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-16 20:15:14 +0000

    Revert webpack changes

---
+++ config/webpack/environment.js
@@ -0,0 +1,13 @@
+const { environment } = require("@rails/webpacker");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"]
+  })
+);
+module.exports = environment;
