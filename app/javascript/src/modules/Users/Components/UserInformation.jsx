6c924c0e3819b4a5badcea9c34ca3e558581c9fd
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-16 19:14:33 +0000

    Revert webpack changes

--- app/javascript/src/modules/Users/Components/UserInformation.jsx
+++ app/javascript/src/modules/Users/Components/UserInformation.jsx
@@ -20,12 +20,12 @@ import UserNotes from './UserNote';
 import UserDetail from './UserProfileDetail';
 import { TabPanel } from '../../../components/Tabs';
 import UserFilledForms from './UserFilledForms';
-import UserMessages from '../../../components/Messaging/UserMessages'
+import UserMessages from '../../../components/Messaging/UserMessages';
 import UserJourney from './UserJourney';
 import { useParamsQuery } from '../../../utils/helpers';
-import RightSideMenu from '../../Menu/component/RightSideMenu'
+import RightSideMenu from '../../Menu/component/RightSideMenu';
 import FeatureCheck from '../../Features';
-import PaymentPlans from '../../Payments/Components/UserTransactions/Plans'
+import PaymentPlans from '../../Payments/Components/UserTransactions/Plans';
 import ShiftButtons from '../../TimeCard/Components/ShiftButtons';
 
 export default function UserInformation({
@@ -67,9 +67,8 @@ export default function UserInformation({
     } else {
       setValue('Contacts');
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [path, tab]);
-
 
   useEffect(() => {
     // open merge modal
@@ -88,7 +87,7 @@ export default function UserInformation({
   }
 
   return (
-    <div style={{overflow: 'hidden'}}>
+    <div style={{ overflow: 'hidden' }}>
       <>
         <Dialog
           open={isDialogOpen}
@@ -109,11 +108,13 @@ export default function UserInformation({
         </Dialog>
 
         <Grid container>
-          <Grid item xs={3}>{' '}</Grid>
-          <Grid item xs={6} style={{textAlign: 'center'}}>
+          <Grid item xs={3}>
+            {' '}
+          </Grid>
+          <Grid item xs={6} style={{ textAlign: 'center' }}>
             <Avatar
               user={data.user}
-                // eslint-disable-next-line react/style-prop-object
+              // eslint-disable-next-line react/style-prop-object
               style="big"
             />
           </Grid>
@@ -126,12 +127,12 @@ export default function UserInformation({
                 aria-haspopup="true"
                 onClick={() => setDrawerOpen(true)}
                 style={{
-                    float: 'right',
-                    marginRight: -23
-                  }}
+                  float: 'right',
+                  marginRight: -23
+                }}
               >
                 <DoubleArrowOutlinedIcon
-                    // this is hacky, it should be replaced with a proper icon
+                  // this is hacky, it should be replaced with a proper icon
                   style={{ transform: 'translate(-50%,-50%) rotate(180deg)' }}
                 />
               </IconButton>
@@ -146,11 +147,15 @@ export default function UserInformation({
         </Grid>
 
         <Grid container>
-          <Grid item xs={matches ? 3 : 1}>{' '}</Grid>
-          <Grid item xs={matches ? 6 : 10} style={{textAlign: 'center', marginTop: '30px'}}>
+          <Grid item xs={matches ? 3 : 1}>
+            {' '}
+          </Grid>
+          <Grid item xs={matches ? 6 : 10} style={{ textAlign: 'center', marginTop: '30px' }}>
             <UserDetail data={data} userType={userType} />
           </Grid>
-          <Grid item xs={matches ? 3 : 1}>{' '}</Grid>
+          <Grid item xs={matches ? 3 : 1}>
+            {' '}
+          </Grid>
         </Grid>
 
         <br />
@@ -158,7 +163,7 @@ export default function UserInformation({
           {authState.user.userType === 'custodian' &&
             ['security_guard', 'contractor'].includes(data.user.userType) && (
               <ShiftButtons userId={userId} />
-          )}
+            )}
         </FeatureCheck>
 
         {['admin'].includes(userType) && (
@@ -168,11 +173,11 @@ export default function UserInformation({
                 <div className="container">
                   <form id="note-form">
                     <div className="form-group">
-                      {t("common:misc.notes")}
+                      {t('common:misc.notes')}
                       <br />
                       <TextField
                         className="form-control"
-                        placeholder={t("common:form_placeholders.add_note")}
+                        placeholder={t('common:form_placeholders.add_note')}
                         id="notes"
                         rows="4"
                         inputRef={register({ required: true })}
@@ -189,7 +194,9 @@ export default function UserInformation({
                       color="primary"
                       variant="outlined"
                     >
-                      {mutationLoading ? t("common:form_actions.saving") : t("common:form_actions.save")}
+                      {mutationLoading
+                        ? t('common:form_actions.saving')
+                        : t('common:form_actions.save')}
                     </Button>
                   </form>
                   <br />
@@ -197,7 +204,6 @@ export default function UserInformation({
                   <UserNotes tabValue={tabValue} userId={data.user.id} />
                 </div>
               </TabPanel>
-
             </FeatureCheck>
             <FeatureCheck features={authState.user.community.features} name="Messages">
               <TabPanel value={tabValue} index="Communication">
@@ -207,22 +213,22 @@ export default function UserInformation({
           </>
         )}
         {!['security_guard', 'custodian'].includes(userType) && (
-        <>
-          <FeatureCheck features={authState.user.community.features} name="Properties">
-            <TabPanel value={tabValue} index="Plots">
-              <UserPlotInfo
-                account={accountData?.user?.accounts || []}
-                userId={data.user.id}
-                userName={data.user.name}
-              />
-            </TabPanel>
-          </FeatureCheck>
-          <FeatureCheck features={authState.user.community.features} name="Forms">
-            <TabPanel value={tabValue} index="Forms">
-              <UserFilledForms userFormsFilled={data.user.formUsers} userId={data.user.id} />
-            </TabPanel>
-          </FeatureCheck>
-        </>
+          <>
+            <FeatureCheck features={authState.user.community.features} name="Properties">
+              <TabPanel value={tabValue} index="Plots">
+                <UserPlotInfo
+                  account={accountData?.user?.accounts || []}
+                  userId={data.user.id}
+                  userName={data.user.name}
+                />
+              </TabPanel>
+            </FeatureCheck>
+            <FeatureCheck features={authState.user.community.features} name="Forms">
+              <TabPanel value={tabValue} index="Forms">
+                <UserFilledForms userFormsFilled={data.user.formUsers} userId={data.user.id} />
+              </TabPanel>
+            </FeatureCheck>
+          </>
         )}
         <FeatureCheck features={authState.user.community.features} name="Payments">
           <TabPanel value={tabValue} index="Plans">
@@ -244,13 +250,8 @@ export default function UserInformation({
 
         <div className="container d-flex justify-content-between">
           {data.user.state === 'valid' && authState.user.userType === 'security_guard' ? (
-            <Button
-              id="log-entry"
-              className="log-entry-btn"
-              color="primary"
-              onClick={onLogEntry}
-            >
-              {t("common:misc.log_entry")}
+            <Button id="log-entry" className="log-entry-btn" color="primary" onClick={onLogEntry}>
+              {t('common:misc.log_entry')}
             </Button>
           ) : null}
 
@@ -262,7 +263,7 @@ export default function UserInformation({
               href={`tel:${authState.user.community.securityManager}`}
               color="primary"
             >
-              {t("common:misc.call_manager")}
+              {t('common:misc.call_manager')}
             </Button>
           ) : null}
         </div>
@@ -297,5 +298,5 @@ const styles = StyleSheet.create({
   linkItem: {
     color: '#000000',
     textDecoration: 'none'
-  },
+  }
 });
