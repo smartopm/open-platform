8f2cf99a719f606214f87e48bba30f047364591b
OlivierJM <manolivier93@gmail.com>
2021-08-17 19:50:49 +0200

    wrap up on the form context

--- app/javascript/src/modules/Forms/components/RenderForm.js
+++ app/javascript/src/modules/Forms/components/RenderForm.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
+import React, { Fragment, useRef, useContext } from 'react';
 import { Grid } from '@material-ui/core';
-import React, { Fragment , useRef , useContext , useState } from 'react'
-import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types'
 import DatePickerDialog from '../../../components/DatePickerDialog';
 import { Context } from '../../../containers/Provider/AuthStateProvider';
 import ImageAuth from '../../../shared/ImageAuth';
@@ -13,200 +11,213 @@ import UploadField from './FormProperties/UploadField';
 import SignaturePad from './FormProperties/SignaturePad';
 import FormPropertyAction from './FormPropertyAction';
 import { FormContext } from '../Context';
+import { convertBase64ToFile } from '../../../utils/helpers';
 
-const initialData = {
-    fieldType: '',
-    fieldName: ' ',
-    date: { value: null },
-    radio: { value: { label: '', checked: null } }
-  };
+export default function RenderForm({ formPropertiesData, formId, refetch, editMode, categoryId }) {
+  const signRef = useRef(null);
+  const authState = useContext(Context);
+  const {
+    formState,
+    formProperties,
+    uploadedImages,
+    setFormProperties,
+    setFormState,
+    onChange,
+    signature,
+  } = useContext(FormContext);
 
-export default  function RenderForm({formPropertiesData, formId, refetch, editMode, categoryId}) {
-    const [properties, setProperties] = useState(initialData);
-    const [message, setMessage] = useState({ err: false, info: '', signed: false });
-    // const [isSubmitting, setSubmitting] = useState(false);
-    const [isUploading, setIsUploading] = useState(false);
-    const [alertOpen, setAlertOpen] = useState(false);
-    const [currentPropId, setCurrentPropertyId] = useState('');
-    const [uploadedImages, setUploadedImages] = useState([]);
-    const signRef = useRef(null);
-    const { t } = useTranslation(['form', 'common']);
-    const authState = useContext(Context);
-    const { setFormProperties } = useContext(FormContext)
 
-    function handleValueChange(event, propId) {
-        const { name, value } = event.target;
-        setFormProperties({
-          ...properties,
-          [name]: { value, form_property_id: propId }
-        });
-      }
-
-      function handleDateChange(date, id) {
-        setFormProperties({
-          ...properties,
-          date: { value: date, form_property_id: id }
-        });
-      }
-
-      function handleRadioValueChange(event, propId, fieldName) {
-        const { name, value } = event.target;
-        setFormProperties({
-          ...properties,
-          [fieldName]: { value: { checked: value, label: name }, form_property_id: propId }
-        });
-      }
-
-      function onImageSelect(event, currentProperty) {
-        setCurrentPropertyId(currentProperty);
-        // onChange(event.target.files[0]);
-        setIsUploading(true);
-      }
-
-      async function handleSignatureUpload() {
-        setMessage({ ...message, signed: true });
-        const url64 = signRef.current.toDataURL('image/png');
-        // convert the file
-        // const signature = await convertBase64ToFile(url64);
-        // await uploadSignature(signature);
-      }
-    const editable = !formPropertiesData.adminUse
-      ? false
-      : !(formPropertiesData.adminUse && authState.user.userType === 'admin');
-    const uploadedFile = uploadedImages.find(im => im.propertyId === formPropertiesData.id);
-    const fields = {
-      text: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <TextInput
-              id={formPropertiesData.id}
-              properties={formPropertiesData}
-              defaultValue={properties.fieldName}
-              handleValue={event => handleValueChange(event, formPropertiesData.id)}
-              editable={editable}
-            />
-          </Grid>
-        </Grid>
-      ),
-      date: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <DatePickerDialog
-              id={formPropertiesData.id}
-              selectedDate={properties.date.value}
-              handleDateChange={date => handleDateChange(date, formPropertiesData.id)}
-              label={formPropertiesData.fieldName}
-            />
-          </Grid>
-        </Grid>
-      ),
-      file_upload: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <UploadField
-              detail={{ type: 'file', label: formPropertiesData.fieldName }}
-              upload={evt => onImageSelect(evt, formPropertiesData.id)}
-              editable={editable}
-              uploaded={!!uploadedFile}
-            />
-          </Grid>
-          {isUploading && currentPropId === formPropertiesData.id ? (
-            <Spinner />
-          ) : (
-            !!uploadedFile && (
-              <ImageAuth
-                type={uploadedFile.contentType.split('/')[0]}
-                imageLink={uploadedFile.url}
-                token={authState.token}
-              />
-            )
-          )}
-        </Grid>
-      ),
-      signature: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <SignaturePad
-              key={formPropertiesData.id}
-            //   detail={{ type: 'signature', status: signatureStatus }}
-              signRef={signRef}
-              onEnd={() => handleSignatureUpload(formPropertiesData.id)}
-            />
-          </Grid>
-        </Grid>
-      ),
-      radio: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <Fragment key={formPropertiesData.id}>
-              <br />
-              <RadioInput
-                properties={formPropertiesData}
-                value={null}
-                handleValue={event =>
-                  handleRadioValueChange(event, formPropertiesData.id, formPropertiesData.fieldName)
-                }
-              />
-              <br />
-            </Fragment>
-          </Grid>
-        </Grid>
-      ),
-      dropdown: (
-        <Grid container spacing={3} key={formPropertiesData.id}>
-          <FormPropertyAction
-            formId={formId}
-            editMode={editMode}
-            propertyId={formPropertiesData.id}
-            refetch={refetch}
-            categoryId={categoryId}
-          />
-          <Grid item xs={editMode ? 10 : 12}>
-            <TextInput
-              id={formPropertiesData.id}
-              properties={formPropertiesData}
-              value=""
-              handleValue={event => handleValueChange(event, formPropertiesData.id)}
-              editable={editable}
-            />
-          </Grid>
-        </Grid>
-      )
-    };
-    return <>{fields[formPropertiesData.fieldType]}</>;
+  function handleValueChange(event, propId) {
+    const { name, value } = event.target;
+    setFormProperties({
+      ...formProperties,
+      [name]: { value, form_property_id: propId }
+    });
   }
+
+  function handleDateChange(date, id) {
+    setFormProperties({
+      ...formProperties,
+      date: { value: date, form_property_id: id }
+    });
+  }
+
+  function handleRadioValueChange(event, propId, fieldName) {
+    const { name, value } = event.target;
+    setFormProperties({
+      ...formProperties,
+      [fieldName]: { value: { checked: value, label: name }, form_property_id: propId }
+    });
+  }
+
+  function onImageSelect(event, currentProperty) {
+    setFormState({
+      ...formState,
+      currentPropId: currentProperty,
+      isUploading: true
+    })
+    onChange(event.target.files[0]);
+  }
+
+  async function handleSignatureUpload() {
+    setFormState({ ...formState, signed: true });
+    const url64 = signRef.current.toDataURL('image/png');
+    // convert the file
+    const convertedSignature = await convertBase64ToFile(url64);
+    await signature.onChange(convertedSignature);
+  }
+
+  const editable = !formPropertiesData.adminUse
+    ? false
+    : !(formPropertiesData.adminUse && authState.user.userType === 'admin');
+  const uploadedFile = uploadedImages.find(im => im.propertyId === formPropertiesData.id);
+  const fields = {
+    text: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <TextInput
+            id={formPropertiesData.id}
+            properties={formPropertiesData}
+            value={formProperties.fieldName}
+            handleValue={event => handleValueChange(event, formPropertiesData.id)}
+            editable={editable}
+          />
+        </Grid>
+      </Grid>
+    ),
+    date: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <DatePickerDialog
+            id={formPropertiesData.id}
+            selectedDate={formProperties.date.value}
+            handleDateChange={date => handleDateChange(date, formPropertiesData.id)}
+            label={formPropertiesData.fieldName}
+          />
+        </Grid>
+      </Grid>
+    ),
+    file_upload: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <UploadField
+            detail={{ type: 'file', label: formPropertiesData.fieldName }}
+            upload={evt => onImageSelect(evt, formPropertiesData.id)}
+            editable={editable}
+            uploaded={!!uploadedFile}
+          />
+        </Grid>
+        {formState.isUploading && formState.currentPropId === formPropertiesData.id ? (
+          <Spinner />
+        ) : (
+          !!uploadedFile && (
+            <ImageAuth
+              type={uploadedFile.contentType.split('/')[0]}
+              imageLink={uploadedFile.url}
+              token={authState.token}
+            />
+          )
+        )}
+      </Grid>
+    ),
+    signature: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <SignaturePad
+            key={formPropertiesData.id}
+            detail={{ type: 'signature', status: signature.status }}
+            signRef={signRef}
+            onEnd={() => handleSignatureUpload(formPropertiesData.id)}
+          />
+        </Grid>
+      </Grid>
+    ),
+    radio: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <Fragment key={formPropertiesData.id}>
+            <br />
+            <RadioInput
+              properties={formPropertiesData}
+              value={null}
+              handleValue={event =>
+                handleRadioValueChange(event, formPropertiesData.id, formPropertiesData.fieldName)
+              }
+            />
+            <br />
+          </Fragment>
+        </Grid>
+      </Grid>
+    ),
+    dropdown: (
+      <Grid container spacing={3} key={formPropertiesData.id}>
+        <FormPropertyAction
+          formId={formId}
+          editMode={editMode}
+          propertyId={formPropertiesData.id}
+          refetch={refetch}
+          categoryId={categoryId}
+        />
+        <Grid item xs={editMode ? 10 : 12}>
+          <TextInput
+            id={formPropertiesData.id}
+            properties={formPropertiesData}
+            value=""
+            handleValue={event => handleValueChange(event, formPropertiesData.id)}
+            editable={editable}
+          />
+        </Grid>
+      </Grid>
+    )
+  };
+  return <>{fields[formPropertiesData.fieldType]}</>;
+}
+
+
+RenderForm.propTypes = {
+  formId: PropTypes.string.isRequired,
+  refetch: PropTypes.func.isRequired,
+  editMode: PropTypes.bool.isRequired,
+  categoryId: PropTypes.string.isRequired,
+  formPropertiesData: PropTypes.shape({
+    id: PropTypes.string,
+    fieldType: PropTypes.string,
+    fieldName: PropTypes.string,
+    adminUse: PropTypes.string
+  }).isRequired,
+}
