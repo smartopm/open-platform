159f869350c3baf70b8bfeef9f45ced0dddfc4ae
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-17 17:30:54 +0000

    Setup subscriptions table

---
+++ app/models/payments/subscription_plan.rb
@@ -0,0 +1,22 @@
+module Payments
+  class SubscriptionPlan < ApplicationRecord
+    belongs_to :community
+
+    enum status: { active: 0, in_active: 1 }, _prefix: true
+    enum plan_type: { starter: 0, basic: 1, standard: 2, premium: 3 }, _prefix: true
+
+    validates :amount, numericality: { greater_than: 0 }
+    validates :start_date, :end_date, :plan_type, presence: true
+    validate :ensure_valid_dates
+
+    private
+
+    def ensure_valid_dates
+      return if start_date.nil? || end_date.nil?
+
+      if end_date <= start_date
+        errors.add(:end_date, :invalid_dates)
+      end
+    end
+  end
+end
