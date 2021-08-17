6974e8d5f6bb71b45e051695c6aa7fec07e5ff5c
OlivierJM <manolivier93@gmail.com>
2021-08-18 01:07:50 +0200

    restructure the category list to be reused on submitted forms

--- app/javascript/src/modules/Forms/components/Category/CategoryList.jsx
+++ app/javascript/src/modules/Forms/components/Category/CategoryList.jsx
@@ -1,85 +1,30 @@
-import React, { useState , useContext } from 'react';
-import { Button, Container } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import { useQuery } from 'react-apollo';
-import { useParams } from 'react-router';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { useTranslation } from 'react-i18next';
-import { DetailsDialog } from '../../../../components/Dialog';
-import CategoryForm from './CategoryForm';
 import CategoryItem from './CategoryItem';
-import { FormCategoriesQuery } from '../../graphql/form_category_queries';
-import { Spinner } from '../../../../shared/Loading';
 import RenderForm from '../RenderForm';
 import FormPropertyCreateForm from '../FormPropertyCreateForm';
-import FormTitle from '../FormTitle';
-import { FormQuery } from '../../graphql/forms_queries';
-import { FormContext } from '../../Context';
-import CenteredContent from '../../../../components/CenteredContent';
-import { Context } from '../../../../containers/Provider/AuthStateProvider';
-import { flattenFormProperties } from '../../utils';
+import { Spinner } from '../../../../shared/Loading';
 
-// This will contain the main category
-// from the main category you should be able to add questions to that category
-// below the main category, you can add another category
-
-export default function CategoryList({ editMode }) {
-  const [categoryFormOpen, setCategoryFormOpen] = useState(false);
-  const [propertyFormOpen, setPropertyFormOpen] = useState(false);
-  const [data, setFormData] = useState({});
-  const { t } = useTranslation('common')
-  const [categoryId, setCategoryId] = useState('');
-  const { formId } = useParams();
-  const categoriesData = useQuery(FormCategoriesQuery, {
-    variables: { formId },
-    fetchPolicy: 'cache-and-network'
-  });
-  const { data: formDetailData, loading } = useQuery(FormQuery, { variables: { id: formId } });
-  const { formState, saveFormData } = useContext(FormContext)
-  const authState = useContext(Context);
-
-  function handleEditCategory(category) {
-    setCategoryFormOpen(true);
-    setFormData(category);
+export default function CategoryList({
+  categoriesData,
+  editMode,
+  formId,
+  propertyFormOpen,
+  categoryId,
+  categoryItem
+}) {
+  if (categoriesData.loading) {
+    return <Spinner />;
   }
-  function handleAddCategory(category) {
-    setCategoryFormOpen(true);
-    setFormData(category);
-  }
-
-  function handleAddField(catId) {
-    setCategoryId(catId);
-    setPropertyFormOpen(!propertyFormOpen);
-  }
-
-  function handleClose() {
-    setCategoryFormOpen(false);
-  }
-  const formData = flattenFormProperties(categoriesData.data?.formCategories)
-
   return (
     <>
-      <DetailsDialog handleClose={handleClose} open={categoryFormOpen} title="Category" color="default">
-        <Container>
-          <CategoryForm data={data} close={handleClose} refetchCategories={categoriesData.refetch} />
-        </Container>
-      </DetailsDialog>
-      <br />
-
-      {loading && <Spinner />}
-
-      {!loading && formDetailData && (
-        <FormTitle name={formDetailData.form?.name} description={formDetailData.form?.description} />
-      )}
-
-      {categoriesData.loading && <Spinner />}
       {categoriesData?.data &&
         categoriesData.data?.formCategories.map(category => (
           <CategoryItem
             name={category.fieldName}
             key={category.id}
-            handleAddField={() => handleAddField(category.id)}
-            handleEditCategory={() => handleEditCategory(category)}
+            handleAddField={() => categoryItem.handleAddField(category.id)}
+            handleEditCategory={() => categoryItem.handleEditCategory(category)}
             collapsed={propertyFormOpen && categoryId === category.id}
             editMode={editMode}
           >
@@ -102,44 +47,34 @@ export default function CategoryList({ editMode }) {
             )}
           </CategoryItem>
         ))}
-      <br />
-      {
-        editMode && (
-          <Button
-            variant="outlined"
-            color="default"
-            startIcon={<AddIcon color="primary" />}
-            style={{ float: 'right' }}
-            onClick={handleAddCategory}
-          >
-            Add Category
-          </Button>
-        )
-      }
-      {
-        !editMode && (
-          <CenteredContent>
-            <Button
-              variant="outlined"
-              type="submit"
-              color="primary"
-              aria-label="form_submit"
-              disabled={formState.isSubmitting}
-              style={{ marginTop: '25px' }}
-              onClick={() => saveFormData(formData, formId, authState.user.id)}
-            >
-              {formState.isSubmitting
-                  ? t('common:form_actions.submitting')
-                  : t('common:form_actions.submit')
-            }
-            </Button>
-          </CenteredContent>
-        )
-      }
     </>
   );
 }
 
+CategoryList.defaultProps = {
+  propertyFormOpen: false
+};
+
 CategoryList.propTypes = {
-  editMode: PropTypes.bool.isRequired
-}
+  // eslint-disable-next-line react/forbid-prop-types
+  categoriesData: PropTypes.shape({
+    refetch: PropTypes.func,
+    loading: PropTypes.bool,
+    data: PropTypes.shape({
+      formCategories: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldName: PropTypes.string.isRequired,
+          id: PropTypes.string.isRequired
+        })
+      )
+    })
+  }).isRequired,
+  editMode: PropTypes.bool.isRequired,
+  formId: PropTypes.string.isRequired,
+  categoryId: PropTypes.string.isRequired,
+  propertyFormOpen: PropTypes.bool,
+  categoryItem: PropTypes.shape({
+    handleAddField: PropTypes.func,
+    handleEditCategory: PropTypes.func
+  }).isRequired
+};
