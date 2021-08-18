4119b920aa62175c16f341adfd27a8799b05204b
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-18 05:56:01 +0000

    Add tests

--- app/graphql/types/subscription_plan_type.rb
+++ app/graphql/types/subscription_plan_type.rb
@@ -6,7 +6,7 @@ module Types
     field :id, ID, null: false
     field :status, String, null: false
     field :plan_type, String, null: false
-    field :amount, String, null: false
+    field :amount, Float, null: false
     field :start_date, GraphQL::Types::ISO8601DateTime, null: false
     field :end_date, GraphQL::Types::ISO8601DateTime, null: false
   end
