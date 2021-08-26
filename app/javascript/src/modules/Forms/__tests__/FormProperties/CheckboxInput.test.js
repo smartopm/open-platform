8583dca729028867ca1e0b98defeeacf1a517569
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-08-26 17:14:15 +0000

    Write test for CheckboxInput and remove the old component

---
+++ app/javascript/src/modules/Forms/__tests__/FormProperties/CheckboxInput.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import CheckboxInput from '../../components/FormProperties/CheckboxInput';
+
+describe('Checkbox component', () => {
+  it('should render properly', () => {
+    const props = {
+      handleValue: jest.fn(),
+      properties: {
+        fieldName: 'Select your favorite colors',
+        required: true,
+        fieldType: 'checkbox',
+        fieldValue: [
+          {
+            value: 'Red',
+            label: 'Red'
+          },
+          {
+            value: 'Green',
+            label: 'Green'
+          },
+          {
+            value: 'Blue',
+            label: 'Blue'
+          }
+        ],
+        id: '3145c47e-1234-1193-9dac-dc723d2e',
+        groupingId: '3145c47e-1001-9dac',
+        adminUse: false,
+        order: '1'
+      }
+    };
+
+    const wrapper = render(<CheckboxInput {...props} />);
+    expect(wrapper.queryAllByText('Select your favorite colors *')[0]).toBeInTheDocument();
+    expect(wrapper.queryAllByText('Green')[0]).toBeInTheDocument();
+    expect(wrapper.queryAllByText('Red')[0]).toBeInTheDocument();
+    expect(wrapper.queryAllByText('Blue')[0]).toBeInTheDocument();
+  });
+});
