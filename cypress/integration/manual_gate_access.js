60a0c203a03b382f20d413ef3a2b68dd78fe2af7
OlivierJM <manolivier93@gmail.com>
2021-09-21 09:05:23 +0200

    add test for manual entry access

--- cypress/integration/manual_gate_access.js
+++ cypress/integration/manual_gate_access.js
@@ -23,15 +23,28 @@ describe('Manual Gate Access', () => {
       cy.login('2347065834175')
       cy.visit('/request')
   
+      // Fill in the manual entry request form
       cy.get('[data-testid=entry_user_name]').type('Test Manual User')
       cy.get('[data-testid=email]').type('email@test.com')
       cy.get('[data-testid=entry_user_nrc]').type('0U2J0J239')
       cy.get('[data-testid=entry_user_phone]').type('0024067740149')
       cy.get('[data-testid=entry_user_vehicle]').type('AQW2714')
       cy.get('[data-testid=companyName]').type('AQW2714')
-      cy.get('[data-testid=entry_user_visit]').select('Client')
-      cy.wait(1000)
-      cy.get('[data-testid=entry_user_grant_request]').click()
+      cy.get('.visiting_reason').click()
+      cy.get('[data-value=client]').click();
+
+      // grant access
+      cy.wait(500)
+      cy.get('[data-testid=entry_user_grant]').click()
+      cy.get('.confirm_grant').click()
+      cy.wait(2000);
+      // Add an observation 
+      cy.get('[data-testid=entry-dialog-field]').type('Observation example ')
+      cy.get('.save_and_record_other').click()
+
+      // Check if Test Manual User was recorded in the logbook
+      cy.visit('/entry_logs')
+      cy.get('.entry-log-visitor-name').should('contain', 'Test Manual User')
     })
   })
   
