0b579e5bbfa02954045350bbf1894443c4d96a83
Daniel Mutuba <daniel@doublegdp.com>
2021-09-22 14:59:41 +0000

    Add translations for SOS icon

--- app/javascript/src/modules/Menu/component/FeedbackButtonSVG.jsx
+++ app/javascript/src/modules/Menu/component/FeedbackButtonSVG.jsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-const FeedbackButtonSVG = () => (
+const FeedbackButtonSVG = ({t}) => (
   <svg width="211px" height="211px" viewBox="0 0 211 211">
     <g id="Panic-Button" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g id="Emergency-Copy" transform="translate(-100.000000, -252.000000)">
@@ -12,7 +12,7 @@ const FeedbackButtonSVG = () => (
           <circle id="Oval-Copy-2" fillOpacity="0.22227382" fill="#FFFFFF" cx="105.5" cy="105.5" r="105.5" />
           <circle id="Oval" fill="#FFFFFF" cx="105.705556" cy="105.705556" r="84.7055556" />
           <text id="SOS" fontFamily="HelveticaNeue-Bold, Helvetica Neue" fontSize="44" fontWeight="bold" fill="#232323">
-            <tspan x="59.328" y="122">SOS </tspan>
+            <tspan x="59.328" y="122">{t('panic_alerts.sos')} </tspan>
           </text>
         </g>
       </g>
--- app/javascript/src/modules/Menu/component/PanicButtonSVG.jsx
+++ app/javascript/src/modules/Menu/component/PanicButtonSVG.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const PanicButtonSVG = ({bind}) => (
+const PanicButtonSVG = ({bind, t}) => (
   <svg width="217px" height="217px" {...bind} data-testid="sos-modal-panic-button">
     <g id="Panic-Button" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g id="Emergency" transform="translate(-97.000000, -156.000000)">
@@ -10,10 +10,10 @@ const PanicButtonSVG = ({bind}) => (
           <circle id="Oval-Copy" fillOpacity="0.22227382" fill="#FFFFFF" filter="url(#filter-1)" cx="96.5" cy="96.5" r="96.5" />
           <circle id="Oval" fill="#FFFFFF" cx="96.7055556" cy="96.7055556" r="84.7055556" />
           <text id="SOS" fontFamily="HelveticaNeue-Bold, Helvetica Neue" fontSize="44" fontWeight="bold" fill="#232323">
-            <tspan x="51.328" y="98">SOS</tspan>
+            <tspan x="51.328" y="98">{t('panic_alerts.sos')}</tspan>
           </text>
           <text id="Press-for-3-seconds" fontFamily="HelveticaNeue, Helvetica Neue" fontSize="13" fontWeight="normal" line-spacing="15" fill="#575757">
-            <tspan x="38.598" y="121">Press for 3 seconds</tspan>
+            <tspan x="38.598" y="121">{t('panic_alerts.press_for_3_seconds')}</tspan>
           </text>
         </g>
       </g>
