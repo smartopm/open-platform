391dfce61ce2e9be2e969bf06acfc096c32b2ce9
tolulope3 <tolulope@doublegdp.com>
2021-10-12 14:12:35 +0000

    start adcapture design

--- app/javascript/src/modules/LogBook/GuestVerification/Components/IdCapture.jsx
+++ app/javascript/src/modules/LogBook/GuestVerification/Components/IdCapture.jsx
@@ -1,16 +1,66 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Button, Grid, Typography, makeStyles, Paper } from '@material-ui/core';
 import PropTypes from 'prop-types'
 
 export default function IDCapture({ handleNext }) {
+  const classes = useStyles();
   return (
-    <p>
-      ID Capture Component 
-      {' '}
-      <Button onClick={handleNext}>continue</Button>
-    </p>
+    <Grid container>
+      <Grid item xs={12} className={classes.body}>
+        <Typography variant='h6' className={classes.header}>Add a photo of your ID with your phone</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Grid container>
+          <Grid item xs={6}>
+            <ul>
+              <li><Typography>Use portrait orientation</Typography></li>
+              <li><Typography>Turn off your camera flash</Typography></li>
+            </ul>
+          </Grid>
+          <Grid item xs={6}>
+            <ul>
+              <li><Typography>Use a dark background</Typography></li>
+              <li><Typography>Take photo on a flat surface</Typography></li>
+            </ul>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <ImageArea />
+        <ImageArea />
+      </Grid>
+      <Grid item xs={12} className={classes.body}>
+        <Button variant='contained' color='primary' onClick={handleNext}>continue</Button>
+      </Grid>
+    </Grid>
   );
 }
+
+function ImageArea() {
+  const classes = useStyles();
+  return (
+    <Grid item xs={12} className={classes.imageGrid}>
+      <Paper elevation={0} className={classes.imageArea} style={{height: '400px'}}>
+        {' '}
+      </Paper>
+    </Grid>
+  )
+}
+
+const useStyles = makeStyles(() => ({ 
+  body: {
+    textAlign: 'center'
+  },
+  header: {
+    fontWeight: 'bold'
+  },
+  imageArea: {
+    border: '1px dotted #D0D0D0'
+  },
+  imageGrid: {
+    padding: '20px 30px'
+  }
+}))
 
 IDCapture.propTypes = {
   /**
--- app/javascript/src/modules/LogBook/GuestVerification/Components/index.js
+++ app/javascript/src/modules/LogBook/GuestVerification/Components/index.js
@@ -2,9 +2,18 @@
 // If there is only one step, it won't show the stepper
 import React from 'react';
 import GuestForm from './GuestForm';
+import IdCapture from './IdCapture';
 
 export default function steps(handleNext) {
   return [
+    {
+      title: 'Guest Form',
+      component: <GuestForm handleNext={handleNext} />,
+    },
+    {
+      title: 'Id Capture',
+      component: <IdCapture handleNext={handleNext} />,
+    },
     {
       title: 'Guest Form',
       component: <GuestForm handleNext={handleNext} />,
