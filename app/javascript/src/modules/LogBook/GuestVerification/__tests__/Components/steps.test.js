c87e6e3cf7647aaa39048edf359ff67a8600a07d
Olivier JM <olivier@doublegdp.com>
2021-10-14 12:48:28 +0000

    fix tests

--- app/javascript/src/modules/LogBook/GuestVerification/__tests__/Components/steps.test.js
+++ app/javascript/src/modules/LogBook/GuestVerification/__tests__/Components/steps.test.js
@@ -12,7 +12,19 @@ describe('steps should return according to the community', () => {
       }
     };
     const handleNextMock = jest.fn();
-    const guestSteps = steps(handleNextMock, communityMock.user.community);
+    const guestSteps = steps(handleNextMock, communityMock.user.community.name);
+    expect(guestSteps).toHaveLength(1);
+  });
+  it('should only return all the steps for other communities', () => {
+    const communityMock = {
+      user: {
+        community: {
+          name: 'CM'
+        }
+      }
+    };
+    const handleNextMock = jest.fn();
+    const guestSteps = steps(handleNextMock, communityMock.user.community.name);
     expect(guestSteps).toHaveLength(3);
   });
 });
