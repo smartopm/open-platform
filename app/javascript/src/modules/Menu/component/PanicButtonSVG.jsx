79407a759a624f5ba3b38e5949cf515f239b9909
victor okeugo <victor@doublegdp.com>
2021-10-14 07:51:59 +0000

    change sos modal design content

--- app/javascript/src/modules/Menu/component/PanicButtonSVG.jsx
+++ app/javascript/src/modules/Menu/component/PanicButtonSVG.jsx
@@ -10,10 +10,7 @@ const PanicButtonSVG = ({bind, t}) => (
           <circle id="Oval-Copy" fillOpacity="0.22227382" fill="#FFFFFF" filter="url(#filter-1)" cx="96.5" cy="96.5" r="96.5" />
           <circle id="Oval" fill="#FFFFFF" cx="96.7055556" cy="96.7055556" r="84.7055556" />
           <text id="SOS" fontFamily="HelveticaNeue-Bold, Helvetica Neue" fontSize="44" fontWeight="bold" fill="#232323">
-            <tspan x="51.328" y="98">{t('panic_alerts.sos')}</tspan>
-          </text>
-          <text id="Press-for-3-seconds" fontFamily="HelveticaNeue, Helvetica Neue" fontSize="13" fontWeight="normal" line-spacing="15" fill="#575757">
-            <tspan x="38.598" y="121">{t('panic_alerts.press_for_3_seconds')}</tspan>
+            <tspan x="51.328" y="115">{t('panic_alerts.sos')}</tspan>
           </text>
         </g>
       </g>
