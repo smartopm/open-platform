12388a99aa9e1bb920a8f8417aa1accdea45c207
Saurabh Shinde <saurabh@doublegdp.com>
2021-10-19 17:46:16 +0530

    Add permissions for payment plan queries and mutations

--- app/graphql/mutations/payment_plan/payment_plan_cancel.rb
+++ app/graphql/mutations/payment_plan/payment_plan_cancel.rb
@@ -23,7 +23,10 @@ module Mutations
 
       # Verifies if current user is admin or not.
       def authorized?(_vals)
-        return true if context[:current_user]&.admin?
+        return true if ::Policy::ApplicationPolicy.new(
+          context[:current_user], nil
+        ).permission?(module: :payment_plan, permission: :can_cancel_payment_plan) ||
+                       context[:current_user]&.admin?
 
         raise GraphQL::ExecutionError, I18n.t('errors.unauthorized')
       end
