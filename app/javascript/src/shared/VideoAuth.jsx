e0f49953007fc6387b5c948ce9d5aed1b01351ac
Olivier JM <olivier@doublegdp.com>
2021-10-20 12:33:21 +0000

    maake the video more responsive and translate error

--- app/javascript/src/shared/VideoAuth.jsx
+++ app/javascript/src/shared/VideoAuth.jsx
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { useMediaQuery } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 import { Context } from '../containers/Provider/AuthStateProvider';
 import { useFetchMedia } from '../utils/customHooks';
+import CenteredContent from '../components/CenteredContent';
 
 export default function VideoAuth({ src }) {
   const authState = useContext(Context);
+  const matches = useMediaQuery('(max-width:600px)')
+  const { t } = useTranslation(['common'])
   const options = {
     method: 'GET',
     headers: {
@@ -14,10 +19,16 @@ export default function VideoAuth({ src }) {
   const { response, } = useFetchMedia(src, options);
 
   return (
-    // eslint-disable-next-line jsx-a11y/media-has-caption
-    <video width="100%" controls src={response.url}>
-      Your browser is not supported
-    </video>
+    <CenteredContent>
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video
+        width={matches ? "100%" : "60%"}
+        controls
+        src={response.url}
+      >
+        {t('errors.flashlight_not_supported')}
+      </video>
+    </CenteredContent>
   );
 }
 
