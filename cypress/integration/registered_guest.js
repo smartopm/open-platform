c29f7d20f0f892bd66ed5c2d6a0558d593bb89ef
victor okeugo <victor@doublegdp.com>
2021-10-27 14:24:53 +0000

    box to allowed time frame

--- cypress/integration/registered_guest.js
+++ cypress/integration/registered_guest.js
@@ -45,83 +45,88 @@ describe('Registered Guests', () => {
     cy.get('[data-testid=submit_button]').click();
     cy.contains('Required').should('exist');
     cy.wait(1000)
-    
-    cy.get('[data-testid=entry_user_name]').type('Guest User')
-    cy.get('[data-testid=entry_user_nrc]').type('0U2J0J239')
-    cy.get('[data-testid=entry_user_phone]').type('0024067740149')
-    cy.get('[data-testid=entry_user_vehicle]').type('AQW2714')
-    cy.get('[data-testid=companyName]').type('Company 1')
-    cy.get('.visiting_reason').click()
-    cy.get('[data-value=client]').click();
-    
-    // Choose a valid visitation date & time
-    cy.get('[data-testid=day_of_visit_input]').type(Cypress.moment().format('YYYY, MM DD'))
-    cy.get('[data-testid=start_time_input]').click()
-    cy.contains('Ok').click();
-    
-    // Accommodate different time periods to make invite valid
-    cy.get('[data-testid=end_time_input]').click()
-    new Date().getHours() <= 12 ? cy.contains('PM').click() :  cy.get('.MuiPickersClock-squareMask').click()
-    cy.contains('Ok').click();
-    cy.wait(1000)
-    
-    // Submit
-    cy.get('[data-testid=submit_button]').click();
-    cy.wait(2000)
-    
-    // Confirm QRCode section to enter email
-    cy.get('.qrcode-enter-email-text').should('exist');
-    cy.get('[data-testid=guest-email-input]').should('exist');
 
-    // Skip QRCode sending
-    cy.get('[data-testid=dont-send-confirmation]').click();
-    cy.wait(2000)
+    // Sandbox in allowed time period
+    const hr = new Date().getHours();
+    if(hr >= 9 && hr < 16){
+      cy.get('[data-testid=entry_user_name]').type('Guest User')
+      cy.get('[data-testid=entry_user_nrc]').type('0U2J0J239')
+      cy.get('[data-testid=entry_user_phone]').type('0024067740149')
+      cy.get('[data-testid=entry_user_vehicle]').type('AQW2714')
+      cy.get('[data-testid=companyName]').type('Company 1')
+      cy.get('.visiting_reason').click()
+      cy.get('[data-value=client]').click();
+      
+      // Choose a valid visitation date & time
+      cy.get('[data-testid=day_of_visit_input]').type(Cypress.moment().format('YYYY, MM DD'))
+      cy.get('[data-testid=start_time_input]').click()
+      cy.contains('Ok').click();
+      
+      // Accommodate different time periods to make invite valid
+      cy.get('[data-testid=end_time_input]').click()
+      new Date().getHours() <= 12 ? cy.contains('PM').click() :  cy.get('.MuiPickersClock-squareMask').click()
+      cy.contains('Ok').click();
+      cy.wait(1000)
+      
+      // Submit
+      cy.get('[data-testid=submit_button]').click();
+      cy.wait(2000)
+      
+      // Confirm QRCode section to enter email
+      cy.get('.qrcode-enter-email-text').should('exist');
+      cy.get('[data-testid=guest-email-input]').should('exist');
+  
+      // Skip QRCode sending
+      cy.get('[data-testid=dont-send-confirmation]').click();
+      cy.wait(2000)
+      
+      // Verify registered guests record was created
+      cy.get('[data-testid=no-invited-guests-available]').should('not.exist');
+      cy.get('[data-testid=guest_name]').should('contain', 'Guest User');
+      cy.get('[data-testid=start_of_visit]').should('exist');
+      cy.get('[data-testid=end_of_visit]').should('exist');
+      cy.get('[data-testid=access_time]').should('exist');
+      cy.get('[data-testid=validity]').should('exist');
+      cy.get('[data-testid=access_actions]').should('exist');
+      cy.get('[data-testid=grant_access_btn]').should('not.be.disabled');
+      cy.contains('Valid').should('exist');
+      
+      // Log out as admin
+      cy.logout()
+      // Log In as Security Guard to grant access
+      cy.login('2347065834175')
+      cy.visitMainMenu('.logbook-menu-item', '.logbook-parent-menu-item')
+      
+      // No initial access granted to registered guest
+      cy.get('[data-testid=visitor_name]').should('not.exist');
+      cy.get('[data-testid=access_status]').should('not.exist');
+      cy.wait(1000)
+      
+      // Go to Registered Guests page
+      cy.contains('Registered Guests').click();
+      cy.wait(1000)
+      cy.get('[data-testid=guest_name]').should('contain', 'Guest User');
+      cy.get('[data-testid=grant_access_btn]').should('not.be.disabled');
+      
+      // Click grant access
+      cy.get('[data-testid=grant_access_btn]').click();
+      cy.wait(1000)
+      
+      // Confirm Observation Dialog shows up
+      cy.get('[data-testid=entry-dialog-title]').should('exist');
+      cy.get('[data-testid=entry-dialog-field]').type('Guest User Observation');
+      cy.get('[data-testid=save]').click();
+      cy.wait(2000)
+      
+      cy.contains('All Visits').click();
+      cy.wait(1000)
+      
+      // Verify access was granted successfully
+      cy.get('[data-testid=visitor_name]').should('contain', 'Guest User');
+      cy.get('[data-testid=acting_user]').should('contain', 'Larry Bird');
+      cy.get('[data-testid=access_status]').should('contain', 'Granted');
+    }
     
-    // Verify registered guests record was created
-    cy.get('[data-testid=no-invited-guests-available]').should('not.exist');
-    cy.get('[data-testid=guest_name]').should('contain', 'Guest User');
-    cy.get('[data-testid=start_of_visit]').should('exist');
-    cy.get('[data-testid=end_of_visit]').should('exist');
-    cy.get('[data-testid=access_time]').should('exist');
-    cy.get('[data-testid=validity]').should('exist');
-    cy.get('[data-testid=access_actions]').should('exist');
-    cy.get('[data-testid=grant_access_btn]').should('not.be.disabled');
-    cy.contains('Valid').should('exist');
-    
-    // Log out as admin
-    cy.logout()
-    // Log In as Security Guard to grant access
-    cy.login('2347065834175')
-    cy.visitMainMenu('.logbook-menu-item', '.logbook-parent-menu-item')
-    
-    // No initial access granted to registered guest
-    cy.get('[data-testid=visitor_name]').should('not.exist');
-    cy.get('[data-testid=access_status]').should('not.exist');
-    cy.wait(1000)
-    
-    // Go to Registered Guests page
-    cy.contains('Registered Guests').click();
-    cy.wait(1000)
-    cy.get('[data-testid=guest_name]').should('contain', 'Guest User');
-    cy.get('[data-testid=grant_access_btn]').should('not.be.disabled');
-    
-    // Click grant access
-    cy.get('[data-testid=grant_access_btn]').click();
-    cy.wait(1000)
-    
-    // Confirm Observation Dialog shows up
-    cy.get('[data-testid=entry-dialog-title]').should('exist');
-    cy.get('[data-testid=entry-dialog-field]').type('Guest User Observation');
-    cy.get('[data-testid=save]').click();
-    cy.wait(2000)
-    
-    cy.contains('All Visits').click();
-    cy.wait(1000)
-    
-    // Verify access was granted successfully
-    cy.get('[data-testid=visitor_name]').should('contain', 'Guest User');
-    cy.get('[data-testid=acting_user]').should('contain', 'Larry Bird');
-    cy.get('[data-testid=access_status]').should('contain', 'Granted');
   });
 
   it('does not grant access to invalid registered guest invite ', () => {
