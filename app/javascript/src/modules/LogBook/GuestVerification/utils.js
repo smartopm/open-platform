ac04788521b2d5f52bf794f5c5cb91fd909284fe
tolulope3 <tolulope@doublegdp.com>
2021-10-29 10:36:43 +0000

    update util.js file

--- app/javascript/src/modules/LogBook/GuestVerification/utils.js
+++ app/javascript/src/modules/LogBook/GuestVerification/utils.js
@@ -1,17 +1,11 @@
 function checkUserInformation(req) {
   const info = req.name || req.email || req.phoneNumber;
-  if (info) {
-    return true;
-  }
-  return false;
+  return !!info
 }
 
 function checkInfo(req) {
   const info = req.name || req.email || req.phoneNumber || req.imageUrls || req.videoUrl;
-  if (info) {
-    return true;
-  }
-  return false;
+  return !!info
 }
 
 export { checkUserInformation,  checkInfo }
