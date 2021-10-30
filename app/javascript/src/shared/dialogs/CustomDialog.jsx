3c03699322dda3b92e4bdcc790a8951cec2a8eee
Olivier JM <olivier@doublegdp.com>
2021-10-30 21:57:09 +0000

    add a custom dialog

---
+++ app/javascript/src/shared/dialogs/CustomDialog.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import PropTypes from 'prop-types';
+import { Box, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+export default function CustomDialog({ open, handleDialogStatus, children, actions, title }) {
+  return (
+    <>
+      <Dialog
+        fullWidth
+        open={open}
+        onClose={handleDialogStatus}
+        aria-labelledby="custom-dialog-title"
+        data-testid="custom-dialog"
+      >
+        <DialogTitle
+          id="custom-dialog-title"
+          data-testid="custom-dialog-title"
+        >
+          <Box display="flex" alignItems="center">
+            <Box flexGrow={1}>{title}</Box>
+            <Box>
+              <IconButton data-testid="custom-dialog-close-icon" onClick={handleDialogStatus}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </DialogTitle>
+        <DialogContent>{children}</DialogContent>
+        <DialogActions>{actions}</DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+
+CustomDialog.propTypes = {
+  open: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  handleDialogStatus: PropTypes.func.isRequired,
+  actions: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
+};
