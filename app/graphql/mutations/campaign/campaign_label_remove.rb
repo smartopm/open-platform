540d2f11c7c547c2c26839c8d85fda35aebda1dd
Daniel Mutuba <daniel@doublegdp.com>
2021-11-02 10:19:46 +0000

    add permissions too campaigns

--- app/graphql/mutations/campaign/campaign_label_remove.rb
+++ app/graphql/mutations/campaign/campaign_label_remove.rb
@@ -20,7 +20,13 @@ module Mutations
 
       # Verifies if current user is admin or not.
       def authorized?(_vals)
-        return true if context[:current_user]&.admin?
+        return true if ::Policy::ApplicationPolicy.new(
+          context[:current_user], nil
+        ).permission?(
+          admin: true,
+          module: :campaign,
+          permission: :can_remove_campaign_label,
+        )
 
         raise GraphQL::ExecutionError, I18n.t('errors.unauthorized')
       end
