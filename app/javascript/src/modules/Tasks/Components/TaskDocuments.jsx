b854f7035ac1e93b42a2194b041eb9b1bba2436b
Olivier JM <olivier@doublegdp.com>
2021-11-10 16:23:09 +0000

    add translationn

--- app/javascript/src/modules/Tasks/Components/TaskDocuments.jsx
+++ app/javascript/src/modules/Tasks/Components/TaskDocuments.jsx
@@ -8,74 +8,92 @@ import {
   ListItemText,
   Menu,
   MenuItem,
-  Typography
+  Typography,
 } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { dateToString } from '../../../components/DateContainer';
+import CenteredContent from '../../../components/CenteredContent';
 
 export default function TaskDocuments({ documents }) {
   const [anchorEl, setAnchorEl] = useState(null);
-  const [docId, setDocId] = useState('');
-  const { t } = useTranslation('common');
+  const [currentDoc, setCurrentDoc] = useState('');
+  const { t } = useTranslation('task');
 
   const menuOpen = Boolean(anchorEl);
 
-  function handleOpenMenu(event, id) {
+  function handleOpenMenu(event, document) {
     event.stopPropagation();
     setAnchorEl(event.currentTarget);
-    setDocId(id);
+    setCurrentDoc(document);
+  }
+
+  // we close the menu after downloading the file
+  function handleDownload() {
+    setAnchorEl(null);
   }
 
   return (
     <>
-      <List>
-        {documents.map(doc => (
-          <Fragment key={doc.id}>
-            <ListItem>
-              <ListItemText
-                primary={doc.filename}
-                secondary={(
-                  <>
-                    <Typography component="span" variant="body2" color="textPrimary">
-                      {`Uploaded at: ${dateToString(doc.created_at)}`}
+      {documents?.length ? (
+        <List>
+          <Typography variant="h6">
+            {t('document.documents')}
+          </Typography>
+          <br />
+          <Divider variant="middle" />
+          {documents.map(doc => (
+            <Fragment key={doc.id}>
+              <ListItem>
+                <ListItemText
+                  disableTypography
+                  primary={(
+                    <Typography variant="body1" color="primary" style={{ fontWeight: 700 }}>
+                      {doc.filename}
                     </Typography>
-                  </>
-                )}
-              />
-              <ListItemSecondaryAction>
-                <IconButton
-                  edge="end"
-                  aria-label="more_details"
-                  onClick={event => handleOpenMenu(event, doc.id)}
-                >
-                  <MoreVertIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
+                  )}
+                  secondary={(
+                    <Typography component="span" variant="body2">
+                      {`${t('document.uploaded_at')}: ${dateToString(doc.created_at)}`}
+                    </Typography>
+                  )}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="more_details"
+                    onClick={event => handleOpenMenu(event, doc)}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
 
-            <Divider variant="middle" />
-          </Fragment>
-        ))}
-      </List>
+              <Divider variant="middle" />
+            </Fragment>
+          ))}
+        </List>
+      ) : (
+        <CenteredContent>
+          <Typography>{t('document.no_documents')}</Typography>
+        </CenteredContent>
+      )}
       <Menu
-        id={`long-menu-${docId}`}
+        id={`long-menu-${currentDoc.id}`}
         anchorEl={anchorEl}
         open={menuOpen}
         onClose={() => setAnchorEl(null)}
         keepMounted={false}
-        PaperProps={{
-          style: {
-            width: 200
-          }
-        }}
       >
-        <MenuItem id="download_button" key="download">
-          Download
-        </MenuItem>
-        <MenuItem id="delete_button" key="delete">
-          Delete
+        <MenuItem id="download_button" key="download" onClick={handleDownload}>
+          <a
+            href={currentDoc.url}
+            download={currentDoc.filename}
+            style={{ textDecoration: 'none', color: '#000000' }}
+          >
+            {t('document.download')}
+          </a>
         </MenuItem>
       </Menu>
     </>
