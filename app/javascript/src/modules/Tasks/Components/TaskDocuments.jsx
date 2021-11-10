a07a986258cacc3a8814b99123f779b8decf31ed
Olivier JM <olivier@doublegdp.com>
2021-11-10 13:16:10 +0000

    add menu to attachments

--- app/javascript/src/modules/Tasks/Components/TaskDocuments.jsx
+++ app/javascript/src/modules/Tasks/Components/TaskDocuments.jsx
@@ -1,45 +1,84 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import {
+  Divider,
   IconButton,
   List,
   ListItem,
   ListItemSecondaryAction,
   ListItemText,
+  Menu,
+  MenuItem,
   Typography
 } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 import { dateToString } from '../../../components/DateContainer';
 
 export default function TaskDocuments({ documents }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [docId, setDocId] = useState('');
+  const { t } = useTranslation('common');
+
+  const menuOpen = Boolean(anchorEl);
+
+  function handleOpenMenu(event, id) {
+    event.stopPropagation();
+    setAnchorEl(event.currentTarget);
+    setDocId(id);
+  }
+
   return (
-    <List>
-      {documents.map((doc) => (
-        <ListItem key={doc.id}>
-          <ListItemText
-            primary={doc.filename}
-            secondary={(
-              <>
-                <Typography
-                  component="span"
-                  variant="body2"
-                  color="textPrimary"
+    <>
+      <List>
+        {documents.map(doc => (
+          <Fragment key={doc.id}>
+            <ListItem>
+              <ListItemText
+                primary={doc.filename}
+                secondary={(
+                  <>
+                    <Typography component="span" variant="body2" color="textPrimary">
+                      {`Uploaded at: ${dateToString(doc.created_at)}`}
+                    </Typography>
+                  </>
+                )}
+              />
+              <ListItemSecondaryAction>
+                <IconButton
+                  edge="end"
+                  aria-label="more_details"
+                  onClick={event => handleOpenMenu(event, doc.id)}
                 >
-                  Uploaded at:
-                  {' '}
-                  {dateToString(doc.created_at)}
-                </Typography>
-              </>
-              )}
-          />
-          <ListItemSecondaryAction>
-            <IconButton edge="end" aria-label="more_details">
-              <MoreVertIcon />
-            </IconButton>
-          </ListItemSecondaryAction>
-        </ListItem>
-      ))}
-    </List>
+                  <MoreVertIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+
+            <Divider variant="middle" />
+          </Fragment>
+        ))}
+      </List>
+      <Menu
+        id={`long-menu-${docId}`}
+        anchorEl={anchorEl}
+        open={menuOpen}
+        onClose={() => setAnchorEl(null)}
+        keepMounted={false}
+        PaperProps={{
+          style: {
+            width: 200
+          }
+        }}
+      >
+        <MenuItem id="download_button" key="download">
+          Download
+        </MenuItem>
+        <MenuItem id="delete_button" key="delete">
+          Delete
+        </MenuItem>
+      </Menu>
+    </>
   );
 }
 
@@ -47,7 +86,7 @@ TaskDocuments.propTypes = {
   documents: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
-      filename: PropTypes.string,
+      filename: PropTypes.string
     })
-  ).isRequired,
+  ).isRequired
 };
