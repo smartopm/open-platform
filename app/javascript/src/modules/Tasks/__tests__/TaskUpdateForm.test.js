9e59f5307da9acf7592cc3e3b582a92b80b24d5d
Olivier JM <olivier@doublegdp.com>
2021-11-11 07:56:38 +0000

    fix tests

--- app/javascript/src/modules/Tasks/__tests__/TaskUpdateForm.test.js
+++ app/javascript/src/modules/Tasks/__tests__/TaskUpdateForm.test.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'
-import { act, render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import { MockedProvider } from '@apollo/react-testing'
-import TaskUpdateForm from '../Components/TaskUpdateForm'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { act, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MockedProvider } from '@apollo/react-testing';
+import TaskUpdateForm from '../Components/TaskUpdateForm';
 
 const data = {
   id: '6v2y3etyu2g3eu2',
@@ -19,21 +19,22 @@ const data = {
   assigneeNotes: [],
   completed: false,
   attachments: []
-}
+};
 const props = {
   data,
   assignUser: jest.fn(),
   refetch: jest.fn(),
   users: [],
   currentUser: { name: 'tester', id: '6523gvhvg' },
-  historyData: [{ action: 'create', noteEntityType: 'Comments::NoteComment', user: {name: 'name'}}],
+  historyData: [
+    { action: 'create', noteEntityType: 'Comments::NoteComment', user: { name: 'name' } }
+  ],
   historyRefetch: jest.fn(),
   authState: {},
   taskId: ''
-}
+};
 describe('task form component', () => {
   it('should render and have editable fields', async () => {
-
     let container;
     await act(async () => {
       container = render(
@@ -42,41 +43,28 @@ describe('task form component', () => {
             <TaskUpdateForm {...props} />
           </BrowserRouter>
         </MockedProvider>
-      )
-    })
+      );
+    });
 
-    expect(container.queryByText('common:form_actions.update_task')).toBeInTheDocument()
+    const taskBtn = container.queryByLabelText('task');
+    const updatesBtn = container.queryByLabelText('updates');
 
-    const description = container.queryByLabelText('task_description')
-    const submitBtn = container.queryByLabelText('task_submit')
-    const taskBtn = container.queryByLabelText('task')
-    const updatesBtn = container.queryByLabelText('updates')
-
-    fireEvent.change(description, {
-      target: { value: 'This is a description of the task' }
-    })
-    expect(description.value).toBe('This is a description of the task')
-
-    expect(container.queryByText('common:form_actions.note_complete')).toBeInTheDocument()
-    expect(container.queryByText('common:form_actions.note_complete')).not.toBeDisabled()
-    expect(container.queryAllByText('tolulope O.')[0]).toBeInTheDocument()
-    expect(container.queryAllByText('another_user')[0]).toBeInTheDocument()
-    expect(container.queryByText('task.task_user_label')).toBeInTheDocument()
+    expect(container.queryByText('common:form_actions.note_complete')).toBeInTheDocument();
+    expect(container.queryByText('common:form_actions.note_complete')).not.toBeDisabled();
+    expect(container.queryAllByText('tolulope O.')[0]).toBeInTheDocument();
+    expect(container.queryAllByText('another_user')[0]).toBeInTheDocument();
     // needs to be updated
-    expect(container.queryAllByTestId('user_chip')).toHaveLength(5)
-    expect(container.queryByTestId('mark_task_complete_checkbox')).toBeInTheDocument()
-    expect(container.queryByText('task.task_body_label')).toBeInTheDocument() // for the toggler
-    expect(submitBtn.textContent).toContain('common:form_actions.update_task')
-    expect(taskBtn).not.toBeNull()
-    expect(updatesBtn).not.toBeNull()
-    expect(submitBtn).not.toBeDisabled()
-  })
+    expect(container.queryAllByTestId('user_chip')).toHaveLength(2);
+    expect(container.queryByTestId('mark_task_complete_checkbox')).toBeInTheDocument();
+    expect(taskBtn).not.toBeNull();
+    expect(updatesBtn).not.toBeNull();
+  });
 
   it('should render the remind-me-later button if current user is an assignee', async () => {
     const newProps = {
       ...props,
       currentUser: { name: 'tolulope 0.', id: '34543' }
-    }
+    };
     let container;
     await act(async () => {
       container = render(
@@ -85,18 +73,18 @@ describe('task form component', () => {
             <TaskUpdateForm {...newProps} />
           </BrowserRouter>
         </MockedProvider>
-      )
-    })
+      );
+    });
 
-    const reminderBtn = container.queryByText('task.reminder_text')
-    expect(reminderBtn).toBeInTheDocument()
+    const reminderBtn = container.queryByText('task.reminder_text');
+    expect(reminderBtn).toBeInTheDocument();
 
-    fireEvent.click(reminderBtn)
+    fireEvent.click(reminderBtn);
 
-    expect(container.queryByText('task.task_reminder_in_1_hr')).toBeInTheDocument()
-    expect(container.queryByText('task.task_reminder_in_24_hr')).toBeInTheDocument()
-    expect(container.queryByText('task.task_reminder_in_72_hr')).toBeInTheDocument()
-  })
+    expect(container.queryByText('task.task_reminder_in_1_hr')).toBeInTheDocument();
+    expect(container.queryByText('task.task_reminder_in_24_hr')).toBeInTheDocument();
+    expect(container.queryByText('task.task_reminder_in_72_hr')).toBeInTheDocument();
+  });
 
   it('should render task comments section', async () => {
     let container;
@@ -107,9 +95,9 @@ describe('task form component', () => {
             <TaskUpdateForm {...props} />
           </BrowserRouter>
         </MockedProvider>
-      )
-    })
+      );
+    });
 
-    expect(container.queryByText(/comments/i)).toBeTruthy()
+    expect(container.queryByText(/comments/i)).toBeTruthy();
   });
-})
+});
