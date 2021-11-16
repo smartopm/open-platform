343d55850784b85db979dd26973ff9680b80dd7f
Saurabh Shinde <saurabh@doublegdp.com>
2021-11-16 16:19:35 +0530

    Remove userEvent

--- app/javascript/src/__tests__/User/UserPrintDetail.test.js
+++ app/javascript/src/__tests__/User/UserPrintDetail.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/react-testing';
