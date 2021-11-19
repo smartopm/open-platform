cf149907d2b4e11633cf1d8e9a0b2bee7c35210c
Daniel Mutuba <daniel@doublegdp.com>
2021-11-19 06:05:40 +0000

    fix rubocop

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Permission class
 class Permission < ApplicationRecord
   belongs_to :role
   validates :module, uniqueness: { scope: :role_id, case_sensitive: false }
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Permission class
 class Role < ApplicationRecord
   belongs_to :community, optional: true
 
