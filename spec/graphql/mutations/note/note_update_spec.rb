7322deb5e502e124522fad9ea1fbed53587e64f8
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2021-12-02 10:23:26 +0000

    Move tests

--- spec/graphql/mutations/note/note_update_spec.rb
+++ spec/graphql/mutations/note/note_update_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::Note::NoteUpdate do
-  describe 'update for note' do
+  describe('update note') do
     let!(:admin_role) { create(:role, name: 'admin') }
     let!(:permission) do
       create(:permission, module: 'note',
@@ -14,41 +14,122 @@ RSpec.describe Mutations::Note::NoteUpdate do
     let!(:user) { create(:user_with_community) }
     let!(:admin) { create(:admin_user, community_id: user.community_id, role: admin_role) }
 
-    let!(:note) do
-      admin.notes.create!(
-        body: 'Note body',
-        user_id: user.id,
-        community_id: user.community_id,
-        author_id: admin.id,
-      )
-    end
-
-    let(:query) do
+    let(:update_query) do
       <<~GQL
-        mutation noteUpdate($id: ID!, $body: String) {
-          noteUpdate(id: $id, body: $body){
+        mutation noteupdate(
+          $id: ID!
+          $body: String
+          $flagged: Boolean
+          $completed: Boolean
+          $dueDate: String
+          $parentNoteId: ID
+          $documentBlobId: String
+        ) {
+          noteUpdate(
+            id: $id
+            body: $body
+            flagged: $flagged
+            completed: $completed
+            dueDate: $dueDate
+            parentNoteId: $parentNoteId
+            documentBlobId: $documentBlobId
+          ) {
             note {
-              id
+              flagged
               body
+              id
+              dueDate
+              parentNote {
+                id
+              }
+              attachments
             }
           }
         }
       GQL
     end
 
+    let!(:note) do
+      admin.notes.create!(
+        body: 'A note about the user',
+        user_id: user.id,
+        community_id: user.community_id,
+        author_id: admin.id,
+      )
+    end
+
     it 'updates a note' do
-      variables = {
+      variable_updates = {
         id: note.id,
-        body: 'Updated body',
+        body: 'A modified note about the user',
       }
-      result = DoubleGdpSchema.execute(query, variables: variables,
-                                              context: {
-                                                current_user: admin,
-                                                site_community: user.community,
-                                              }).as_json
+
+      result = DoubleGdpSchema.execute(update_query, variables: variable_updates,
+                                                     context: {
+                                                       current_user: admin,
+                                                       site_community: user.community,
+                                                     }).as_json
 
       expect(result.dig('data', 'noteUpdate', 'note', 'id')).not_to be_nil
-      expect(result.dig('data', 'noteUpdate', 'note', 'body')).to eql 'Updated body'
+      expect(result.dig('data', 'noteUpdate', 'note', 'body')).to include 'modified'
+      expect(Notes::NoteHistory.count).to eql 1
+      expect(result['errors']).to be_nil
+
+      result = DoubleGdpSchema.execute(update_query, variables: variable_updates,
+                                                     context: {
+                                                       current_user: user,
+                                                       site_community: user.community,
+                                                     }).as_json
+
+      expect(result.dig('data', 'noteUpdate', 'note', 'id')).to be_nil
+      expect(Notes::NoteHistory.count).to eql 1
+      expect(result.dig('errors', 0, 'message')).to include 'Unauthorized'
+    end
+
+    it 'updates parent_note_id' do
+      other_user_note = create(:note, community_id: user.community_id,
+                                      user_id: user.id, author_id: admin.id, flagged: true)
+      variable_updates = {
+        id: note.id,
+        body: 'An updated subtask',
+        parentNoteId: other_user_note.id,
+      }
+
+      result = DoubleGdpSchema.execute(update_query, variables: variable_updates,
+                                                     context: {
+                                                       current_user: admin,
+                                                       site_community: user.community,
+                                                     }).as_json
+
+      expect(result.dig('data', 'noteUpdate', 'note', 'id')).not_to be_nil
+      expect(result.dig('data', 'noteUpdate', 'note', 'parentNote')['id']).to eq(other_user_note.id)
+    end
+
+    it 'adds attachments to note' do
+      upload = ActiveStorage::Blob.create_and_upload!(
+        io: File.open(Rails.root.join('spec/support/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png',
+      )
+
+      variables = {
+        id: note.id,
+        body: 'Updated note',
+        documentBlobId: upload.signed_id,
+      }
+      result = DoubleGdpSchema.execute(update_query, variables: variables,
+                                                     context: {
+                                                       current_user: admin,
+                                                       site_community: user.community,
+                                                     }).as_json
+
+      expect(result.dig('data', 'noteUpdate', 'note', 'id')).not_to be_nil
+      expect(result.dig('data', 'noteUpdate', 'note', 'attachments', 0, 'filename')).to eq(
+        'test_image.png',
+      )
+      expect(result.dig('data', 'noteUpdate', 'note', 'attachments', 0, 'uploaded_by')).to eq(
+        admin.name,
+      )
       expect(result['errors']).to be_nil
     end
 
@@ -57,11 +138,11 @@ RSpec.describe Mutations::Note::NoteUpdate do
         id: note.id,
         body: 'Updated body',
       }
-      result = DoubleGdpSchema.execute(query, variables: variables,
-                                              context: {
-                                                current_user: nil,
-                                                site_community: user.community,
-                                              }).as_json
+      result = DoubleGdpSchema.execute(update_query, variables: variables,
+                                                     context: {
+                                                       current_user: nil,
+                                                       site_community: user.community,
+                                                     }).as_json
       expect(result.dig('errors', 0, 'message')).to include('Unauthorized')
       expect(result['errors']).not_to be_nil
     end
