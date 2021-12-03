13dad85268286780d369e00e960abc68f07b39ae
Olivier JM <olivier@doublegdp.com>
2021-12-03 13:40:04 +0000

    remove refernce of entry times

--- app/javascript/src/modules/LogBook/__tests__/GuestBook.test.js
+++ app/javascript/src/modules/LogBook/__tests__/GuestBook.test.js
@@ -30,15 +30,6 @@ describe('Should render Guest Book Component', () => {
               id: '162f7517',
               name: 'Js user x',
             },
-            entryTimes: [
-              {
-                visitEndDate: null,
-                visitationDate: '2021-08-20T10:51:00+02:00',
-                endsAt: '2021-10-31 22:51',
-                startsAt: '2021-10-31 02:51',
-                occursOn: []
-              }
-            ],
             closestEntryTime: {
                 visitEndDate: null,
                 visitationDate: '2021-08-20T10:51:00+02:00',
@@ -69,15 +60,6 @@ describe('Should render Guest Book Component', () => {
               id: '162f7517',
               name: 'Js user x',
             },
-            entryTimes: [
-              {
-                visitEndDate: null,
-                visitationDate: '2021-08-20T10:51:00+02:00',
-                endsAt: '2021-10-31 22:51',
-                startsAt: '2021-10-31 02:51',
-                occursOn: []
-              }
-            ],
             closestEntryTime: {
                 visitEndDate: null,
                 visitationDate: '2021-08-20T10:51:00+02:00',
