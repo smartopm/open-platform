b8c6c6c6936581a4e34f70aa000b080f01dc382c
Daniel Mutuba <daniel@doublegdp.com>
2021-12-09 13:01:40 +0000

    remove backward compatibility from campaign module

--- app/graphql/mutations/campaign/campaign_update.rb
+++ app/graphql/mutations/campaign/campaign_update.rb
@@ -59,13 +59,8 @@ module Mutations
 
       # Verifies if current user is admin or not.
       def authorized?(_vals)
-        return true if ::Policy::ApplicationPolicy.new(
-          context[:current_user], nil
-        ).permission?(
-          admin: true,
-          module: :campaign,
-          permission: :can_update_campaign,
-        )
+        return true if permitted?(module: :campaign,
+                                  permission: :can_update_campaign)
 
         raise GraphQL::ExecutionError, I18n.t('errors.unauthorized')
       end
