d3ef7553104eac6fc513b8cf48cdeb961a33a6c0
Olivier JM <olivier@doublegdp.com>
2021-12-21 15:12:30 +0000

    remov unecessary test

--- app/javascript/src/__tests__/CenteredContent.test.js
+++ app/javascript/src/__tests__/CenteredContent.test.js
@@ -4,22 +4,11 @@ import CenteredContent from '../shared/CenteredContent'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('component that centers divs', () => {
-  it('should not render with wrong props', () => {
-    const props = {
-      title: 'Here i am '
-    }
-    const container = render(<CenteredContent {...props}>{null}</CenteredContent>)
-    expect(container.queryByText('Here i am')).toBeNull()
-    expect(container.queryByText('Here i am')).not.toBeInTheDocument()
-  })
 
   it('should render with the proper props', () => {
-
     const container = render(<CenteredContent><p>I am a paragraph</p></CenteredContent>)
       expect(container.queryByText('I am a paragraph')).not.toBeNull()
       expect(container.queryByText('I am a paragraph')).toBeInTheDocument()
-      expect(container.queryByText('I am another paragraph')).not.toBeNull()
-      expect(container.queryByText('I am another paragraph')).toBeInTheDocument()
-      expect(container.queryAllByText('I am another paragraph')).toHaveLength(1)
+      expect(container.queryAllByText('I am a paragraph')).toHaveLength(1)
   })
 })
