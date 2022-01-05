be94d2815e9d1c808f3aeda5195831501f8c6055
Daniel Mutuba <daniel@doublegdp.com>
2022-01-05 13:33:08 +0000

    use mui V5 imports for new component

--- app/javascript/src/shared/CustomProgressBar.jsx
+++ app/javascript/src/shared/CustomProgressBar.jsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@mui/styles';
 import LinearProgress from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
 import { Typography } from '@material-ui/core';
