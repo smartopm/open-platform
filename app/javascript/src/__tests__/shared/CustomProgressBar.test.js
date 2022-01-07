d65a23d6d7ccf497271a960d612d58f0f5c1c62a
Olivier JM <olivier@doublegdp.com>
2022-01-07 14:15:32 +0000

    fix tests

--- app/javascript/src/__tests__/shared/CustomProgressBar.test.js
+++ app/javascript/src/__tests__/shared/CustomProgressBar.test.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import '@testing-library/jest-dom/extend-expect';
 import CustomProgressBar from '../../shared/CustomProgressBar';
+import MockedThemeProvider from '../../modules/__mocks__/mock_theme';
 
 describe('Editable Field component', () => {
-
-  const theme = createTheme();
   const task = {
-      description: 'some description',
-      body: 'some body',
-      parentNote: { id: '1234', body: 'some parent body' },
-      progress: { complete: 2, progress_percentage: 67, total: 3 }
+    description: 'some description',
+    body: 'some body',
+    parentNote: { id: '1234', body: 'some parent body' },
+    progress: { complete: 2, progress_percentage: 67, total: 3 }
   };
 
-  
   it('should properly render when large screen', () => {
     const wrapper = render(
-      <ThemeProvider theme={theme}>
+      <MockedThemeProvider>
         <CustomProgressBar task={task} smDown={false} />
-      </ThemeProvider>);
+      </MockedThemeProvider>
+    );
     expect(wrapper.queryByTestId('custom_progress_bar')).toBeInTheDocument();
     expect(wrapper.queryByTestId('custom_progress_bar_text')).toBeInTheDocument();
   });
 
-
   it('should properly render when small screen', () => {
     const wrapper = render(
-      <ThemeProvider theme={theme}>
+      <MockedThemeProvider>
         <CustomProgressBar task={task} smDown />
-      </ThemeProvider>);
+      </MockedThemeProvider>
+    );
     expect(wrapper.queryByTestId('custom_progress_bar_mobile')).toBeInTheDocument();
     expect(wrapper.queryByTestId('custom_progress_bar_text_mobile')).toBeInTheDocument();
   });
