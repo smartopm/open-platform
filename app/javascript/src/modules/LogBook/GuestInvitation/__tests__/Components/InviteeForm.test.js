a024a9b3bd357c4bb33ebb752f73cda2ec347c2e
Olivier JM <olivier@doublegdp.com>
2022-01-11 08:12:15 +0000

    fix invitee form tests

--- app/javascript/src/modules/LogBook/GuestInvitation/__tests__/Components/InviteeForm.test.js
+++ app/javascript/src/modules/LogBook/GuestInvitation/__tests__/Components/InviteeForm.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-statements */
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
@@ -8,13 +7,12 @@ import userMock from '../../../../../__mocks__/authstate';
 import InviteeForm from '../../Components/InviteeForm';
 
 describe('Guest Invitation Form', () => {
-  it('should render the invitation form', async () => {
-    const guest = { firstName: 'some name', lastName: 'Jan', phoneNumber: null, isAdded: false };
-    const handlePhoneNumber = jest.fn()
-    const handleInputChange = jest.fn()
-    const handleRemoveUser = jest.fn()
-    const handleAddUser = jest.fn()
+  const guest = { firstName: 'some name', lastName: 'Jan', phoneNumber: null, isAdded: false };
+  const handlePhoneNumber = jest.fn();
+  const handleInputChange = jest.fn();
+  const handleAction = jest.fn();
 
+  it('should render the non-primary invitation form', () => {
     const { getByTestId, getByText } = render(
       <MemoryRouter>
         <Context.Provider value={userMock}>
@@ -22,8 +20,7 @@ describe('Guest Invitation Form', () => {
             guestData={guest}
             handlePhoneNumber={handlePhoneNumber}
             handleInputChange={handleInputChange}
-            handleRemoveUser={handleRemoveUser}
-            handleAddUser={handleAddUser}
+            handleAction={handleAction}
             guestCount={1}
           />
         </Context.Provider>
@@ -45,14 +42,41 @@ describe('Guest Invitation Form', () => {
     fireEvent.change(lastName, { target: { value: 'Some random lastName' } });
     expect(lastName.value).toBe('Some random lastName');
 
-    expect(phoneNumber).not.toBeDisabled()
+    expect(phoneNumber).not.toBeDisabled();
     fireEvent.change(phoneNumber, { target: { value: '090909090' } });
     expect(phoneNumber.value).toBe('090909090');
 
     expect(getByTestId('add_remove_guest_btn')).toBeInTheDocument();
-    expect(getByTestId('add_remove_guest_btn').textContent).toContain('misc.add')
+    expect(getByTestId('add_remove_guest_btn').textContent).toContain('misc.remove');
 
     fireEvent.click(getByTestId('add_remove_guest_btn'));
-    expect(handleAddUser).toBeCalled()
+    expect(handleAction).toBeCalled();
+  });
+
+  it('should render the primary form to add other guests', () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Context.Provider value={userMock}>
+          <InviteeForm
+            guestData={guest}
+            handlePhoneNumber={handlePhoneNumber}
+            handleInputChange={handleInputChange}
+            handleAction={handleAction}
+            primary
+          />
+        </Context.Provider>
+      </MemoryRouter>
+    );
+
+    fireEvent.change(getByTestId('guest_entry_first_name'), {
+      target: { value: 'Some random firstName' }
+    });
+    expect(getByTestId('guest_entry_first_name').value).toBe('Some random firstName');
+
+    expect(getByTestId('add_remove_guest_btn')).toBeInTheDocument();
+    expect(getByTestId('add_remove_guest_btn').textContent).toContain('misc.add');
+
+    fireEvent.click(getByTestId('add_remove_guest_btn'));
+    expect(handleAction).toBeCalled();
   });
 });
