2b17bb5ba05a67c0279849eb84e68e0b0d495bf0
Olivier JM <olivier@doublegdp.com>
2022-01-11 12:03:44 +0000

    add more tests

--- app/javascript/src/modules/LogBook/GuestInvitation/__tests__/Components/helpers.test.js
+++ app/javascript/src/modules/LogBook/GuestInvitation/__tests__/Components/helpers.test.js
@@ -1,4 +1,4 @@
-import { filterEmptyObjectByKey } from '../../helpers';
+import { filterEmptyObjectByKey, validateGuest } from '../../helpers';
 
 describe('helpers', () => {
   it('should remove items that dont have certain keys values', () => {
@@ -26,5 +26,36 @@ describe('helpers', () => {
   it('should return empty arr if none is provied', () => {
     expect(filterEmptyObjectByKey([], 'id')).toHaveLength(0);
     expect(filterEmptyObjectByKey(null, 'id')).toEqual([]);
+  });
+  it('validates guests and invitation time', () => {
+    const t1 = jest.fn(() => 'invalid');
+
+    expect(validateGuest({ guests: [], userIds: [], t: t1, guestData: {} }).valid).toBe(false);
+    expect(validateGuest({ guests: [], userIds: [], t: t1, guestData: {} }).msg).toEqual('invalid');
+
+    expect(
+      validateGuest({ guests: [], userIds: ['ajsdhsa'], t: t1, guestData: { startsAt: null } }).msg
+    ).toEqual('invalid');
+    expect(
+      validateGuest({ guests: [], userIds: ['ajsdhsa'], t: t1, guestData: { startsAt: null } })
+        .valid
+    ).toBe(false);
+
+    expect(
+      validateGuest({
+        guests: [],
+        userIds: ['ajsdhsa'],
+        t: t1,
+        guestData: { startsAt: new Date() }
+      }).msg
+    ).toBeNull();
+    expect(
+      validateGuest({
+        guests: [],
+        userIds: ['ajsdhsa'],
+        t: t1,
+        guestData: { startsAt: new Date() }
+      }).valid
+    ).toBe(true);
   });
 });
