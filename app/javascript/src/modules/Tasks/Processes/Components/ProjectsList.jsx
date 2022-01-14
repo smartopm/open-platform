edd7c6274e60c7723e88fce2d2f056bc8c471568
tolulope3 <tolulope@doublegdp.com>
2022-01-14 12:03:49 +0000

    revert bact to v4

--- app/javascript/src/modules/Tasks/Processes/Components/ProjectsList.jsx
+++ app/javascript/src/modules/Tasks/Processes/Components/ProjectsList.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable max-statements */
 import React from 'react';
 import { useQuery } from 'react-apollo';
-import { Grid, Typography, Breadcrumbs } from '@mui/material';
+import { Grid, Typography, Breadcrumbs } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@mui/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import { formatError } from '../../../../utils/helpers';
 import { Spinner } from '../../../../shared/Loading';
 import CenteredContent from '../../../../shared/CenteredContent';
