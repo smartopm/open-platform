d82c8001d6f58c00bb7745ad0485e8a5d9543b5c
Olivier JM <olivier@doublegdp.com>
2022-01-21 09:09:31 +0000

    hide back arrow on small devices

--- app/javascript/src/modules/Menu/component/BackArrow.jsx
+++ app/javascript/src/modules/Menu/component/BackArrow.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 export default function BackArrow({ path }) {
   const classes = useStyles();
-  const matches = useMediaQuery('(max-width:600px)');
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.only('xs'));
   const history = useHistory();
+  console.log(matches)
   return (
     <>
-      {path !== '/' && (
-        <IconButton className={classes.body} color='primary' data-testid='arrow' style={matches ? {marginLeft: '-30px'} : {}} onClick={() => history.goBack()}>
+      {path !== '/' && !matches && (
+        <IconButton className={classes.body} color='primary' data-testid='arrow' onClick={() => history.goBack()}>
           <ArrowBackIcon />
         </IconButton>
       )}
