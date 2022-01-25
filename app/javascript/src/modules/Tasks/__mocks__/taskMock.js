ac080401a2b13f80011ec4238ca1cd4dc48f2f22
Bonny Mwenda <bonny@doublegdp.com>
2022-01-25 15:20:39 +0000

    Fix failing test

--- app/javascript/src/modules/Tasks/__mocks__/taskMock.js
+++ app/javascript/src/modules/Tasks/__mocks__/taskMock.js
@@ -41,48 +41,48 @@ export default {
   documents: [],
   subTasks: [
     {
-        body: "Subtask assigned",
-        createdAt: "2022-01-05T09:47:22+02:00",
-        id: "1588b0f3-5978-47e4-834f-407db59b2e79",
-        completed: true,
-        category: "to_do",
-        description: "Subtask assigned",
-        dueDate: "2022-01-05T09:46:59+02:00",
-        progress: {
-            complete: 1,
-            total: 0,
-            progress_percentage: null
-        },
-        user: {
-            id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
-            name: "John Doctor",
-            imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c"
-        },
-        author: {
-            id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
-            name: "John Doctor",
-            imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c",
-            avatarUrl: null
-        },
-        assignees: [
-            {
-                id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
-                name: "John Tester",
-                imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c",
-                avatarUrl: null
-            }
-        ],
-        assigneeNotes: [
-            {
-                id: "2415a3b9-5548-47ac-bff0-54ebd2605fe5",
-                userId: "bdf23d62-071c-4fdf-8ee5-7add18236090",
-                reminderTime: null
-            }
-        ],
-        parentNote: {
-            id: "23"
-        },
-        documents: []
+      body: "Subtask assigned",
+      createdAt: "2022-01-05T09:47:22+02:00",
+      id: "1588b0f3-5978-47e4-834f-407db59b2e79",
+      completed: true,
+      category: "to_do",
+      description: "Subtask assigned",
+      dueDate: "2022-01-05T09:46:59+02:00",
+      progress: {
+          complete: 1,
+          total: 0,
+          progress_percentage: null
+      },
+      user: {
+          id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
+          name: "John Doctor",
+          imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c"
+      },
+      author: {
+          id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
+          name: "John Doctor",
+          imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c",
+          avatarUrl: null
+      },
+      assignees: [
+          {
+              id: "bdf23d62-071c-4fdf-8ee5-7add18236090",
+              name: "John Tester",
+              imageUrl: "https://lh3.googleusercontent.com/a-/AOh14Gh40stJUGHRhbpdm5UmSRMSdebN5MqclpqDwB8j=s96-c",
+              avatarUrl: null
+          }
+      ],
+      assigneeNotes: [
+          {
+              id: "2415a3b9-5548-47ac-bff0-54ebd2605fe5",
+              userId: "bdf23d62-071c-4fdf-8ee5-7add18236090",
+              reminderTime: null
+          }
+      ],
+      parentNote: {
+          id: "23"
+      },
+      documents: []
     }
-],
+  ],
 };
