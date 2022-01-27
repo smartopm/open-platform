00214f19a3fbc19049b8fb91c70f7a8ceaa34cd6
tolulope3 <tolulope@doublegdp.com>
2022-01-27 10:04:43 +0000

    add more test

--- app/javascript/src/modules/Tasks/__tests__/TaskDetailAccordion.test.js
+++ app/javascript/src/modules/Tasks/__tests__/TaskDetailAccordion.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom/'
 import { MockedProvider } from '@apollo/react-testing'
 import TaskDetailAccordion from '../Components/TaskDetailAccordion'
@@ -7,7 +7,7 @@ import '@testing-library/jest-dom/extend-expect'
 
 describe('Task detail accordion Component', () => {
 
-  it('component should be in the component', () => {
+  it('component should render without error', () => {
     const container = render(
       <MockedProvider>
         <BrowserRouter>
@@ -15,7 +15,6 @@ describe('Task detail accordion Component', () => {
             icon={<div />}
             title='sample-title'
             component={<div />}
-            openDetail
           />
         </BrowserRouter>
       </MockedProvider>
@@ -24,5 +23,8 @@ describe('Task detail accordion Component', () => {
     expect(container.queryByTestId('body')).toBeInTheDocument();
     expect(container.queryByTestId('title')).toHaveTextContent('sample-title');
     expect(container.queryByTestId('toggle-icon')).toBeInTheDocument();
+
+    fireEvent.click(container.queryByTestId('toggle-icon'));
+    expect(container.queryByTestId('component')).toBeInTheDocument();
   })
 })
