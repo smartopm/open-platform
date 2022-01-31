be154c336d868cf412b3bf23ae85ecc8c371b500
Olivier JM <olivier@doublegdp.com>
2022-01-31 10:45:14 +0000

    add tests on the f.e

--- app/javascript/src/modules/Tasks/Processes/__tests__/utils.test.js
+++ app/javascript/src/modules/Tasks/Processes/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { hrefsExtractor, sentenceToSnakeCase } from '../utils';
+import { hrefsExtractor, sentenceToSnakeCase, snakeCaseToSentence } from '../utils';
 
 describe('find hrefs in a string', () => {
   const hrefsString = "sample string <a href='href1'>link1</a> string <a href='href2'>link2</a>"
@@ -12,5 +12,10 @@ describe('sentenceToSnakeCase', () => {
   it('convertes text to snake_case', () => {
     const text = "This Should Be Converted To Snake Case";
     expect(sentenceToSnakeCase(text)).toEqual('this_should_be_converted_to_snake_case');
+  });
+  it('convertes snake_case to normal title cased text', () => {
+    const snakeText = "this_should_be_converted_to_snake_case";
+    expect(snakeCaseToSentence(snakeText)).toEqual('This Should Be Converted To Snake Case');
+    expect(snakeCaseToSentence(null)).toBeNull();
   });
 });
