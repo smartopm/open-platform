0c54f7909272e771792b16262c2caf61634fbb89
Daniel Mutuba <daniel@doublegdp.com>
2022-02-04 11:14:40 +0000

    revert changes in app/javascript/src/modules/__mocks__/mock_theme.js

--- app/javascript/src/modules/__mocks__/mock_theme.js
+++ app/javascript/src/modules/__mocks__/mock_theme.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
-import { createTheme } from '@material-ui/core/styles'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import PropTypes from 'prop-types'
 
 function MockedThemeProvider({ children }) {
-  const theme = createTheme({
+  const theme = createMuiTheme({
     props: { MuiWithWidth: { initialWidth: "sm" } },
   });
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
-
-
 
 MockedThemeProvider.propTypes = {
     children: PropTypes.node.isRequired
