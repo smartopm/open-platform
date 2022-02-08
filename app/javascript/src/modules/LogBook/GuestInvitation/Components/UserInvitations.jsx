049010f6c8f482ffbfb5020050b2314ed3dcbb7d
Olivier JM <olivier@doublegdp.com>
2022-02-08 09:20:14 +0000

    create a reusable invitation history for user

---
+++ app/javascript/src/modules/LogBook/GuestInvitation/Components/UserInvitations.jsx
@@ -0,0 +1,85 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { useMediaQuery } from '@material-ui/core';
+import Chip from '@mui/material/Chip';
+import { useTranslation } from 'react-i18next';
+import Card from '../../../../shared/Card';
+// import { dateTimeToString, dateToString } from '../../../../components/DateContainer';
+import { checkRequests } from '../../utils';
+import { Context as AuthStateContext } from '../../../../containers/Provider/AuthStateProvider';
+import Avatar from '../../../../components/Avatar';
+
+export default function UserInvitations({ invitation, handleViewUser }) {
+  const authState = useContext(AuthStateContext);
+  const { t } = useTranslation('logbook');
+
+  const matches = useMediaQuery('(max-width:800px)');
+
+  const timeZone = authState.user.community.timezone;
+  const isValid = checkRequests(invitation.closestEntryTime, t, timeZone).valid;
+  return (
+    <Card key={invitation.id}>
+      <Grid container spacing={1}>
+        <Grid item md={2} xs={4}>
+          <Avatar imageUrl={invitation.user.imageUrl} user={invitation.user} alt="avatar-image" />
+        </Grid>
+        <Grid item md={2} xs={8}>
+          <Typography variant="caption" color="primary" onClick={handleViewUser}>
+            <Link to={`/user/${invitation.user.id}`}>{invitation.user.name}</Link>
+          </Typography>
+        </Grid>
+        <Grid item md={3} xs={12} style={!matches ? { paddingTop: '15px' } : {}}>
+          <Typography variant="caption" gutterButtom>
+            Starts:
+          </Typography>
+          <Typography variant="caption" gutterButtom>
+            Ends:
+          </Typography>
+        </Grid>
+        <Grid item md={3} xs={12} style={!matches ? { paddingTop: '15px' } : {}}>
+          <Typography variant="caption" gutterButtom>
+            Repeats:
+          </Typography>
+          <Typography variant="caption" gutterButtom>
+            Invited Created:
+          </Typography>
+        </Grid>
+        <Grid item md={2} xs={12} style={!matches ? { paddingTop: '15px' } : {}}>
+          <Chip
+            label={isValid ? t('guest_book.valid') : t('guest_book.invalid_now')}
+            color={isValid ? 'success' : 'error'}
+            size="small"
+          />
+        </Grid>
+      </Grid>
+    </Card>
+  );
+}
+
+UserInvitations.propTypes = {
+  invitation: PropTypes.shape({
+    closestEntryTime: PropTypes.shape({
+      id: PropTypes.string,
+      startsAt: PropTypes.string,
+      endsAt: PropTypes.string,
+      invitationationDate: PropTypes.string
+    }),
+    user: PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.string,
+      imageUrl: PropTypes.string
+    }),
+    guest: PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.string
+    }),
+    multipleInvites: PropTypes.bool,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    status: PropTypes.string
+  }).isRequired,
+  handleViewUser: PropTypes.func.isRequired
+};
