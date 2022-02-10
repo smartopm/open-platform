6e26f8b7ebadaa8bdf83c1fdc9f626529ff06272
Saurabh Shinde <saurabh@doublegdp.com>
2022-02-10 17:07:39 +0530

    Update columns for lead management

--- db/migrate/20220209065133_add_columns_for_lead_management.rb
+++ db/migrate/20220209065133_add_columns_for_lead_management.rb
@@ -1,7 +1,5 @@
 class AddColumnsForLeadManagement < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :title, :string
-    add_column :users, :linkedin_url, :string
     add_column :users, :company_name, :string
     add_column :users, :country, :string
     add_column :users, :company_description, :string
@@ -10,6 +8,8 @@ class AddColumnsForLeadManagement < ActiveRecord::Migration[6.1]
     add_column :users, :company_employees, :integer
     add_column :users, :company_annual_revenue, :string
     add_column :users, :company_contacted, :string
+    add_column :users, :industry_sub_sector, :string
+    add_column :users, :industry_business_activity, :string
     add_column :users, :industry, :string
     add_column :users, :level_of_internationalization, :string
     add_column :users, :lead_temperature, :string
@@ -18,11 +18,12 @@ class AddColumnsForLeadManagement < ActiveRecord::Migration[6.1]
     add_column :users, :lead_owner, :string
     add_column :users, :lead_type, :string
     add_column :users, :client_category, :string
-    add_column :users, :next_steps, :string
+    add_column :users, :next_steps, :text
     add_column :users, :first_contact_date, :datetime
     add_column :users, :last_contact_date, :datetime
     add_column :users, :created_by, :string
     add_column :users, :modified_by, :string
     add_column :users, :relevant_link, :string
+    add_column :users, :contact_details, :jsonb
   end
 end
