062d79bd680a428589750e2b6bd1ba99b1a6ac1c
tolulope3 <tolulope@doublegdp.com>
2022-02-10 14:51:21 +0000

    add select button

--- app/javascript/src/modules/Users/Components/UserHeader.jsx
+++ app/javascript/src/modules/Users/Components/UserHeader.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useRef } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import SelectButton from '../../../shared/buttons/SelectButton'
+import SelectButton from '../../../shared/buttons/SelectButton';
+import { objectAccessor } from '../../../utils/helpers';
 
 export default function UserHeader({ setCampaignOption }) {
   const [open, setOpen] = useState(false);
@@ -9,13 +10,14 @@ export default function UserHeader({ setCampaignOption }) {
   const anchorRef = useRef(null);
   const options = {
     all: 'All',
-    'all_on_the_page': 'All on this page',
+    all_on_the_page: 'All on this page',
     none: 'none'
-  }
+  };
 
-  // const selectedOptions = checkedOptions === 'none' ? 'select' : objectAccessor(options, selectedKey)
+  const selectedOptions =
+    selectedKey === 'none' || selectedKey === '' ? 'select' : objectAccessor(options, selectedKey);
 
-  const handleClose = (event) => {
+  const handleClose = event => {
     if (anchorRef.current && anchorRef.current.contains(event.target)) {
       return;
     }
@@ -23,29 +25,29 @@ export default function UserHeader({ setCampaignOption }) {
     setOpen(false);
   };
 
-  function handleMenuItemClick(key){
-    setSelectedKey(key)
+  function handleMenuItemClick(key) {
+    setCampaignOption(key);
+    setSelectedKey(key);
     setOpen(false);
-    // handleCheckOptions(key)
   }
   return (
     <Grid container>
       <Grid item lg={12} md={12} sm={12}>
-        <Typography variant='h5'>Users</Typography>
+        <Typography variant="h5">Users</Typography>
       </Grid>
       <Grid item lg={3} md={3} sm={3}>
-        <SelectButton 
-          buttonText='select'
+        <SelectButton
+          buttonText={selectedOptions}
           open={open}
           anchorEl={anchorRef.current}
           anchorRef={anchorRef}
           handleClose={handleClose}
           options={options}
           selectedKey={selectedKey}
-          handleMenuItemClick={setCampaignOption}
-          handleClick={() => setOpen(!open)} 
+          handleMenuItemClick={handleMenuItemClick}
+          handleClick={() => setOpen(!open)}
         />
       </Grid>
     </Grid>
-  )
+  );
 }
