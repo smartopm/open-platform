9ceb54821dc9c653dcfc8f62059ed83b586cdebb
Daniel Mutuba <daniel@doublegdp.com>
2022-02-10 17:29:11 +0000

    add select fields values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_065133) do
+ActiveRecord::Schema.define(version: 2022_01_26_113114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2022_02_09_065133) do
     t.uuid "record_id", null: false
     t.uuid "blob_id", null: false
     t.datetime "created_at", null: false
-    t.integer "status", default: 0
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
