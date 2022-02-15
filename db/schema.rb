75b12919704daade6410028b9352bc18e4cf87bd
Daniel Mutuba <daniel@doublegdp.com>
2022-02-15 21:34:10 +0000

    update schema version number to fix failing test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_105950) do
+ActiveRecord::Schema.define(version: 2022_02_09_065133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
