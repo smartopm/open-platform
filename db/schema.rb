c50e73f7a80761b4be125d20963d586cc37bf924
OlivierJM <manolivier93@gmail.com>
2022-02-22 16:12:57 +0200

    Sync with master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_081951) do
+ActiveRecord::Schema.define(version: 2022_02_09_065133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
