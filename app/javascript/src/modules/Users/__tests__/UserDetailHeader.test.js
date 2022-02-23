a72e596ed1711ad38080b0e887d5509430bb27ed
tolulope3 <tolulope@doublegdp.com>
2022-02-23 23:07:39 +0000

    add more tests

--- app/javascript/src/modules/Users/__tests__/UserDetailHeader.test.js
+++ app/javascript/src/modules/Users/__tests__/UserDetailHeader.test.js
@@ -11,23 +11,39 @@ describe('User Detail Header Component', () => {
     user: {
       id: '37286ew3'
     }
-  }
+  };
 
   const authState = {
     user: {
-      permissions: [{module: 'user', permissions: ['can_see_menu_item']}],
+      permissions: [
+        { module: 'user', permissions: ['can_see_menu_item'] },
+        { module: 'entry_request', permissions: ['can_see_menu_item'] },
+        { module: 'communication', permissions: ['can_see_menu_item'] },
+        { module: 'forms', permissions: ['can_see_menu_item'] }
+      ],
       community: {
         features: {
-          Users: { features: [] }
+          Users: { features: [] },
+          Messages: { features: [] },
+          LogBook: { features: [] },
+          Payments: { features: [] },
+          Properties: { features: [] },
+          Forms: { features: [] },
+          'Customer Journey': { features: [] }
         }
       }
     }
-  }
+  };
   it('should render the user detail header component', () => {
     const container = render(
       <MockedProvider>
         <BrowserRouter>
-          <UserDetailHeader data={data} userType='admin' currentTab='Contacts' authState={authState} />
+          <UserDetailHeader
+            data={data}
+            userType="admin"
+            currentTab="Contacts"
+            authState={authState}
+          />
         </BrowserRouter>
       </MockedProvider>
     );
@@ -41,7 +57,12 @@ describe('User Detail Header Component', () => {
       <MockedProvider>
         <BrowserRouter>
           <MockedThemeProvider>
-            <UserDetailHeader data={data} userType='admin' currentTab='Contacts' authState={authState} />
+            <UserDetailHeader
+              data={data}
+              userType="admin"
+              currentTab="Contacts"
+              authState={authState}
+            />
           </MockedThemeProvider>
         </BrowserRouter>
       </MockedProvider>
@@ -52,8 +73,28 @@ describe('User Detail Header Component', () => {
     fireEvent.click(container.queryByTestId('arrow-icon'));
     expect(container.queryByText('User Settings')).toBeInTheDocument();
 
-    fireEvent.click(container.queryByText('User Settings'))
+    fireEvent.click(container.queryByText('User Settings'));
     expect(container.queryByText('Edit User')).toBeInTheDocument();
-    fireEvent.click(container.queryByTestId('user-detail'))
+    fireEvent.click(container.queryByText('Edit User'));
+    fireEvent.click(container.queryByText('Print ID'));
+    fireEvent.click(container.queryByText('User Settings'));
+    expect(container.queryByText('Edit User')).not.toBeInTheDocument();
+    expect(container.queryByText('Print ID')).not.toBeInTheDocument();
+    fireEvent.click(container.queryByText('User Logs'));
+    fireEvent.click(container.queryByText('Merge User'));
+    fireEvent.click(container.queryByText('Plans'));
+    fireEvent.click(container.queryByText('Plots'));
+    fireEvent.click(container.queryByText('LeadManagement'));
+    fireEvent.click(container.queryByText('Forms'));
+    fireEvent.click(container.queryByText('CustomerJourney'));
+    fireEvent.click(container.queryByText('Communications'));
+    expect(container.queryByText('Communication')).toBeInTheDocument();
+    expect(container.queryByText('Send SMS')).toBeInTheDocument();
+    fireEvent.click(container.queryByText('Send SMS'));
+    fireEvent.click(container.queryByText('Send OTP'));
+    fireEvent.click(container.queryByText('Message Support'));
+    fireEvent.click(container.queryByText('Communication'));
+    expect(container.queryByText('Send SMS')).not.toBeInTheDocument();
+    expect(container.queryByText('Send OTP')).not.toBeInTheDocument();
   });
 });
