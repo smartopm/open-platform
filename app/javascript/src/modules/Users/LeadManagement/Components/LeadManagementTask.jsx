e9680f91da783917d6d7084107ada234efd81ff6
Daniel Mutuba <daniel@doublegdp.com>
2022-02-24 18:45:06 +0000

    fix mobile responsiveness

--- app/javascript/src/modules/Users/LeadManagement/Components/LeadManagementTask.jsx
+++ app/javascript/src/modules/Users/LeadManagement/Components/LeadManagementTask.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import { Grid } from '@mui/material';
 import TaskUpdate from '../../../Tasks/containers/TaskUpdate';
 import CenteredContent from '../../../../shared/CenteredContent';
 
@@ -9,13 +8,13 @@ export default function LeadManagementTask({ taskId }) {
   const { t } = useTranslation('task');
 
   return (
-    <Grid container style={{ margin: '0 -45px 0 -45px' }}>
+    <div style={{ margin: '0 -45px 0 -45px' }}>
       {taskId !== null ? (
         <TaskUpdate taskId={taskId} />
       ) : (
         <CenteredContent>{t('task.no_tasks')}</CenteredContent>
       )}
-    </Grid>
+    </div>
   );
 }
 
