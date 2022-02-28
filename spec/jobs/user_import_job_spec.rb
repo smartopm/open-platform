232803a4a471fe19278922bab26a3ba7a9deacc1
Saurabh Shinde <saurabh@doublegdp.com>
2022-02-28 16:56:58 +0530

    Use ascv gem to detect CSV delimiter

--- spec/jobs/user_import_job_spec.rb
+++ spec/jobs/user_import_job_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe UserImportJob, type: :job do
   let!(:prospective_client_role) { create(:role, name: 'prospective_client') }
   let!(:user) { create(:admin_user, community_id: non_admin.community_id) }
   csv_string = "Name,Email primary,Phone number primary,Phone number secondary 1,Phone number secondary 2,User type,Labels,State,Expiration date,Notes on client\nThomas Shalongolo,thomas@gmail.com,+234979063360,,,Prospective Client,Residency program Waitlist;Some other label,valid,,some notes here\nJide Babs,jide@gmail.com,+260979013360,,,Prospective Client,Residency program Waitlist;Some other label,pending,,some notes here"
+  semicolon_csv_string = "Name;Email primary;Phone number primary;Phone number secondary 1;Phone number secondary 2;User type;Labels;State;Expiration date;Notes on client\nJohn Doe;john@gmail.com;+234979063360;;;Prospective Client;Residency program Waitlist,Some other label;valid;;some notes here\nJane Doe;jane@gmail.com;+260979013360;;;Prospective Client;Residency program Waitlist,Some other label;pending;;some notes here"
 
   describe '#perform_later' do
     before do
@@ -39,6 +40,17 @@ RSpec.describe UserImportJob, type: :job do
       UserImportJob.perform_later(csv_string, 'A File.csv', user)
 
       expect(Users::User.count).to eql(prev_user_count)
+    end
+
+    context 'when CSV string is semicolon separated' do
+      it 'is expected to create new users' do
+        prev_user_count = Users::User.count
+        ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+
+        UserImportJob.perform_later(semicolon_csv_string, 'A File.csv', user)
+
+        expect(Users::User.count).to eql(prev_user_count + 2)
+      end
     end
   end
 end
