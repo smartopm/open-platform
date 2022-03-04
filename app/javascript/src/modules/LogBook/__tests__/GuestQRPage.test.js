6862cf07b03065dc4155220f4c71be70179d2683
Saurabh Shinde <saurabh@doublegdp.com>
2022-03-04 12:13:03 +0530

    Update test

--- app/javascript/src/modules/LogBook/__tests__/GuestQRPage.test.js
+++ app/javascript/src/modules/LogBook/__tests__/GuestQRPage.test.js
@@ -3,21 +3,19 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom/';
 import { MockedProvider } from '@apollo/react-testing';
-import { act } from 'react-dom/test-utils'; 
 import { Context } from '../../../containers/Provider/AuthStateProvider';
 import authState from '../../../__mocks__/authstate';
-import GuestQRPage from '../Components/GuestQRPage';
+import GuestQRPage, { GuestQRCode } from '../Components/GuestQRPage';
 import { Spinner } from '../../../shared/Loading';
 
-describe('Log Events Component', () => {
-  it('renders log event component', () => {
-    act(() => {
-    const container = render(
+describe('Guest QRPage Component', () => {
+  it('renders GuestQRPage component', () => {
+    render(
       <MockedProvider>
         <BrowserRouter>
           <Context.Provider value={authState}>
             <GuestQRPage
-              match={{params: {id: '22'}}}
+              match={{params: {id: 'd6765-b10e-4865-bee7'}}}
             />
           </Context.Provider>
         </BrowserRouter>
@@ -26,9 +24,31 @@ describe('Log Events Component', () => {
     
       const loader = render(<Spinner />);
       expect(loader.queryAllByTestId('loader')[0]).toBeInTheDocument();
+  });
 
+  it('renders GuestQRCode component', () => {
+    const data = {
+      user: {
+        id: 'a54d6184-b10e-4865-bee7-7957701d423d',
+        name: 'John Doe',
+        userType: 'visitor',
+      }
+    };
+
+    const container = render(
+      <MockedProvider>
+        <BrowserRouter>
+          <Context.Provider value={authState}>
+            <GuestQRCode
+              data={data}
+              requestId="d6765-b10e-4865-bee7"
+            />
+          </Context.Provider>
+        </BrowserRouter>
+      </MockedProvider>
+    );
       expect(container.queryByText('guest.qr_code')).toBeInTheDocument();
-    });
+      expect(container.queryByText('John Doe')).toBeInTheDocument();
   });
 
 });
