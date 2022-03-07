fd0801e87bd0305794942e0d0e0e09d77d494158
tolulope3 <tolulope@doublegdp.com>
2022-03-07 10:54:38 +0000

    fix processes page for developer role

--- app/javascript/src/modules/Tasks/Processes/Components/ClientPilotViewItem.jsx
+++ app/javascript/src/modules/Tasks/Processes/Components/ClientPilotViewItem.jsx
@@ -1,85 +1,97 @@
-import React, { useContext } from 'react'
-import { Grid,Typography } from '@mui/material';
-import { useHistory } from 'react-router-dom'
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useContext } from 'react';
+import { Grid, Typography } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import Divider from '@mui/material/Divider';
 import { removeNewLines, sanitizeText } from '../../../../utils/helpers';
-import { TaskContext } from "../../Context";
+import { TaskContext } from '../../Context';
 import ProjectSteps from './Steps';
 import ProjectItem from './ProjectItem';
 
-export default function ClientPilotViewItem({project, refetch}) {
-    const taskId  = project?.id
-    const history = useHistory()
-    const { t } = useTranslation('task')
-    const { handleStepCompletion } = useContext(TaskContext);
+export default function ClientPilotViewItem({ project, refetch }) {
+  const taskId = project?.id;
+  const history = useHistory();
+  const classes = useStyles();
+  const { t } = useTranslation('task');
+  const { handleStepCompletion } = useContext(TaskContext);
 
-    function handleProjectStepClick() {
-      return history.push(`/processes/drc/projects/${taskId}?tab=processes`)
-    }
+  function handleProjectStepClick(tab = 'processes') {
+    return history.push(`/processes/drc/projects/${taskId}?tab=${tab}`);
+  }
 
-    return (
-      <Grid container spacing={2}>
-        <Grid item md={12} xs={12}>
-          <Typography variant="h6">
-            <span
-              data-testid='task-title'
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                  __html: sanitizeText(removeNewLines(project.body))
-              }}
-            />
-          </Typography>
-        </Grid>
-        <Grid item md={12} xs={12} data-testid="project-container">
-          <Grid container spacing={6} data-testid="project-open-tasks">
-            <Grid item md={6} xs={12}>
-              <Typography variant="h6">{t('processes.your_tasks')}</Typography>
-              <br />
-              <div>
-                <ProjectItem taskId={taskId} clientView />
-              </div>
-            </Grid>
-            <Grid item md={6} xs={12} data-testid="project-step-information">
-              <Typography variant="h6">{t('processes.process_steps')}</Typography>
-              <br />
-              <ProjectSteps
-                data={project?.subTasks}
-                handleProjectStepClick={handleProjectStepClick}
-                handleStepCompletion={(id, completed) => handleStepCompletion(id, completed, refetch)}
-                clientView
-              />
-            </Grid>
-
-          </Grid>
-
-        </Grid>
-
-        <Grid item md={12} xs={12} style={{ marginBottom: '2px'}}><Divider /></Grid>
-
+  return (
+    <Grid container spacing={2}>
+      <Grid item md={12} xs={12}>
+        <Typography
+          variant="h6"
+          className={classes.projectTitle}
+          onClick={() => handleProjectStepClick('overview')}
+        >
+          <p
+            data-testid="task-title"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: sanitizeText(removeNewLines(project.body))
+            }}
+          />
+        </Typography>
       </Grid>
-    )
-  }
+      <Grid item md={12} xs={12} data-testid="project-container">
+        <Grid container spacing={6} data-testid="project-open-tasks">
+          <Grid item md={6} xs={12}>
+            <Typography variant="h6">{t('processes.your_tasks')}</Typography>
+            <br />
+            <div>
+              <ProjectItem taskId={taskId} clientView />
+            </div>
+          </Grid>
+          <Grid item md={6} xs={12} data-testid="project-step-information">
+            <Typography variant="h6">{t('processes.process_steps')}</Typography>
+            <br />
+            <ProjectSteps
+              data={project?.subTasks}
+              handleProjectStepClick={handleProjectStepClick}
+              handleStepCompletion={(id, completed) => handleStepCompletion(id, completed, refetch)}
+              clientView
+            />
+          </Grid>
+        </Grid>
+      </Grid>
 
-  const Task = {
-    id: PropTypes.string,
-    body: PropTypes.string,
-    completed: PropTypes.bool,
-    author: PropTypes.shape({
-      name: PropTypes.string,
-      id: PropTypes.string
-    }),
-    assignees: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string
-      })
-      ),
-      subTasks: PropTypes.arrayOf(PropTypes.object)
-    };
+      <Grid item md={12} xs={12} style={{ marginBottom: '2px' }}>
+        <Divider />
+      </Grid>
+    </Grid>
+  );
+}
 
-  ClientPilotViewItem.propTypes = {
-    project: PropTypes.shape(Task).isRequired,
-    refetch: PropTypes.func.isRequired,
+const useStyles = makeStyles(() => ({
+  projectTitle: {
+    cursor: 'pointer'
   }
+}));
+
+const Task = {
+  id: PropTypes.string,
+  body: PropTypes.string,
+  completed: PropTypes.bool,
+  author: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.string
+  }),
+  assignees: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string
+    })
+  ),
+  subTasks: PropTypes.arrayOf(PropTypes.object)
+};
+
+ClientPilotViewItem.propTypes = {
+  project: PropTypes.shape(Task).isRequired,
+  refetch: PropTypes.func.isRequired
+};
