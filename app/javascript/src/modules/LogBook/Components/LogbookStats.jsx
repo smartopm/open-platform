16b48323b478c5c16897aadb7a25c7643bc82a05
OlivierJM <manolivier93@gmail.com>
2022-03-09 13:38:15 +0200

    align stat cards

--- app/javascript/src/modules/LogBook/Components/LogbookStats.jsx
+++ app/javascript/src/modules/LogBook/Components/LogbookStats.jsx
@@ -7,12 +7,15 @@ import { useTranslation } from 'react-i18next';
 import CenteredContent from '../../../shared/CenteredContent';
 import { LogbookStatsQuery } from '../graphql/guestbook_queries';
 import { Spinner } from '../../../shared/Loading';
+import CardComponent from '../../../shared/Card';
+import useLogbookStyles from '../styles'
 
 export default function LogbookStats({ tabValue, shouldRefetch }) {
   const { t } = useTranslation('logbook');
   const [loadStats, { data, loading, error }] = useLazyQuery(LogbookStatsQuery, {
     fetchPolicy: 'cache-and-network'
   });
+  const classes = useLogbookStyles()
 
   useEffect(() => {
     if (tabValue === 2) {
@@ -20,26 +23,55 @@ export default function LogbookStats({ tabValue, shouldRefetch }) {
     }
   }, [tabValue, loadStats, shouldRefetch]);
 
+  if(loading) return <Spinner />
   return (
-    <Grid container>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Grid item xs={12} md={3}>
+    <Grid container spacing={1} alignItems="center" alignContent="center">
+      <Grid item xs={4}>
+        <CardComponent className={classes.statCard}>
+          <Typography gutterBottom variant="caption" data-testid="stats_title">
+            {t('logbook.total_in_city')}
+          </Typography>
+          <CenteredContent>
+            <Typography variant="h3" component="div" gutterBottom data-testid="stats_count">
+              {error || !data?.communityPeopleStatistics.peoplePresent
+                ? 0
+                : data?.communityPeopleStatistics.peoplePresent}
+            </Typography>
+          </CenteredContent>
+        </CardComponent>
+      </Grid>
+      <Grid item xs={4}>
+        <CardComponent className={classes.statCard}>
           <CenteredContent>
             <Typography gutterBottom variant="caption" data-testid="stats_title">
               {t('logbook.total_in_city')}
             </Typography>
           </CenteredContent>
           <CenteredContent>
-            <Typography variant="h1" component="div" gutterBottom data-testid="stats_count">
+            <Typography variant="h3" component="div" gutterBottom data-testid="stats_count">
               {error || !data?.communityPeopleStatistics.peoplePresent
                 ? 0
                 : data?.communityPeopleStatistics.peoplePresent}
             </Typography>
           </CenteredContent>
-        </Grid>
-      )}
+        </CardComponent>
+      </Grid>
+      <Grid item xs={4}>
+        <CardComponent className={classes.statCard}>
+          <CenteredContent>
+            <Typography gutterBottom variant="caption" data-testid="stats_title">
+              {t('logbook.total_in_city')}
+            </Typography>
+          </CenteredContent>
+          <CenteredContent>
+            <Typography variant="h3" component="div" gutterBottom data-testid="stats_count">
+              {error || !data?.communityPeopleStatistics.peoplePresent
+                ? 0
+                : data?.communityPeopleStatistics.peoplePresent}
+            </Typography>
+          </CenteredContent>
+        </CardComponent>
+      </Grid>
     </Grid>
   );
 }
