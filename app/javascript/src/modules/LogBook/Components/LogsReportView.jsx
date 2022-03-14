5209560e6ccd45596d2f10c2e6aa48c8fdb59ae4
OlivierJM <manolivier93@gmail.com>
2022-03-14 18:54:14 +0200

    better alignment

--- app/javascript/src/modules/LogBook/Components/LogsReportView.jsx
+++ app/javascript/src/modules/LogBook/Components/LogsReportView.jsx
@@ -2,16 +2,20 @@ import { Grid, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
 import DatePickerDialog from '../../../components/DatePickerDialog';
-
 
 export default function LogsReportView({ startDate, endDate, handleChange, isSmall, children }) {
   const { t } = useTranslation(['logbook', 'common']);
+  const classes = useStyles();
   return (
     <>
-      <br />
-      <Typography>{t('guest_book.gate_flow_report')}</Typography>
-      <br />
+      <div className={classes.flowReportTitle}>
+        <Typography variant="subtitle1">
+          {t('guest_book.gate_flow_report')}
+        </Typography>
+      </div>
+      
       <Grid container spacing={1} alignItems="center">
         <Grid item xs={5} md={3}>
           <DatePickerDialog
@@ -51,5 +55,12 @@ LogsReportView.propTypes = {
   endDate: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
   handleChange: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
-  isSmall: PropTypes.bool.isRequired,
+  isSmall: PropTypes.bool.isRequired
 };
+
+const useStyles = makeStyles(() => ({
+  flowReportTitle: {
+    marginTop: 13,
+    marginBottom: 9
+  }
+}));
