6174a91690f593170340acb849db89c596c1fac7
OlivierJM <manolivier93@gmail.com>
2022-03-23 14:08:34 +0200

    add tests for filter list

--- app/javascript/src/shared/SearchFilterList.jsx
+++ app/javascript/src/shared/SearchFilterList.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { Grid, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export default function SearchFilterList({ filters, handleClearFilters }) {
   const { t } = useTranslation('search');
-  const validFilters = filters.filter(Boolean)
-  if(!validFilters?.length) return null
+  const validFilters = filters.filter(Boolean);
+  if (!validFilters?.length) return null;
   return (
     <Grid container spacing={2} alignItems="center">
       <Grid item>
-        <Typography display="inline-block" variant='subtitle2'>{`${t('search.search_results')}:  `}</Typography> 
-        {"  "}
-        <Typography display="inline-block" variant='caption'>{filters.filter(Boolean).map(filter => `${filter}`).join(', ')}</Typography>
+        <Typography display="inline-block" variant="subtitle2" data-testid="search_results">
+          {`${t(
+          'search.search_results'
+        )}:  `}
+        </Typography>
+        {'  '}
+        <Typography display="inline-block" variant="caption" data-testid="filters_list">
+          {filters
+            .filter(Boolean)
+            .map(filter => `${filter}`)
+            .join(', ')}
+        </Typography>
       </Grid>
       <Grid item>
-        <Button variant="outlined" color="primary" onClick={handleClearFilters}>
+        <Button variant="outlined" color="primary" onClick={handleClearFilters} data-testid="clear_filters_btn">
           {t('search.clear_filters')}
         </Button>
       </Grid>
@@ -24,8 +33,7 @@ export default function SearchFilterList({ filters, handleClearFilters }) {
   );
 }
 
-
 SearchFilterList.propTypes = {
-    filters: PropTypes.arrayOf(PropTypes.string).isRequired,
-    handleClearFilters: PropTypes.func.isRequired,
-}
+  filters: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleClearFilters: PropTypes.func.isRequired
+};
