961f8eb5d4eaecb4c314d4b4f019d92a000f7432
tolulope3 <tolulope@doublegdp.com>
2022-03-24 09:04:40 +0000

    fix jest log

--- app/javascript/src/modules/Tasks/Processes/__tests__/ProjectDocument.test.js
+++ app/javascript/src/modules/Tasks/Processes/__tests__/ProjectDocument.test.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/react-testing';
 import MockedThemeProvider from '../../../__mocks__/mock_theme';
 import ProjectDocument from '../Components/ProjectDocument';
-import authState from "../../../../__mocks__/authstate";
-import { Context } from '../../../../containers/Provider/AuthStateProvider'
-
+import authState from '../../../../__mocks__/authstate';
+import { Context } from '../../../../containers/Provider/AuthStateProvider';
 
 jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
 describe('Project Document Component', () => {
-  const attachments = [{
-    id: 'jwhedwedwedwe',
-    filename: 'sample.pdf',
-    task_id: 'wejdghjfewf',
-    created_at: '2022-05-05',
-    uploaded_by: "sample name",
-    task_name: "sample task name"
-  }]
+  const attachments = [
+    {
+      id: 'jwhedwedwedwe',
+      filename: 'sample.pdf',
+      task_id: 'wejdghjfewf',
+      created_at: '2022-05-05',
+      uploaded_by: 'sample name',
+      task_name: 'sample task name'
+    }
+  ];
   it('should render correctly', async () => {
     render(
       <Context.Provider value={authState}>
@@ -32,22 +33,24 @@ describe('Project Document Component', () => {
       </Context.Provider>
     );
 
-    expect(screen.getByTestId("documents")).toBeInTheDocument();
-    expect(screen.getByText("sample.pdf")).toBeInTheDocument();
-    expect(screen.getByText("sample task name")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByTestId('documents')).toBeInTheDocument();
+      expect(screen.getByText('sample.pdf')).toBeInTheDocument();
+      expect(screen.getByText('sample task name')).toBeInTheDocument();
 
-    fireEvent.click(screen.getByTestId("more_details"))
-    expect(screen.getByText("document.download")).toBeInTheDocument();
-    expect(screen.getByText("document.delete")).toBeInTheDocument();
+      fireEvent.click(screen.getByTestId('more_details'));
+      expect(screen.getByText('document.download')).toBeInTheDocument();
+      expect(screen.getByText('document.delete')).toBeInTheDocument();
 
-    fireEvent.click(screen.queryByTestId('delete_button'));
-    expect(screen.queryByText('document.delete_confirmation_message')).toBeInTheDocument();
-    expect(screen.queryByTestId('proceed_button')).toBeInTheDocument();
+      fireEvent.click(screen.queryByTestId('delete_button'));
+      expect(screen.queryByText('document.delete_confirmation_message')).toBeInTheDocument();
+      expect(screen.queryByTestId('proceed_button')).toBeInTheDocument();
 
-    fireEvent.click(screen.queryByTestId('proceed_button'));
+      fireEvent.click(screen.queryByTestId('proceed_button'));
+    });
   });
 
-  it('should render No Document Information', async () => {
+  it('should render No Document Information', () => {
     render(
       <Context.Provider value={authState}>
         <MockedProvider>
@@ -63,7 +66,7 @@ describe('Project Document Component', () => {
     expect(screen.getByText('document.no_document')).toBeInTheDocument();
   });
 
-  it('should render loader', async () => {
+  it('should render loader', () => {
     render(
       <Context.Provider value={authState}>
         <MockedProvider>
@@ -79,13 +82,18 @@ describe('Project Document Component', () => {
     expect(screen.getByTestId('loader')).toBeInTheDocument();
   });
 
-  it('should render error', async () => {
+  it('should render error', () => {
     render(
       <Context.Provider value={authState}>
         <MockedProvider>
           <BrowserRouter>
             <MockedThemeProvider>
-              <ProjectDocument attachments={[]} loading={false} refetch={jest.fn()} error='sample error message' />
+              <ProjectDocument
+                attachments={[]}
+                loading={false}
+                refetch={jest.fn()}
+                error="sample error message"
+              />
             </MockedThemeProvider>
           </BrowserRouter>
         </MockedProvider>
