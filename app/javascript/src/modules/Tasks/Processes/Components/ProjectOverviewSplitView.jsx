5abadec57b8c6b9b6b14a3cecb1e9fa28c177801
Bonny Mwenda <bonny@doublegdp.com>
2022-03-31 15:15:18 +0000

    Add translations

--- app/javascript/src/modules/Tasks/Processes/Components/ProjectOverviewSplitView.jsx
+++ app/javascript/src/modules/Tasks/Processes/Components/ProjectOverviewSplitView.jsx
@@ -2,22 +2,24 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Typography } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { TaskContext } from '../../Context';
 import ProjectSteps from './Steps';
 
 export default function ProjectOverviewSplitView({ data, refetch, handleProjectStepClick }) {
-  const { setSelectedStep, handleStepCompletion } = useContext(TaskContext);
+  const { t } = useTranslation('task');
   const matches = useMediaQuery('(max-width:600px)');
+  const { setSelectedStep, handleStepCompletion } = useContext(TaskContext);
 
   return (
     <>
       <Grid container style={matches ? { marginLeft: '-20px' } : { marginLeft: 0 }}>
         <Grid item md={12} data-testid="requirements-section">
-          <Typography variant="subtitle1" style={{fontWeight: 400}}>Requirements</Typography>
-          <Typography variant="caption">Please read the required guideline. </Typography>
+          <Typography variant="subtitle1" style={{fontWeight: 400}}>{t('processes.requirements')}</Typography>
+          <Typography variant="caption">{t('processes.requirements_guideline')}</Typography>
           <Link to="/news/post/8">
-            <Typography variant="caption">Go to Guideline.</Typography>
+            <Typography variant="caption">{t('processes.go_to_guideline')}</Typography>
           </Link>
         </Grid>
         <Grid item md={12} xs={12}>
