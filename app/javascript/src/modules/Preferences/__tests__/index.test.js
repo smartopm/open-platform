c5288afda675067c9958b9ccf5e76fcefc530c57
OlivierJM <manolivier93@gmail.com>
2022-04-03 13:33:27 +0200

    Fix failing tests

--- app/javascript/src/modules/Preferences/__tests__/index.test.js
+++ app/javascript/src/modules/Preferences/__tests__/index.test.js
@@ -4,7 +4,7 @@ import t from "../../__mocks__/t";
 describe('Preferences', () => {
   it('exports necessary info', () => {
     expect(Preferences.routeProps.path).toBe('/settings')
-    expect(Preferences.name(t)).toBe('misc.preferences')
+    expect(Preferences.name(t)).toBe('menu.preferences')
     expect(Preferences.featureName).toBe('Preferences')
   });
 });
