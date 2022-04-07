ccf97c89a82309e71ec22bf1ddcc4067fcc80b7d
Bonny Mwenda <bonny@doublegdp.com>
2022-04-07 09:54:04 +0000

    Fix optional process association

--- spec/models/notes/note_list_spec.rb
+++ spec/models/notes/note_list_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Notes::NoteList, type: :model do
 
   describe 'associations' do
     it { is_expected.to belong_to(:community) }
-    it { is_expected.to belong_to(:process).class_name('Processes::Process') }
+    it { is_expected.to belong_to(:process).class_name('Processes::Process').optional(true) }
     it { is_expected.to have_many(:notes).class_name('Notes::Note').dependent(:destroy) }
   end
 end
