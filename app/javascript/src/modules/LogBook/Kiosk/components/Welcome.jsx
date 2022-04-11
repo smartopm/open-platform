2edc0c947c34015f94a912eccbf8c182a061968f
OlivierJM <manolivier93@gmail.com>
2022-04-11 11:24:55 +0200

    align the kiosk welcome screen

--- app/javascript/src/modules/LogBook/Kiosk/components/Welcome.jsx
+++ app/javascript/src/modules/LogBook/Kiosk/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Box, Container } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 import VerticallyCentered from '../../../../shared/VerticallyCentered';
 import CommunityName from '../../../../shared/CommunityName';
@@ -12,45 +12,50 @@ export default function Welcome() {
 
   return (
     <VerticallyCentered isVerticallyCentered={false}>
-      <CenteredContent>
-        <CommunityName authState={authState} />
-      </CenteredContent>
-      <CenteredContent>
-        <Typography variant="h2" textAlign="center">
-          Welcome
-        </Typography>
-      </CenteredContent>
-      <br />
-      <CenteredContent>
-        <Typography variant="h4" textAlign="center">
-          Please press the button below to scan youl OR code
-        </Typography>
-      </CenteredContent>
-      <br />
-      <br />
-      <br />
+      <Container maxWidth="sm">
+        <Box component="div" sx={{ marginTop: '200px', marginLeft: '30px' }}>
+          <CommunityName authState={authState} />
+        </Box>
+        <br />
+        <br />
+        <CenteredContent>
+          <Typography variant="h2" textAlign="center">
+            Welcome
+          </Typography>
+        </CenteredContent>
+        <br />
+     
+        <CenteredContent>
+          <Typography variant="h4" textAlign="center">
+            Please press the button below to scan your QR code
+          </Typography>
+        </CenteredContent>
+        <br />
+        <br />
+        <br />
 
-      <CenteredContent>
-        <Button
-          variant="contained"
-          color="primary"
-          disableElevation
-          style={{ width: '70%' }}
-          size="large"
-          onClick={() => history.push('/logbook/kiosk/scan')}
-        >
-          Start Scan
-        </Button>
-      </CenteredContent>
-      <br />
-      <br />
-      <br />
-
-      <CenteredContent>
-        <Typography variant='body' textAlign="center">
-          If you do not have a QR code, please speak to the guard on duty
-        </Typography>
-      </CenteredContent>
+        <CenteredContent>
+          <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            style={{ width: '70%' }}
+            size="large"
+            onClick={() => history.push('/logbook/kiosk/scan')}
+          >
+            Start Scan
+          </Button>
+        </CenteredContent>
+        <br />
+        <br />
+        <br />
+      
+        <CenteredContent>
+          <Typography variant='subtitle1' textAlign="center">
+            If you do not have a QR code, please speak to the guard on duty
+          </Typography>
+        </CenteredContent>
+      </Container>
     </VerticallyCentered>
   );
 }
