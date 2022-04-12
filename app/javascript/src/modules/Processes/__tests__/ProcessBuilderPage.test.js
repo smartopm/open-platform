547c70751cfee3be0e1ce4da7a51837158ab668f
victor okeugo <victor@doublegdp.com>
2022-04-12 10:41:22 +0000

    add tests

--- app/javascript/src/modules/Processes/__tests__/ProcessBuilderPage.test.js
+++ app/javascript/src/modules/Processes/__tests__/ProcessBuilderPage.test.js
@@ -7,46 +7,18 @@ import { render, screen, waitFor } from '@testing-library/react';
 import MockedThemeProvider from '../../__mocks__/mock_theme';
 import { Context } from '../../../containers/Provider/AuthStateProvider'
 import authState from '../../../__mocks__/authstate'
-import { ProcessFormsQuery, ProcessTaskListsQuery } from '../graphql/process_list_queries';
-import ProcessCreate from '../Components/ProcessCreate';
+import ProcessBuilderPage from '../Components/ProcessBuilderPage';
 
 jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
 describe('Create Process Form', () => {
-  const mocks = [
-    {
-      request: {
-        query: ProcessFormsQuery,
-      },
-      result: {
-        data: {
-          forms: [{ id: '123', name: 'Form 1' }]
-        },
-        loading: false,
-      }
-    },
-
-    {
-      request: {
-        query: ProcessTaskListsQuery,
-      },
-      result: {
-        data: {
-          processTaskLists: [{ id: '456', name: 'TaskList 1'}]
-        },
-        loading: false,
-      }
-    }
-  ];
-
-
   it('renders form to create a process with necessary elements', async () => {
     const adminUser = { userType: 'admin', ...authState }
     render(
-      <MockedProvider mocks={mocks} addTypename={false}>
+      <MockedProvider mocks={[]} addTypename={false}>
         <Context.Provider value={adminUser}>
           <BrowserRouter>
             <MockedThemeProvider>
-              <ProcessCreate />
+              <ProcessBuilderPage />
             </MockedThemeProvider>
           </BrowserRouter>
         </Context.Provider>
@@ -54,7 +26,6 @@ describe('Create Process Form', () => {
     );
 
     await waitFor(() => {
-      // TODO: SOS- cannot find other elements
       expect(screen.queryByTestId('loader')).toBeInTheDocument();
     });
   });
