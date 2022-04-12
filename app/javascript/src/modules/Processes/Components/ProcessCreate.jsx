6d26f0afc3333567b01f9a1232bd87e42267b395
victor okeugo <victor@doublegdp.com>
2022-04-12 08:16:58 +0000

    make note list nullable

--- app/javascript/src/modules/Processes/Components/ProcessCreate.jsx
+++ app/javascript/src/modules/Processes/Components/ProcessCreate.jsx
@@ -21,12 +21,14 @@ import { Spinner } from '../../../shared/Loading';
 import CenteredContent from '../../../shared/CenteredContent';
 import { ProcessCreateMutation } from '../graphql/process_list_mutation';
 import { formatError } from '../../../utils/helpers';
+import MessageAlert from '../../../components/MessageAlert';
 
 export default function ProcessCreate() {
   const classes = useStyles();
   const matches = useMediaQuery('(max-width:900px)');
   const history = useHistory();
   const { t } = useTranslation(['process', 'common']);
+  const [alertOpen, setAlertOpen] = useState(false);
   const [processData, setProcessData] = useState({ processName: '', formId: '', noteListId: '' })
   const [info, setInfo] = useState({ loading: false, error: false, message: '' })
 
@@ -47,6 +49,11 @@ const [processCreate] = useMutation(ProcessCreateMutation)
       ...processData,
       [property]: e.target.value,
     });
+  }
+
+  function handleClose() {
+    setMessage({ isError: false, detail: '' });
+    setAlertOpen(false);
   }
 
   function handleCreateProcess(e){
@@ -74,133 +81,141 @@ const [processCreate] = useMutation(ProcessCreateMutation)
           loading: false,
         });
 
-      history.push('processes/templates/create')
+      history.push('/processes/templates/create')
     })
-    .catch(err => setInfo({ 
-      ...info,
-      error: true,
-      message: formatError(err.message),
-    }))
+    .catch(err =>{
+      setInfo({ 
+        ...info,
+        error: true,
+        message: formatError(err.message),
+      });
+      setAlertOpen(true)
+    })
   }
 
   if (formLoading || taskListLoading) return <Spinner />;
 
   return(
-    <div className="container">
-      <Grid container spacing={1}>
-        <Grid item md={12} xs={12} style={{ paddingLeft: '10px' }}>
-          <div role="presentation">
-            <Breadcrumbs aria-label="breadcrumb" style={{ paddingBottom: '10px' }}>
-              <Link to="/processes">
-                <Typography color="primary" style={{ marginLeft: '5px' }}>
-                  {t('breadcrumbs.processes')}
-                </Typography>
-              </Link>
-              <Typography color="text.primary">{t('breadcrumbs.create_process')}</Typography>
-            </Breadcrumbs>
-          </div>
-        </Grid>
-        <Grid container>
-          <Grid item md={11} xs={10} className={classes.header}>
-            <Grid container>
-              <Grid item md={9} xs={10}>
-                <Typography variant="h4" style={{ marginLeft: '5px', marginBottom: '24px' }}>
-                  {t('templates.create_process')}
-                </Typography>
+    <>
+      <MessageAlert
+        type={info.error ? 'error' : 'success'}
+        message={info.message}
+        open={alertOpen}
+        handleClose={handleClose}
+      />
+      <div className="container">
+        <Grid container spacing={1}>
+          <Grid item md={12} xs={12} style={{ paddingLeft: '10px' }}>
+            <div role="presentation">
+              <Breadcrumbs aria-label="breadcrumb" style={{ paddingBottom: '10px' }}>
+                <Link to="/processes">
+                  <Typography color="primary" style={{ marginLeft: '5px' }}>
+                    {t('breadcrumbs.processes')}
+                  </Typography>
+                </Link>
+                <Typography color="text.primary">{t('breadcrumbs.create_process')}</Typography>
+              </Breadcrumbs>
+            </div>
+          </Grid>
+          <Grid container>
+            <Grid item md={11} xs={10} className={classes.header}>
+              <Grid container>
+                <Grid item md={9} xs={10}>
+                  <Typography variant="h4" style={{ marginLeft: '5px', marginBottom: '24px' }}>
+                    {t('templates.create_process')}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-   
-      <Grid container spacing={3} style={matches ? { padding: '10px 10px' } : { padding: '20px 150px' }}>
-        <Grid item md={12} xs={12}>
-          <TextField
-            name="processName"
-            id="process-name"
-            label={t('templates.process_name_label')}
-            helperText={t('templates.process_name_helper_text')}
-            variant="outlined"
-            className='process-txt-input'
-            onChange={handleInputChange}
-            value={processData.name}
-            inputProps={{
-            'data-testid': 'new-process-name'
-          }}
-            fullWidth
-            required
-          />
-        </Grid>
-        <Grid item md={12} xs={12}>
-          <FormControl fullWidth>
-            <InputLabel id="noteListId">{t('templates.process_form_label')}</InputLabel>
-            <Select
-              name="formId"
-              id="formId"
-              label={t('templates.process_form_label')}
-              value={processData.formId}
+        <Grid container spacing={3} style={matches ? { padding: '10px 10px' } : { padding: '20px 150px' }}>
+          <Grid item md={12} xs={12}>
+            <TextField
+              name="processName"
+              id="process-name"
+              label={t('templates.process_name_label')}
+              helperText={t('templates.process_name_helper_text')}
+              variant="outlined"
+              className='process-txt-input'
               onChange={handleInputChange}
-              data-testid="process-form-dropdown"
+              value={processData.name}
+              inputProps={{
+              'data-testid': 'new-process-name'
+            }}
+              fullWidth
               required
-            >
-              {formData?.forms.map((form) => (
-                <MenuItem key={form.id} value={form.id}>
-                  {form.name}
-                </MenuItem>
-              ))}
-            </Select>
-            <FormHelperText data-testid="process-form-helper-text">
-              {t('templates.process_form_helper_text')}
-              <Link to="/forms">
-                {t('templates.process_form_helper_text_link_text')}
-              </Link>
-            </FormHelperText>
-          </FormControl>
+            />
+          </Grid>
+          <Grid item md={12} xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="noteListId">{t('templates.process_form_label')}</InputLabel>
+              <Select
+                name="formId"
+                id="formId"
+                label={t('templates.process_form_label')}
+                value={processData.formId}
+                onChange={handleInputChange}
+                data-testid="process-form-dropdown"
+                required
+              >
+                {formData?.forms.map((form) => (
+                  <MenuItem key={form.id} value={form.id}>
+                    {form.name}
+                  </MenuItem>
+                ))}
+              </Select>
+              <FormHelperText data-testid="process-form-helper-text">
+                {t('templates.process_form_helper_text')}
+                <Link to="/forms">
+                  {t('templates.process_form_helper_text_link_text')}
+                </Link>
+              </FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item md={12} xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="noteListId">{t('templates.process_task_list_label')}</InputLabel>
+              <Select
+                name="noteListId"
+                id="noteListId"
+                label={t('templates.process_task_list_label')}
+                value={processData.noteListId}
+                onChange={handleInputChange}
+                data-testid="process-note-list-dropdown"
+                required
+              >
+                {taskListData?.processTaskLists?.map((taskList) => (
+                  <MenuItem key={taskList.id} value={taskList.id}>
+                    {taskList.name}
+                  </MenuItem>
+                ))}
+              </Select>
+              <FormHelperText data-testid="process-note-list-helper-text">
+                {t('templates.process_task_list_helper_text')}
+                <Link to="/tasks/task_lists">
+                  {t('templates.process_task_list_helper_text_link_text')}
+                </Link>
+              </FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item md={12} xs={12}>
+            <CenteredContent>
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={(!processData.formId || !processData.noteListId) || info.loading}
+                aria-label="process-submit"
+                data-testid="process-submit-btn"
+                onClick={handleCreateProcess}
+              >
+                {info.loading ? t('templates.saving_process') : t('templates.save_process')}
+              </Button>
+            </CenteredContent>
+          </Grid>
         </Grid>
-
-        <Grid item md={12} xs={12}>
-          <FormControl fullWidth>
-            <InputLabel id="noteListId">{t('templates.process_task_list_label')}</InputLabel>
-            <Select
-              name="noteListId"
-              id="noteListId"
-              label={t('templates.process_task_list_label')}
-              value={processData.noteListId}
-              onChange={handleInputChange}
-              data-testid="process-note-list-dropdown"
-              required
-            >
-              {taskListData?.processTaskLists?.map((taskList) => (
-                <MenuItem key={taskList.id} value={taskList.id}>
-                  {taskList.name}
-                </MenuItem>
-              ))}
-            </Select>
-            <FormHelperText data-testid="process-note-list-helper-text">
-              {t('templates.process_task_list_helper_text')}
-              <Link to="/tasks/task_lists">
-                {t('templates.process_task_list_helper_text_link_text')}
-              </Link>
-            </FormHelperText>
-          </FormControl>
-        </Grid>
-
-        <Grid item md={12} xs={12}>
-          <CenteredContent>
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={(!processData.formId || !processData.noteListId) || info.loading}
-              aria-label="process-submit"
-              data-testid="process-submit-btn"
-              onClick={handleCreateProcess}
-            >
-              {info.loading ? t('templates.saving_process') : t('templates.save_process')}
-            </Button>
-          </CenteredContent>
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   )
 }
 
