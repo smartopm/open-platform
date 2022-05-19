7845169b566d836fddc3660c4e16bd47f8f26e7e
Saurabh Shinde <saurabh@doublegdp.com>
2022-05-19 17:47:41 +0530

    Refactor permission check for labels queries and update rspecs for lead labels query

--- app/graphql/types/queries/label.rb
+++ app/graphql/types/queries/label.rb
@@ -53,12 +53,9 @@ module Types::Queries::Label
   end
 
   def lead_labels(user_id:)
-    unless permitted?(module: :label, permission: :can_view_lead_labels)
-      raise GraphQL::ExecutionError, I18n.t('errors.unauthorized')
-    end
-  
-    user = context[:site_community].users.find(user_id)
+    validate_authorization(:label, :can_view_lead_labels)
 
+    user = context[:site_community].users.find(user_id)
     context[:site_community].labels
                             .joins(:user_labels)
                             .where(user_labels: { user_id: user.id },
