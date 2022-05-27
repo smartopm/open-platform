08993627dd1c0a4ba9bdb854a9dc660fdad43939
OlivierJM <manolivier93@gmail.com>
2022-05-27 10:57:14 +0200

    Fix routes for make payment

--- app/javascript/src/modules/Payments/index.js
+++ app/javascript/src/modules/Payments/index.js
@@ -3,6 +3,7 @@ import CreditCardIcon from '@mui/icons-material/CreditCard';
 import PaymentSummary from './PaymentSummary'
 import Payments from "./Components/Payments";
 import AccessCheck from '../Permissions/Components/AccessCheck';
+import  RenderPayment from './RavePayments';
 
 const paymentsPermissions = ['can_access_all_payments'];
 const currentModule = 'plan_payment';
@@ -15,6 +16,36 @@ function RenderPaymentsList() {
   )
 }
 
+
+const RavePayment = [
+  {
+    routeProps: {
+      path: '/payments',
+      component: RenderPaymentsList,
+      exact: true,
+    },
+    styleProps: {
+      icon: <CreditCardIcon />,
+    },
+    name: t => t('menu.payment', { count: 1 }),
+    featureName: 'Payments',
+    accessibleBy: [],
+    moduleName: currentModule,
+  },
+  {
+    routeProps: {
+      path: '/payments/rave',
+      component: RenderPayment,
+      exact: true,
+    },
+    name: t => t('common:misc.make_a_payment'),
+    featureName: 'Payments',
+    moduleName: currentModule,
+    accessibleBy: [],
+  },
+]
+
+
 export default {
   routeProps: {
     path: '/payments',
@@ -26,7 +57,9 @@ export default {
   accessibleBy: [],
   moduleName: currentModule,
   name: t => t('menu.payment', { count: 0 }),
-  featureName: 'Payments'
+  featureName: 'Payments',
+  subRoutes: RavePayment,
+  subMenu: RavePayment,
 };
 
 export { PaymentSummary }
