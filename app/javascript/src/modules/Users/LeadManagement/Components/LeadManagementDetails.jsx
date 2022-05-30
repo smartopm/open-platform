3e6fb73286d09a789cce8d5e2a75c7097cd8cad5
Daniel Mutuba <daniel@doublegdp.com>
2022-05-30 08:37:30 +0000

    Add values and labels to Lead Status selector on Details tab

--- app/javascript/src/modules/Users/LeadManagement/Components/LeadManagementDetails.jsx
+++ app/javascript/src/modules/Users/LeadManagement/Components/LeadManagementDetails.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Grid, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-apollo';
 import Box from '@mui/material/Box';
 import PropTypes from 'prop-types';
-import { LeadDetailsQuery } from '../../../../graphql/queries';
+import { LeadDetailsQuery, LeadLabelsQuery } from '../../../../graphql/queries';
 import { Spinner } from '../../../../shared/Loading';
 import CenteredContent from '../../../../shared/CenteredContent';
 import LeadManagementForm from './LeadManagementForm';
@@ -22,6 +22,17 @@ export default function LeadManagementDetails({ userId }) {
     fetchPolicy: 'cache-and-network'
   });
 
+  const {
+    loading: leadLabelsLoading,
+    error: leadLabelsError,
+    data: LeadLabelsData
+    // refetch: refetchLeadLabels
+  } = useQuery(LeadLabelsQuery, {
+    variables: { userId },
+    fetchPolicy: 'cache-and-network'
+  });
+
+  console.log('Mutuba', LeadLabelsData);
   const TAB_VALUES = {
     details: 0,
     tasks: 1,
@@ -31,12 +42,13 @@ export default function LeadManagementDetails({ userId }) {
     setTabValue(Number(newValue));
   }
 
-  const Item = styled(Box)(({ theme }) => ({
-    padding: theme.spacing(1)
-  }));
+  // const Item = styled(Box)(({ theme }) => ({
+  //   padding: theme.spacing(1)
+  // }));
 
-  if (loading) return <Spinner />;
-  if (error) return <CenteredContent>{formatError(error.message)}</CenteredContent>;
+  if (loading || leadLabelsLoading) return <Spinner />;
+  const err = error || leadLabelsError;
+  if (err) return <CenteredContent>{formatError(err.message)}</CenteredContent>;
 
   return (
     <Grid
@@ -45,58 +57,87 @@ export default function LeadManagementDetails({ userId }) {
       columns={{ xs: 12, md: 12 }}
     >
       <Grid item md={6} xs={10} data-testid="lead-management-container-header">
-        <Item>
-          <Typography variant="h5">{t('lead_management.main_header')}</Typography>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <StyledTabs
-              value={tabValue}
-              onChange={handleTabValueChange}
-              aria-label="lead-management-tabs"
-              data-testid="lead-management-tabs"
-              variant="standard"
+        {/* <Item> */}
+        <Grid
+          container
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between'
+            // gridTemplateColumns: 'repeat(2, 1fr)'
+          }}
+        >
+          <Grid item md={6} xs={12}>
+            <Typography variant="h5">{t('lead_management.main_header')}</Typography>
+          </Grid>
+          <Grid item md={6} xs={6} style={{ justifyContent: 'flex-end' }}>
+            <Grid
+              container
+              style={{
+                display: 'flex',
+                // alignItems: 'center',
+                justifyContent: 'space-between'
+                // gridTemplateColumns: 'repeat(2, 1fr)'
+              }}
             >
-              <StyledTab
-                label={t('lead_management.detail_header')}
-                style={
-                  tabValue === objectAccessor(TAB_VALUES, 'details')
-                    ? { fontSize: '10px', textAlign: 'left', borderBottom: 'solid 1px' }
-                    : { fontSize: '10px', textAlign: 'left' }
-                }
-                {...a11yProps(0)}
-              />
-              <StyledTab
-                label={t('lead_management.task_header')}
-                style={
-                  tabValue === objectAccessor(TAB_VALUES, 'details')
-                    ? { fontSize: '10px', textAlign: 'left', borderBottom: 'solid 1px' }
-                    : { fontSize: '10px', textAlign: 'left' }
-                }
-                {...a11yProps(1)}
-              />
+              <Grid item md={6} xs={12}>
+                <Typography variant="h5">{t('lead_management.main_header')}</Typography>
+              </Grid>
+              <Grid item md={6} xs={12} style={{ padding: 0, justifyContent: 'flex-end' }}>
+                <Typography variant="h5">{t('lead_management.main_header')}</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <StyledTabs
+            value={tabValue}
+            onChange={handleTabValueChange}
+            aria-label="lead-management-tabs"
+            data-testid="lead-management-tabs"
+            variant="standard"
+          >
+            <StyledTab
+              label={t('lead_management.detail_header')}
+              style={
+                tabValue === objectAccessor(TAB_VALUES, 'details')
+                  ? { fontSize: '10px', textAlign: 'left', borderBottom: 'solid 1px' }
+                  : { fontSize: '10px', textAlign: 'left' }
+              }
+              {...a11yProps(0)}
+            />
+            <StyledTab
+              label={t('lead_management.task_header')}
+              style={
+                tabValue === objectAccessor(TAB_VALUES, 'details')
+                  ? { fontSize: '10px', textAlign: 'left', borderBottom: 'solid 1px' }
+                  : { fontSize: '10px', textAlign: 'left' }
+              }
+              {...a11yProps(1)}
+            />
 
-              <StyledTab
-                label={t('lead_management.event_header')}
-                style={
-                  tabValue === objectAccessor(TAB_VALUES, 'events')
-                    ? { fontSize: '10px', borderBottom: 'solid 1px' }
-                    : { fontSize: '10px' }
-                }
-                {...a11yProps(1)}
-              />
-            </StyledTabs>
-          </Box>
+            <StyledTab
+              label={t('lead_management.event_header')}
+              style={
+                tabValue === objectAccessor(TAB_VALUES, 'events')
+                  ? { fontSize: '10px', borderBottom: 'solid 1px' }
+                  : { fontSize: '10px' }
+              }
+              {...a11yProps(1)}
+            />
+          </StyledTabs>
+        </Box>
 
-          <TabPanel value={tabValue} index={0} data-testid="lead-management-details-tab">
-            {data && <LeadManagementForm data={data} refetch={refetch} />}
-          </TabPanel>
-          <TabPanel value={tabValue} index={1} data-testid="lead-management-task-tab">
-            <LeadManagementTask taskId={data?.user?.taskId} tabValue={tabValue} />
-          </TabPanel>
+        <TabPanel value={tabValue} index={0} data-testid="lead-management-details-tab">
+          {data && <LeadManagementForm data={data} refetch={refetch} />}
+        </TabPanel>
+        <TabPanel value={tabValue} index={1} data-testid="lead-management-task-tab">
+          <LeadManagementTask taskId={data?.user?.taskId} tabValue={tabValue} />
+        </TabPanel>
 
-          <TabPanel value={tabValue} index={2} data-testid="lead-management-event-tab">
-            <LeadEvents userId={userId} data={data} refetch={refetch} />
-          </TabPanel>
-        </Item>
+        <TabPanel value={tabValue} index={2} data-testid="lead-management-event-tab">
+          <LeadEvents userId={userId} data={data} refetch={refetch} />
+        </TabPanel>
+        {/* </Item> */}
       </Grid>
     </Grid>
   );
