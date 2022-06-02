d8e4b06b5a2e6292d35215e9fe8932343dd05b11
OlivierJM <manolivier93@gmail.com>
2022-06-02 08:35:27 +0200

    Fix payment

--- app/javascript/src/modules/Payments/RavePayments/Components/PaymentForm.jsx
+++ app/javascript/src/modules/Payments/RavePayments/Components/PaymentForm.jsx
@@ -1,24 +1,27 @@
-import { Button, Container, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { closePaymentModal, useFlutterwave } from 'flutterwave-react-v3';
 import React, { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Context } from '../../../../containers/Provider/AuthStateProvider';
+import CenteredContent from '../../../../shared/CenteredContent';
+import PageWrapper from '../../../../shared/PageWrapper';
+import { currencies } from '../../../../utils/constants';
+import { objectAccessor } from '../../../../utils/helpers';
 
 export default function PaymentForm() {
   const { t } = useTranslation();
   const authState  = useContext(Context)
   const [inputValue, setInputValue] = useState({
-    name: '',
     invoiceNumber: '',
     amount: '',
     description: ''
   });
 
   const config = {
-    public_key: '',
+    public_key: 'FLWPUBK_TEST-b0c6086aa3f333d8cbd3e92cfbbb2380-X',
     tx_ref: Date.now(),
     amount: inputValue.amount,
-    currency: authState.user.community.currency,
+    currency: objectAccessor(currencies, authState.user.community.currency),
     payment_options: 'card,mobilemoney,ussd',
     customer: {
       email: authState.user.email,
@@ -28,6 +31,7 @@ export default function PaymentForm() {
     customizations: {
       title: 'Pay For this item',
       description: inputValue.description,
+      // TODO: replace with community logo
       logo:
         'https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg'
     }
@@ -35,19 +39,14 @@ export default function PaymentForm() {
 
   const handleFlutterPayment = useFlutterwave(config);
 
+  function verifyTransaction(response) {
+    // use this response to verify before saving the transaction in our db
+    closePaymentModal(response);
+  }
+
   return (
-    <Container maxWidth="xl">
+    <PageWrapper>
       <TextField
-        autoFocus
-        margin="normal"
-        id="user-name"
-        label={t('common:form_fields.full_name')}
-        value={inputValue.name}
-        onChange={event => setInputValue({ ...inputValue, name: event.target.value })}
-        fullWidth
-      />
-      <TextField
-        autoFocus
         margin="normal"
         id="invoice-number"
         label={t('common:form_fields.invoice_number')}
@@ -56,7 +55,6 @@ export default function PaymentForm() {
         fullWidth
       />
       <TextField
-        autoFocus
         margin="normal"
         id="payment-amount"
         label={t('common:table_headers.amount')}
@@ -65,7 +63,6 @@ export default function PaymentForm() {
         fullWidth
       />
       <TextField
-        autoFocus
         margin="normal"
         id="description"
         label={t('common:form_fields.description')}
@@ -74,22 +71,25 @@ export default function PaymentForm() {
         fullWidth
       />
       <br />
-      <Button
-        variant="contained"
-        color="primary"
-        disableElevation
-        data-testid="make_a_payment_btn"
-        onClick={() => {
+      <br />
+      <CenteredContent>
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          data-testid="make_a_payment_btn"
+          onClick={() => {
           handleFlutterPayment({
-            callback: () => {
-              closePaymentModal();
+            callback: (response) => {
+              verifyTransaction(response)
             },
             onClose: () => {}
           });
         }}
-      >
-        {t('common:misc.make_a_payment')}
-      </Button>
-    </Container>
+        >
+          {t('common:misc.make_a_payment')}
+        </Button>
+      </CenteredContent>
+    </PageWrapper>
   );
 }
