0542e3a919248ce0556106fe5b581a91735aaa7d
victor okeugo <victor@doublegdp.com>
2022-06-06 11:12:34 +0000

    add tests

--- app/javascript/src/__tests__/landparcel/CreatePointOfInterest.test.js
+++ app/javascript/src/__tests__/landparcel/CreatePointOfInterest.test.js
@@ -1,36 +1,45 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/react-testing'
 import { BrowserRouter } from 'react-router-dom/'
 
 import CreatePointOfInterest from '../../components/LandParcels/CreatePointOfInterest'
 
 describe('Create Point of Interest Component', () => {
-  it('should render add new point of interest form', () => {
+  it('should render add new point of interest form', async () => {
     const refetch = jest.fn()
-    const container = render(
+    render(
       <MockedProvider>
         <BrowserRouter>
           <CreatePointOfInterest refetch={refetch} />
         </BrowserRouter>
       </MockedProvider>)
 
-      const newPoiButton = container.queryByTestId('new-poi-button')
-      fireEvent.click(newPoiButton)
+      await waitFor(() => {
+        expect(screen.queryByTestId('new-poi-button')).toBeInTheDocument()
+        expect(screen.getByText('buttons.new_point_of_interest')).toBeInTheDocument()
+        
+        const newPoiButton = screen.queryByTestId('new-poi-button')
+        fireEvent.click(newPoiButton)
 
-      const poiName = container.getByTestId('poi-name')
-      fireEvent.change(poiName, { target: { value: 'Hotel' } })
-      expect(poiName.value).toBe('Hotel')
-  
-      const GeoLongitudeX = container.queryByTestId('long_x')
-      fireEvent.change(GeoLongitudeX, { target: { value: '28.535' } })
-      expect(GeoLongitudeX.value).toBe('28.535')
-  
-      const GeoLatitudeY = container.queryByTestId('lat_y')
-      fireEvent.change(GeoLatitudeY, { target: { value: '-15.255' } })
-      expect(GeoLatitudeY.value).toBe('-15.255')
+        const poiName = screen.getByTestId('poi-name')
+        fireEvent.change(poiName, { target: { value: 'Hotel' } })
+        expect(poiName.value).toBe('Hotel')
+        
+        const poiDescription = screen.getByTestId('poi-description')
+        fireEvent.change(poiDescription, { target: { value: 'Hotel Description' } })
+        expect(poiDescription.value).toBe('Hotel Description')
 
-      expect(newPoiButton).toBeInTheDocument()
+        const GeoLongitudeX = screen.queryByTestId('long_x')
+        fireEvent.change(GeoLongitudeX, { target: { value: '28.535' } })
+        expect(GeoLongitudeX.value).toBe('28.535')
+    
+        const GeoLatitudeY = screen.queryByTestId('lat_y')
+        fireEvent.change(GeoLatitudeY, { target: { value: '-15.255' } })
+        expect(GeoLatitudeY.value).toBe('-15.255')
+  
+        expect(newPoiButton).toBeInTheDocument()
+      }, 10)
   })
 })
