398eae58a414778239e3d6fcc321ae612e53af04
victor okeugo <victor@doublegdp.com>
2022-06-06 12:21:54 +0000

    add tests

--- app/javascript/src/shared/__tests__/MediaStepper.test.js
+++ app/javascript/src/shared/__tests__/MediaStepper.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import { MockedProvider } from '@apollo/react-testing';
 import MockedThemeProvider from '../../modules/__mocks__/mock_theme';
-import CustomMediaStepper from '../MediaStepper';
+import CustomMediaStepper, { Pagination, PaginationDot } from '../MediaStepper';
 
 describe('CustomMediaStepper component', () => {
   it('should render without error', async () => {
@@ -33,6 +33,77 @@ describe('CustomMediaStepper component', () => {
       expect(screen.queryByTestId('KeyboardArrowRightIcon')).toBeInTheDocument()
 
       expect(screen.queryByTestId('pagination-dot-container')).toBeInTheDocument()
+      expect(screen.queryByTestId('pagination_dot_btn')).toBeInTheDocument()
+    }, 10)
+  });
+});
+
+describe('Pagination component', () => {
+  it('should render 3 media pagination correctly', async () => {
+    const props = {
+      dots:3,
+      index: 1,
+      onChangeIndex: jest.fn(),
+    };
+    render(
+      <BrowserRouter>
+        <MockedThemeProvider>
+          <MockedProvider>
+            <Pagination {...props} />
+          </MockedProvider>
+        </MockedThemeProvider>
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('pagination-dot-container')).toBeInTheDocument()
+      expect(screen.queryAllByTestId('pagination_dot_btn')).toHaveLength(3)
+    }, 10)
+  });
+
+  it('should render 1 dot pagination correctly', async () => {
+    const props = {
+      dots:1,
+      index: 0,
+      onChangeIndex: jest.fn(),
+    };
+    render(
+      <BrowserRouter>
+        <MockedThemeProvider>
+          <MockedProvider>
+            <Pagination {...props} />
+          </MockedProvider>
+        </MockedThemeProvider>
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      screen.debug()
+      expect(screen.queryByTestId('pagination-dot-container')).toBeInTheDocument()
+      expect(screen.queryAllByTestId('pagination_dot_btn')).toHaveLength(1)
+    }, 10)
+  });
+});
+
+describe('PaginationDot component', () => {
+  it('should render correctly', async () => {
+    const props = {
+      active:false,
+      activeIndex: 1,
+      onClick: jest.fn(),
+    };
+    render(
+      <BrowserRouter>
+        <MockedThemeProvider>
+          <MockedProvider>
+            <PaginationDot {...props} />
+          </MockedProvider>
+        </MockedThemeProvider>
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      screen.debug(undefined, 2000)
       expect(screen.queryByTestId('pagination_dot_btn')).toBeInTheDocument()
     }, 10)
   });
