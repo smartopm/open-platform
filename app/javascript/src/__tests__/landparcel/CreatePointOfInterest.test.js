71074c8f017d3c2e9a5c7754e4d0a9d6dac9836d
victor okeugo <victor@doublegdp.com>
2022-06-07 11:29:39 +0000

    prefer ReactTestUtils for fireEvent

--- app/javascript/src/__tests__/landparcel/CreatePointOfInterest.test.js
+++ app/javascript/src/__tests__/landparcel/CreatePointOfInterest.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import ReactTestUtils from 'react-dom/test-utils';
+import { render, waitFor, screen } from '@testing-library/react'
 import { MockedProvider } from '@apollo/react-testing'
 import { BrowserRouter } from 'react-router-dom/'
 import MockedThemeProvider from '../../modules/__mocks__/mock_theme'
@@ -23,22 +24,22 @@ describe('Create Point of Interest Component', () => {
         expect(screen.getByText('buttons.new_point_of_interest')).toBeInTheDocument()
         
         const newPoiButton = screen.queryByTestId('new-poi-button')
-        fireEvent.click(newPoiButton)
+        ReactTestUtils.Simulate.click(newPoiButton)
 
         const poiName = screen.getByTestId('poi-name')
-        fireEvent.change(poiName, { target: { value: 'Hotel' } })
+        ReactTestUtils.Simulate.change(poiName, { target: { value: 'Hotel' } })
         expect(poiName.value).toBe('Hotel')
         
         const poiDescription = screen.getByTestId('poi-description')
-        fireEvent.change(poiDescription, { target: { value: 'Hotel Description' } })
+        ReactTestUtils.Simulate.change(poiDescription, { target: { value: 'Hotel Description' } })
         expect(poiDescription.value).toBe('Hotel Description')
 
         const GeoLongitudeX = screen.queryByTestId('long_x')
-        fireEvent.change(GeoLongitudeX, { target: { value: '28.535' } })
+        ReactTestUtils.Simulate.change(GeoLongitudeX, { target: { value: '28.535' } })
         expect(GeoLongitudeX.value).toBe('28.535')
     
         const GeoLatitudeY = screen.queryByTestId('lat_y')
-        fireEvent.change(GeoLatitudeY, { target: { value: '-15.255' } })
+        ReactTestUtils.Simulate.change(GeoLatitudeY, { target: { value: '-15.255' } })
         expect(GeoLatitudeY.value).toBe('-15.255')
   
         expect(newPoiButton).toBeInTheDocument()
--- app/javascript/src/__tests__/landparcel/PointOfInterestModal.test.js
+++ app/javascript/src/__tests__/landparcel/PointOfInterestModal.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-undef */
 import React from 'react'
-import { render, fireEvent, screen, waitFor } from '@testing-library/react'
+import ReactTestUtils from 'react-dom/test-utils';
+import { render, screen, waitFor } from '@testing-library/react'
 import { MockedProvider } from '@apollo/react-testing'
 import { BrowserRouter } from 'react-router-dom/'
 import MockedThemeProvider from '../../modules/__mocks__/mock_theme'
@@ -39,19 +40,19 @@ describe('Point Of Interest Modal Component', () => {
       expect(screen.queryByTestId('add_type')).toBeInTheDocument()
 
       const poiName = screen.getByTestId('poi-name')
-      fireEvent.change(poiName, { target: { value: 'Hotel' } })
+      ReactTestUtils.Simulate.change(poiName, { target: { value: 'Hotel' } })
       expect(poiName.value).toBe('Hotel')
 
       const poiDescription = screen.getByTestId('poi-description')
-      fireEvent.change(poiDescription, { target: { value: 'Hotel description' } })
+      ReactTestUtils.Simulate.change(poiDescription, { target: { value: 'Hotel description' } })
       expect(poiDescription.value).toBe('Hotel description')
   
       const GeoLongitudeX = screen.queryByTestId('long_x')
-      fireEvent.change(GeoLongitudeX, { target: { value: '28.535' } })
+      ReactTestUtils.Simulate.change(GeoLongitudeX, { target: { value: '28.535' } })
       expect(GeoLongitudeX.value).toBe('28.535')
   
       const GeoLatitudeY = screen.queryByTestId('lat_y')
-      fireEvent.change(GeoLatitudeY, { target: { value: '-15.255' } })
+      ReactTestUtils.Simulate.change(GeoLatitudeY, { target: { value: '-15.255' } })
       expect(GeoLatitudeY.value).toBe('-15.255')
     }, 10)
 
