4e4a2bfe9dfd483d8af55bea5985e78cc4dd0a9c
OlivierJM <manolivier93@gmail.com>
2022-06-07 13:28:17 +0200

    replace userevent with test utils

--- app/javascript/src/modules/Payments/TransactionLogs/__tests__/PaymentForm.test.js
+++ app/javascript/src/modules/Payments/TransactionLogs/__tests__/PaymentForm.test.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MockedProvider } from '@apollo/react-testing';
+import ReactTestUtils from 'react-dom/test-utils';
 import PaymentForm from '../Components/PaymentForm';
 import { Context } from '../../../../containers/Provider/AuthStateProvider';
 import userMock from '../../../../__mocks__/authstate';
 
 describe('PaymentForm', () => {
+  const userEvent = ReactTestUtils.Simulate
   it('should render the payment form', () => {
     const container = render(
       <MockedProvider>
@@ -24,6 +26,17 @@ describe('PaymentForm', () => {
     expect(container.queryByTestId('make_a_payment_btn')).toBeInTheDocument();
     expect(container.queryByTestId('make_a_payment_btn').textContent).toContain(
       'misc.next'
-    );
+      );
+      userEvent.change(container.queryByLabelText('task:task.optional_description'), { target: { value: 'some description' } });
+      expect(container.queryByLabelText('task:task.optional_description').value).toContain('some description');
+
+      userEvent.change(container.queryByLabelText('form_fields.invoice_number'), { target: { value: 'some invoice number' } })
+      expect(container.queryByLabelText('form_fields.invoice_number').value).toContain('some invoice number');
+
+      userEvent.change(container.queryByLabelText('table_headers.amount'), { target: { value: '10000' } })
+      expect(container.queryByLabelText('table_headers.amount').value).toContain('10000');
+
+      userEvent.change(container.queryByLabelText('form_fields.account_name'), { target: { value: 'JU' } })
+      expect(container.queryByLabelText('form_fields.account_name').value).toContain('JU');
   });
 });
