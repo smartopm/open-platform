fc8a71c709997b20f570f7c2bbafde8102945793
Taofeek Olalere <taofeek@doublegdp.com>
2022-06-16 20:50:22 +0000

    Implement download on forms

--- app/javascript/src/modules/Forms/containers/FormPage.jsx
+++ app/javascript/src/modules/Forms/containers/FormPage.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-param-reassign */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { Container, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useMutation, useQuery } from 'react-apollo';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 import FormUpdate from '../components/FormUpdate';
 import { Context } from '../../../containers/Provider/AuthStateProvider';
 import FormContextProvider from '../Context';
@@ -25,6 +30,7 @@ export default function FormPage() {
   const { t } = useTranslation(['common', 'form']);
   const path = useParamsQuery('');
   const id = path.get('formId');
+  const download = path.get('download')
   const { data: formDetailData, loading } = useQuery(FormQuery, { variables: { id: formId } });
   const [loginPublicUser] = useMutation(PublicUserMutation)
   const categoriesData = useQuery(FormCategoriesQuery, {
@@ -32,7 +38,7 @@ export default function FormPage() {
     fetchPolicy: 'no-cache'
   });
   const isFormFilled = pathname.includes('user_form');
-  const [isError, setIsError] = useState()
+  const [isError, setIsError] = useState();
 
   useEffect(() => {
     // check route and auto log the user
@@ -55,12 +61,40 @@ export default function FormPage() {
     }
   }, [authState?.user?.userType, formDetailData?.form.isPublic]);
 
-  if(isError) {
+  useEffect(() => {
+    function savePdf() {
+      const domElement = document.querySelector('#form_container');
+      html2canvas(domElement, {
+        scale: '1',
+        onclone: (doc) => {
+          doc.querySelector('#save_as_draft_btn').style.visibility = 'hidden';
+          doc.querySelector('#submit_form_btn').style.visibility = 'hidden';
+        }
+      }).then(canvas => {
+        const img = canvas.toDataURL('image/jpeg');
+        const pdf = new jsPDF('p', 'pt', 'a4');
+        const imgProps = pdf.getImageProperties(img);
+        const width = pdf.internal.pageSize.getWidth();
+        const height = (imgProps.height * width) / imgProps.width;
+        pdf.addImage(img, 'JPEG', 0, 20, width, height);
+        pdf.save('download.pdf');
+      });
+    }
+    let timer;
+    if (download === 'true' && !loading) {
+      timer = window.setTimeout(() => {
+        savePdf();
+      }, 500)
+    }
+    return () => { window.clearTimeout(timer) }
+  }, [download, loading]);
+
+  if (isError) {
    return (
      <CenteredContent>
        <Typography>{t('errors.something_went_wrong_forms')}</Typography>
      </CenteredContent>
-     ) 
+     )
   }
 
   return (
@@ -89,7 +123,7 @@ export default function FormPage() {
               />
             </AccessCheck>
           </div>
-          <Container maxWidth="md">
+          <Container maxWidth="md" id="form_container">
             <Form
               editMode={false}
               formId={formId}
