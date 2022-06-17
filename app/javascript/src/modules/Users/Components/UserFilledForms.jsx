78e5d4332904cfe6eb1eec9176d3a378393d9f95
DMutuba <daniel@doublegdp.com>
2022-06-17 18:36:56 +0300

    Add submit a form select option on my forms page

--- app/javascript/src/modules/Users/Components/UserFilledForms.jsx
+++ app/javascript/src/modules/Users/Components/UserFilledForms.jsx
@@ -1,46 +1,131 @@
-import React, { Fragment, useState } from 'react';
+/* eslint-disable no-use-before-define */
+import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Divider from '@mui/material/Divider';
+import { Grid, useMediaQuery } from '@mui/material';
+import { useQuery, useLazyQuery } from 'react-apollo';
+import { useHistory } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import { useLazyQuery } from 'react-apollo';
 import FormItem from '../../Forms/UserForms/Components/FormUserItem';
-import { SubmittedFormCommentsQuery } from '../../Forms/graphql/forms_queries';
+import { FormsQuery, SubmittedFormCommentsQuery } from '../../Forms/graphql/forms_queries';
 import CenteredContent from '../../../shared/CenteredContent';
+import SelectButton from '../../../shared/buttons/SelectButton';
+import { Spinner } from '../../../shared/Loading';
+import { formatError } from '../../../utils/helpers';
 
 export default function UserFilledForms({ userFormsFilled, userId, currentUser }) {
-  const { t } = useTranslation(['common', 'task']);
-  const [currentFormUserId, setCurrentFormUserId] = useState(null);
-  const [fetchComments, formData] = useLazyQuery(SubmittedFormCommentsQuery, { fetchPolicy: 'cache-and-network' });
+  const { data, error, loading } = useQuery(FormsQuery, {
+    fetchPolicy: 'cache-and-network'
+  });
 
-  function handleShowComments(event, formId){
+  const history = useHistory();
+  const { t } = useTranslation('common');
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [currentFormUserId, setCurrentFormUserId] = useState(null);
+  const [fetchComments, formData] = useLazyQuery(SubmittedFormCommentsQuery, {
+    fetchPolicy: 'cache-and-network'
+  });
+
+  const mobile = useMediaQuery('(max-width:800px)');
+
+  function handleClose() {
+    setAnchorEl(null);
+    setOpen(false);
+  }
+
+  const menuOptions = data?.forms?.map(form => ({
+    key: form.id,
+    value: form.name,
+    name: form.name,
+    handleMenuItemClick: () => history.push(`/form/${form.id}/private`),
+    show: true
+  }));
+
+  function handleShowComments(event, formId) {
     event.stopPropagation();
     setCurrentFormUserId(formId);
     fetchComments({ variables: { formUserId: formId } });
   }
 
-  if (!userFormsFilled || !userFormsFilled.length) {
-    return <CenteredContent>{t('misc.no_forms')}</CenteredContent>;
+  function handleSelectButtonClick(e) {
+    setOpen(!open);
+    setAnchorEl(e.currentTarget);
   }
+
+  if (loading) return <Spinner />;
+  if (error) return <CenteredContent>{formatError(error.message)}</CenteredContent>;
 
   return (
     <div className="container">
-      {userFormsFilled.length &&
-        userFormsFilled.map(
-          userForm =>
-            (userForm.status !== 'draft' || userForm.userId === currentUser) && (
-              <Fragment key={userForm.id}>
-                <FormItem
-                  formUser={userForm}
-                  handleShowComments={handleShowComments}
-                  currentFormUserId={currentFormUserId}
-                  formData={formData}
-                  userId={userId}
-                  t={t}
-                />
-                <Divider />
-              </Fragment>
-            )
-        )}
+      {data?.forms?.length && (
+        <Grid
+          container
+          spacing={1}
+          style={{
+            marginTop:
+              !mobile && userFormsFilled.length === 0
+                ? -80
+                : !mobile && userFormsFilled.length >= 1 && -60
+          }}
+        >
+          <Grid
+            item
+            md={12}
+            xs={12}
+            style={{
+              display: 'flex',
+              marginTop: !mobile && userFormsFilled.length >= 1 && -40,
+              marginBottom: 12,
+              justifyContent: 'center'
+            }}
+          >
+            <SelectButton
+              options={menuOptions}
+              open={open}
+              anchorEl={anchorEl}
+              handleClose={handleClose}
+              handleClick={handleSelectButtonClick}
+              defaultButtonText={t('common:menu.submit_form')}
+              style={{ marginLeft: mobile && '-40px' }}
+            />
+          </Grid>
+        </Grid>
+      )}
+      {!userFormsFilled ||
+        (!userFormsFilled.length && (
+          <>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              style={{ marginTop: 20 }}
+            >
+              {t('misc.no_forms')}
+            </Grid>
+          </>
+        ))}
+
+      <div>
+        {userFormsFilled.length >= 1 &&
+          userFormsFilled.map(
+            userForm =>
+              (userForm.status !== 'draft' || userForm.userId === currentUser) && (
+                <Fragment key={userForm.id}>
+                  <FormItem
+                    formUser={userForm}
+                    handleShowComments={handleShowComments}
+                    currentFormUserId={currentFormUserId}
+                    formData={formData}
+                    userId={userId}
+                    t={t}
+                  />
+                  <Divider />
+                </Fragment>
+              )
+          )}
+      </div>
     </div>
   );
 }
