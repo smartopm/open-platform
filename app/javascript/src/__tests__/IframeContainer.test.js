78310b60948d85330db2386208cd23477da3dfa5
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2022-06-22 17:28:10 +0000

    Update IframeContainer.test

--- app/javascript/src/__tests__/IframeContainer.test.js
+++ app/javascript/src/__tests__/IframeContainer.test.js
@@ -1,26 +1,15 @@
-/* eslint-disable */
-import React from 'react'
-import { mount } from 'enzyme'
-import IframeContainer from '../components/IframeContainer'
+import React from 'react';
+import { render } from '@testing-library/react';
+import IframeContainer from '../components/IframeContainer';
 
 describe('iframe container component', () => {
   const props = {
     link: 'https://app.doublegdp.com/',
     height: 200,
     width: 345
-  }
-  const iframe = mount(<IframeContainer {...props} />)
+  };
+  const iframe = render(<IframeContainer {...props} />);
   it('renders correctly', () => {
-    expect(iframe).toMatchSnapshot()
-  })
-  it('gets the correct props', () => {
-    const { link, height, width } = iframe.props()
-    expect(link).toBe(props.link)
-    expect(height).toBe(props.height)
-    expect(width).toBe(props.width)
-  })
-  it('renders only one div element and one iframe element ', () => {
-    expect(iframe.find('iframe')).toHaveLength(1)
-    expect(iframe.find('div')).toHaveLength(1)
-  })
-})
+    expect(iframe.queryByTestId('iframe')).toBeInTheDocument();
+  });
+});
