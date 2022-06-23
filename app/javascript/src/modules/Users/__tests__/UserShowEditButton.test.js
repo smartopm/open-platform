37345b739219be52698739e73277877db755f22a
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2022-06-23 20:04:46 +0000

    Fix eslint issues

--- app/javascript/src/modules/Users/__tests__/UserShowEditButton.test.js
+++ app/javascript/src/modules/Users/__tests__/UserShowEditButton.test.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { MockedProvider } from '@apollo/react-testing'
-import { MemoryRouter } from 'react-router-dom'
-import { render, waitFor } from '@testing-library/react'
-import UserShow from '../Containers/UserShow'
-import { UserQuery, UserAccountQuery } from '../../../graphql/queries';
+import React from 'react';
+import { MockedProvider } from '@apollo/react-testing';
+import { MemoryRouter } from 'react-router-dom';
+import { render, waitFor } from '@testing-library/react';
 import ReactRouter from 'react-router';
+import UserShow from '../Containers/UserShow';
+import { UserQuery, UserAccountQuery } from '../../../graphql/queries';
 
-jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn())
+jest.mock('@rails/activestorage/src/file_checksum', () => jest.fn());
 describe('Renders UserShow', () => {
-  jest.spyOn(ReactRouter, 'useParams').mockReturnValue({id: '123'})
+  jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '123' });
   const mocks = [
     {
       request: {
@@ -49,26 +49,30 @@ describe('Renders UserShow', () => {
       request: {
         query: UserAccountQuery,
         variable: {
-          id: '123',
+          id: '123'
         }
       },
       result: {
         data: {
           user: {
             id: '123',
-            accounts: [{
-              id: '456',
-              updatedAt: '2021-11-15T15:41:42+02:00',
-              landParcels: [{
-                id: '678',
-                parcelNumber: '910',
-                parcelType: 'basic',
-                longX: '62.5',
-                latY: '35.5',
-                geom: '14.67',
+            accounts: [
+              {
+                id: '456',
                 updatedAt: '2021-11-15T15:41:42+02:00',
-              }]
-            }]
+                landParcels: [
+                  {
+                    id: '678',
+                    parcelNumber: '910',
+                    parcelType: 'basic',
+                    longX: '62.5',
+                    latY: '35.5',
+                    geom: '14.67',
+                    updatedAt: '2021-11-15T15:41:42+02:00'
+                  }
+                ]
+              }
+            ]
           }
         }
       }
@@ -78,10 +82,10 @@ describe('Renders UserShow', () => {
   it('runs queries', async () => {
     const wrapper = render(
       <MemoryRouter>
-      <MockedProvider mock={mocks} addTypename={false}>
-        <UserShow />
-      </MockedProvider>
-    </MemoryRouter>
+        <MockedProvider mock={mocks} addTypename={false}>
+          <UserShow />
+        </MockedProvider>
+      </MemoryRouter>
     );
 
     expect(wrapper.queryByTestId('loader')).toBeInTheDocument();
@@ -90,4 +94,4 @@ describe('Renders UserShow', () => {
       expect(wrapper.queryByText('loader')).not.toBeInTheDocument();
     }, 10);
   });
-})
+});
