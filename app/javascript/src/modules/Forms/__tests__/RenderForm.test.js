4379861e80fb1b9f2cffaf9a63f7d801c4fa784c
OlivierJM <manolivier93@gmail.com>
2022-06-24 14:35:24 +0200

    Fix RenderForm tests

--- app/javascript/src/modules/Forms/__tests__/RenderForm.test.js
+++ app/javascript/src/modules/Forms/__tests__/RenderForm.test.js
@@ -40,73 +40,107 @@ describe('Render Form Component', () => {
     expect(wrapper.queryByLabelText('text-input')).toBeInTheDocument();
     expect(wrapper.queryByLabelText('text-input')).toHaveTextContent('Where are you ?');
   });
+  it('should contain proper form properies with upload field and returns no errors for correct file type', () => {
+    const props = {
+      formPropertiesData: {
+        id: '2342345',
+        fieldName: 'Resume',
+        fieldType: 'file_upload',
+        adminUse: false
+      },
+      formId: '2342342',
+      refetch: jest.fn(),
+      editMode: true,
+      categoryId: '232121',
+      number: 1,
+      formDetailRefetch: () => {}
+    };
+  
+    const wrapper = render(
+      <MockedProvider>
+        <Context.Provider value={userMock}>
+          <FormContextProvider>
+            <MockedThemeProvider>
+              <RenderForm {...props} />
+            </MockedThemeProvider>
+          </FormContextProvider>
+        </Context.Provider>
+      </MockedProvider>
+    );
+    const str = JSON.stringify('someRandomValues');
+    const blob = new Blob([str]);
+    const file = new File([blob], 'test_file.pdf', 'application/pdf');
+    expect(wrapper.getByText('Resume')).toBeInTheDocument();
+    expect(wrapper.getByText('form:misc.select_file')).toBeInTheDocument();
+    const inputElement = wrapper.queryByTestId('form-file-input');
+    fireEvent.change(inputElement, { target: { files: [file] } });
+    expect(wrapper.queryByText('form:errors.wrong_file_type')).not.toBeInTheDocument();
+  });
+  
+  it('should contain proper form properies with upload field and returns errors for wrong file type', () => {
+    const props = {
+      formPropertiesData: {
+        id: '2342345',
+        fieldName: 'Resume',
+        fieldType: 'file_upload',
+        adminUse: false
+      },
+      formId: '2342342',
+      refetch: jest.fn(),
+      editMode: true,
+      categoryId: '232121',
+      number: 1,
+      formDetailRefetch: () => {}
+    };
+  
+    const wrapper = render(
+      <MockedProvider>
+        <Context.Provider value={userMock}>
+          <FormContextProvider>
+            <MockedThemeProvider>
+              <RenderForm {...props} />
+            </MockedThemeProvider>
+          </FormContextProvider>
+        </Context.Provider>
+      </MockedProvider>
+    );
+  
+    expect(wrapper.getByText('Resume')).toBeInTheDocument();
+    expect(wrapper.getByText('form:misc.select_file')).toBeInTheDocument();
+  });
+
+  it('should render payment form', () => {
+        const props = {
+          formPropertiesData: {
+            id: '2342345',
+            fieldName: 'Registration Fees',
+            fieldType: 'payment',
+            adminUse: false,
+            shortDesc: '500',
+            longDesc: 'This is long description'
+          },
+          formId: '2342342',
+          refetch: jest.fn(),
+          editMode: true,
+          categoryId: '232121',
+          number: 1,
+          formDetailRefetch: () => {},
+        };
+
+        const wrapper = render(
+          <MockedProvider>
+            <Context.Provider value={userMock}>
+              <FormContextProvider>
+                <MockedThemeProvider>
+                  <RenderForm {...props} />
+                </MockedThemeProvider>
+              </FormContextProvider>
+            </Context.Provider>
+          </MockedProvider>
+        );
+    expect(wrapper.queryByText('Registration Fees')).toBeInTheDocument();
+    expect(wrapper.queryByText('This is long description')).toBeInTheDocument();
+    expect(wrapper.queryByText('NGN 500')).toBeInTheDocument();
+  })
 });
 
-it('should contain proper form properies with upload field and returns no errors for correct file type', () => {
-  const props = {
-    formPropertiesData: {
-      id: '2342345',
-      fieldName: 'Resume',
-      fieldType: 'file_upload',
-      adminUse: false
-    },
-    formId: '2342342',
-    refetch: jest.fn(),
-    editMode: true,
-    categoryId: '232121',
-    number: 1,
-    formDetailRefetch: () => {}
-  };
-
-  const wrapper = render(
-    <MockedProvider>
-      <Context.Provider value={userMock}>
-        <FormContextProvider>
-          <MockedThemeProvider>
-            <RenderForm {...props} />
-          </MockedThemeProvider>
-        </FormContextProvider>
-      </Context.Provider>
-    </MockedProvider>
-  );
-  const str = JSON.stringify('someRandomValues');
-  const blob = new Blob([str]);
-  const file = new File([blob], 'test_file.pdf', 'application/pdf');
-  expect(wrapper.getByText('Resume')).toBeInTheDocument();
-  expect(wrapper.getByText('form:misc.select_file')).toBeInTheDocument();
-  const inputElement = wrapper.queryByTestId('form-file-input');
-  fireEvent.change(inputElement, { target: { files: [file] } });
-  expect(wrapper.queryByText('form:errors.wrong_file_type')).not.toBeInTheDocument();
-});
-
-it('should contain proper form properies with upload field and returns errors for wrong file type', () => {
-  const props = {
-    formPropertiesData: {
-      id: '2342345',
-      fieldName: 'Resume',
-      fieldType: 'file_upload',
-      adminUse: false
-    },
-    formId: '2342342',
-    refetch: jest.fn(),
-    editMode: true,
-    categoryId: '232121',
-    number: 1,
-    formDetailRefetch: () => {}
-  };
-
-  const wrapper = render(
-    <MockedProvider>
-      <Context.Provider value={userMock}>
-        <FormContextProvider>
-          <MockedThemeProvider>
-            <RenderForm {...props} />
-          </MockedThemeProvider>
-        </FormContextProvider>
-      </Context.Provider>
-    </MockedProvider>
-  );
-
-  expect(wrapper.getByText('Resume')).toBeInTheDocument();
-  expect(wrapper.getByText('form:misc.select_file')).toBeInTheDocument();
-});
