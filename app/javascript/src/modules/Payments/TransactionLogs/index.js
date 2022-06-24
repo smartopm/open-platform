40e6efcb4a8090f91c1d039d96ee0fc9304097aa
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2022-06-24 10:26:24 +0000

    Separate transaction and plan_payment

--- app/javascript/src/modules/Payments/TransactionLogs/index.js
+++ app/javascript/src/modules/Payments/TransactionLogs/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from 'react';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
 import AccessCheck from '../../Permissions/Components/AccessCheck';
 import PaymentForm from './Components/PaymentForm';
 
 const paymentPermission = ['can_make_payment', 'can_see_menu_item'];
 
-const currentModule = 'transaction'
+const currentModule = 'transaction';
 
-export default function RenderPayment() {
+function RenderPayment() {
   return (
     <AccessCheck module={currentModule} allowedPermissions={paymentPermission}>
       <PaymentForm />
     </AccessCheck>
-  )
+  );
 }
+
+export default {
+  routeProps: {
+    path: '/payments/pay',
+    component: RenderPayment
+  },
+  styleProps: {
+    icon: <CreditCardIcon />
+  },
+  name: t => t('common:misc.make_a_payment'),
+  featureName: 'Transactions',
+  moduleName: currentModule,
+  accessibleBy: []
+};
