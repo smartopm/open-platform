c9aa3d03638215aa5fcf4267d874cf333d50210b
Saurabh Shinde <saurabh@doublegdp.com>
2022-06-24 11:32:13 +0530

    Add query to fetch all transaction logs by admin

--- app/graphql/types/queries/transaction_log.rb
+++ app/graphql/types/queries/transaction_log.rb
@@ -11,6 +11,12 @@ module Types::Queries::TransactionLog
       argument :offset, Integer, required: false
       argument :limit, Integer, required: false
     end
+
+    field :transaction_logs, [Types::TransactionLogType], null: true do
+      description 'Get all Transaction Logs'
+      argument :offset, Integer, required: false
+      argument :limit, Integer, required: false
+    end
   end
 
   def user_transaction_logs(user_id:, offset: 0, limit: 10)
@@ -20,6 +26,12 @@ module Types::Queries::TransactionLog
     raise GraphQL::ExecutionError, I18n.t('errors.unauthorized') unless authorized_user(user.id)
 
     context[:site_community].transaction_logs.where(user_id: user_id).offset(offset).limit(limit)
+  end
+
+  def transaction_logs(offset: 0, limit: 10)
+    validate_authorization(:transaction, :can_view_all_transaction_logs)
+
+    context[:site_community].transaction_logs.offset(offset).limit(limit)
   end
 
   def authorized_user(user_id)
