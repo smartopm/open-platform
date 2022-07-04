0e9ec9ad6c7d59c5179325866683fa7e654a2387
Taofeek Olalere <taofeek@doublegdp.com>
2022-07-04 18:48:57 +0000

    Updated: UserActionMenu test cases

--- app/javascript/src/modules/Users/__tests__/UserActionMenu.test.js
+++ app/javascript/src/modules/Users/__tests__/UserActionMenu.test.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-
 import { BrowserRouter } from 'react-router-dom'
+import { checkAllowedCommunityFeatures } from '../../../utils/helpers'
 import UserActionMenu from '../Components/UserActionMenu'
 
+jest.mock('../../../utils/helpers');
+
 describe('user action menu component', () => {
-  it('show correct action menu', () => {
-    const props = {
-      data: {
-        user: {
-          phoneNumber: '34342',
-          name: 'User Name',
-          email: 'user@dgdp.com',
-          id: '34543543rfsf3',
-          expiresAt: new Date('03-03-2020'),
-          status: 'active'
-        }
-      },
-      userType: 'admin'
+  const props = {
+  data: {
+    user: {
+      phoneNumber: '34342',
+      name: 'User Name',
+      email: 'user@dgdp.com',
+      id: '34543543rfsf3',
+      expiresAt: new Date('03-03-2020'),
+      status: 'active'
     }
+  },
+  userType: 'admin'
+  }
+
+  it('show correct action menu', () => {
     const container = render(
       <BrowserRouter>
         <UserActionMenu
@@ -40,6 +43,26 @@ describe('user action menu component', () => {
     expect(container.queryByText('menu.print_id')).toBeInTheDocument()
     expect(container.queryByText('menu.send_otp')).toBeInTheDocument()
     expect(container.queryByText('menu.deactivate_user')).toBeInTheDocument()
-    expect(container.queryByText('menu.view_plans')).toBeInTheDocument();
+    expect(container.queryByText('menu.view_plans')).not.toBeInTheDocument();
   })
+
+  it('show correct action menu with "View Payment Plans" option', () => {
+    checkAllowedCommunityFeatures.mockReturnValue(true);
+    const container = render(
+      <BrowserRouter>
+        <UserActionMenu
+          data={props.data}
+          router={jest.fn()}
+          anchorEl={document.createElement('button')}
+          handleClose={jest.fn()}
+          userType={props.userType}
+          CSMNumber="353453"
+          open
+          OpenMergeDialog={jest.fn()}
+        />
+      </BrowserRouter>
+    );
+    expect(checkAllowedCommunityFeatures).toHaveBeenCalled();
+    expect(container.queryByText('menu.view_plans')).toBeInTheDocument();
+  });
 })
