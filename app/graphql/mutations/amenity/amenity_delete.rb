790d5f7bd4f0368d90f9a5f33b91d4b0100d57d6
Olivier JM <olivier@doublegdp.com>
2022-07-04 07:13:50 +0000

    Add amenity delete mutation

---
+++ app/graphql/mutations/amenity/amenity_delete.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Mutations
+    module Amenity
+      # Delete Amenity
+      class AmenityDelete < BaseMutation
+        argument :id, ID, required: true
+        argument :status, String, required: true
+
+        field :amenity, Types::AmenityType, null: false
+
+        def resolve(vals)
+          validate_authorization(:amenity, :can_delete_amenities)
+
+          amenity = context[:site_community].amenities.find(vals[:id])
+          return { amenity: amenity } if amenity.update(vals)
+
+          raise_error_message(amenity.errors.full_messages&.join(', '))
+        end
+      end
+    end
+  end
