33b7bff7d0a0d01febab2714134c3baa594a92a3
Taofeek Olalere <taofeek@doublegdp.com>
2022-07-14 19:15:42 +0000

    Updated: cypress properties command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -86,7 +86,7 @@ Cypress.Commands.add("visitUserProfile", (user) => {
 })
 
 Cypress.Commands.add("visitUserMenu", (menuItem) => {
-  cy.get('.option_menu_toggler').click();
+  cy.get('.option_menu_toggler').click({ multiple: true });
   cy.wait(1000);
   cy.get(`${menuItem}`).click();
   cy.wait(1000);
