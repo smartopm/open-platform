8fa86369b873e445e255100d2b8d8bd34fa7c657
Olivier JM <olivier@doublegdp.com>
2022-07-22 15:52:10 +0000

    create task and respond when a message is received

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,17 +9,10 @@ class TwilioController < ApplicationController
     def webhook
       response = Twilio::TwiML::MessagingResponse.new
       response.message(body: "Hi #{params['ProfileName']}, Thank you for your message! A member of our team will be in touch with you soon.")
-      puts "==================hrlllop========================"
-      # "ProfileName": "OlivierJM",
-      # "WaId": "260971500748",
-      # "Body": "Test",
-
       user = current_community.users.find_by(phone_number: params['WaId'])
-      puts user.to_json
       # Check if there is a task created by this user
-      task = task_exists?(params['WaId'])
-      puts "====================#{task}================"
-      create_task(params) unless task
+      task = task_exists?(params['WaId'], user)
+      create_task(params, user) #unless task
       # If there is a task then add comments to that task
       # If there is no task then go ahead and create the task
       render xml: response
@@ -30,22 +23,20 @@ class TwilioController < ApplicationController
 
     # private
 
-    def task_exists?(phone_number)
-      user = current_community.users.find_by(phone_number: phone_number)
+    def task_exists?(phone_number, user)
       task = current_community.notes.find_by(author_id: user.id)
       task.present?
     end
 
-    def create_task(params)
-      user = current_community.users.find_by(phone_number: params['WaId'])
-      current_community.notes.create!(
+    def create_task(params, user)
+     note = current_community.notes.create!(
         author_id: user.id,
+        user_id: user.id,
         body: params['Body'],
         flagged: true,
-        category: 'whatsapp',
+        category: 'to_do',
       )
+    # note.assign_or_unassign_user('1ce39aa1-57bd§-49e3-a3a8-6bc0d1446a5a')
     end
-
-
 end
 
