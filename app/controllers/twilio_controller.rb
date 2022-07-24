e4a5f0b331e66198723c8375263e9c2c3493191d
Olivier JM <olivier@doublegdp.com>
2022-07-24 18:50:17 +0000

    create a comment under a task when it already exists

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -11,10 +11,15 @@ class TwilioController < ApplicationController
       response.message(body: "Hi #{params['ProfileName']}, Thank you for your message! A member of our team will be in touch with you soon.")
       user = current_community.users.find_by(phone_number: params['WaId'])
       # Check if there is a task created by this user
-      task = task_exists?(params['WaId'], user)
-      create_task(params, user) #unless task
+      task = current_community.notes.where(category: 'whatsapp', completed: false).find_by(author_id: user.id)
       # If there is a task then add comments to that task
-      # If there is no task then go ahead and create the task
+      if task.present?
+        create_comments(params, task)
+      else
+        # If there is no task then go ahead and create the task
+        create_task(params, user)
+      end
+      # Respond to the user who's sent the message
       render xml: response
     rescue StandardError => e
       Rollbar.error(e)
@@ -22,21 +27,26 @@ class TwilioController < ApplicationController
     end
 
     # private
-
-    def task_exists?(phone_number, user)
-      task = current_community.notes.find_by(author_id: user.id)
-      task.present?
-    end
-
     def create_task(params, user)
      note = current_community.notes.create!(
         author_id: user.id,
         user_id: user.id,
         body: params['Body'],
         flagged: true,
-        category: 'to_do',
+        category: 'whatsapp',
+        completed: false
       )
     # note.assign_or_unassign_user('1ce39aa1-57bd§-49e3-a3a8-6bc0d1446a5a')
     end
+
+    def create_comments(params, task)
+      comment = task.note_comments.create!(
+        note_id: task.id,
+        body: params['Body'],
+        user_id: task.user.id,
+        status: 'active'
+      )
+    end
+
 end
 
