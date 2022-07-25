1105e7f81cf1500f79553dfb1b5d916cfc7b5408
Taofeek Olalere <taofeek@doublegdp.com>
2022-07-25 12:59:49 +0000

    Updated: search filter list to include search count

--- app/javascript/src/shared/search/SearchFilterList.jsx
+++ app/javascript/src/shared/search/SearchFilterList.jsx
@@ -1,34 +1,46 @@
 import React from 'react';
-import { Grid, Typography, Button } from '@mui/material';
+import { Grid, Typography, Button, useMediaQuery, useTheme } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 export default function SearchFilterList({ filters, handleClearFilters, isSmall, count }) {
   const { t } = useTranslation('search');
   const validFilters = filters.filter(Boolean);
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   if (!validFilters?.length) return null;
   return (
-    <Grid container spacing={isSmall ? 1: 2} alignItems="center">
+    <Grid container spacing={isSmall ? 1 : 2} alignItems="center">
       <Grid item xs="auto">
-        <Typography display="inline-block" variant="subtitle2" data-testid="search_results">
-          {`${t(
-          'search.search_results'
-        )}:  `}
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleClearFilters}
+          data-testid="clear_filters_btn"
+        >
+          {t('search.clear')}
+        </Button>
+      </Grid>
+      <Grid item xs>
+        <Typography
+          display={mobile ? 'inline-block' : 'block'}
+          variant="subtitle2"
+          data-testid="search_results"
+        >
+          {`${t('search.search_results')}:  ${count > 50 ? `${count}+` : count}`}
         </Typography>
         {'  '}
-        <Typography display={isSmall ? "block" : "inline-block"} variant="caption" data-testid="filters_list">
-          {count}
+        <Typography
+          display={mobile ? 'inline-block' : 'block'}
+          variant="caption"
+          data-testid="filters_list"
+        >
           {filters
             .filter(Boolean)
             .map(filter => `${filter}`)
             .join(', ')}
         </Typography>
-      </Grid>
-      <Grid item xs>
-        <Button variant="outlined" color="primary" onClick={handleClearFilters} data-testid="clear_filters_btn">
-          {t('search.clear_filters')}
-        </Button>
       </Grid>
     </Grid>
   );
