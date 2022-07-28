9af50fab0911c35ca8e9684ebfccbca57e8f0f25
DMutuba <daniel@doublegdp.com>
2022-07-28 17:07:17 +0300

    allow null for username

--- db/migrate/20220726063601_add_username_password_password_reset_token_sign_in_count_to_user.rb
+++ db/migrate/20220726063601_add_username_password_password_reset_token_sign_in_count_to_user.rb
@@ -1,14 +1,14 @@
 class AddUsernamePasswordPasswordResetTokenSignInCountToUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :username, :string, unique: true, null: false, default: ""
-    add_column :users, :encrypted_password, :string, null: false, default: ""
+    add_column :users, :username, :string, unique: true
+    add_column :users, :encrypted_password, :string
 
     ## Recoverable
     add_column :users, :reset_password_token, :string, null: true
     add_column :users, :reset_password_sent_at, :datetime, null: true
 
     ## Trackable
-    add_column :users, :sign_in_count, :integer, null: false, default: 0
+    add_column :users, :sign_in_count, :integer
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :datetime
     add_column :users, :current_sign_in_ip, :string
--- db/schema.rb
+++ db/schema.rb
@@ -967,11 +967,11 @@ ActiveRecord::Schema.define(version: 2022_07_26_063601) do
     t.string "decision_timeline"
     t.integer "status", default: 0
     t.string "division"
-    t.string "username", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.integer "sign_in_count", default: 0, null: false
+    t.integer "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
