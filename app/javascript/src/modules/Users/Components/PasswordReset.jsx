6dd214fa4bba8579ca7ae39d44dedf6f4f56cbfa
Daniel Mutuba <daniel@doublegdp.com>
2022-07-31 13:55:37 +0000

    refactor user informatioon component and add tests for reset logic

---
+++ app/javascript/src/modules/Users/Components/PasswordReset.jsx
@@ -0,0 +1,139 @@
+import React, { useState, useContext } from 'react';
+import { useMutation } from 'react-apollo';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
+import { Dialog, DialogTitle, DialogContent, Box, IconButton } from '@mui/material';
+import { Utils as QbUtils } from 'react-awesome-query-builder';
+import { Spinner } from '../../../shared/Loading';
+import { formatError } from '../../../utils/helpers';
+import { ResetUserPasswordUserMutation } from '../../../graphql/mutations/user';
+import { SnackbarContext } from '../../../shared/snackbar/Context';
+
+export default function PasswordRest({ openModal, setOpenModal, data }) {
+  const { showSnackbar, messageType } = useContext(SnackbarContext);
+  const [resetPassword] = useMutation(ResetUserPasswordUserMutation);
+
+  const [loading, setLoading] = useState(false);
+  const { t } = useTranslation(['users', 'common']);
+
+  const username =
+    data?.user?.username !== null
+      ? data?.user?.username
+      : data?.user?.name.replace(' ', '').concat(
+          Math.random()
+            .toString(36)
+            .slice(2, 7)
+        );
+  const password = QbUtils.uuid();
+  function handlePasswordReset() {
+    setOpenModal(false);
+    setLoading(true);
+    resetPassword({
+      variables: {
+        userId: data.user.id,
+        username,
+        password,
+      },
+    })
+      .then(() => {
+        setLoading(false);
+        showSnackbar({
+          type: messageType.success,
+          message: t('common:misc.reset_password_successful'),
+        });
+      })
+      .catch(err => {
+        setLoading(false);
+        showSnackbar({ type: messageType.error, message: formatError(err.message) });
+      });
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div>
+      <Dialog
+        open={openModal}
+        fullWidth
+        maxWidth="sm"
+        scroll="paper"
+        aria-labelledby="reset_user_password"
+      >
+        <DialogTitle id="reset_user_password" data-testid="reset_user_password">
+          <Box display="flex" alignItems="center">
+            <Box flexGrow={1} style={{ color: '', fontSize: '24px' }}>
+              {t('users.confirm_reset_password')}
+            </Box>
+            <Box>
+              <IconButton
+                data-testid="password-reset-close-icon"
+                onClick={() => setOpenModal(false)}
+                size="large"
+              >
+                <CloseIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        </DialogTitle>
+
+        <DialogContent style={{ paddingTop: '6px', paddingBottom: '10px' }}>
+          <Typography gutterBottom>{t('common:misc.reset_disclaimer')}</Typography>
+          <Typography gutterBottom>{t('common:misc.copy_credentials')}</Typography>
+          <br />
+          <Typography variant="subtitle2" gutterBottom>
+            {t('users.username', { username })}
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            {t('users.password', { password })}
+          </Typography>
+        </DialogContent>
+
+        <Box textAlign="center">
+          <Button
+            disableElevation
+            onClick={handlePasswordReset}
+            color="primary"
+            variant="contained"
+            data-testid="post-btn"
+            style={{ color: 'white', width: '30%', marginBottom: '12px' }}
+            autoFocus
+          >
+            {t('common:misc.reset')}
+          </Button>
+        </Box>
+      </Dialog>
+    </div>
+  );
+}
+const User = PropTypes.shape({
+  id: PropTypes.string,
+  name: PropTypes.string,
+  username: PropTypes.string,
+  userType: PropTypes.string,
+  state: PropTypes.string,
+  status: PropTypes.string,
+  accounts: PropTypes.arrayOf(PropTypes.object),
+  formUsers: PropTypes.arrayOf(PropTypes.object),
+  permissions: PropTypes.arrayOf(PropTypes.object),
+  community: PropTypes.shape({
+    features: PropTypes.shape({
+      Tasks: { features: [] },
+      Messages: { features: [] },
+      Payments: { features: [] },
+      Properties: { features: [] },
+      LogBook: { features: [] },
+    }),
+    securityManager: PropTypes.string,
+  }),
+});
+
+PasswordRest.propTypes = {
+  openModal: PropTypes.bool.isRequired,
+  setOpenModal: PropTypes.func.isRequired,
+  data: PropTypes.shape({ user: User }).isRequired,
+};
--- app/javascript/src/modules/Users/Components/UserInformation.jsx
+++ app/javascript/src/modules/Users/Components/UserInformation.jsx
@@ -1,18 +1,13 @@
 /* eslint-disable max-lines */
 /* eslint-disable max-statements */
 /* eslint-disable complexity */
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from '@mui/material/Button';
 import { useHistory } from 'react-router-dom';
 import PhoneIcon from '@mui/icons-material/Phone';
-import Typography from '@mui/material/Typography';
-import { useMutation } from 'react-apollo';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Dialog, DialogTitle, DialogContent, Container, Box, IconButton } from '@mui/material';
-import { Utils as QbUtils } from 'react-awesome-query-builder';
-import DialogContentText from '@mui/material/DialogContentText';
-import CloseIcon from '@mui/icons-material/Close';
+import { Dialog, DialogTitle, DialogContent, Container } from '@mui/material';
 import { css, StyleSheet } from 'aphrodite';
 import PropTypes from 'prop-types';
 
@@ -26,7 +21,6 @@ import {
   useParamsQuery,
   objectAccessor,
   checkAccessibilityForUserType as handler,
-  formatError,
 } from '../../../utils/helpers';
 import FeatureCheck from '../../Features';
 import PaymentPlans from '../../Payments/Components/UserTransactions/Plans';
@@ -41,9 +35,7 @@ import { userTabList, selectOptions, createMenuContext } from '../utils';
 import SelectButton from '../../../shared/buttons/SelectButton';
 import UserLabelTitle from './UserLabelTitle';
 import UserLabels from './UserLabels';
-import { SnackbarContext } from '../../../shared/snackbar/Context';
-import { ResetUserPasswordUserMutation } from '../../../graphql/mutations/user';
-import { Spinner } from '../../../shared/Loading';
+import PasswordRest from './PasswordReset';
 
 export default function UserInformation({
   data,
@@ -68,9 +60,9 @@ export default function UserInformation({
   const [selectedKey, setSelectKey] = useState('');
   const [anchorEl, setAnchorEl] = useState(null);
   const userType = authState.user.userType.toLowerCase();
-  const [resetPassword] = useMutation(ResetUserPasswordUserMutation);
-  const [loading, setLoading] = useState(false);
-  const { showSnackbar, messageType } = useContext(SnackbarContext);
+  // const [resetPassword] = useMutation(ResetUserPasswordUserMutation);
+  // const [loading, setLoading] = useState(false);
+  // const { showSnackbar, messageType } = useContext(SnackbarContext);
   const [openModal, setOpenModal] = useState(false);
   const options = selectOptions(
     setSelectKey,
@@ -137,38 +129,6 @@ export default function UserInformation({
     history.push(`/user/${data.user.id}?type=MergeUser`);
     setOpen(false);
   }
-  const username =
-    data?.user?.username !== null
-      ? data.user.username
-      : data.user.name.replace(' ', '').concat(
-          Math.random()
-            .toString(36)
-            .slice(2, 7)
-        );
-  const password = QbUtils.uuid();
-
-  function handlePasswordReset() {
-    setOpenModal(false);
-    setLoading(true);
-    resetPassword({
-      variables: {
-        userId: data.user.id,
-        username,
-        password,
-      },
-    })
-      .then(() => {
-        setLoading(false);
-        showSnackbar({
-          type: messageType.success,
-          message: t('common:misc.reset_password_successful'),
-        });
-      })
-      .catch(err => {
-        setLoading(false);
-        showSnackbar({ type: messageType.error, message: formatError(err.message) });
-      });
-  }
 
   function handleResetPasswordItemClick() {
     setOpen(false);
@@ -224,9 +184,6 @@ export default function UserInformation({
     router.push(`/user/${userId}?tab=${tabValue}`);
   }
 
-  if (loading) {
-    return <Spinner />;
-  }
   return (
     <PageWrapper
       breadCrumbObj={breadCrumbObj}
@@ -254,60 +211,8 @@ export default function UserInformation({
             </DialogContent>
           </Dialog>
 
-          <br />
-          <Dialog
-            open={openModal}
-            fullWidth
-            maxWidth="sm"
-            scroll="paper"
-            aria-labelledby="reset_user_password"
-          >
-            <DialogTitle id="reset_user_password" data-testid="reset_user_password">
-              <Box display="flex" alignItems="center">
-                <Box flexGrow={1} style={{ color: '', fontSize: '24px' }}>
-                  {t('users.confirm_reset_password')}
-                </Box>
-                <Box>
-                  <IconButton
-                    data-testid="password-reset-close-icon"
-                    onClick={() => setOpenModal(false)}
-                    size="large"
-                  >
-                    <CloseIcon />
-                  </IconButton>
-                </Box>
-              </Box>
-            </DialogTitle>
-
-            <DialogContent style={{ paddingTop: '6px', paddingBottom: '10px' }}>
-              <DialogContentText>
-                <Typography gutterBottom>{t('common:misc.reset_disclaimer')}</Typography>
-                <Typography gutterBottom>{t('common:misc.copy_credentials')}</Typography>
-                <br />
-                <Typography variant="subtitle2" gutterBottom>
-                  {t('users.username', { username })}
-                </Typography>
-                <Typography variant="subtitle2" gutterBottom>
-                  {t('users.password', { password })}
-                </Typography>
-              </DialogContentText>
-            </DialogContent>
-
-            <Box textAlign="center">
-              <Button
-                disableElevation
-                onClick={handlePasswordReset}
-                color="primary"
-                variant="contained"
-                data-testid="post-btn"
-                style={{ color: 'white', width: '30%', marginBottom: '12px' }}
-                autoFocus
-              >
-                {t('common:misc.reset')}
-              </Button>
-            </Box>
-          </Dialog>
-          <br />
+          <PasswordRest openModal={openModal} setOpenModal={setOpenModal} data={data} />
+          
           {isLabelOpen && (
             <Container maxWidth="md">
               <UserLabels
