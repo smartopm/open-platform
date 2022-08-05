a00aae69ae19a0b8a653e3974474a1b6bb11c803
Daniel Mutuba <daniel@doublegdp.com>
2022-08-05 11:49:56 +0000

    implement feedback cc: nurudeeen

--- app/graphql/mutations/user/reset_password_after_login.rb
+++ app/graphql/mutations/user/reset_password_after_login.rb
@@ -10,7 +10,7 @@ module Mutations
       field :auth_token, String, null: true
 
       def resolve(vals)
-        user = find_user_by_id(vals[:user_id])
+        user = context[:site_community].users.find(vals[:user_id])
         auth_object = user.reset_password_on_first_login(user.username, vals[:password])
         raise_reset_password_error if auth_object.nil?
         auth_token = auth_object.auth_token
@@ -26,14 +26,6 @@ module Mutations
       # @return [GraphQL::ExecutionError]
       def raise_reset_password_error
         raise GraphQL::ExecutionError, I18n.t('errors.authentication.reset_password_error')
-      end
-
-      def find_user_by_id(id)
-        user = context[:site_community].users.find(id)
-        return user if user.present?
-
-        raise GraphQL::ExecutionError,
-              I18n.t('errors.user.not_found')
       end
     end
   end
