a823419d29826f5090f33dfd141eebe598c93ce4
Nurudeen Ibrahim <nurudeen@doublegdp.com>
2022-08-12 11:51:30 +0000

    Fix jest log

--- app/javascript/src/modules/Forms/__tests__/FormProperties/FormPropertyWrapper.test.js
+++ app/javascript/src/modules/Forms/__tests__/FormProperties/FormPropertyWrapper.test.js
@@ -18,6 +18,7 @@ describe('FormPropertyWrapper', () => {
       },
       editMode: true,
       number: 12,
+      childreen: <div>Child</div>
     };
     const container = render(
       <MockedProvider>
